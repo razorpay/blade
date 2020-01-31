2a1b7b51f24cf89dde19a0b0b66bee5febbee916
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-01-31 13:15:44 +0530

    feat(decorator): add a space around decorator

--- storybook/native/index.js
+++ storybook/native/index.js
@@ -1,12 +1,25 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import { getStorybookUI, configure } from '@storybook/react-native';
+import { getStorybookUI, configure, addDecorator } from '@storybook/react-native';
+import { ThemeProvider } from 'styled-components';
+import SpaceAround from '../../src/decorators/SpaceAround/SpaceAround.native';
 
 import './rn-addons';
+
+const theme = {};
 
 // import stories
 configure(() => {
   require('../../src/atoms/Button/Button.stories');
 }, module);
+
+addDecorator((Story) => (
+  <ThemeProvider theme={theme}>
+    <SpaceAround>
+      <Story />
+    </SpaceAround>
+  </ThemeProvider>
+));
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
