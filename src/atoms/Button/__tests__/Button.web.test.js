b0975af459a06f9e4ba64aab0fa762fbbf797039
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-01-31 14:45:15 +0530

    test(button): add snapshots for native and web button (#4)

--- src/atoms/Button/__tests__/Button.web.test.js
+++ src/atoms/Button/__tests__/Button.web.test.js
@@ -4,6 +4,11 @@ import { render, fireEvent } from '@testing-library/react';
 import Button from '../index';
 
 describe('Renders <Button /> correctly', () => {
+  it('snapshot testing', () => {
+    const { container } = render(<Button onClick={() => {}}>{'Click Me'}</Button>);
+    expect(container).toMatchSnapshot();
+  });
+
   it('onPress works as expected', () => {
     const mockOnPress = jest.fn();
     const { getByText } = render(<Button onClick={mockOnPress}>{'Click Me'}</Button>);
