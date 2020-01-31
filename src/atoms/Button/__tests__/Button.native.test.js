e37a366c04c4712138ffc1d9cebffe0c031de4aa
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-01-31 14:06:01 +0530

    feat(tests): add support for testing web and native components (#2)

---
+++ src/atoms/Button/__tests__/Button.native.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+
+import Button from '../index';
+
+describe('Renders <Button /> correctly', () => {
+  it('renders button title correctly, defaultProps are assigned', () => {
+    const { getByText, baseElement } = render(<Button onClick={() => {}}>{'Click Me'}</Button>);
+    const button = getByText('Click Me');
+    expect(button).toBeTruthy();
+    expect(baseElement.props.children[0].props.children.props.onClick()).toBeUndefined();
+  });
+
+  it('onPress works as expected', () => {
+    const mockOnPress = jest.fn();
+    const { getByText } = render(<Button onClick={mockOnPress}>{'Click Me'}</Button>);
+    const button = getByText('Click Me');
+    fireEvent.press(button);
+    expect(mockOnPress.mock.calls.length).toBe(1);
+  });
+});
