101024ef748789ba01d9a4c46602b559c36ba7c1
Shubhnik Singh <shubhniksingh@gmail.com>
2020-02-03 21:35:06 +0530

    feat: tokens for design system

---
+++ src/tokens/fonts.js
@@ -0,0 +1,33 @@
+const fonts = {
+  family: {
+    lato: 'Lato',
+  },
+  sizes: {
+    xxs: '10px',
+    xs: '12px',
+    s: '13px',
+    m: '14px',
+    l: '16px',
+    xl: '18px',
+    xxl: '20px',
+    xxxl: '24px',
+    xxxxl: '32px',
+  },
+  weight: {
+    light: '300',
+    regular: '400',
+    bold: '700',
+  },
+  lineHeight: {
+    s: 1.2,
+    m: 1.25,
+    l: 1.5,
+  },
+  letterSpaceing: {
+    s: 0,
+    m: 0.5,
+    l: 1,
+  },
+};
+
+export default fonts;
---
+++ src/tokens/index.js
@@ -0,0 +1,4 @@
+import colors from './colors';
+import fonts from './fonts';
+
+export { colors, fonts };
