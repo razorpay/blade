c5978c67a15db89ff73224c3ba7a8d41e4c42a94
kamleshchandnani <kamlesh.chandnani@gmail.com>
2020-02-04 20:24:33 +0530

    build(scripts): add build script to ship the design system

--- .babelrc.js
+++ .babelrc.js
@@ -27,7 +27,7 @@ module.exports = {
       ],
     },
     'web-development': {
-      presets: ['@babel/preset-env', '@babel/preset-react'],
+      presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
       plugins: [
         [
           'babel-plugin-styled-components',
@@ -40,7 +40,7 @@ module.exports = {
       ],
     },
     'web-production': {
-      presets: ['@babel/preset-env', '@babel/preset-react'],
+      presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
       plugins: [
         [
           'babel-plugin-styled-components',
