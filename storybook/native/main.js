b7c2207658118619665895b4131abf039c271769
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-10 12:44:20 +0530

    build(async storage): add async storage support to storybook

--- storybook/native/main.js
+++ storybook/native/main.js
@@ -23,12 +23,10 @@ addDecorator((Story) => (
   </ThemeProvider>
 ));
 
-// Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
-// To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({
+  asyncStorage: require('@react-native-community/async-storage'),
+});
 
-// If you are using React Native vanilla write your app name here.
-// If you use Expo you can safely remove this line.
 AppRegistry.registerComponent('blade', () => StorybookUIRoot);
 
 export default StorybookUIRoot;
