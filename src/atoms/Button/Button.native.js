47f3aa8b8815cd23d25801b9c6df132a0eb08021
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-13 14:16:03 +0530

    style(button): use theme for button

--- src/atoms/Button/Button.native.js
+++ src/atoms/Button/Button.native.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components/native';
+import styled, { ThemeContext } from 'styled-components/native';
 import { TouchableOpacity } from 'react-native';
 import Text from '../Text';
 
 const StyledButton = styled(TouchableOpacity)(
   (props) => `
-  background-color: ${props.theme.colors.primary[900]};
-  padding-top: 14px;`,
+  background-color: ${props.theme.colors.shade[800]};
+  padding: 14px;`,
 );
 
-const Button = ({ onClick, children }) => (
-  <StyledButton onPress={onClick} testID="ds-button">
-    <Text color="white">{children}</Text>
-  </StyledButton>
-);
+const Button = ({ onClick, children }) => {
+  const theme = useContext(ThemeContext);
+
+  return (
+    <StyledButton onPress={onClick} testID="ds-button">
+      <Text color={theme.colors.tone[900]} align="center">
+        {children}
+      </Text>
+    </StyledButton>
+  );
+};
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
