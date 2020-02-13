eb15c2b2b764ce937789ff6134a3f68385382f03
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-13 15:51:37 +0530

    test(text): update tests for text component

--- src/atoms/Text/__tests__/Text.native.test.js
+++ src/atoms/Text/__tests__/Text.native.test.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import Text from '../index';
 import { renderWithTheme } from '../../../utils/testing';
 
 describe('Renders <Text /> correctly', () => {
-  it('snapshot testing', () => {
+  it('snapshot testing with regular text', () => {
     const displayText = 'Displaying some text';
     const { container } = renderWithTheme(<Text>{displayText}</Text>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('snapshot testing with bold text', () => {
+    const displayText = 'Displaying some text';
+    const { container } = renderWithTheme(<Text weight="bold">{displayText}</Text>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('snapshot testing with underlined text', () => {
+    const displayText = 'Displaying some text';
+    const { container } = renderWithTheme(<Text underline>{displayText}</Text>);
     expect(container).toMatchSnapshot();
   });
 
@@ -17,34 +28,6 @@ describe('Renders <Text /> correctly', () => {
     expect(textElement).toBeTruthy();
     expect(textElement.props.weight).toBe('regular');
     expect(textElement.props.size).toBe('m');
-    expect(StyleSheet.flatten(textElement.props.style).fontFamily).toBe('Lato-Regular');
     expect(textElement.props.underline).toBe(false);
-  });
-
-  it('renders normal text', () => {
-    const displayText = 'Displaying some text';
-    const { getByTestId } = renderWithTheme(<Text weight="regular">{displayText}</Text>);
-    const textElement = getByTestId('ds-text');
-    expect(textElement).toBeTruthy();
-    expect(textElement.props.weight).toBe('regular');
-    expect(StyleSheet.flatten(textElement.props.style).fontFamily).toBe('Lato-Regular');
-  });
-
-  it('renders bold text', () => {
-    const displayText = 'Displaying some text';
-    const { getByTestId } = renderWithTheme(<Text weight="bold">{displayText}</Text>);
-    const textElement = getByTestId('ds-text');
-    expect(textElement).toBeTruthy();
-    expect(textElement.props.weight).toBe('bold');
-    expect(StyleSheet.flatten(textElement.props.style).fontFamily).toBe('Lato-Bold');
-  });
-
-  it('renders underlined text', () => {
-    const displayText = 'Displaying some text';
-    const { getByTestId } = renderWithTheme(<Text underline>{displayText}</Text>);
-    const textElement = getByTestId('ds-text');
-    expect(textElement).toBeTruthy();
-    expect(textElement.props.underline).toBe(true);
-    expect(StyleSheet.flatten(textElement.props.style).textDecorationLine).toBe('underline');
   });
 });
