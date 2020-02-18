ce62312acf18917d70e2c3a0c209f38fe6ec37e5
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-18 12:21:49 +0530

    chore: create TextInput base

---
+++ src/atoms/TextInput/TextInput.native.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, TextInput as NativeTextInput } from 'react-native';
+
+const TextInput = () => {
+  return (
+    <View>
+      <NativeTextInput />
+    </View>
+  );
+};
+
+export default TextInput;
