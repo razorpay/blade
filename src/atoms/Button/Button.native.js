457510589edc914910d3c27f5a4a1310add8b6c9
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-18 22:13:58 +0530

    revert(button): reset button to master

--- src/atoms/Button/Button.native.js
+++ src/atoms/Button/Button.native.js
@@ -1,26 +1,31 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { ThemeContext } from 'styled-components/native';
-import { TouchableOpacity } from 'react-native';
-import Text from '../Text';
+import styled from 'styled-components/native';
+import { TouchableOpacity, Text } from 'react-native';
 
 const StyledButton = styled(TouchableOpacity)(
   (props) => `
   background-color: ${props.theme.colors.shade[800]};
-  padding: 14px;`,
+  padding: 14px;
+  border-radius: 6px;
+  margin-top: 7px;
+`,
 );
 
-const Button = ({ onClick, children }) => {
-  const theme = useContext(ThemeContext);
+const StyledText = styled(Text)(
+  (props) => `
+  color: ${props.theme.colors.tone[900]};
+  align-self: center;
+  font-size: 18px;
+  letter-spacing: ${props.theme.fonts.letterSpacing.l};
+`,
+);
 
-  return (
-    <StyledButton onPress={onClick} testID="ds-button">
-      <Text color={theme.colors.tone[900]} align="center">
-        {children}
-      </Text>
-    </StyledButton>
-  );
-};
+const Button = ({ onClick, children, ...rest }) => (
+  <StyledButton onPress={onClick} {...rest}>
+    <StyledText {...rest}>{children}</StyledText>
+  </StyledButton>
+);
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
--- src/atoms/Button/__tests__/Button.native.test.js
+++ src/atoms/Button/__tests__/Button.native.test.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { fireEvent } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 
 import Button from '../index';
-import { renderWithTheme } from '../../../utils/testing';
 
 describe('Renders <Button /> correctly', () => {
   it('snapshot testing', () => {
-    const { container } = renderWithTheme(<Button onClick={() => {}}>{'Click Me'}</Button>);
+    const { container } = render(<Button onClick={() => {}}>{'Click Me'}</Button>);
     expect(container).toMatchSnapshot();
   });
 
   it('renders button title correctly, defaultProps are assigned', () => {
-    const { getByTestId } = renderWithTheme(<Button onClick={() => {}}>{'Click Me'}</Button>);
-    const button = getByTestId('ds-button');
+    const { getByText, baseElement } = render(<Button onClick={() => {}}>{'Click Me'}</Button>);
+    const button = getByText('Click Me');
     expect(button).toBeTruthy();
-    expect(button.props.onClick).toBeUndefined();
+    expect(baseElement.props.children[0].props.children.props.onClick()).toBeUndefined();
   });
 
   it('onPress works as expected', () => {
     const mockOnPress = jest.fn();
-    const { getByTestId } = renderWithTheme(<Button onClick={mockOnPress}>{'Click Me'}</Button>);
-    const button = getByTestId('ds-button');
+    const { getByText } = render(<Button onClick={mockOnPress}>{'Click Me'}</Button>);
+    const button = getByText('Click Me');
     fireEvent.press(button);
     expect(mockOnPress.mock.calls.length).toBe(1);
   });
