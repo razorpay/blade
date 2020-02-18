77efb3c6857e33b6ea10780a6fd9b055405203ff
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-18 10:37:57 +0530

    test(icon): add snapshot test for icon

--- src/atoms/Icon/Icon.native.js
+++ src/atoms/Icon/Icon.native.js
@@ -9,19 +9,29 @@ const iconSize = {
   large: 24,
 };
 
-const Icon = ({ size, name, fill, ...rest }) => {
+const Icon = ({ size, name, fill, testID, ...rest }) => {
   const IconComponent = icons[name];
-  return <IconComponent fill={fill} height={iconSize[size]} width={iconSize[size]} {...rest} />;
+  return (
+    <IconComponent
+      fill={fill}
+      height={iconSize[size]}
+      width={iconSize[size]}
+      testID={testID}
+      {...rest}
+    />
+  );
 };
 
 Icon.defaultProps = {
   size: 'medium',
+  testID: 'ds-icon',
 };
 
 Icon.propTypes = {
   size: PropTypes.oneOf(['xsmall', 'small', 'medium', 'large']),
   name: PropTypes.oneOf(Object.keys(icons)).isRequired,
   fill: PropTypes.string,
+  testID: PropTypes.string,
 };
 
 export default Icon;
