ea3bc3b0e1081fe878693af11db522c4c9482157
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-18 12:23:06 +0530

    feat(icon): add Icon component [ME-4248] (#18)
    * feat(icon): add Icon component
    
    * docs(icon): change storybook name
    
    * chore(info icon): update svg
    
    * fix(icon): add default size prop
    
    * chore: cleanup icon
    
    * chore(icons): use inline requires
    
    * test(icon): add snapshot test for icon
    
    Co-authored-by: Kamlesh Chandnani <kamlesh.chandnani@gmail.com>

---
+++ src/icons/Info/Info.native.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+function Info(props) {
+  return (
+    <Svg width={16} height={16} viewBox="0 0 24 24" {...props}>
+      <Path d="M11 9h2V7h-2m1 13c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8m0-18A10 10 0 002 12a10 10 0 0010 10 10 10 0 0010-10A10 10 0 0012 2m-1 15h2v-6h-2v6z" />
+    </Svg>
+  );
+}
+
+export default Info;
