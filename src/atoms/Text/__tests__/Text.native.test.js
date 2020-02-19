8ee27c74956c9edfb405cafe3cda37b0f0f851c6
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-19 15:27:16 +0530

    feat(text): Add Text component (#13)
    * feat(text): Create Text component
    
    * style(button): use theme for button
    
    * docs(text): use large size for text storybook
    
    * test(text): update tests for text component
    
    * chore(tokens): change s,m,l to small,medium,large
    
    * chore(text): remove letterSpacing prop
    
    * chore: restrict text size
    
    * chore: add support for dynamic testID
    
    * test: update text test and snapshots
    
    * chore(text): add empty text.web component
    
    * chore(color): resolve dot notation colors
    
    * chore(colors): pass all colors to theme
    
    * chore(colors): use reduce to resolve color names
    
    * refactor: change underline to isUnderlined
    
    * refactor: move font families to fonts
    
    * refactor: move colors to top
    
    * refactor: rename getColor and getColorKeys function
    
    * revert(button): reset button to master
    
    * refactor: fix Text styling and tests
    
    * refactor: add else statement
    
    * chore(private props): make weight & isUnderlined private props
    
    * chore: move private props to the end
    
    * chore: put else in braces
    
    * chore(text): fix font family structure
    
    * chore(text): change align prop to left, right & center
    
    * chore(_helpers): move utils to _helpers
    
    * chore(lineheight): add lineHeight prop
    
    * chore(letter spacing): add _letterSpacing prop
    
    * chore(lineheight): remove lineHeight prop
    
    * refactor(text): add default color prop, update tests and move imports
    
    * chore: remove backticks

---
+++ src/atoms/Text/__tests__/Text.native.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Text from '../index';
+import { renderWithTheme } from '../../../_helpers/testing';
+
+describe('Renders <Text /> correctly', () => {
+  it('snapshot testing with regular text', () => {
+    const displayText = 'Displaying some text';
+    const { container } = renderWithTheme(<Text>{displayText}</Text>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('snapshot testing with bold text', () => {
+    const displayText = 'Displaying some text';
+    const { container } = renderWithTheme(<Text _weight="bold">{displayText}</Text>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('snapshot testing with underlined text', () => {
+    const displayText = 'Displaying some text';
+    const { container } = renderWithTheme(<Text _isUnderlined>{displayText}</Text>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders default text, defaultProps are assigned', () => {
+    const displayText = 'Displaying some text';
+    const { getByTestId } = renderWithTheme(<Text>{displayText}</Text>);
+    const textElement = getByTestId('ds-text');
+    expect(textElement).toBeTruthy();
+    expect(textElement.props.size).toBe('large');
+    expect(textElement.props.color).toBe('shade.800');
+    expect(textElement.props._weight).toBe('regular');
+    expect(textElement.props._isUnderlined).toBe(false);
+    expect(textElement.props._letterSpacing).toBe('small');
+  });
+});
