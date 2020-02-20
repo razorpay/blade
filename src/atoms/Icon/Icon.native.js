3533a9cd460604711f709c6b58d35d28376f0473
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-20 13:13:39 +0530

    chore(icon): use new color resolution method

--- src/atoms/Icon/Icon.native.js
+++ src/atoms/Icon/Icon.native.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { ThemeContext } from 'styled-components/native';
 import icons from '../../icons';
+import { getColorKeys, getColor } from '../../_helpers/colors';
 
 const iconSize = {
   xsmall: 12,
@@ -11,9 +13,10 @@ const iconSize = {
 
 const Icon = ({ size, name, fill, testID, ...rest }) => {
   const IconComponent = icons[name];
+  const theme = useContext(ThemeContext);
   return (
     <IconComponent
-      fill={fill}
+      fill={getColor(theme, fill)}
       height={iconSize[size]}
       width={iconSize[size]}
       testID={testID}
@@ -25,12 +28,13 @@ const Icon = ({ size, name, fill, testID, ...rest }) => {
 Icon.defaultProps = {
   size: 'medium',
   testID: 'ds-icon',
+  fill: 'shade.500',
 };
 
 Icon.propTypes = {
   size: PropTypes.oneOf(['xsmall', 'small', 'medium', 'large']),
   name: PropTypes.oneOf(Object.keys(icons)).isRequired,
-  fill: PropTypes.string,
+  fill: PropTypes.oneOf(getColorKeys()),
   testID: PropTypes.string,
 };
 
