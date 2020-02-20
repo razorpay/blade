9d3617349c7b4c99d606c53cfcaaae434ad62995
Shridhar Bavannawar <shridhar.b@razorpay.com>
2020-02-20 18:28:04 +0530

    refactor(checkbox/checkbox.web.js): remove Checkbox web contents

--- src/atoms/Checkbox/Checkbox.web.js
+++ src/atoms/Checkbox/Checkbox.web.js
@@ -1,32 +1,3 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-const Checkbox = ({ checked }) => {
-  return (
-    <div>
-      <input type="checkbox" id="scales" name="scales" checked={checked} />
-      <label htmlFor="scales">Checkbox label</label>
-    </div>
-  );
-};
-
-Checkbox.propTypes = {
-  // onClick: PropTypes.func,
-  checked: PropTypes.bool,
-  //size: PropTypes.oneOf(['l', 'm', 's']),
-  //title: PropTypes.string,
-  //helpText: PropTypes.string,
-  //disabled: PropTypes.bool,
-};
-
-Checkbox.defaultProps = {
-  // onClick: () => {},
-  checked: false,
-  // size: 'm',
-  //title: '',
-  // helpText: '',
-  // disabled: false,
-  // testID: 'checkbox',
-};
+const Checkbox = () => null;
 
 export default Checkbox;
