58e5127e1228c841f71f80e23d9a4cfa2f7ce233
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-20 16:23:31 +0530

    chore(Icon): use new color resolution method (#26)
    * chore(icon): use new color resolution method
    
    * fix: fix default size

--- src/atoms/Icon/Icon.native.js
+++ src/atoms/Icon/Icon.native.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { ThemeContext } from 'styled-components/native';
 import icons from '../../icons';
+import { getColorKeys, getColor } from '../../_helpers/colors';
 
 const iconSize = {
   xsmall: 12,
   small: 16,
-  medium: 40,
+  medium: 20,
   large: 24,
 };
 
 const Icon = ({ size, name, fill, testID, ...rest }) => {
   const IconComponent = icons[name];
+  const theme = useContext(ThemeContext);
   return (
     <IconComponent
-      fill={fill}
+      fill={getColor(theme, fill)}
       height={iconSize[size]}
       width={iconSize[size]}
       testID={testID}
@@ -25,12 +28,13 @@ const Icon = ({ size, name, fill, testID, ...rest }) => {
 Icon.defaultProps = {
   size: 'medium',
   testID: 'ds-icon',
+  fill: 'shade.500',
 };
 
 Icon.propTypes = {
   size: PropTypes.oneOf(['xsmall', 'small', 'medium', 'large']),
   name: PropTypes.oneOf(Object.keys(icons)).isRequired,
-  fill: PropTypes.string,
+  fill: PropTypes.oneOf(getColorKeys()),
   testID: PropTypes.string,
 };
 
