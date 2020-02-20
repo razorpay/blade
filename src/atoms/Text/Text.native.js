6d61dd0c46cb5adba0b666b41d6282bf0b1b68a9
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-20 16:04:28 +0530

    refactor: re-arrange props

--- src/atoms/Text/Text.native.js
+++ src/atoms/Text/Text.native.js
@@ -37,7 +37,7 @@ const styles = {
   letterSpacing({ theme, _letterSpacing }) {
     return `${theme.fonts.letterSpacing[_letterSpacing]}`;
   },
-  lineHeight({ theme, _lineHeight, size }) {
+  lineHeight({ theme, size, _lineHeight }) {
     return getLineHeight(theme.fonts.size[size], theme.fonts.lineHeight[_lineHeight]);
   },
 };
