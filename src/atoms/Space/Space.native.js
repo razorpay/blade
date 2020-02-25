e8ef8cead362aedc6992a2eb131506e594d8aa6a
Shridhar Bavannawar <shridhar.b@razorpay.com>
2020-02-25 15:41:51 +0530

    refactor(space.native.js): validate for single node child

--- src/atoms/Space/Space.native.js
+++ src/atoms/Space/Space.native.js
@@ -2,21 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
 
-import { makePxValues } from '../../_helpers/theme';
+import { makePxValue } from '../../_helpers/theme';
 
-const SpaceEnhancer = styled(({ margin, padding, children, ...props }) =>
-  React.cloneElement(children, props),
-)`
-  ${(props) => (props.margin ? `margin: ${makePxValues(props.margin)}` : '')};
-  ${(props) => (props.padding ? `padding: ${makePxValues(props.padding)}` : '')};
-`;
-
-const Space = ({ margin, padding, children, ...props }) => {
-  if (React.Children.toArray(children).length === 1) {
-    return <SpaceEnhancer margin={margin} padding={padding} children={children} {...props} />;
+const Space = styled(({ margin, padding, children, ...props }) => {
+  if (React.Children.toArray(children).length !== 1) {
+    throw new Error('Expected a single child for Space component');
   }
-  throw new Error('Expected a single child for Space component');
-};
+  return React.cloneElement(children, props);
+})`
+  ${(props) => (props.margin ? `margin: ${makePxValue(props.margin)}` : '')};
+  ${(props) => (props.padding ? `padding: ${makePxValue(props.padding)}` : '')};
+`;
 
 Space.propTypes = {
   margin: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.number, PropTypes.string])),
