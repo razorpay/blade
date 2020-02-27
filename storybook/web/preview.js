ccf98ddf87d2a92a5525d79c2fc17d144fed0bbb
Susheela <susheela.choudry@razorpay.com>
2020-02-27 16:12:40 +0530

    fix[client]: add web knobs to the story.

--- storybook/web/preview.js
+++ storybook/web/preview.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { addDecorator } from '@storybook/react';
+// import { withKnobs as withOnDeviceKnobs } from '@storybook/addon-ondevice-knobs';
+import { withKnobs } from '@storybook/addon-knobs/react';
 
 const theme = {};
 
+addDecorator(withKnobs);
+// addDecorator(withOnDeviceKnobs);
 addDecorator((Story) => (
   <ThemeProvider theme={theme}>
     <Story />
