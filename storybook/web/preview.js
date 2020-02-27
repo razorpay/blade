5c8120c03f280c1af7051f10bccc6eb9b73bb9d3
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-02-27 17:51:30 +0530

    chore: move knobs decorator to main.js to fix web crashes (#38)
    * chore: move knobs decorator to main.js to fix web crashes
    
    * chore: add knobs decorator for web

--- storybook/web/preview.js
+++ storybook/web/preview.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { addDecorator } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs/react';
 
 const theme = {};
+
+addDecorator(withKnobs);
 
 addDecorator((Story) => (
   <ThemeProvider theme={theme}>
