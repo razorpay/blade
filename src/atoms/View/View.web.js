d06c07b4595b75ac965b110d4763b7feea4d7d5a
Susheela <susheela.choudry@razorpay.com>
2020-03-03 18:26:36 +0530

    refactor(render): added a helper function called renderwiththeme

--- src/atoms/View/View.web.js
+++ src/atoms/View/View.web.js
@@ -2,19 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const View = styled(({ component, children, ...props }) =>
-  React.createElement(component, props, children),
-)`
+const View = styled(({ as, children, ...props }) => React.createElement(as, props, children))`
   ${''}
 `;
 
 View.propTypes = {
-  component: PropTypes.string.isRequired,
+  as: PropTypes.string.isRequired,
   children: PropTypes.node,
 };
 
 View.defaultProps = {
-  component: 'div',
+  as: 'div',
 };
 
 export default View;
