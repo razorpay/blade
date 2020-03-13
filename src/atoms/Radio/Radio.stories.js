6d084efd917d5e8fb73c82541e925ac4a979bdb1
shridharbavannawar <shridhar.b@razorpay.com>
2020-03-13 18:31:41 +0530

    feat(radio): add radio component for react-native (#48)
    * feat(radiobutton): add radio button component
    
    * refactor(radiobutton.native.js, __tests__): remove fixed padding from Backdrop, Add tests cases
    
    * style(tab/backdrop.native.js): format Backdrop styled component
    
    * refactor(radiobutton.native.js): use Size component
    
    * test(radiobutton.native.test.js): update snapshots
    
    * refactor(src/_helpers/isdefined): rename isPropDefined to isDefined
    
    * refactor(src/_helpers/isdefined): rename arg to value
    
    * refactor(radio/radiobuttongroup.native.js): add radio-button-group
    
    * refactor(radiobutton/radiobutton.native.js): add Radio component as context provider
    
    * refactor(switch.native.js): rename isPropDefined to isDefined
    
    * refactor(radiobutton.native.js): add default props to Radio
    
    * refactor(radiobutton.native.js, radiobutton.stories.js): rename onValueChange -> onChange
    
    * refactor(radiobutton.native.js): replace if by ternary operator
    
    * refactor(radiobutton/radiobutton.native.js): remove onPress event
    
    * test(radiobutton/snapshots): update snapshots
    
    * refactor(radiobutton.native.js, radiobuttoncontext.native.js): radio and Radio Option as diff files
    
    * test(radiobutton.native.test.js): update test cases and snapshots
    
    * refactor(radio): rename RadioButton to Radio

---
+++ src/atoms/Radio/Radio.stories.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, boolean, select } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import Radio from './Radio';
+import Flex from '../Flex';
+import View from '../View';
+
+const sizeOptions = {
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+};
+
+storiesOf('RadioButton', module)
+  .addParameters({
+    component: Radio,
+  })
+  .add('default', () => (
+    <Radio
+      value="1"
+      onChange={(value) => {
+        action('Value', value);
+      }}
+    >
+      <Radio.Option
+        value="1"
+        size={select('Size', sizeOptions, 'large')}
+        title={text('Title', 'Enable Beast Mode')}
+        helpText={text('Help Text', 'Play with addons to see changes')}
+        disabled={boolean('Disabled', false)}
+        errorText={text('Error Text', '')}
+      />
+    </Radio>
+  ))
+  .add('onChange', () => (
+    <Radio
+      onChange={(value) => {
+        action('Value', value);
+      }}
+    >
+      <Radio.Option value="1" size="large" title="Unchecked" helpText="I'm unchecked" />
+      <Radio.Option value="2" size="large" title="Checked" helpText="I'm active" />
+      <Radio.Option value="3" size="large" disabled title="Disabled" helpText="I'm disabled" />
+      <Radio.Option value="4" size="large" title="Error" errorText="Something's not right!" />
+    </Radio>
+  ))
+  .add('controlled', () => (
+    <Flex flexDirection="row" flexWrap="wrap">
+      <View>
+        <Radio
+          value="1"
+          onChange={(value) => {
+            action('Value', value);
+          }}
+        >
+          <Radio.Option value="1" size="large" title="Unchecked" helpText="I'm unchecked" />
+          <Radio.Option value="2" size="large" title="Checked" helpText="I'm active" />
+          <Radio.Option value="3" size="large" disabled title="Disabled" helpText="I'm disabled" />
+          <Radio.Option value="4" size="large" title="Error" errorText="Something's not right!" />
+        </Radio>
+      </View>
+    </Flex>
+  ));
---
+++ src/atoms/Radio/Radio.web.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RadioButton = () => <div>WIP</div>;
+
+export default RadioButton;
