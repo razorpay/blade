3abfda96bddb88a593c29869b06505077709bdf8
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-03-19 17:18:36 +0530

    feat(Amount): create Amount component for react-native (#65)
    * feat: create Amount component
    
    * fix: line heights
    
    * docs: add comment
    
    * chore: use intl polyfill for android
    
    * docs: remove comment
    
    * refactor: move from atoms to molecules
    
    * refactor: remove short circuits
    
    * chore: remove align prop
    
    * refactor: remove switch case
    
    * chore: allow changing currency
    
    * chore: add prop types for currency
    
    * chore: move geISOCurrencyList to molecules/Amount
    
    Co-authored-by: Kamlesh Chandnani <kamlesh.c@razorpay.com>

---
+++ src/molecules/Amount/Amount.native.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Text as NativeText, Platform } from 'react-native';
+import automation from '../../_helpers/automation-attributes';
+import Flex from '../../atoms/Flex';
+import Text from '../../atoms/Text';
+import Heading from '../../atoms/Heading';
+import geISOCurrencyList from './geISOCurrencyList';
+
+const IS_ANDROID = Platform.OS === 'android';
+
+const styles = {
+  text: {
+    color({ size }) {
+      if (size === 'xxxlarge') {
+        return 'shade.950';
+      }
+
+      return 'shade.940';
+    },
+    size({ size }) {
+      switch (size) {
+        case 'medium':
+          return 'xxsmall';
+        case 'large':
+          return 'xsmall';
+        case 'xlarge':
+          return 'medium';
+        case 'xxlarge':
+          return 'medium';
+        case 'xxxlarge':
+          return 'large';
+        default:
+          return 'xxsmall';
+      }
+    },
+    weight({ size }) {
+      switch (size) {
+        case 'medium':
+          return 'regular';
+        case 'large':
+          return 'bold';
+        case 'xlarge':
+          return 'bold';
+        case 'xxlarge':
+          return 'bold';
+        case 'xxxlarge':
+          return 'bold';
+        default:
+          return 'bold';
+      }
+    },
+    lineHeight({ size }) {
+      switch (size) {
+        case 'medium':
+          return 'medium';
+        case 'large':
+          return 'medium';
+        case 'xlarge':
+          return 'xlarge';
+        case 'xxlarge':
+          return 'xxlarge';
+        case 'xxxlarge':
+          return 'xxlarge';
+        default:
+          return 'xxlarge';
+      }
+    },
+  },
+};
+
+const formatAmount = ({ amount, currency }) => {
+  if (IS_ANDROID) {
+    // Polyfill for Android
+    require('intl');
+    require('intl/locale-data/jsonp/en-IN');
+  }
+
+  const formattedAmount = (+amount).toLocaleString('en-IN', {
+    style: 'currency',
+    currencyDisplay: 'symbol',
+    currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+  return formattedAmount;
+};
+
+const Amount = ({ size, testID, children, currency }) => {
+  if (isNaN(children)) {
+    throw new Error(`Expected children to be number \n(Eg. "1234", "12.34")`);
+  }
+
+  const formattedAmount = formatAmount({ amount: children, currency });
+  const [currencySymbol, amount] = formattedAmount.split(/\s/);
+  const [integerPart, fractionPart] = amount.split('.');
+
+  return (
+    <Flex>
+      <NativeText {...automation(testID)}>
+        <Text
+          color={styles.text.color({ size })}
+          size={styles.text.size({ size })}
+          _weight={styles.text.weight({ size })}
+          _lineHeight={styles.text.lineHeight({ size })} // First text component within nested texts dictate the line height
+        >
+          {`${currencySymbol} `}
+        </Text>
+        <Heading color="shade.980" size={size}>
+          {integerPart}
+        </Heading>
+        <Text
+          color={styles.text.color({ size })}
+          size={styles.text.size({ size })}
+          _weight={styles.text.weight({ size })}
+        >{`.${fractionPart}`}</Text>
+      </NativeText>
+    </Flex>
+  );
+};
+
+Amount.propTypes = {
+  children: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['medium', 'large', 'xlarge', 'xxlarge', 'xxxlarge']),
+  testID: PropTypes.string,
+  currency: PropTypes.oneOf(geISOCurrencyList()),
+};
+
+Amount.defaultProps = {
+  size: 'medium',
+  testID: 'ds-amount',
+  currency: 'INR',
+};
+
+export default Amount;
---
+++ src/molecules/Amount/Amount.stories.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { text, select } from '@storybook/addon-knobs';
+import Amount from './Amount';
+import Space from '../../atoms/Space';
+import ScrollView from '../../atoms/ScrollView';
+import View from '../../atoms/View';
+import Size from '../../atoms/Size';
+import geISOCurrencyList from './geISOCurrencyList';
+
+const currencyOptions = geISOCurrencyList().reduce(
+  (options, option) => ({ ...options, [option]: option }),
+  {},
+);
+
+storiesOf('Amount', module)
+  .addParameters({
+    component: Amount,
+  })
+  .add('default', () => (
+    <Size height="100%">
+      <ScrollView>
+        <Space margin={[4, 0, 0, 0]}>
+          <View>
+            <Amount size="medium" currency={select('Currency', currencyOptions, 'INR')}>
+              {text('Amount', '1234')}
+            </Amount>
+          </View>
+        </Space>
+        <Space margin={[4, 0, 0, 0]}>
+          <View>
+            <Amount size="large" currency={select('Currency', currencyOptions, 'INR')}>
+              {text('Amount', '1234')}
+            </Amount>
+          </View>
+        </Space>
+        <Space margin={[4, 0, 0, 0]}>
+          <View>
+            <Amount size="xlarge" currency={select('Currency', currencyOptions, 'INR')}>
+              {text('Amount', '1234')}
+            </Amount>
+          </View>
+        </Space>
+        <Space margin={[4, 0, 0, 0]}>
+          <View>
+            <Amount size="xxlarge" currency={select('Currency', currencyOptions, 'INR')}>
+              {text('Amount', '1234')}
+            </Amount>
+          </View>
+        </Space>
+        <Space margin={[4, 0, 0, 0]}>
+          <View>
+            <Amount size="xxxlarge" currency={select('Currency', currencyOptions, 'INR')}>
+              {text('Amount', '1234')}
+            </Amount>
+          </View>
+        </Space>
+      </ScrollView>
+    </Size>
+  ));
