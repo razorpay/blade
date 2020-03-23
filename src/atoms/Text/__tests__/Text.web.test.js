e79b9e52a76683182540e6c1049059d318990ff3
Susheela <susheela.choudry@razorpay.com>
2020-03-23 16:28:57 +0530

    fix(text): added test cases and incorporated the PR feedback

--- src/atoms/Text/__tests__/Text.web.test.js
+++ src/atoms/Text/__tests__/Text.web.test.js
@@ -23,7 +23,8 @@ describe('Renders <Text /> correctly', () => {
   });
 
   it('snapshot testing with maxLines in text', () => {
-    const displayText = 'Displaying some text';
+    const displayText =
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
     const { container } = renderWithTheme(<Text maxLines={2}>{displayText}</Text>);
     expect(container).toMatchSnapshot();
   });
