135d2a0c0cfa606788503315e5a5c3d8553a7d8d
Susheela Choudry <susheela.choudry@razorpay.com>
2020-03-23 17:54:21 +0530

    feat(text): implementation of text component (#54)
    * feat(text): implementation of text component
    
    * test(text): added testcases and incorporated the feedback
    
    * fix(text): extacting style out so , no one can override the style
    
    * fix(text): added default value for truncate prop in text component
    
    * refactor(text): incorporate the PR feedback
    
    * feat(text): added number of lines support in web
    
    * fix(text): adding truncate behavaiour if tsxt exceeds more than the required lines
    
    * fix(text): added ellipses for more than one lines
    
    * refactor(text): incorporated the pr feedback
    
    * refactor(text): incorporated the PR feedback
    
    * refactor(test): remove lineheight from props and added getLineHeight
    
    * fix(text): removed unneccesary props
    
    * refactor(text): alignment changes
    
    * refactor(test): updated the testcases and stories
    
    * refactor(text): added isDefined helper function to check undefined
    
    n
    
    * refactor(text): incorporated the PR feedback
    
    * fix(text): added test cases and incorporated the PR feedback
    
    * fix(text): incorporate the pr feedback
    
    * fix(text): one last commit, incorporated the PR feedback
    
    * fix(text): incorporated the feedback
    
    Co-authored-by: Kamlesh Chandnani <kamlesh.c@razorpay.com>

--- src/atoms/Text/Text.web.js
+++ src/atoms/Text/Text.web.js
@@ -1,3 +1,108 @@
-const Text = () => null;
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import baseTheme from '../../tokens/theme';
+import isDefined from '../../_helpers/isDefined';
+import { getColorKeys, getColor, getLineHeight } from '../../_helpers/theme';
+
+const styles = {
+  color({ theme, color }) {
+    return getColor(theme, color);
+  },
+  align({ align }) {
+    switch (align) {
+      case 'left':
+        return 'flex-start';
+      case 'center':
+        return 'center';
+      case 'right':
+        return 'flex-end';
+      default:
+        return 'flex-start';
+    }
+  },
+  lineHeight({ theme, size, _lineHeight }) {
+    if (_lineHeight) {
+      return theme.fonts.lineHeight[_lineHeight];
+    } else {
+      return getLineHeight(theme, size);
+    }
+  },
+  maxHeight({ theme, size, maxLines }) {
+    if (isDefined(maxLines)) {
+      const lineHeight = getLineHeight(theme, size);
+      const maxHeight = `${parseFloat(lineHeight) * maxLines}px`;
+      return maxHeight;
+    }
+    return 'initial';
+  },
+  overflow({ maxLines }) {
+    if (isDefined(maxLines)) {
+      return 'hidden';
+    }
+    return 'initial';
+  },
+  textOverflow({ maxLines }) {
+    if (isDefined(maxLines)) {
+      return 'ellipsis';
+    }
+    return 'initial';
+  },
+  textDecoration({ _isUnderlined }) {
+    if (_isUnderlined) {
+      return 'underline';
+    }
+    return 'none';
+  },
+  ellipsis({ maxLines }) {
+    if (isDefined(maxLines)) {
+      return "'...'";
+    }
+    return '';
+  },
+};
+
+const Text = styled.div`
+  font-family: ${(props) => props.theme.fonts.family.lato[props._weight]};
+  font-size: ${(props) => props.theme.fonts.size[props.size]};
+  color: ${styles.color};
+  text-decoration: ${styles.textDecoration};
+  align-self: ${styles.align};
+  letter-spacing: ${(props) => props.theme.fonts.letterSpacing[props._letterSpacing]};
+  line-height: ${styles.lineHeight};
+  overflow: ${styles.overflow};
+  text-overflow: ${styles.textOverflow};
+  max-height: ${styles.maxHeight};
+  position: relative;
+  &&:after {
+    content: ${styles.ellipsis};
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    background: white;
+  }
+`;
+
+Text.propTypes = {
+  size: PropTypes.oneOf(['xxsmall', 'xsmall', 'small', 'medium', 'large']),
+  color: PropTypes.oneOf(getColorKeys()),
+  align: PropTypes.oneOf(['left', 'center', 'right']),
+  'data-testid': PropTypes.string,
+  maxLines: PropTypes.number,
+  _weight: PropTypes.oneOf(Object.keys(baseTheme.fonts.weight)),
+  _isUnderlined: PropTypes.bool,
+  _letterSpacing: PropTypes.oneOf(Object.keys(baseTheme.fonts.letterSpacing)),
+  _lineHeight: PropTypes.oneOf(Object.keys(baseTheme.fonts.lineHeight)),
+};
+
+Text.defaultProps = {
+  size: 'large',
+  align: 'left',
+  color: 'shade.980',
+  maxLines: undefined,
+  'data-testid': 'ds-text',
+  _weight: 'regular',
+  _isUnderlined: false,
+  _letterSpacing: 'small',
+};
 
 export default Text;
