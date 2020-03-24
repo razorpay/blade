135d30803b1663b3df4cb9343b056e4af94fe8ac
Susheela <susheela.choudry@razorpay.com>
2020-03-24 10:20:28 +0530

    feat(view): added text component

--- src/atoms/View/View.stories.js
+++ src/atoms/View/View.stories.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import View from './View';
+import Text from '../Text';
 
 storiesOf('View', module)
   .addParameters({
     component: View,
   })
-  .add('with text', () => <View>Simple Div</View>);
+  .add('with text', () => (
+    <View>
+      <Text>Simple Div</Text>
+    </View>
+  ));
