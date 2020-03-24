28d32b940514fdcc982ee6ac0c53fb0ea284a34b
Susheela <susheela.choudry@razorpay.com>
2020-03-24 11:25:38 +0530

    refactor(view): updated test cases

--- src/atoms/View/__tests__/View.web.test.js
+++ src/atoms/View/__tests__/View.web.test.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import { renderWithTheme } from '../../../_helpers/testing';
-
+import Text from '../../Text';
 import View from '../index';
 
-describe('Renders with text ', () => {
-  it('snapshot testing', () => {
-    const { container } = renderWithTheme(<View>{'simple div'}</View>);
-    expect(container).toMatchSnapshot();
+describe('<View />', () => {
+  describe('as', () => {
+    it('renders View as div', () => {
+      const { container } = renderWithTheme(
+        <View as="div">
+          <Text>{'simple div'}</Text>
+        </View>,
+      );
+      expect(container).toMatchSnapshot();
+    });
+    it('renders View as span', () => {
+      const { container } = renderWithTheme(
+        <View as="span">
+          <Text>{'simple span'}</Text>
+        </View>,
+      );
+      expect(container).toMatchSnapshot();
+    });
+    it('renders View as p', () => {
+      const { container } = renderWithTheme(
+        <View as="p">
+          <Text>{'simple p'}</Text>
+        </View>,
+      );
+      expect(container).toMatchSnapshot();
+    });
   });
 });
