fa98e0331508984269cc0d44ed10f92d9bb201f8
krishnaacc123 <krishnaacc123@gmail.com>
2020-03-26 21:09:37 +0530

    fix(flex web): feat(flex/web): add stories

--- src/atoms/Flex/Flex.stories.js
+++ src/atoms/Flex/Flex.stories.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Flex from './Flex';
+import { select, text } from '@storybook/addon-knobs';
+
 import Text from '../Text';
 import View from '../View';
+import Space from '../Space';
+import Flex from './Flex';
+
+const fillOptions = {
+  row: 'row',
+  column: 'column',
+};
 
 storiesOf('Flex', module)
   .addParameters({
@@ -11,14 +19,32 @@ storiesOf('Flex', module)
   .add('default', () => (
     <Flex>
       <View>
-        <Text>Flex Child 1</Text>
-        <Text>Flex Child 2</Text>
+        <Text>{text('Display Text', 'Flex child #1')}</Text>
+        <Text>{text('Display Text', 'Flex child #2')}</Text>
       </View>
     </Flex>
   ))
-  .add('Flex properties', () => (
-    <Flex flex={1} flexDirection="row">
-      <View>
+  .add('Flex directions', () => (
+    <Flex flexDirection={select('Direction', fillOptions, 'row')}>
+      <View as="div">
+        <Space margin={[1, 2]}>
+          <View>Flex Child #1</View>
+        </Space>
+        <Space padding={[1, 2]}>
+          <View>Flex Child #2</View>
+        </Space>
+        <Space padding={[1, 2]}>
+          <View>Flex Child #3</View>
+        </Space>
+        <Space margin={[1, 2]}>
+          <View>Flex Child #4</View>
+        </Space>
+      </View>
+    </Flex>
+  ))
+  .add('Flex flex', () => (
+    <Flex flex={1}>
+      <View as="div">
         <View>Flex Child 1</View>
         <View>Flex Child 2</View>
         <View>Flex Child 3</View>
