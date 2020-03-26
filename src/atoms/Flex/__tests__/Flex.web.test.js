3131398507900c41adb7f9d87529a276282fe60a
krishnaacc123 <krishnaacc123@gmail.com>
2020-03-26 20:33:17 +0530

    feat(flex web): feat(flex/web): add test cases

--- src/atoms/Flex/__tests__/Flex.web.test.js
+++ src/atoms/Flex/__tests__/Flex.web.test.js
@@ -4,33 +4,39 @@ import { render } from '@testing-library/react';
 import View from '../../View';
 import Flex from '../index';
 
-describe('Renders <Flex /> correctly', () => {
-  it('renders child of <Flex />(no styles) correctly', () => {
-    const { container } = render(
-      <Flex>
-        <View />
-      </Flex>,
-    );
-    expect(container).toMatchSnapshot();
-  });
-  it('renders an inline styled child of <Flex />(with styles) correctly', () => {
-    const viewStyles = { backgroundColor: 'red', height: 100 };
-    const { container } = render(
-      <Flex flex={1} flexDirection="row">
-        <View style={viewStyles} />
-      </Flex>,
-    );
-    expect(container).toMatchSnapshot();
-  });
-  it('should throw error when more than one nodes are passed as children', () => {
-    const expectedErrorMessage = 'Expected a single child for Space component';
-    expect(() =>
-      render(
-        <Flex>
-          <View />
+describe('<Flex />', () => {
+  describe('flexDirection', () => {
+    it('renders a child with flexDirection attribute and flex properties correctly', () => {
+      const { container } = render(
+        <Flex flexDirection="row">
           <View />
         </Flex>,
-      ),
-    ).toThrow(expectedErrorMessage);
+      );
+      expect(container).toMatchSnapshot();
+    });
+  });
+  describe('flex', () => {
+    it('renders an inline styled child with flex attribute and flex properties correctly', () => {
+      const viewStyles = { backgroundColor: 'red', height: 100 };
+      const { container } = render(
+        <Flex flex={1}>
+          <View style={viewStyles} />
+        </Flex>,
+      );
+      expect(container).toMatchSnapshot();
+    });
+  });
+  describe('error', () => {
+    it('should throw error when more than one nodes are passed as children', () => {
+      const expectedErrorMessage = 'Expected a single child for Space component';
+      expect(() =>
+        render(
+          <Flex>
+            <View />
+            <View />
+          </Flex>,
+        ),
+      ).toThrow(expectedErrorMessage);
+    });
   });
 });
