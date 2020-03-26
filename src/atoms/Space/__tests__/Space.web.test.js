e6478a39b0e143f1456f4e148eacebce84d18c91
krishnaacc123 <krishnaacc123@gmail.com>
2020-03-26 16:03:31 +0530

    fix(space): bug(space/web): change test cases to nesting test cases

--- src/atoms/Space/__tests__/Space.web.test.js
+++ src/atoms/Space/__tests__/Space.web.test.js
@@ -7,35 +7,35 @@ beforeAll(() => jest.spyOn(console, 'error').mockImplementation());
 afterAll(() => jest.restoreAllMocks());
 
 describe('Renders <Space /> correctly', () => {
-  it('should apply padding to child component', () => {
-    const displayText = 'Hello';
-    const { container } = renderWithTheme(
-      <Space padding={[1, 2]}>
-        <View>{displayText}</View>
-      </Space>,
-    );
-    expect(container).toMatchSnapshot();
-  });
-
-  it('should apply margin to child component', () => {
-    const displayText = 'Hello';
-    const { container } = renderWithTheme(
-      <Space margin={[5, 5]}>
-        <View>{displayText}</View>
-      </Space>,
-    );
-    expect(container).toMatchSnapshot();
-  });
-
-  it('should throw error when more than one nodes are passed as children', () => {
-    const expectedErrorMessage = 'Expected a single child for Space component';
-    expect(() =>
-      renderWithTheme(
-        <Space margin={[5, 5]}>
-          <View />
-          <View />
+  describe('with view component.', () => {
+    it('Should apply padding to child component', () => {
+      const displayText = 'Flex Item';
+      const { container } = renderWithTheme(
+        <Space padding={[1, 2]}>
+          <View>{displayText}</View>
         </Space>,
-      ),
-    ).toThrow(expectedErrorMessage);
+      );
+      expect(container).toMatchSnapshot();
+    });
+    it('Should apply margin to child component', () => {
+      const displayText = 'Flex Item';
+      const { container } = renderWithTheme(
+        <Space margin={[5, 5]}>
+          <View>{displayText}</View>
+        </Space>,
+      );
+      expect(container).toMatchSnapshot();
+    });
+    it('Should throw error when more than one nodes are passed as children', () => {
+      const expectedErrorMessage = 'Expected a single child for Space component';
+      expect(() =>
+        renderWithTheme(
+          <Space margin={[5, 5]}>
+            <View />
+            <View />
+          </Space>,
+        ),
+      ).toThrow(expectedErrorMessage);
+    });
   });
 });
