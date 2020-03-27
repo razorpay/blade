c068fbbb604965824c2cf2ea5cc9795fb485d5d7
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-03-27 17:17:42 +0530

    fix(Amount/native): add more variants, weights and sizes (#70)
    * chore(Amount/native): add support for more variants, weight and sizes
    
    - Adds support for camel, normal, camel-subtle and normal-subtle
    variants
    - Adds support for weight prop
    - Adds support for more sizes
    
    * chore: add subtle as a prop & remove -subtle variants
    
    * test: update titles
    
    * refactor: rearrange variantColor & variant props
    
    * refactor: Text to AtomText
    
    * refactor: fix weird formatting

--- src/molecules/Amount/Amount.native.js
+++ src/molecules/Amount/Amount.native.js
@@ -3,23 +3,33 @@ import PropTypes from 'prop-types';
 import { Text as NativeText, Platform } from 'react-native';
 import automation from '../../_helpers/automation-attributes';
 import Flex from '../../atoms/Flex';
-import Text from '../../atoms/Text';
+import AtomText from '../../atoms/Text';
 import Heading from '../../atoms/Heading';
+import baseTheme from '../../tokens/theme';
+import { getVariantColorKeys } from '../../_helpers/theme';
 import geISOCurrencyList from './geISOCurrencyList';
 
 const IS_ANDROID = Platform.OS === 'android';
 
 const styles = {
   text: {
-    color({ size }) {
-      if (size === 'xxxlarge') {
-        return 'shade.950';
+    color({ variant, variantColor, subtle }) {
+      switch (variant) {
+        case 'camel':
+          return `${variantColor}.950`;
+        case 'normal':
+          if (subtle) {
+            return `${variantColor}.960`;
+          }
+          return `${variantColor}.980`;
+        default:
+          return `${variantColor}.950`;
       }
-
-      return 'shade.940';
     },
     size({ size }) {
       switch (size) {
+        case 'xsmall':
+          return 'xxsmall';
         case 'medium':
           return 'xxsmall';
         case 'large':
@@ -32,22 +42,6 @@ const styles = {
           return 'large';
         default:
           return 'xxsmall';
-      }
-    },
-    weight({ size }) {
-      switch (size) {
-        case 'medium':
-          return 'regular';
-        case 'large':
-          return 'bold';
-        case 'xlarge':
-          return 'bold';
-        case 'xxlarge':
-          return 'bold';
-        case 'xxxlarge':
-          return 'bold';
-        default:
-          return 'bold';
       }
     },
     lineHeight({ size }) {
@@ -64,6 +58,24 @@ const styles = {
           return 'xxlarge';
         default:
           return 'xxlarge';
+      }
+    },
+  },
+  heading: {
+    color({ variant, variantColor, subtle }) {
+      switch (variant) {
+        case 'camel':
+          if (subtle) {
+            return `${variantColor}.950`;
+          }
+          return `${variantColor}.980`;
+        case 'normal':
+          if (subtle) {
+            return `${variantColor}.960`;
+          }
+          return `${variantColor}.980`;
+        default:
+          return `${variantColor}.980`;
       }
     },
   },
@@ -87,7 +99,7 @@ const formatAmount = ({ amount, currency }) => {
   return formattedAmount;
 };
 
-const Amount = ({ size, testID, children, currency }) => {
+const Amount = ({ size, testID, children, currency, weight, variant, variantColor, subtle }) => {
   if (isNaN(children)) {
     throw new Error(`Expected children to be number \n(Eg. "1234", "12.34")`);
   }
@@ -96,25 +108,48 @@ const Amount = ({ size, testID, children, currency }) => {
   const [currencySymbol, amount] = formattedAmount.split(/\s/);
   const [integerPart, fractionPart] = amount.split('.');
 
+  const Text = size === 'xlarge' || size === 'xxlarge' || size === 'xxxlarge' ? Heading : AtomText;
+
+  if (variant === 'normal') {
+    return (
+      <Text
+        color={styles.text.color({ variant, variantColor, subtle })}
+        size={size}
+        _weight={weight}
+        weight={weight}
+        {...automation(testID)}
+      >
+        {formattedAmount}
+      </Text>
+    );
+  }
+
   return (
     <Flex>
       <NativeText {...automation(testID)}>
-        <Text
-          color={styles.text.color({ size })}
+        <AtomText
+          color={styles.text.color({ variant, variantColor, subtle })}
           size={styles.text.size({ size })}
-          _weight={styles.text.weight({ size })}
+          _weight={weight}
           _lineHeight={styles.text.lineHeight({ size })} // First text component within nested texts dictate the line height
         >
           {`${currencySymbol} `}
-        </Text>
-        <Heading color="shade.980" size={size}>
-          {integerPart}
-        </Heading>
+        </AtomText>
         <Text
-          color={styles.text.color({ size })}
+          color={styles.heading.color({ variant, variantColor, subtle })}
+          size={size}
+          weight={weight}
+          _weight={weight}
+        >
+          {integerPart}
+        </Text>
+        <AtomText
+          color={styles.text.color({ variant, variantColor, subtle })}
           size={styles.text.size({ size })}
-          _weight={styles.text.weight({ size })}
-        >{`.${fractionPart}`}</Text>
+          _weight={weight}
+        >
+          {`.${fractionPart}`}
+        </AtomText>
       </NativeText>
     </Flex>
   );
@@ -122,15 +157,23 @@ const Amount = ({ size, testID, children, currency }) => {
 
 Amount.propTypes = {
   children: PropTypes.string.isRequired,
-  size: PropTypes.oneOf(['medium', 'large', 'xlarge', 'xxlarge', 'xxxlarge']),
+  size: PropTypes.oneOf(['xsmall', 'medium', 'large', 'xlarge', 'xxlarge', 'xxxlarge']),
   testID: PropTypes.string,
   currency: PropTypes.oneOf(geISOCurrencyList()),
+  weight: PropTypes.oneOf(Object.keys(baseTheme.fonts.weight)),
+  variant: PropTypes.oneOf(['camel', 'normal']),
+  variantColor: PropTypes.oneOf(getVariantColorKeys()),
+  subtle: PropTypes.bool,
 };
 
 Amount.defaultProps = {
   size: 'medium',
   testID: 'ds-amount',
   currency: 'INR',
+  weight: 'bold',
+  variant: 'camel',
+  variantColor: 'shade',
+  subtle: false,
 };
 
 export default Amount;
