80550c32689b9b934e79a6418c52a09592f7bf12
krishnaacc123 <krishnaacc123@gmail.com>
2020-04-07 12:30:22 +0530

    fix(ScrollView/react): change test case

--- src/atoms/ScrollView/__tests__/ScrollView.web.test.js
+++ src/atoms/ScrollView/__tests__/ScrollView.web.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { renderWithTheme } from '../../../_helpers/testing';
 import ScrollView from '../index';
+import Text from '../../Text';
 
 describe('<ScrollView />', () => {
-  test('should match snapshot for ScrollView', () => {
-    const { container } = renderWithTheme(<ScrollView />);
+  it('renders Text', () => {
+    const { container } = renderWithTheme(
+      <ScrollView>
+        <Text>Hello World</Text>
+      </ScrollView>,
+    );
     expect(container).toMatchSnapshot();
   });
 });
