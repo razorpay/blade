8207f50ccc2902c115b2d693f6fd3ec9c99c165b
shridharbavannawar <shridhar.b@razorpay.com>
2020-04-07 16:27:23 +0530

    fix(Text/native): adds text-align prop (#90)
    * fix(Text/native): adds text-align prop to text component
    
    * test(src/native(snapshots)): updated snapshots

--- src/atoms/Text/__tests__/Text.native.test.js
+++ src/atoms/Text/__tests__/Text.native.test.js
@@ -2,32 +2,57 @@ import React from 'react';
 import Text from '../index';
 import { renderWithTheme } from '../../../_helpers/testing';
 
-describe('Renders <Text /> correctly', () => {
-  it('snapshot testing with regular text', () => {
+describe('<Text />', () => {
+  it('renders regular text', () => {
     const displayText = 'Displaying some text';
     const { container } = renderWithTheme(<Text>{displayText}</Text>);
     expect(container).toMatchSnapshot();
   });
 
-  it('snapshot testing with bold text', () => {
+  it('renders bold text', () => {
     const displayText = 'Displaying some text';
     const { container } = renderWithTheme(<Text _weight="bold">{displayText}</Text>);
     expect(container).toMatchSnapshot();
   });
 
-  it('snapshot testing with underlined text', () => {
+  it('renders underlined text', () => {
     const displayText = 'Displaying some text';
     const { container } = renderWithTheme(<Text _isUnderlined>{displayText}</Text>);
     expect(container).toMatchSnapshot();
   });
 
-  it('renders default text, defaultProps are assigned', () => {
+  it('renders text with display content center aligned', () => {
+    const displayText = 'Displaying some text';
+    const { container } = renderWithTheme(<Text align="center">{displayText}</Text>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders text with display content right aligned', () => {
+    const displayText = 'Displaying some text';
+    const { container } = renderWithTheme(<Text align="right">{displayText}</Text>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders text with display content auto aligned when align prop is of value "initial"', () => {
+    const displayText = 'Displaying some text';
+    const { container } = renderWithTheme(<Text align="initial">{displayText}</Text>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders text with display content auto aligned when align prop is of value "inherit"', () => {
+    const displayText = 'Displaying some text';
+    const { container } = renderWithTheme(<Text align="inherit">{displayText}</Text>);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders default text', () => {
     const displayText = 'Displaying some text';
     const { getByTestId } = renderWithTheme(<Text>{displayText}</Text>);
     const textElement = getByTestId('ds-text');
     expect(textElement).toBeTruthy();
     expect(textElement.props.size).toBe('large');
     expect(textElement.props.color).toBe('shade.980');
+    expect(textElement.props.align).toBe('left');
     expect(textElement.props._weight).toBe('regular');
     expect(textElement.props._isUnderlined).toBe(false);
     expect(textElement.props._letterSpacing).toBe('small');
