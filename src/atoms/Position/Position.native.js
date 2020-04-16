b9c3518dff89b421b458fdfffd56f07409d303ab
Shridhar Bavannawar <shridhar.b@razorpay.com>
2020-04-16 07:46:24 +0530

    feat(Position): add native Position component

---
+++ src/atoms/Position/Position.native.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components/native';
+import { makePxValue } from '../../_helpers/theme';
+
+const Position = styled(({ children, style, ...props }) => {
+  if (React.Children.toArray(children).length !== 1) {
+    throw new Error('Expected a single child for Position component');
+  }
+  return React.cloneElement(children, {
+    ...props,
+    style: [style, children.props.style].filter(Boolean),
+  });
+})`
+  ${(props): string => (props.position ? `position: ${props.position};` : '')};
+  ${(props): string => (props.top ? `top: ${makePxValue(props.top)};` : '')};
+  ${(props): string => (props.right ? `right: ${makePxValue(props.right)};` : '')};
+  ${(props): string => (props.bottom ? `bottom: ${makePxValue(props.bottom)};` : '')};
+  ${(props): string => (props.left ? `left: ${makePxValue(props.left)};` : '')};
+  ${(props): string => (props.zIndex ? `z-index: ${props.zIndex};` : '')};
+`;
+
+Position.propTypes = {
+  position: PropTypes.oneOf(['relative', 'absolute']),
+  top: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  right: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  bottom: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  left: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  zIndex: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+};
+
+Position.defaultProps = {
+  position: 'relative',
+  top: undefined,
+  right: undefined,
+  bottom: undefined,
+  left: undefined,
+  zIndex: undefined,
+};
+
+export default Position;
