71ca0f2554ce9d578bd706d8950341e19d45d73f
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-04-27 11:07:22 +0530

    feat(Icon): add Subscription icon

--- src/icons/index.js
+++ src/icons/index.js
@@ -14,6 +14,7 @@ const icons = {
   info: require('./Info').default,
   paymentLink: require('./PaymentLink').default,
   paymentGateway: require('./PaymentGateway').default,
+  subscription: require('./Subscription').default,
 };
 
 export default icons;
