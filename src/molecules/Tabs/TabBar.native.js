f4247f3dbfe0190268d85eee3d140883b8088f1b
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-05-04 12:53:11 +0530

    fix(native/Tabs): update TabBar propTypes

--- src/molecules/Tabs/TabBar.native.js
+++ src/molecules/Tabs/TabBar.native.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import View from '../../atoms/View';
 import Flex from '../../atoms/Flex';
+import { getIconNames } from '../../_helpers/icon';
 import TabButton from './TabButton';
 
 const TabBar = ({ navigationState, jumpTo }) => {
@@ -32,11 +33,28 @@ const TabBar = ({ navigationState, jumpTo }) => {
 };
 
 TabBar.propTypes = {
-  navigationState: PropTypes.object,
+  navigationState: PropTypes.shape({
+    index: PropTypes.number,
+    routes: PropTypes.arrayOf(
+      PropTypes.shape({
+        key: PropTypes.string,
+        label: PropTypes.string,
+        index: PropTypes.number,
+        value: PropTypes.string,
+        testID: PropTypes.string,
+        active: PropTypes.bool,
+        onPress: PropTypes.func,
+        disabled: PropTypes.bool,
+        icon: PropTypes.oneOf(getIconNames()),
+      }),
+    ),
+  }),
   jumpTo: PropTypes.func,
-  routes: PropTypes.object,
 };
 
-TabBar.defaultProps = {};
+TabBar.defaultProps = {
+  navigationState: { routes: [] },
+  jumpTo: () => {},
+};
 
 export default TabBar;
