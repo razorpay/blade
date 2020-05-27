8fd1907cff9590980effb45de471f02bad0e1788
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-05-27 15:51:24 +0530

    test(native/Snackbar): add tests for useSnackbar

---
+++ src/molecules/Snackbar/__tests__/useSnackbar.native.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import PropTypes from 'prop-types';
+import { ThemeProvider } from 'styled-components/native';
+import useSnackbar from '../useSnackbar';
+import SnackbarProvider from '../SnackbarProvider';
+import theme from '../../../tokens/theme';
+
+const Wrapper = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <SnackbarProvider>{children}</SnackbarProvider>
+  </ThemeProvider>
+);
+
+Wrapper.propTypes = {
+  children: PropTypes.node,
+};
+
+describe('useSnackbar hook', () => {
+  it('show the snackbar', () => {
+    const { result } = renderHook(() => useSnackbar(), { wrapper: Wrapper });
+    expect(result.current.isVisible).toBe(false);
+    act(() => {
+      result.current.show({ text: 'Snackbar text here', autoDismiss: false });
+    });
+    expect(result.current.isVisible).toBe(true);
+  });
+
+  it('dismiss the snackbar', () => {
+    const { result } = renderHook(() => useSnackbar(), { wrapper: Wrapper });
+    expect(result.current.isVisible).toBe(false);
+    act(() => {
+      result.current.show({ text: 'Snackbar text here', autoDismiss: false });
+    });
+    expect(result.current.isVisible).toBe(true);
+    act(() => {
+      result.current.dismiss();
+    });
+    expect(result.current.isVisible).toBe(false);
+  });
+
+  it('auto-dismiss the snackbar', () => {
+    jest.useFakeTimers(); // Uses fake timer to resolve setTimeout
+    const { result } = renderHook(() => useSnackbar(), { wrapper: Wrapper });
+    expect(result.current.isVisible).toBe(false);
+    act(() => {
+      result.current.show({ text: 'Snackbar text here', autoDismiss: true });
+    });
+    expect(result.current.isVisible).toBe(true);
+    act(() => {
+      jest.runAllTimers(); // Resolve auto dismiss timer
+    });
+    expect(result.current.isVisible).toBe(false);
+  });
+});
