03c79aea52eff1abe2695ea3ca4040690710fe83
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-06-13 14:06:10 +0530

    fix(native/Tabs): add support for gestureHandlerProps (#151)

--- src/molecules/Tabs/Tabs.native.js
+++ src/molecules/Tabs/Tabs.native.js
@@ -52,7 +52,7 @@ const getRouteIndexFromValue = ({ value, routes }) =>
 const getRouteValueFromIndex = ({ index, routes }) =>
   routes.find((route) => route.index === index)?.value;
 
-const Tabs = ({ children, defaultValue, value, onChange }) => {
+const Tabs = ({ children, defaultValue, value, onChange, gestureHandlerProps }) => {
   const tabs = getTabs({ children });
   const routes = getRoutes({ tabs });
   const disableSwipe = shouldDisableSwipe({ tabs });
@@ -94,6 +94,7 @@ const Tabs = ({ children, defaultValue, value, onChange }) => {
       swipeEnabled={!disableSwipe}
       tabBarPosition="top"
       lazy={true} // Renders screen only when we navigate to it
+      gestureHandlerProps={gestureHandlerProps}
     />
   );
 };
@@ -103,6 +104,30 @@ Tabs.propTypes = {
   defaultValue: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func,
+  gestureHandlerProps: PropTypes.shape({
+    minOffsetY: PropTypes.number,
+    activeOffsetY:
+      PropTypes.oneOf([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]) ||
+      PropTypes.arrayOf(PropTypes.number),
+    activeOffsetX:
+      PropTypes.oneOf([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]) ||
+      PropTypes.arrayOf(PropTypes.number),
+    failOffsetY:
+      PropTypes.oneOf([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]) ||
+      PropTypes.arrayOf(PropTypes.number),
+    failOffsetX:
+      PropTypes.oneOf([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]) ||
+      PropTypes.arrayOf(PropTypes.number),
+    minDist: PropTypes.number,
+    minVelocity: PropTypes.number,
+    minVelocityX: PropTypes.number,
+    minVelocityY: PropTypes.number,
+    minPointers: PropTypes.number,
+    maxPointers: PropTypes.number,
+    avgTouches: PropTypes.bool,
+    onGestureEvent: PropTypes.func,
+    onHandlerStateChange: PropTypes.func,
+  }),
 };
 
 Tabs.defaultProps = {};
