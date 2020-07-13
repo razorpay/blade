96caf47cc8c27a618406b1bc50c79b7d01e38a16
Shridhar Bavannawar <shridhar.b@razorpay.com>
2020-07-13 14:47:33 +0530

    test(molecules/BottomSheet/__tests__): adds tests

---
+++ src/molecules/BottomSheet/__tests__/BottomSheet.native.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { act } from '@testing-library/react-native';
+import BottomSheet from '../BottomSheet';
+import { renderWithTheme } from '../../../_helpers/testing';
+import View from '../../../atoms/View';
+import Text from '../../../atoms/Text';
+
+describe('<BottomSheet />', () => {
+  it('renders default BottomSheet', () => {
+    const bottomSheetRef = React.createRef();
+    const { container } = renderWithTheme(<BottomSheet ref={bottomSheetRef} />);
+
+    act(() => {
+      bottomSheetRef.current.open();
+    });
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('renders BottomSheet with Header, Footer and Content Defined', () => {
+    const bottomSheetRef = React.createRef();
+    const { container } = renderWithTheme(
+      <BottomSheet
+        ref={bottomSheetRef}
+        HeaderComponent={
+          <View>
+            <Text>Header</Text>
+          </View>
+        }
+        FooterComponent={
+          <View>
+            <Text>Footer</Text>
+          </View>
+        }
+      >
+        <View>
+          <Text>Bottomsheet content</Text>
+        </View>
+      </BottomSheet>,
+    );
+
+    act(() => {
+      bottomSheetRef.current.open();
+    });
+
+    expect(container).toMatchSnapshot();
+  });
+});
