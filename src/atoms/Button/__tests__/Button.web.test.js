eac38fef24a1332366021f2dc4efdb710955952c
Zameer Haque <zameer.haque@razorpay.com>
2020-07-14 00:22:03 +0530

    test(Button/react): update tests and snapshots

--- src/atoms/Button/__tests__/Button.web.test.js
+++ src/atoms/Button/__tests__/Button.web.test.js
@@ -8,168 +8,209 @@ import { renderWithTheme } from '../../../_helpers/testing';
 beforeAll(() => jest.spyOn(console, 'error').mockImplementation());
 afterAll(() => jest.restoreAllMocks());
 
-describe('Renders <Button /> correctly', () => {
-  it('Throws error when <Button /> is not passed a string as children', () => {
-    const errorMessage = 'Error in Button: expected `children` of type `string` but found object';
-    expect(() =>
-      renderWithTheme(
-        <Button size="small">
-          <View />
-        </Button>,
-      ),
-    ).toThrow(errorMessage);
+describe('<Button />', () => {
+  describe('variant', () => {
+    it('should render a primary(default) button', () => {
+      const { container } = renderWithTheme(<Button>Click Me</Button>);
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should render a secondary button', () => {
+      const { container } = renderWithTheme(<Button variant="secondary">Click Me</Button>);
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should render a tertiary button', () => {
+      const { container } = renderWithTheme(<Button variant="tertiary">click me</Button>);
+      expect(container).toMatchSnapshot();
+    });
   });
 
-  it('Renders Primary block <Button />', () => {
-    const { container, getByText } = renderWithTheme(<Button block>Click Me</Button>);
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
+  describe('error', () => {
+    it('should throw error if children is not string', () => {
+      const errorMessage = 'Error in Button: expected `children` of type `string` but found object';
+      expect(() =>
+        renderWithTheme(
+          <Button>
+            <View />
+          </Button>,
+        ),
+      ).toThrow(errorMessage);
+    });
   });
 
-  it('Renders xsmall Primary <Button />', () => {
-    const { container, getByText } = renderWithTheme(<Button size="xsmall">Click Me</Button>);
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
+  describe('block', () => {
+    it('should render a block button', () => {
+      const { container } = renderWithTheme(<Button block>click me</Button>);
+      expect(container).toMatchSnapshot();
+    });
   });
 
-  it('Renders a small Primary <Button />', () => {
-    const { container, getByText } = renderWithTheme(<Button size="small">Click Me</Button>);
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
+  describe('size', () => {
+    it('should render a medium(default) button', () => {
+      const { container } = renderWithTheme(<Button>click me</Button>);
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should render a large button', () => {
+      const { container } = renderWithTheme(<Button size="large">click me</Button>);
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should render a small button', () => {
+      const { container } = renderWithTheme(<Button size="small">click me</Button>);
+      expect(container).toMatchSnapshot();
+    });
+
+    it('should render a xsmall button', () => {
+      const { container } = renderWithTheme(<Button size="xsmall">click me</Button>);
+      expect(container).toMatchSnapshot();
+    });
   });
 
-  it('Renders a medium(Default) Primary <Button />', () => {
-    const { container, getByText } = renderWithTheme(<Button>Click Me</Button>);
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
+  describe('icon', () => {
+    describe('with children', () => {
+      describe('with iconAlign left', () => {
+        it('should render a button with text with icon on left', () => {
+          const { container } = renderWithTheme(
+            <Button icon="info" iconAlign="left">
+              Button
+            </Button>,
+          );
+          expect(container).toMatchSnapshot();
+        });
+      });
+
+      describe('with iconAlign right', () => {
+        it('should render a button with text with icon on right', () => {
+          const { container } = renderWithTheme(
+            <Button icon="info" iconAlign="right">
+              Button
+            </Button>,
+          );
+          expect(container).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('without children', () => {
+      it('should render icon only button', () => {
+        const { container } = renderWithTheme(<Button icon="info" />);
+        expect(container).toMatchSnapshot();
+      });
+    });
   });
 
-  it('Renders a large Primary <Button />', () => {
-    const { container, getByText } = renderWithTheme(<Button size="large">Click Me</Button>);
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
+  describe('icon', () => {
+    describe('with children', () => {
+      describe('with iconAlign left', () => {
+        it('should render a button with text with icon on left', () => {
+          const { container } = renderWithTheme(
+            <Button icon="info" iconAlign="left">
+              Button
+            </Button>,
+          );
+          expect(container).toMatchSnapshot();
+        });
+      });
+
+      describe('with iconAlign right', () => {
+        it('should render a button with text with icon on right', () => {
+          const { container } = renderWithTheme(
+            <Button icon="info" iconAlign="right">
+              Button
+            </Button>,
+          );
+          expect(container).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('without children', () => {
+      it('should render icon only button', () => {
+        const { container } = renderWithTheme(<Button icon="info" />);
+        expect(container).toMatchSnapshot();
+      });
+    });
   });
 
-  it('Renders a small Secondary <Button />', () => {
-    const { container, getByText } = renderWithTheme(
-      <Button variant="secondary" size="small">
-        Click Me
-      </Button>,
-    );
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
+  describe('disabled', () => {
+    it('should render a disabled button', () => {
+      const { container } = renderWithTheme(<Button disabled>Disabled</Button>);
+      expect(container).toMatchSnapshot();
+    });
   });
 
-  it('Renders a medium(Default) Secondary <Button />', () => {
-    const { container, getByText } = renderWithTheme(<Button variant="secondary">Click Me</Button>);
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
+  describe('onChange', () => {
+    describe('without disabled', () => {
+      it('should call onChange handler exactly one time', () => {
+        const handleClick = jest.fn();
+        const { container, getByTestId } = renderWithTheme(
+          <Button onClick={handleClick}>Click Me</Button>,
+        );
+        const button = getByTestId('ds-button');
+        fireEvent.click(button);
+        expect(handleClick).toHaveBeenCalledTimes(1);
+        expect(container).toMatchSnapshot();
+      });
+    });
+
+    describe('with disabled', () => {
+      it('should not call onChange handler', () => {
+        const handleClick = jest.fn();
+        const { container, getByTestId } = renderWithTheme(
+          <Button disabled onClick={handleClick}>
+            Click Me
+          </Button>,
+        );
+        const button = getByTestId('ds-button');
+        fireEvent.click(button);
+        expect(handleClick).not.toBeCalled();
+        expect(container).toMatchSnapshot();
+      });
+    });
   });
 
-  it('Renders a large Secondary <Button />', () => {
-    const { container, getByText } = renderWithTheme(
-      <Button variant="secondary" size="large">
-        Click Me
-      </Button>,
-    );
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
-  });
+  describe('type', () => {
+    it('should call form submit handler if type is submit', () => {
+      const handleSubmit = jest.fn();
+      const { container, getByTestId } = renderWithTheme(
+        <form onSubmit={handleSubmit}>
+          <Button type="submit">Click Me</Button>
+        </form>,
+      );
+      const button = getByTestId('ds-button');
+      fireEvent.click(button);
+      expect(handleSubmit).toHaveBeenCalledTimes(1);
+      expect(container).toMatchSnapshot();
+    });
 
-  it('Renders a small Tertiary <Button />', () => {
-    const { container, getByText } = renderWithTheme(
-      <Button variant="tertiary" size="small">
-        Click Me
-      </Button>,
-    );
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
-  });
+    // Skipping since Jest 24 does not handle the reset event correctly due to JSDOM 11
+    // It is supposed to be fixed in Jest 25 (JSDOM 15)
+    it.skip('should call form reset handler if type is reset', () => {
+      const handleReset = jest.fn();
+      const { container, getByTestId } = renderWithTheme(
+        <form onReset={handleReset}>
+          <Button type="reset">Click Me</Button>
+        </form>,
+      );
+      const button = getByTestId('ds-button');
+      fireEvent.click(button);
+      expect(handleReset).toHaveBeenCalledTimes(1);
+      expect(container).toMatchSnapshot();
+    });
 
-  it('Renders a medium(Default) Tertiary <Button />', () => {
-    const { container, getByText } = renderWithTheme(<Button variant="tertiary">Click Me</Button>);
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
-  });
-
-  it('Renders a large Tertiary <Button />', () => {
-    const { container, getByText } = renderWithTheme(
-      <Button variant="tertiary" size="large">
-        Click Me
-      </Button>,
-    );
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
-  });
-
-  it('Renders a disabled Primary <Button />', () => {
-    const { container, getByText } = renderWithTheme(<Button disabled>Click Me</Button>);
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
-  });
-
-  it('Renders a disabled Secondary <Button />', () => {
-    const { container, getByText } = renderWithTheme(
-      <Button disabled variant="secondary">
-        Click Me
-      </Button>,
-    );
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
-  });
-
-  it('Renders a disabled Tertiary <Button />', () => {
-    const { container, getByText } = renderWithTheme(
-      <Button disabled variant="tertiary">
-        Click Me
-      </Button>,
-    );
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
-  });
-});
-
-describe('Renders an icon <Button /> correctly', () => {
-  it('Renders a Primary <Button /> with left(Default) icon', () => {
-    const { container, getByText } = renderWithTheme(<Button icon="info">Click Me</Button>);
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
-  });
-
-  it('Renders a large Primary <Button /> with right icon', () => {
-    const { container, getByText } = renderWithTheme(
-      <Button size="large" icon="info" iconPosition="right">
-        Click Me
-      </Button>,
-    );
-    expect(getByText(/click me/i)).toBeTruthy();
-    expect(container).toMatchSnapshot();
-  });
-
-  it('Renders an icon only <Button />', () => {
-    const { container } = renderWithTheme(<Button icon="info" />);
-    expect(container).toMatchSnapshot();
-  });
-});
-
-describe('Checks onClick functionality of <Button />', () => {
-  it('Checks onClick is called for <Button />', () => {
-    const mockClick = jest.fn();
-    const { getByText } = renderWithTheme(<Button onClick={mockClick}>Click Me</Button>);
-    const button = getByText(/click me/i).parentNode;
-    fireEvent.click(button);
-    expect(mockClick).toBeCalledTimes(1);
-  });
-
-  it('Checks onClick is not called for disabled <Button />', () => {
-    const mockClick = jest.fn();
-    const { getByText } = renderWithTheme(
-      <Button disabled onClick={mockClick}>
-        Click Me
-      </Button>,
-    );
-    const button = getByText(/click me/i).parentNode;
-    fireEvent.click(button);
-    expect(mockClick).not.toBeCalled();
+    it('should not call submit handler if type is button', () => {
+      const handleSubmit = jest.fn();
+      const { container, getByTestId } = renderWithTheme(
+        <form onSubmit={handleSubmit}>
+          <Button type="button">Click Me</Button>
+        </form>,
+      );
+      const button = getByTestId('ds-button');
+      fireEvent.click(button);
+      expect(handleSubmit).not.toBeCalled();
+      expect(container).toMatchSnapshot();
+    });
   });
 });
