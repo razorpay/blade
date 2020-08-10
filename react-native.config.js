57bff9ceaadbcbaacfaee162d54ca81a38b69917
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-08-10 16:35:25 +0530

    build(native): use custom gesture handler repo (#170)

--- react-native.config.js
+++ react-native.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  dependencies: {
+    'react-native-gesture-handler': {
+      platforms: {
+        android: null, // disable Android platform, other platforms will still autolink if provided
+      },
+    },
+  },
   dependency: {
     assets: ['public/fonts/'],
   },
