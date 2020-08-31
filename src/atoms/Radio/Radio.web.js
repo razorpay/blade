2ee15f7ab0f92969118745ed2b4e5793d5eb833a
Zameer Haque <20766601+haquezameer@users.noreply.github.com>
2020-08-31 15:51:23 +0530

    feat(Radio/react): add component (#168)
    * feat(Checkbox/react): wip checkbox
    
    * fix(Radio/react): fix alignment
    
    * fix(Radio/react): fix background-color on backdrop & accept name for keyboard nav on moz
    
    * fix(Radio/react): remove default padding,margin and border on fieldset element
    
    * refactor(Radio/react): store width,height,borderRadius etc in maps
    
    * fix(Radio/react): move click from input to label
    
    * refactor(Radio/react): club description styles into descriptionText
    
    * fix(Radio/react): fix border-radius rendering issue on chrome
    
    * fix(Radio/react): add disabled check for calling onChange
    
    * refactor(Radio/react): extract out Input,Label,Fieldset etc
    
    * feat(Radio/react): change background with variantColor
    
    * feat(Radio/react): accept id prop
    
    * test(Radio/react): update test snapshots
    
    * refactor(Radio/react): return borderWidth, width, height etc in dimensions
    
    * refactor(Radio/react): refactor descriptionText and title margin
    
    * fix(Radio/react): use id in label using htmlFor
    
    * refactor(Radio/react): move variantColor and size to context
    
    * refactor(Radio/react): update stories with variantColor and size in context
    
    * test(Radio/react): update tests and snapshots
    
    * refactor(Radio/react): incorporate feedback from Checkbox PR
    
    * test(Radio/react): update tests and snapshots
    
    * refactor(Radio/react): use pointer-events: none on disabled input and use View as label
    
    * refactor(Radio/react): rename RadioButtonContext to RadioContext
    
    * refactor(Radio/rect): rename Fieldset to RadioGroup
    
    * test(Radio/react): update tests
    
    * test(Radio/react): add comments regarding controlled component tests
    
    * test(Radio/react): update tests and snapshots

--- src/atoms/Radio/Radio.web.js
+++ src/atoms/Radio/Radio.web.js
@@ -1,5 +1,50 @@
-import React from 'react';
+import React, { useState, useCallback, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { getVariantColorKeys } from '../../_helpers/theme';
+import RadioContext from './RadioContext';
+import RadioOption from './RadioOption';
+import RadioGroup from './RadioGroup';
 
-const RadioButton = () => <div>WIP</div>;
+const Radio = ({ value, onChange, defaultValue, variantColor, size, children }) => {
+  const [selected, setSelected] = useState(value || defaultValue);
 
-export default RadioButton;
+  const onValueChange = useCallback(
+    (newValue) => {
+      if (!value) {
+        setSelected(newValue);
+      }
+      onChange(newValue);
+    },
+    [onChange, value],
+  );
+
+  const contextValue = useMemo(
+    () => ({ value: selected, onChange: onValueChange, variantColor, size }),
+    [onValueChange, selected, variantColor, size],
+  );
+
+  return (
+    <RadioContext.Provider value={contextValue}>
+      <RadioGroup>{children}</RadioGroup>
+    </RadioContext.Provider>
+  );
+};
+
+Radio.propTypes = {
+  value: PropTypes.string,
+  defaultValue: PropTypes.string,
+  onChange: PropTypes.func,
+  children: PropTypes.node,
+  variantColor: PropTypes.oneOf(getVariantColorKeys()),
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+};
+
+Radio.defaultProps = {
+  onChange: () => {},
+  variantColor: 'primary',
+  size: 'medium',
+};
+
+Radio.Option = RadioOption;
+
+export default Radio;
