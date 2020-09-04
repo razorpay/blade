d16c4a22157b3fc0a4b7ecd95d2529a62d17d56f
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-09-04 09:57:11 +0530

    fix(native/Position): extract out props before cloning to prevent passing them forward (#187)

--- src/atoms/Position/Position.native.js
+++ src/atoms/Position/Position.native.js
@@ -2,22 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
 import { makePxValue } from '../../_helpers/theme';
+import isDefined from '../../_helpers/isDefined';
 
-const Position = styled(({ children, style, ...props }) => {
-  if (React.Children.toArray(children).length !== 1) {
-    throw new Error('Expected a single child for Position component');
-  }
-  return React.cloneElement(children, {
-    ...props,
-    style: [style, children.props.style].filter(Boolean),
-  });
-})`
-  ${(props) => (props.position ? `position: ${props.position};` : '')};
-  ${(props) => (props.top ? `top: ${makePxValue(props.top)};` : '')};
-  ${(props) => (props.right ? `right: ${makePxValue(props.right)};` : '')};
-  ${(props) => (props.bottom ? `bottom: ${makePxValue(props.bottom)};` : '')};
-  ${(props) => (props.left ? `left: ${makePxValue(props.left)};` : '')};
-  ${(props) => (props.zIndex ? `z-index: ${props.zIndex};` : '')};
+const Position = styled(
+  ({ position, top, right, bottom, left, zIndex, children, style, ...props }) => {
+    if (React.Children.toArray(children).length !== 1) {
+      throw new Error('Expected a single child for Position component');
+    }
+    return React.cloneElement(children, {
+      ...props,
+      style: [style, children.props.style].filter(Boolean),
+    });
+  },
+)`
+  ${(props) => (isDefined(props.position) ? `position: ${props.position};` : '')};
+  ${(props) => (isDefined(props.top) ? `top: ${makePxValue(props.top)};` : '')};
+  ${(props) => (isDefined(props.right) ? `right: ${makePxValue(props.right)};` : '')};
+  ${(props) => (isDefined(props.bottom) ? `bottom: ${makePxValue(props.bottom)};` : '')};
+  ${(props) => (isDefined(props.left) ? `left: ${makePxValue(props.left)};` : '')};
+  ${(props) => (isDefined(props.zIndex) ? `z-index: ${props.zIndex};` : '')};
 `;
 
 Position.propTypes = {
