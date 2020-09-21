01cf7ba076bfa0ebc8fe06d0835563569168da00
Zameer Haque <20766601+haquezameer@users.noreply.github.com>
2020-09-21 10:48:54 +0530

    feat(icons): add helpCircle (#197)
    * feat(HelpCircle): add helpCircle icon
    
    * feat(Icon): add size and color to knobs for Icon

--- src/atoms/Icon/Icon.stories.js
+++ src/atoms/Icon/Icon.stories.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { select } from '@storybook/addon-knobs';
 import { getIconNames } from '../../_helpers/icon';
+import { getColorKeys } from '../../_helpers/theme';
 import Icon from './Icon';
+
+const sizes = {
+  xsmall: 'xsmall',
+  small: 'small',
+  medium: 'medium',
+  large: 'large',
+  xlarge: 'xlarge',
+};
 
 const iconOptions = getIconNames().reduce(
   (options, option) => ({ ...options, [option]: option }),
@@ -14,5 +23,11 @@ storiesOf('Icon', module)
     component: Icon,
   })
   .add('default', () => {
-    return <Icon name={select('Name', iconOptions, 'info')} fill="shade.980" size="medium" />;
+    return (
+      <Icon
+        name={select('Name', iconOptions, 'info')}
+        fill={select('fill', getColorKeys(), 'shade.980')}
+        size={select('sizes', sizes, 'medium')}
+      />
+    );
   });
