cfc0668e5f6178e4949f22bc64893ac6a9acc059
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-11-02 16:33:56 +0530

    feat(icon): add star icon

---
+++ src/icons/Star/Star.native.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Svg, { Path } from 'react-native-svg';
+import { getThemeColors } from '../../_helpers/theme';
+import colors from '../../tokens/colors';
+
+function Star({ width, height, fill }) {
+  return (
+    <Svg width={width} height={height} viewBox="0 0 24 24" fill="none">
+      <Path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12 1a1 1 0 01.897.557l2.857 5.79 6.39.934a1 1 0 01.554 1.705l-4.623 4.503 1.09 6.362a1 1 0 01-1.45 1.054L12 18.9l-5.715 3.005a1 1 0 01-1.45-1.054l1.09-6.362-4.623-4.503a1 1 0 01.553-1.705l6.39-.935 2.858-5.789A1 1 0 0112 1zm0 3.26L9.807 8.702a1 1 0 01-.752.546l-4.907.718 3.55 3.457a1 1 0 01.288.885l-.838 4.883 4.386-2.307a1 1 0 01.931 0l4.387 2.307-.838-4.883a1 1 0 01.288-.885l3.55-3.457-4.907-.718a1 1 0 01-.752-.546L12 4.259z"
+        fill={fill}
+      />
+    </Svg>
+  );
+}
+
+Star.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  fill: PropTypes.oneOf(getThemeColors()),
+};
+
+Star.defaultProps = {
+  width: 24,
+  height: 24,
+  fill: colors.sapphire[800],
+};
+
+export default Star;
--- src/icons/index.js
+++ src/icons/index.js
@@ -38,6 +38,7 @@ const icons = {
   search: require('./Search').default,
   share: require('./Share').default,
   smartCollect: require('./SmartCollect').default,
+  star: require('./Star').default,
   subscription: require('./Subscription').default,
   success: require('./Success').default,
   trash: require('./Trash').default,
