a221453fb0b49223435d43c8a039931eba1156c0
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2020-11-04 15:22:37 +0530

    feat(icon): add starOutlined and starFilled icons (#205)

--- src/icons/index.js
+++ src/icons/index.js
@@ -38,6 +38,8 @@ const icons = {
   search: require('./Search').default,
   share: require('./Share').default,
   smartCollect: require('./SmartCollect').default,
+  starFilled: require('./StarFilled').default,
+  starOutlined: require('./StarOutlined').default,
   subscription: require('./Subscription').default,
   success: require('./Success').default,
   trash: require('./Trash').default,
