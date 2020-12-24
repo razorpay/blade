1d85e830ec6ff4d386d1184dcd786a90349cce46
kamleshchandnani <kamlesh.chandnani@gmail.com>
2020-12-24 14:34:48 +0530

    feat(tokens): add colors

---
+++ packages/blade/tokens/opacity.ts
@@ -0,0 +1,15 @@
+type opacity = Readonly<{
+  1: number;
+  2: number;
+  3: number;
+  9: number;
+}>;
+
+const opacity: opacity = {
+  1: 0.09,
+  2: 0.18,
+  3: 0.32,
+  9: 1.0,
+};
+
+export default opacity;
