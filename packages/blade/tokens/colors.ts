f350a3faf4e00484c0c67211b85c7094d864dc9c
kamleshchandnani <kamlesh.chandnani@gmail.com>
2020-12-28 17:14:12 +0530

    chore(types): re-structure typings for color

--- packages/blade/tokens/colors.ts
+++ packages/blade/tokens/colors.ts
@@ -1,42 +1,55 @@
 import opacities from './opacities';
 
+type colorChromaticScale = Readonly<{
+  50: string;
+  100: string;
+  200: string;
+  300: string;
+  400: string;
+  500: string;
+  600: string;
+  700: string;
+  800: string;
+  900: string;
+  950: string;
+  a50: string;
+  a100: string;
+  a200: string;
+}>;
+
+type colorNeutralScale = Readonly<{
+  0: string;
+  50: string;
+  100: string;
+  200: string;
+  300: string;
+  400: string;
+  500: string;
+  600: string;
+  700: string;
+  800: string;
+  900: string;
+  1000: string;
+  1100: string;
+  1200: string;
+  1300: string;
+}>;
+
 type color = Readonly<{
   chromatic: {
-    [key: string]: {
-      50: string;
-      100: string;
-      200: string;
-      300: string;
-      400: string;
-      500: string;
-      600: string;
-      700: string;
-      800: string;
-      900: string;
-      950: string;
-      a50: string;
-      a100: string;
-      a200: string;
-    };
+    azure: colorChromaticScale;
+    emerald: colorChromaticScale;
+    crimson: colorChromaticScale;
+    cider: colorChromaticScale;
+    orchid: colorChromaticScale;
+    magenta: colorChromaticScale;
+    sapphire: colorChromaticScale;
   };
   neutral: {
-    [key: string]: {
-      0: string;
-      50: string;
-      100: string;
-      200: string;
-      300: string;
-      400: string;
-      500: string;
-      600: string;
-      700: string;
-      800: string;
-      900: string;
-      1000: string;
-      1100: string;
-      1200: string;
-      1300: string;
-    };
+    blueGray: colorNeutralScale;
+    warmGray: colorNeutralScale;
+    navyGray: colorNeutralScale;
+    coolGray: colorNeutralScale;
   };
 }>;
 
