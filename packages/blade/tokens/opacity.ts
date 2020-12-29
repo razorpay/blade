408d142ce862c0f7903cbb7526c2b298cbf8e7f7
kamleshchandnani <kamlesh.chandnani@gmail.com>
2020-12-29 11:50:39 +0530

    refactor: rename opacity file

--- packages/blade/tokens/opacity.ts
+++ packages/blade/tokens/opacity.ts
@@ -2,6 +2,11 @@ type opacity = Readonly<{
   1: number;
   2: number;
   3: number;
+  4: number;
+  5: number;
+  6: number;
+  7: number;
+  8: number;
   9: number;
 }>;
 
@@ -9,6 +14,11 @@ const opacities: opacity = {
   1: 0.09,
   2: 0.18,
   3: 0.32,
+  4: 1.0,
+  5: 1.0,
+  6: 1.0,
+  7: 1.0,
+  8: 1.0,
   9: 1.0,
 };
 
