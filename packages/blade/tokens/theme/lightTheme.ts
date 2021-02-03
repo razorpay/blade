f6d8101d63829a77ab9dc297665d0b9395892352
kamleshchandnani <kamlesh.chandnani@gmail.com>
2021-02-03 12:46:17 +0530

    feat: add lightTheme theme aliases

--- packages/blade/tokens/theme/lightTheme.ts
+++ packages/blade/tokens/theme/lightTheme.ts
@@ -2,66 +2,137 @@ import globalColors from '../global/colors';
 
 const lightTheme = {
   colors: {
-    background: {
-      lightest: {
-        onLightMode: {
-          value: globalColors.neutral.blueGray[0],
+    black: globalColors.neutral.blueGray[1300],
+    primary: globalColors.chromatic.azure[500],
+    white: globalColors.neutral.blueGray[0],
+    action: {
+      background: {
+        primary: {
+          default: globalColors.chromatic.azure[500],
+          hover: globalColors.chromatic.azure[400],
+          focused: globalColors.chromatic.azure[700],
+          disabled: globalColors.chromatic.azure.a200,
         },
-        onDarkMode: {
-          value: globalColors.neutral.warmGray[0],
-        },
-      },
-      lighter: {
-        onLightMode: {
-          value: globalColors.neutral.blueGray[50],
-        },
-        onDarkMode: {
-          value: globalColors.neutral.warmGray[50],
-        },
-      },
-      light: {
-        onLightMode: {
-          value: globalColors.neutral.blueGray[100],
-        },
-        onDarkMode: {
-          value: globalColors.neutral.warmGray[100],
+        secondary: {
+          default: '',
+          hover: globalColors.chromatic.azure.a50,
+          focused: globalColors.chromatic.azure[700],
+          disabled: '', // confirm with saurav
         },
       },
-      dark: {
-        onLightMode: {
-          value: globalColors.neutral.blueGray[1100],
+      border: {
+        primary: {
+          default: globalColors.chromatic.azure[500],
+          hover: globalColors.chromatic.azure[400],
+          focused: globalColors.chromatic.azure[700],
+          disabled: globalColors.chromatic.azure.a200,
         },
-        onDarkMode: {
-          value: globalColors.neutral.warmGray[1100],
-        },
-      },
-      darkest: {
-        onLightMode: {
-          value: globalColors.neutral.blueGray[1300],
-        },
-        onDarkMode: {
-          value: globalColors.neutral.warmGray[1300],
+        secondary: {
+          default: globalColors.chromatic.azure[500],
+          hover: globalColors.chromatic.azure[400], // check with saurav
+          focused: globalColors.chromatic.azure[700],
+          disabled: globalColors.chromatic.azure.a200,
         },
       },
-    },
-    border: {
-      light: {
-        onLightMode: {
-          value: globalColors.neutral.blueGray[400],
+      text: {
+        primary: {
+          default: globalColors.neutral.blueGray[0],
+          hover: globalColors.neutral.blueGray[0],
+          focused: globalColors.neutral.blueGray[0],
+          disabled: globalColors.neutral.blueGray[0],
         },
-        onDarkMode: {
-          value: globalColors.neutral.warmGray[400],
-        },
-      },
-      dark: {
-        onLightMode: {
-          value: globalColors.neutral.blueGray[800],
-        },
-        onDarkMode: {
-          value: globalColors.neutral.warmGray[800],
+        secondary: {
+          default: globalColors.chromatic.azure[500], // check with saurav it should have neutral scale
+          hover: globalColors.chromatic.azure[500], // check with saurav the color looks different but the name is same,
+          focused: globalColors.chromatic.azure[500],
+          disabled: globalColors.chromatic.azure.a200,
         },
       },
     },
+    feedback: {
+      background: {
+        positive: {
+          highContrast: globalColors.chromatic.emerald[600],
+          lowContrast: globalColors.chromatic.emerald.a50,
+        },
+        negative: {
+          highContrast: globalColors.chromatic.crimson[600],
+          lowContrast: globalColors.chromatic.crimson.a50,
+        },
+        notice: {
+          highContrast: globalColors.chromatic.cider[600],
+          lowContrast: globalColors.chromatic.cider.a50,
+        },
+        information: {
+          highContrast: globalColors.chromatic.sapphire[500],
+          lowContrast: globalColors.chromatic.sapphire.a50,
+        },
+      },
+      border: {
+        positive: {
+          highContrast: globalColors.chromatic.emerald[600],
+          lowContrast: globalColors.chromatic.emerald.a200,
+        },
+        negative: {
+          highContrast: globalColors.chromatic.crimson[600],
+          lowContrast: globalColors.chromatic.crimson.a200,
+        },
+        notice: {
+          highContrast: globalColors.chromatic.cider[600],
+          lowContrast: globalColors.chromatic.cider.a200,
+        },
+        information: {
+          highContrast: globalColors.chromatic.sapphire[500],
+          lowContrast: globalColors.chromatic.sapphire.a200,
+        },
+      },
+      text: {
+        positive: {
+          highContrast: globalColors.neutral.blueGray[0],
+          lowContrast: globalColors.neutral.blueGray[1000],
+        },
+        negative: {
+          highContrast: globalColors.neutral.blueGray[0],
+          lowContrast: globalColors.neutral.blueGray[1000],
+        },
+        notice: {
+          highContrast: globalColors.neutral.blueGray[0],
+          lowContrast: globalColors.neutral.blueGray[1000],
+        },
+        information: {
+          highContrast: globalColors.neutral.blueGray[0],
+          lowContrast: globalColors.neutral.blueGray[1000],
+        },
+      },
+      icon: {
+        positive: {
+          highContrast: globalColors.neutral.blueGray[0],
+          lowContrast: globalColors.chromatic.emerald[500],
+        },
+        negative: {
+          highContrast: globalColors.neutral.blueGray[0],
+          lowContrast: globalColors.chromatic.crimson[600],
+        },
+        notice: {
+          highContrast: globalColors.neutral.blueGray[0],
+          lowContrast: globalColors.chromatic.cider[600],
+        },
+        infromation: {
+          highContrast: globalColors.neutral.blueGray[0],
+          lowContrast: globalColors.chromatic.sapphire[600],
+        },
+      },
+    },
+    surface: {
+      text: {
+        1: globalColors.neutral.blueGray[1200],
+        2: globalColors.neutral.blueGray[1000],
+        3: globalColors.neutral.blueGray[800],
+        4: globalColors.neutral.blueGray[600],
+        5: globalColors.neutral.blueGray[500],
+      },
+    },
+    body: {},
   },
   fonts: {},
   spacing: {},
