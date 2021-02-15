bffa9c368068da6fbfd931b509526a1e04c58509
kamleshchandnani <kamlesh.chandnani@gmail.com>
2021-02-15 17:04:48 +0530

    feat(tokens): add spacing scale

---
+++ packages/blade/tokens/global/spacing.ts
@@ -0,0 +1,29 @@
+type spacing = Readonly<{
+  0: string;
+  1: string;
+  2: string;
+  3: string;
+  4: string;
+  5: string;
+  6: string;
+  7: string;
+  8: string;
+  9: string;
+  10: string;
+}>;
+
+const spacing: spacing = {
+  0: '0px',
+  1: '4px',
+  2: '8px',
+  3: '12px',
+  4: '16px',
+  5: '20px',
+  6: '24px',
+  7: '32px',
+  8: '40px',
+  9: '48px',
+  10: '56px',
+};
+
+export default spacing;
