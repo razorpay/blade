bd50813107a3ab62155d0af4ffc5c6d5b8454d67
kamleshchandnani <kamlesh.chandnani@gmail.com>
2021-03-11 11:28:20 +0530

    fix(tokens): make spacing token unitless

--- packages/blade/tokens/global/spacing.ts
+++ packages/blade/tokens/global/spacing.ts
@@ -1,29 +1,29 @@
-type spacing = Readonly<{
-  0: string;
-  1: string;
-  2: string;
-  3: string;
-  4: string;
-  5: string;
-  6: string;
-  7: string;
-  8: string;
-  9: string;
-  10: string;
+type Spacing = Readonly<{
+  0: number;
+  1: number;
+  2: number;
+  3: number;
+  4: number;
+  5: number;
+  6: number;
+  7: number;
+  8: number;
+  9: number;
+  10: number;
 }>;
 
-const spacing: spacing = {
-  0: '0px',
-  1: '4px',
-  2: '8px',
-  3: '12px',
-  4: '16px',
-  5: '20px',
-  6: '24px',
-  7: '32px',
-  8: '40px',
-  9: '48px',
-  10: '56px',
+const spacing: Spacing = {
+  0: 0,
+  1: 4,
+  2: 8,
+  3: 12,
+  4: 16,
+  5: 20,
+  6: 24,
+  7: 32,
+  8: 40,
+  9: 48,
+  10: 56,
 };
 
 export default spacing;
