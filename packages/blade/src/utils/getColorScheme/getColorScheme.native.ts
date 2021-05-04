e57a7cfbb29918fe5a1a147329c1ce8eb37218b6
kamleshchandnani <kamlesh.chandnani@gmail.com>
2021-05-04 11:38:26 +0530

    chore(typo): fix typo in comments

--- packages/blade/src/utils/getColorScheme/getColorScheme.native.ts
+++ packages/blade/src/utils/getColorScheme/getColorScheme.native.ts
@@ -2,7 +2,7 @@ import { Appearance } from 'react-native';
 import { ColorSchemeNames, ColorSchemeNamesInput } from '../../tokens/theme';
 
 const getColorScheme = (colorScheme: ColorSchemeNamesInput = 'light'): ColorSchemeNames => {
-  // @TODO: convert this to hook as Appearance API also adds an eventListener which subsribes to the colorscheme changes on the device
+  // @TODO: convert this to hook as Appearance API also adds an eventListener which subscribes to the colorscheme changes on the device
   if (colorScheme === 'light' || colorScheme === 'dark') {
     return colorScheme;
   }
