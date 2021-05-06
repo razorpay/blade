7d7cbe686d599b9109c7fc01f29a9a27c526a312
kamleshchandnani <kamlesh.chandnani@gmail.com>
2021-05-06 14:31:39 +0530

    fix(theme): add missing files

--- packages/blade/src/utils/index.ts
+++ packages/blade/src/utils/index.ts
@@ -1,1 +1,5 @@
 export { default as useColorScheme } from './useColorScheme';
+export { default as isEqual } from './isEqual';
+export { default as merge } from './merge';
+export { default as getColorScheme } from './getColorScheme';
+export { default as isPartialMatchObjectKeys } from './isPartialMatchObjectKeys';
