70fe71c9862d28120f8e77b13019acac3e4494e3
Kamlesh Chandnani <kamlesh.c@razorpay.com>
2021-05-06 13:22:09 +0530

    test(theme): add theme utils tests (#266)
    Co-authored-by: Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>

---
+++ packages/blade/jest.native.config.js
@@ -0,0 +1,24 @@
+const ignores = ['/node_modules/'];
+
+module.exports = {
+  preset: 'react-native',
+  testPathIgnorePatterns: [...ignores, 'web.test'],
+  collectCoverageFrom: ['./src/**/*.native.{ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75,
+    },
+  },
+  moduleFileExtensions: ['native.ts', 'native.tsx', 'ts', 'tsx', 'js', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.native.test.{ts,tsx}', '**/__tests__/**/*.test.{ts,tsx}'],
+  transform: {
+    '\\.(js|ts|tsx)?$': './jest-preprocess.js',
+  },
+  testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native.*|@react-native.*|@?react-navigation.*|@?react-navigation-stack)/)',
+  ],
+};
---
+++ packages/blade/jest.web.config.js
@@ -0,0 +1,21 @@
+const ignores = ['/node_modules/'];
+
+module.exports = {
+  testPathIgnorePatterns: [...ignores, 'native.test'],
+  collectCoverageFrom: ['./src/**/*.web.{ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75,
+    },
+  },
+  moduleFileExtensions: ['web.ts', 'web.tsx', 'ts', 'tsx', 'js', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.web.test.{ts,tsx}', '**/__tests__/**/*.test.{ts,tsx}'],
+  transform: {
+    '\\.(js|ts|tsx)?$': 'babel-jest',
+  },
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+};
