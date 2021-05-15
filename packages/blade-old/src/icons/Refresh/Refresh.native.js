f0b620984f57f52110b61271e0c6b50a35c6f9a6
Nikhil Khadilkar <nikhilkhadilkar1993@gmail.com>
2021-05-15 18:06:38 +0530

    refactor: new icon

---
+++ packages/blade-old/src/icons/Refresh/Refresh.native.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Svg, { Path } from 'react-native-svg';
+import { getThemeColors } from '../../_helpers/theme';
+import colors from '../../tokens/colors';
+export default function Refresh({ width, height, fill }) {
+  return (
+    <Svg width={width} height={height} viewBox="0 0 24 24" fill="none">
+      <Path
+        d="M10.229 4.2a8 8 0 017.446 2.169L20.525 9H17a1 1 0 100 2h6a1 1 0 001-1V4a1 1 0 10-2 0v3.64l-2.944-2.718A10 10 0 002.567 8.666a1 1 0 001.886.668A8 8 0 0110.229 4.2zM2 16.36V20a1 1 0 11-2 0v-6a1 1 0 011-1h6a1 1 0 110 2H3.474l2.85 2.631a8 8 0 0013.223-2.965 1 1 0 011.886.668 10 10 0 01-16.489 3.744L2 16.361z"
+        fill={fill}
+      />
+    </Svg>
+  );
+}
+
+Refresh.propTypes = {
+  width: PropTypes.number,
+  height: PropTypes.number,
+  fill: PropTypes.oneOf(getThemeColors()),
+};
+
+Refresh.defaultProps = {
+  width: 24,
+  height: 24,
+  fill: colors.sapphire[800],
+};
