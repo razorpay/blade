ecbc864bb1f043748be835e1be16aed348e1085f
rzpcibot <tools+cibot@razorpay.com>
2021-05-25 22:02:01 +0530

    feat(tokens): add new tokens

--- packages/blade/scripts/uploadTokens.js
+++ packages/blade/scripts/uploadTokens.js
@@ -42,7 +42,7 @@ const uploadColorTokens = async () => {
   fs.writeFileSync(bankingThemePath, updatedBankingTheme);
 
   // 5. create branch
-  execa.commandSync(`git checkout -b add/tokens`);
+  execa.commandSync(`git checkout -b tokens/dummy/pr`);
   execa.commandSync(`git config user.email ${GITHUB_BOT_EMAIL}`);
   execa.commandSync(`git config user.name ${GITHUB_BOT_USERNAME}`);
 
@@ -54,6 +54,7 @@ const uploadColorTokens = async () => {
   });
 
   // 7. Raise a PR: Output PR link
-  execa.commandSync(`hub pull-request --push --message feat(tokens):\\ add\\ new\\ tokens`);
+  // execa.commandSync(`hub pull-request --push --message feat(tokens):\\ add\\ new\\ tokens`);
+  execa.commandSync(`gh pr create --title feat(tokens):\\ add\\ new\\ tokens`);
 };
 uploadColorTokens();
