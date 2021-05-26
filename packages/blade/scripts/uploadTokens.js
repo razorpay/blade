9818ae8020fdd9518d07e9ffa49f3d55261498d5
kamleshchandnani <kamlesh.chandnani@gmail.com>
2021-05-26 19:46:51 +0530

    ci: split the color token processing into 2 parts

--- packages/blade/scripts/uploadTokens.js
+++ packages/blade/scripts/uploadTokens.js
@@ -12,35 +12,39 @@ const uploadColorTokens = async () => {
 
   const colorRegex = /const colors: Colors = {(.|\n)+?};/gm;
 
-  // 2. read the paymentTheme File
-  const paymentThemePath = path.resolve(__dirname, '../src/tokens/theme/paymentTheme.ts');
-  const paymentTheme = fs.readFileSync(paymentThemePath, 'utf8');
+  if (tokens.paymentThemeColors) {
+    // 2. read the paymentTheme File
+    const paymentThemePath = path.resolve(__dirname, '../src/tokens/theme/paymentTheme.ts');
+    const paymentTheme = fs.readFileSync(paymentThemePath, 'utf8');
 
-  // 3. write the new tokens to paymentTheme file
-  const updatedPaymentThemeColors = JSON.stringify(tokens.paymentThemeColors.colors).replace(
-    /"/g,
-    '',
-  );
-  const updatedPaymentTheme = paymentTheme.replace(
-    colorRegex,
-    `const colors: Colors = ${updatedPaymentThemeColors};`,
-  );
-  fs.writeFileSync(paymentThemePath, updatedPaymentTheme);
+    // 3. write the new tokens to paymentTheme file
+    const updatedPaymentThemeColors = JSON.stringify(tokens.paymentThemeColors.colors).replace(
+      /"/g,
+      '',
+    );
+    const updatedPaymentTheme = paymentTheme.replace(
+      colorRegex,
+      `const colors: Colors = ${updatedPaymentThemeColors};`,
+    );
+    fs.writeFileSync(paymentThemePath, updatedPaymentTheme);
+  }
 
-  // 4. read the bankingTheme File
-  const bankingThemePath = path.resolve(__dirname, '../src/tokens/theme/bankingTheme.ts');
-  const bankingTheme = fs.readFileSync(bankingThemePath, 'utf8');
+  if (tokens.bankingThemeColors) {
+    // 4. read the bankingTheme File
+    const bankingThemePath = path.resolve(__dirname, '../src/tokens/theme/bankingTheme.ts');
+    const bankingTheme = fs.readFileSync(bankingThemePath, 'utf8');
 
-  // 5. write the new tokens to bankingTheme file
-  const updatedBankingThemeColors = JSON.stringify(tokens.bankingThemeColors.colors).replace(
-    /"/g,
-    '',
-  );
-  const updatedBankingTheme = bankingTheme.replace(
-    colorRegex,
-    `const colors: Colors = ${updatedBankingThemeColors};`,
-  );
-  fs.writeFileSync(bankingThemePath, updatedBankingTheme);
+    // 5. write the new tokens to bankingTheme file
+    const updatedBankingThemeColors = JSON.stringify(tokens.bankingThemeColors.colors).replace(
+      /"/g,
+      '',
+    );
+    const updatedBankingTheme = bankingTheme.replace(
+      colorRegex,
+      `const colors: Colors = ${updatedBankingThemeColors};`,
+    );
+    fs.writeFileSync(bankingThemePath, updatedBankingTheme);
+  }
 
   // 6. create branch
   const branchName = randomNameGenerator
