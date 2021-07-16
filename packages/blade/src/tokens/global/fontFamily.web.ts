ed019ca408e3d0eb07d2da9e548ed68a8da470d6
Kamlesh Chandnani <kamlesh.c@razorpay.com>
2021-07-16 11:41:04 +0530

    feat(blade): add type generation scripts (#314)

--- packages/blade/src/tokens/global/fontFamily.web.ts
+++ packages/blade/src/tokens/global/fontFamily.web.ts
@@ -1,4 +1,6 @@
-const fontFamily = {
+import type { FontFamily } from './fontFamily.d';
+
+const fontFamily: FontFamily = {
   text:
     'Lato, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
   code: 'monospace',
