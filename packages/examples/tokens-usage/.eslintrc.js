554ffa3be8919b0c503f9e709a6bbce0ba82bf7c
kamleshchandnani <kamlesh.chandnani@gmail.com>
2021-07-23 12:16:08 +0530

    fix: add TS project

--- packages/examples/tokens-usage/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rules: {
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/no-unstable-nested-components': 'off',
-    'no-unsafe-optional-chaining': 'off',
-    'no-nonoctal-decimal-escape': 'off',
-  },
-};
