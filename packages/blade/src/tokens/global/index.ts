0c20041ae43c4a60d932f0a4807c1332a105cff9
kamleshchandnani <kamlesh.chandnani@gmail.com>
2021-07-29 14:24:50 +0530

    fix(blade): export all the types of global tokens for consumers

--- packages/blade/src/tokens/global/index.ts
+++ packages/blade/src/tokens/global/index.ts
@@ -1,1 +1,7 @@
+export type { Border } from './border';
 export { default as colors } from './colors';
+export type { Color } from './colors';
+export type { FontFamily } from './fontFamily.d';
+export type { Opacity } from './opacity';
+export type { Typography } from './typography';
+export type { Spacing } from './spacing';
