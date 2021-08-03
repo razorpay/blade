64c9d6d14e869c5cf4951dcddc4c9201b34cff65
Kamlesh Chandnani <kamlesh.c@razorpay.com>
2021-08-03 10:18:39 +0530

    fix(blade): add description in token types (#330)

---
+++ .changeset/purple-lemons-collect.md
@@ -0,0 +1,5 @@
+---
+"@razorpay/blade": patch
+---
+
+fix(blade): add description in token types
--- packages/blade/src/tokens/global/fontFamily.d.ts
+++ packages/blade/src/tokens/global/fontFamily.d.ts
@@ -1,5 +1,7 @@
 export type FontFamily = {
+  /** Used for all type of textual content except code snippets */
   text: string;
+  /** Used for code snippets  */
   code: string;
 };
 
