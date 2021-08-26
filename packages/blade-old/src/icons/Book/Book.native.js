520a5c46175f8e66a8a4ca1c9ab18e0df97829b2
mohit23x <mohit.lodha@razorpay.com>
2021-08-26 16:53:21 +0530

    updated iconsn viewBox to 24

--- packages/blade-old/src/icons/Book/Book.native.js
+++ packages/blade-old/src/icons/Book/Book.native.js
@@ -1,36 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Svg, { Path, Defs, ClipPath, Mask, G } from 'react-native-svg';
+import Svg, { Path } from 'react-native-svg';
 import { getThemeColors } from '../../_helpers/theme';
 import colors from '../../tokens/colors';
 
 function Book({ width, height, fill }) {
   return (
-    <Svg
-      width={width}
-      height={height}
-      viewBox="0 0 16 16"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <G clipPath="url(#clip0)">
-        <Mask id="a" maskUnits="userSpaceOnUse" x={0} y={0} width={16} height={16}>
-          <Path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M14 1.334a.667.667 0 00-.667-.667h-9A2.333 2.333 0 002 3v10a2.333 2.333 0 002.333 2.334h9a.667.667 0 00.667-.667V1.334zM3.333 3a1 1 0 011-1h8.334v8.667H4.333c-.358 0-.697.08-1 .225V3zm0 10a1 1 0 001 1h8.334v-2H4.333a1 1 0 00-1 1z"
-            fill="#fff"
-          />
-        </Mask>
-        <G mask="url(#a)">
-          <Path fill={fill} d="M0 0H16V16H0z" />
-        </G>
-      </G>
-      <Defs>
-        <ClipPath id="clip0">
-          <Path fill="#fff" d="M0 0H16V16H0z" />
-        </ClipPath>
-      </Defs>
+    <Svg width={width} height={height} viewBox="0 0 24 24" fill="none">
+      <Path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M21 2a1 1 0 00-1-1H6.5A3.5 3.5 0 003 4.5v15A3.5 3.5 0 006.5 23H20a1 1 0 001-1V2zM5 4.5A1.5 1.5 0 016.5 3H19v13H6.5c-.537 0-1.045.12-1.5.337V4.5zm0 15A1.5 1.5 0 006.5 21H19v-3H6.5A1.5 1.5 0 005 19.5z"
+        fill={fill}
+      />
     </Svg>
   );
 }
--- packages/blade-old/src/icons/Headphone/Headphone.native.js
+++ packages/blade-old/src/icons/Headphone/Headphone.native.js
@@ -1,50 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Svg, { Path, Mask, G, Defs, ClipPath } from 'react-native-svg';
+import Svg, { Path } from 'react-native-svg';
 import { getThemeColors } from '../../_helpers/theme';
 import colors from '../../tokens/colors';
 
-function Gallery({ width, height, fill }) {
+function Headphone({ width, height, fill }) {
   return (
-    <Svg
-      width={width}
-      height={height}
-      viewBox="0 0 16 16"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <G clipPath="url(#clip0)">
-        <Mask id="a" maskUnits="userSpaceOnUse" x={0} y={0} width={16} height={16}>
-          <Path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M2.667 8a5.333 5.333 0 0110.666 0v.666H12a2 2 0 00-2 2v2a2 2 0 002 2h.667a2 2 0 002-2V8A6.667 6.667 0 001.333 8v4.666a2 2 0 002 2H4a2 2 0 002-2v-2a2 2 0 00-2-2H2.667V8zm0 2v2.666c0 .368.298.667.666.667H4a.667.667 0 00.667-.667v-2A.667.667 0 004 10H2.667zm10.666 0H12a.667.667 0 00-.667.666v2c0 .368.299.667.667.667h.667a.667.667 0 00.666-.667V10z"
-            fill="#fff"
-          />
-        </Mask>
-        <G mask="url(#a)">
-          <Path fill={fill} d="M0 0H16V16H0z" />
-        </G>
-      </G>
-      <Defs>
-        <ClipPath id="clip0">
-          <Path fill="#fff" d="M0 0H16V16H0z" />
-        </ClipPath>
-      </Defs>
+    <Svg width={width} height={height} viewBox="0 0 24 24" fill="none">
+      <Path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4 12a8 8 0 1116 0v1h-2a3 3 0 00-3 3v3a3 3 0 003 3h1a3 3 0 003-3v-7c0-5.523-4.477-10-10-10S2 6.477 2 12v7a3 3 0 003 3h1a3 3 0 003-3v-3a3 3 0 00-3-3H4v-1zm0 3v4a1 1 0 001 1h1a1 1 0 001-1v-3a1 1 0 00-1-1H4zm16 0h-2a1 1 0 00-1 1v3a1 1 0 001 1h1a1 1 0 001-1v-4z"
+        fill={fill}
+      />
     </Svg>
   );
 }
 
-Gallery.propTypes = {
+Headphone.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
   fill: PropTypes.oneOf(getThemeColors()),
 };
 
-Gallery.defaultProps = {
+Headphone.defaultProps = {
   width: 24,
   height: 24,
   fill: colors.sapphire[800],
 };
 
-export default Gallery;
+export default Headphone;
