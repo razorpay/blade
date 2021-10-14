666dd32beca1a1c5559e8877b2ac30d60e051111
kamleshchandnani <kamlesh.chandnani@gmail.com>
2021-10-14 13:19:49 +0530

    fix(blade): build errors

--- packages/blade/src/tokens/theme/index.ts
+++ packages/blade/src/tokens/theme/index.ts
@@ -3,12 +3,11 @@ import type { ColorSchemeNamesInput } from './theme.d';
 export { default as paymentTheme } from './paymentTheme';
 export { default as bankingTheme } from './bankingTheme';
 export { default as overrideTheme } from './overrideTheme';
-export {
+export type {
   ColorSchemeNames,
   ColorSchemeNamesInput,
   Shadows,
   ColorsWithModes,
   ThemeTokens,
 } from './theme.d';
-// export type { Theme } from './theme.d';
 export const colorSchemeNamesInput: ColorSchemeNamesInput[] = ['light', 'dark', 'system'];
