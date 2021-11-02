c2ee9454a75ee9f9f756261d186c9fbd970beb6a
Kamlesh Chandnani <kamlesh.c@razorpay.com>
2021-11-02 14:27:03 +0530

    fix(blade): update example to use the new theme token structure (#359)

--- packages/examples/tokens-usage/src/App.tsx
+++ packages/examples/tokens-usage/src/App.tsx
@@ -1,33 +1,13 @@
-// import './App.css';
-import React from 'react';
-import { ThemeProvider } from '@razorpay/blade/components';
+import { BladeProvider } from '@razorpay/blade/components';
 import { paymentTheme } from '@razorpay/blade/tokens';
-import { createGlobalStyle } from 'styled-components';
-// import logo from './logo.svg';
 import Card from './Card';
 import '@fontsource/lato';
 
-const GlobalStyle = createGlobalStyle`
-html {
-  box-sizing: border-box;
-}
-body {
-    margin: 16px;
-    font-family: ${paymentTheme.typography.desktop.fonts.family.text};
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background-color: ${paymentTheme.colors.surface.background.level1.lowContrast.onLight};
-  }
-`;
-
 function App(): JSX.Element {
   return (
-    <React.Fragment>
-      <GlobalStyle />
-      <ThemeProvider theme={paymentTheme}>
-        <Card />
-      </ThemeProvider>
-    </React.Fragment>
+    <BladeProvider themeTokens={paymentTheme} colorScheme="system">
+      <Card />
+    </BladeProvider>
   );
 }
 
