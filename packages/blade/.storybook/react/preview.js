d25135b5425d1ebe9ffece557b89b02c74642f71
kamleshchandnani <kamlesh.chandnani@gmail.com>
2021-12-28 15:47:48 +0530

    docs(blade): resolve conflicts

--- packages/blade/.storybook/react/preview.js
+++ packages/blade/.storybook/react/preview.js
@@ -73,3 +73,67 @@ export const globalTypes = {
     },
   },
 };
+
+export const decorators = [
+  (Story, context) => {
+    const getThemeTokens = () => {
+      console.log(context.globals.themeTokens);
+      if (context.globals.themeTokens === 'paymentTheme') {
+        console.log('payment coming');
+        return paymentTheme;
+      }
+      if (context.globals.themeTokens === 'bankingTheme') {
+        console.log('banking coming');
+        return bankingTheme;
+      }
+    };
+
+    const getColorScheme = () => {
+      return context.globals.colorScheme;
+    };
+
+    return (
+      <BladeProvider
+        key={`${context.globals.themeTokens}-${context.globals.colorScheme}`}
+        themeTokens={getThemeTokens()}
+        colorScheme={getColorScheme()}
+      >
+        <Story />
+      </BladeProvider>
+    );
+  },
+];
+
+export const globalTypes = {
+  themeTokens: {
+    name: 'Theme Tokens',
+    description: 'Theme Tokens for Blade',
+    defaultValue: 'paymentTheme',
+    toolbar: {
+      icon: 'paintbrush',
+      // Array of plain string values or MenuItem shape (see below)
+      items: [
+        { value: 'paymentTheme', title: 'Payment' },
+        { value: 'bankingTheme', title: 'Banking' },
+      ],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+    },
+  },
+  colorScheme: {
+    name: 'Color Scheme',
+    description: 'Color Scheme for Blade',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'eye',
+      // Array of plain string values or MenuItem shape (see below)
+      items: [
+        { value: 'light', title: 'Light' },
+        { value: 'dark', title: 'Dark' },
+        { value: 'system', title: 'System' },
+      ],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+    },
+  },
+};
