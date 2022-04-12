f226deea392f3e231ad8bdf40052a1be1245fa21
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-04-12 17:38:05 +0530

    docs: add visual examples for easing

---
+++ packages/blade/docs/components/MovingDiv.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const MovingDiv = styled.div`
+  height: 40px;
+  width: 40px;
+  background-color: ${(props) => props.theme.colors.brand.primary[500]};
+  animation: ${(props) => `move 3s ${props.easing || 'linear'}  infinite`};
+  @keyframes move {
+    0% {
+      transform: translateX(0px);
+    }
+    50% {
+      transform: translateX(100px);
+    }
+    100% {
+      transform: translateX(0px);
+    }
+  }
+`;
+
+export default MovingDiv;
