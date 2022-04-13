9efccc287fbc3b9bedb4a6a47e43b08bf12a8ffb
kamleshchandnani <kamlesh.chandnani@gmail.com>
2022-04-13 15:28:25 +0530

    fix(blade): infra changes and removal of examples directory

---
+++ packages/blade/src/storybook-recipes/SimpleCardWithTokens/CardWithTokens.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Card from './Card';
+
+const CardMeta: ComponentMeta<typeof Card> = {
+  title: 'Recipes/CardWithTokens',
+  component: Card,
+};
+export default CardMeta;
+
+export const CardStory: ComponentStory<typeof Card> = () => <Card />;
