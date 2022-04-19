b0462cdf4328b578a1507045adc9e7f7099d6e51
kamleshchandnani <kamlesh.chandnani@gmail.com>
2022-04-19 16:33:05 +0530

    docs(blade): update BaseText stories

---
+++ packages/blade/src/components/Typography/BaseText.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, Meta } from '@storybook/react';
+import BaseTextComponent from './BaseText';
+
+export default {
+  title: 'Components/Typography',
+  component: BaseTextComponent,
+  args: {
+    color: 'surface.text.normal.lowContrast',
+    fontFamily: 'text',
+    fontSize: 700,
+    fontWeight: 'regular',
+    fontStyle: 'normal',
+    textAlign: 'left',
+    textDecorationLine: 'none',
+    lineHeight: 'm',
+    name: 'Storybook',
+    children: 'Base Text',
+  },
+} as Meta;
+
+const BaseTextTemplate: ComponentStory<typeof BaseTextComponent> = (args) => {
+  return <BaseTextComponent {...args}>{args.children}</BaseTextComponent>;
+};
+
+export const BaseText = BaseTextTemplate.bind({});
