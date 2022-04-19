41b6b0717d28c49c88b22f6e1572d3dd747198a4
kamleshchandnani <kamlesh.chandnani@gmail.com>
2022-04-19 13:57:10 +0530

    fix(blade): recipes for react-native

--- packages/blade/src/storybook-recipes/SimpleCardWithTokens/Card.d.ts
+++ packages/blade/src/storybook-recipes/SimpleCardWithTokens/Card.d.ts
@@ -1,1 +1,1 @@
-export { default } from './Card.web'
+export { default } from './Card.web';
--- packages/blade/src/storybook-recipes/SimpleCardWithTokens/Card.native.tsx
+++ packages/blade/src/storybook-recipes/SimpleCardWithTokens/Card.native.tsx
@@ -95,24 +95,24 @@ const FlexRow = styled.View`
 const Card = (): React.ReactElement => {
   return (
     <React.Fragment>
-      <DisplayLarge>Cash Advance </DisplayLarge>
+      <DisplayLarge>{`Cash Advance `}</DisplayLarge>
       <StyledCard>
-        <LeadBold>Total Repayable Amount</LeadBold>
-        <DisplayMedium>₹16,666.67</DisplayMedium>
+        <LeadBold>{`Total Repayable Amount`}</LeadBold>
+        <DisplayMedium>{`₹16,666.67`}</DisplayMedium>
         <FlexRow>
-          <CaptionRegular>Principal:</CaptionRegular>
-          <CaptionBold>₹16,666.67</CaptionBold>
-          <CaptionRegular>Interest:</CaptionRegular>
-          <CaptionBold>₹450.67</CaptionBold>
+          <CaptionRegular>{`Principal:`}</CaptionRegular>
+          <CaptionBold>{`₹16,666.67`}</CaptionBold>
+          <CaptionRegular>{`Interest:`}</CaptionRegular>
+          <CaptionBold>{`₹450.67`}</CaptionBold>
         </FlexRow>
         <AlertInformation>
-          The interest charged will be deposited back into your bank account within a day of
-          repayment.
+          {`The interest charged will be deposited back into your bank account within a day of
+          repayment.`}
         </AlertInformation>
         <Divider />
         <CaptionRegular>
-          This amount will be deducted in 3 instalments from your settlement balance between Feb
-          18-20 on a daily basis.
+          {`This amount will be deducted in 3 instalments from your settlement balance between Feb
+          18-20 on a daily basis.`}
         </CaptionRegular>
       </StyledCard>
     </React.Fragment>
