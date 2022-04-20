4993ab6f42cd01d72cb9c8c5db4508472e40edba
kamleshchandnani <kamlesh.chandnani@gmail.com>
2022-04-20 12:46:14 +0530

    fix(blade): remove unnecessary prop

--- packages/blade/src/components/Typography/BaseText/BaseText.d.ts
+++ packages/blade/src/components/Typography/BaseText/BaseText.d.ts
@@ -27,7 +27,6 @@ export type BaseTextProps = {
   lineHeight: keyof Theme['typography']['lineHeights'];
   as?: 'code' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
   textAlign?: 'center' | 'justify' | 'left' | 'right';
-  name?: string;
   children?: ReactNode;
   theme: Theme;
 };
