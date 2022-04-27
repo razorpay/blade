7cef1c0ea527c16738f06e25f86b71d458408926
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-04-27 16:37:10 +0530

    feat: add rupee icon

--- packages/blade/src/components/Icons/iconMap.ts
+++ packages/blade/src/components/Icons/iconMap.ts
@@ -1,8 +1,11 @@
 import CreditCardIconComponent from './CreditCardIcon';
+import RupeeIconComponent from './RupeeIcon';
 import type { IconProps } from '.';
 
-export const Icons: Record<string, React.FC<IconProps>> = {
+// add new icons here to make them visible in storybook
+export const Icons: Record<string, React.ComponentType<IconProps>> = {
   CreditCardIcon: CreditCardIconComponent,
+  RupeeIcon: RupeeIconComponent,
 };
 
 export const IconNames = Object.keys(Icons);
