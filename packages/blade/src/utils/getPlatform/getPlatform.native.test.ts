a4de013c83c3550397bd8edee2c841c2ba5ce950
kamleshchandnani <kamlesh.chandnani@gmail.com>
2022-05-06 10:29:21 +0530

    fix(blade): remove unwanted utils

--- packages/blade/src/utils/getPlatform/getPlatform.native.test.ts
+++
@@ -1,7 +0,0 @@
-import getPlatform from './getPlatform';
-
-describe('getPlatform', () => {
-  it(`should return 'react-native'`, () => {
-    expect(getPlatform()).toEqual('react-native');
-  });
-});
--- packages/blade/src/utils/getPlatform/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from './getPlatform';
-export * from './getPlatform';
