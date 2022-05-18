1020d84a9dab58a8c019a6d81395e69ac3c3ade5
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-05-18 17:38:42 +0530

    feat: add CloseIcon

--- packages/blade/src/components/Icons/iconMap.ts
+++ packages/blade/src/components/Icons/iconMap.ts
@@ -1,3 +1,4 @@
+import CloseIconComponent from './CloseIcon';
 import ChevronDownIconComponent from './ChevronDownIcon';
 import ChevronLeftIconComponent from './ChevronLeftIcon';
 import ChevronUpIconComponent from './ChevronUpIcon';
@@ -11,6 +12,7 @@ import type { IconProps } from '.';
 
 // add new icons here to make them visible in storybook
 const iconMap: Record<string, React.ComponentType<IconProps>> = {
+  CloseIcon: CloseIconComponent,
   ChevronDownIcon: ChevronDownIconComponent,
   ChevronLeftIcon: ChevronLeftIconComponent,
   ChevronUpIcon: ChevronUpIconComponent,
--- packages/blade/src/components/Icons/index.tsx
+++ packages/blade/src/components/Icons/index.tsx
@@ -15,6 +15,7 @@ export type IconProps = {
   size: IconSize;
 };
 
+export { default as CloseIcon } from './CloseIcon';
 export { default as ChevronDownIcon } from './ChevronDownIcon';
 export { default as ChevronLeftIcon } from './ChevronLeftIcon';
 export { default as ChevronRightIcon } from './ChevronRightIcon';
