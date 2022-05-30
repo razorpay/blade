ed6775030f4354c87fc1496e8fbdbe8840449eae
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-05-30 17:25:30 +0530

    chore: use style objects & change height to minHeight

--- packages/blade/src/components/Button/BaseButton/getBaseButtonStyles.ts
+++ packages/blade/src/components/Button/BaseButton/getBaseButtonStyles.ts
@@ -1,31 +1,32 @@
+import type { CSSObject } from 'styled-components';
 import type { StyledBaseButtonProps } from './StyledBaseButton';
 
 const getBaseButtonStyles = ({
   color,
   hoverColor,
   activeColor,
-}: Omit<StyledBaseButtonProps, 'children' | 'onClick'>): string => `
-  display:flex;
-  flex-direction: row;
-  height: 48px;
-  cursor: pointer;
-  background-color: ${color};
-  border: 2px solid ${color};
-  color: white;
-  border-radius: 2px;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-  align-self: center;
-  padding: 0px 20px 0px 20px;
-  &:hover {
-    background-color: ${hoverColor};
-    border: 2px solid ${hoverColor};
-  }
-  &:active {
-    background-color: ${activeColor};
-    border: 2px solid ${activeColor};
-  }
-`;
+}: Omit<StyledBaseButtonProps, 'children' | 'onClick'>): CSSObject => ({
+  display: 'flex',
+  flexDirection: 'row',
+  minHeight: '48px',
+  cursor: 'pointer',
+  backgroundColor: color,
+  border: `2px solid ${color}`,
+  color: 'white',
+  borderRadius: '2px',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '8px',
+  alignSelf: 'center',
+  padding: '0px 20px 0px 20px',
+  '&:hover': {
+    backgroundColor: hoverColor,
+    border: `2px solid ${hoverColor}`,
+  },
+  '&:active': {
+    backgroundColor: activeColor,
+    border: `2px solid ${activeColor}`,
+  },
+});
 
 export default getBaseButtonStyles;
