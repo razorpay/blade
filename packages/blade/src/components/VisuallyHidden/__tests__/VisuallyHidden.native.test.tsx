91e3cf2aa77c2cd029399f6d0f310d5702bb5f2c
anuraghazra <anurag.hazra@razorpay.com>
2022-06-02 16:05:15 +0530

    chore: add tests

---
+++ packages/blade/src/components/VisuallyHidden/__tests__/VisuallyHidden.native.test.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text } from 'react-native';
+import renderWithTheme from '../../../_helpers/testing/renderWithTheme.native';
+import VisuallyHidden from '../VisuallyHidden';
+
+describe('<VisuallyHidden />', () => {
+  // TODO: Enable css testing
+  it('should render VisuallyHidden content', () => {
+    const displayText = 'Text only visible to screen readers';
+    const { toJSON, getByText } = renderWithTheme(
+      <VisuallyHidden>
+        <Text>{displayText}</Text>
+      </VisuallyHidden>,
+    );
+    expect(getByText(displayText)).toBeTruthy();
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
---
+++ packages/blade/src/components/VisuallyHidden/__tests__/VisuallyHidden.web.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderWithTheme from '../../../_helpers/testing/renderWithTheme.web';
+import VisuallyHidden from '../VisuallyHidden';
+
+describe('<VisuallyHidden />', () => {
+  // TODO: Enable css testing
+  it('should render VisuallyHidden content', () => {
+    const displayText = 'Text only visible to screen readers';
+    const { container } = renderWithTheme(<VisuallyHidden>{displayText}</VisuallyHidden>);
+    expect(container).toMatchSnapshot();
+  });
+});
