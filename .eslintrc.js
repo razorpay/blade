6efdd5bae4c261a6262a59d94e1c9e876803bd12
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-06-10 16:27:18 +0530

    feat: enable internal stories  (Button [5]) (#490)

--- .eslintrc.js
+++ .eslintrc.js
@@ -100,7 +100,7 @@ module.exports = {
       },
     },
     {
-      files: ['**/*.stories.{ts,tsx}'],
+      files: ['**/*.stories.{ts,tsx}', '**/*.stories.internal.{ts,tsx}'],
       rules: {
         'react/display-name': ['off'],
       },
