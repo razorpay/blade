24d5941828a5bae1e3475add4c92dabfaf8860de
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-07-21 17:30:59 +0530

    chore: generate rest of the themes

--- packages/blade/scripts/generateCSSTokens.js
+++ packages/blade/scripts/generateCSSTokens.js
@@ -1,9 +1,8 @@
-const fs = require('fs');
 const { getLeaves } = require('any-leaf');
 const StyleDictionary = require('style-dictionary');
-const _ = require('lodash');
+const set = require('lodash/fp/set');
 
-const { paymentTheme } = require('../generated/themeBundle');
+const { paymentTheme, bankingTheme } = require('../generated/themeBundle');
 
 function getThemeFromTokens({ onColorMode, onDeviceType, themeTokens }) {
   return {
@@ -35,32 +34,134 @@ function getStyledDictionaryConfig({ outputFileName, tokens }) {
   };
 }
 
-const convertToStyledDictionarySchema = (themeTokens) => {
+function convertToStyledDictionarySchema({ themeTokens }) {
   const leafNodes = getLeaves(themeTokens);
 
   leafNodes.forEach(({ path, value }) => {
-    _.set(themeTokens, path, { value });
+    themeTokens = set(path, { value }, themeTokens);
   });
   return themeTokens;
-};
+}
 
 const getThemeFromTokensCSSTokens = () => {
-  const styledDictionaryPaymentTheme = convertToStyledDictionarySchema(paymentTheme);
+  const styledDictionaryPaymentTheme = convertToStyledDictionarySchema({
+    themeTokens: paymentTheme,
+  });
+  const styledDictionaryBankingTheme = convertToStyledDictionarySchema({
+    themeTokens: bankingTheme,
+  });
 
+  // Payment Light Desktop
   const paymentThemeLightDesktop = getThemeFromTokens({
     onColorMode: 'onLight',
     onDeviceType: 'onDesktop',
     themeTokens: { ...styledDictionaryPaymentTheme },
   });
 
-  fs.writeFileSync(
-    './generated/paymentThemeLightDesktop.json',
-    JSON.stringify(paymentThemeLightDesktop),
-  );
   StyleDictionary.extend(
     getStyledDictionaryConfig({
       outputFileName: 'paymentThemeLightDesktop',
       tokens: paymentThemeLightDesktop,
+    }),
+  ).buildAllPlatforms();
+
+  // Payment Dark Desktop
+  const paymentThemeDarkDesktop = getThemeFromTokens({
+    onColorMode: 'onDark',
+    onDeviceType: 'onDesktop',
+    themeTokens: { ...styledDictionaryPaymentTheme },
+  });
+
+  StyleDictionary.extend(
+    getStyledDictionaryConfig({
+      outputFileName: 'paymentThemeDarkDesktop',
+      tokens: paymentThemeDarkDesktop,
+    }),
+  ).buildAllPlatforms();
+
+  // Payment Light Mobile
+  const paymentThemeLightMobile = getThemeFromTokens({
+    onColorMode: 'onLight',
+    onDeviceType: 'onMobile',
+    themeTokens: { ...styledDictionaryPaymentTheme },
+  });
+
+  StyleDictionary.extend(
+    getStyledDictionaryConfig({
+      outputFileName: 'paymentThemeLightMobile',
+      tokens: paymentThemeLightMobile,
+    }),
+  ).buildAllPlatforms();
+
+  // Payment Dark Mobile
+  const paymentThemeDarkMobile = getThemeFromTokens({
+    onColorMode: 'onDark',
+    onDeviceType: 'onMobile',
+    themeTokens: { ...styledDictionaryPaymentTheme },
+  });
+
+  StyleDictionary.extend(
+    getStyledDictionaryConfig({
+      outputFileName: 'paymentThemeDarkMobile',
+      tokens: paymentThemeDarkMobile,
+    }),
+  ).buildAllPlatforms();
+
+  // Banking
+
+  // Banking Light Desktop
+  const bankingThemeLightDesktop = getThemeFromTokens({
+    onColorMode: 'onLight',
+    onDeviceType: 'onDesktop',
+    themeTokens: { ...styledDictionaryBankingTheme },
+  });
+
+  StyleDictionary.extend(
+    getStyledDictionaryConfig({
+      outputFileName: 'bankingThemeLightDesktop',
+      tokens: bankingThemeLightDesktop,
+    }),
+  ).buildAllPlatforms();
+
+  // Banking Dark Desktop
+  const bankingThemeDarkDesktop = getThemeFromTokens({
+    onColorMode: 'onDark',
+    onDeviceType: 'onDesktop',
+    themeTokens: { ...styledDictionaryBankingTheme },
+  });
+
+  StyleDictionary.extend(
+    getStyledDictionaryConfig({
+      outputFileName: 'bankingThemeDarkDesktop',
+      tokens: bankingThemeDarkDesktop,
+    }),
+  ).buildAllPlatforms();
+
+  // Banking Light Mobile
+  const bankingThemeLightMobile = getThemeFromTokens({
+    onColorMode: 'onLight',
+    onDeviceType: 'onMobile',
+    themeTokens: { ...styledDictionaryBankingTheme },
+  });
+
+  StyleDictionary.extend(
+    getStyledDictionaryConfig({
+      outputFileName: 'bankingThemeLightMobile',
+      tokens: bankingThemeLightMobile,
+    }),
+  ).buildAllPlatforms();
+
+  // Banking Dark Mobile
+  const bankingThemeDarkMobile = getThemeFromTokens({
+    onColorMode: 'onDark',
+    onDeviceType: 'onMobile',
+    themeTokens: { ...styledDictionaryBankingTheme },
+  });
+
+  StyleDictionary.extend(
+    getStyledDictionaryConfig({
+      outputFileName: 'bankingThemeDarkMobile',
+      tokens: bankingThemeDarkMobile,
     }),
   ).buildAllPlatforms();
 };
