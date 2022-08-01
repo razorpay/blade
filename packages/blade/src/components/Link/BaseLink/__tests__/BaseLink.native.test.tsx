7c7bf2450fb9931239bb728f030075819227bdd1
Anurag Hazra <anurag.hazra@razorpay.com>
2022-08-01 16:08:15 +0530

    refactor(blade): use aliased imports in link (#588)

--- packages/blade/src/components/Link/BaseLink/__tests__/BaseLink.native.test.tsx
+++ packages/blade/src/components/Link/BaseLink/__tests__/BaseLink.native.test.tsx
@@ -1,8 +1,9 @@
 import { fireEvent, waitFor } from '@testing-library/react-native';
 import { Linking } from 'react-native';
-import renderWithTheme from '../../../../_helpers/testing/renderWithTheme.native';
-import { InfoIcon } from '../../../Icons';
 import BaseLink from '../BaseLink';
+import renderWithTheme from '~src/_helpers/testing/renderWithTheme.native';
+import { InfoIcon } from '~components/Icons';
+
 jest.mock('react-native/Libraries/Linking/Linking', () => ({
   openURL: jest.fn(() => Promise.resolve()),
   canOpenURL: jest.fn(() => Promise.resolve(true)),
