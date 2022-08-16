7909d7cc9ad02b7036e454ed714886fb48fc9233
Anurag Hazra <anurag.hazra@razorpay.com>
2022-08-16 15:50:51 +0530

    fix(blade): Checkbox design changes  (#634)
    * fix(blade): checkbox spacing/height/paddings
    
    * fix: gutter between label
    
    * chore: update snapshots
    
    * Create clever-crews-smell.md

--- packages/blade/src/components/Form/FormLabel.tsx
+++ packages/blade/src/components/Form/FormLabel.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { VisuallyHidden } from '~components/VisuallyHidden';
 import { Text } from '~components/Typography';
 import { BaseText } from '~components/Typography/BaseText';
-import { getPlatformType, useBreakpoint } from '~utils';
+import { getPlatformType, makeSpace, useBreakpoint } from '~utils';
 import Box from '~components/Box';
 import { useTheme } from '~components/BladeProvider';
 
@@ -88,11 +88,11 @@ const FormLabel = ({
       flexWrap="wrap"
     >
       <BaseText
-        lineHeight="s"
+        lineHeight={position === 'left' ? 'l' : 's'}
         fontFamily="text"
         fontWeight="bold"
         color="surface.text.subtle.lowContrast"
-        fontSize={75}
+        fontSize={position === 'left' ? 100 : 75}
       >
         {children}
         {computedAccessibilityNode}
@@ -104,7 +104,11 @@ const FormLabel = ({
 
   // What harm can it do?
   if (isReactNative) {
-    return <Box marginBottom="spacing.1">{textNode}</Box>;
+    return (
+      <Box marginRight="spacing.4" marginBottom="spacing.1">
+        {textNode}
+      </Box>
+    );
   }
 
   const Component = as;
@@ -112,7 +116,11 @@ const FormLabel = ({
   const width = position === 'left' && isDesktop ? '120px' : 'auto';
 
   return (
-    <Component htmlFor={htmlFor} style={{ width }} id={id}>
+    <Component
+      htmlFor={htmlFor}
+      style={{ width, marginRight: makeSpace(theme.spacing[4]) }}
+      id={id}
+    >
       <Box marginBottom="spacing.1">{textNode}</Box>
     </Component>
   );
