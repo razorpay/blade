0146fe6d615733d61ad70dfb4af6505f5935feb5
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-08-17 16:48:54 +0530

    fix: button spinner layout (#641)

--- packages/blade/src/components/Box/Box.d.ts
+++ packages/blade/src/components/Box/Box.d.ts
@@ -9,6 +9,11 @@ export type BoxProps = {
   alignItems?: CSSObject['alignItems'];
   justifyContent?: CSSObject['justifyContent'];
   alignSelf?: CSSObject['alignSelf'];
+  position?: CSSObject['position'];
+  top?: CSSObject['top'];
+  right?: CSSObject['right'];
+  bottom?: CSSObject['bottom'];
+  left?: CSSObject['left'];
   paddingTop?: DotNotationSpacingStringToken | 'auto';
   paddingBottom?: DotNotationSpacingStringToken | 'auto';
   paddingLeft?: DotNotationSpacingStringToken | 'auto';
