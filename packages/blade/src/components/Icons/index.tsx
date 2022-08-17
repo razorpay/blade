7c125e1ce6cf4e838006230d93b336c348f3f723
Divyanshu Maithani <div.blackcat@gmail.com>
2022-08-17 15:24:26 +0530

    feat: add IconButton (internal) (#613)
    Adds internal IconButton for making clickable icons

--- packages/blade/src/components/Icons/index.tsx
+++ packages/blade/src/components/Icons/index.tsx
@@ -16,7 +16,10 @@ type ActionIconColors = `action.icon.${DotNotationColorStringToken<
 
 export type IconSize = 'large' | 'medium' | 'small' | 'xlarge' | 'xsmall' | 'xxsmall';
 export type IconProps = {
-  color: ActionIconColors | FeedbackIconColors | FeedbackActionIconColors;
+  /**
+   * Color token (not to be confused with actual hsla value)
+   */
+  color: ActionIconColors | FeedbackIconColors | FeedbackActionIconColors | 'currentColor'; // currentColor is useful for letting the SVG inherit color property from its container
   size: IconSize;
 };
 export type IconComponent = React.ComponentType<IconProps>;
