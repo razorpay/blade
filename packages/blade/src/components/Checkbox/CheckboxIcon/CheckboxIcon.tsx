f995e02bcfee0b916357dc0461d50dd8739965e5
Anurag Hazra <anurag.hazra@razorpay.com>
2022-08-23 18:09:44 +0530

    fix(blade): prevent dashed props in svg path (#658)
    * fix: prevented dashed props in svg path
    
    * chore: fix snapshot
    
    * chore: rename to NonDashedProps

--- packages/blade/src/components/Checkbox/CheckboxIcon/CheckboxIcon.tsx
+++ packages/blade/src/components/Checkbox/CheckboxIcon/CheckboxIcon.tsx
@@ -12,8 +12,8 @@ const CheckedIcon = ({ color }: { color: string }) => {
   return (
     <Svg width="16px" height="16px" viewBox="0 0 16 16" fill="none">
       <Path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M12.3536 4.64645C12.5488 4.84171 12.5488 5.15829 12.3536 5.35355L6.85355 10.8536C6.65829 11.0488 6.34171 11.0488 6.14645 10.8536L3.64645 8.35355C3.45118 8.15829 3.45118 7.84171 3.64645 7.64645C3.84171 7.45118 4.15829 7.45118 4.35355 7.64645L6.5 9.79289L11.6464 4.64645C11.8417 4.45118 12.1583 4.45118 12.3536 4.64645Z"
         fill={color}
       />
@@ -25,8 +25,8 @@ const IndeterminateIcon = ({ color }: { color: string }) => {
   return (
     <Svg width="16px" height="16px" viewBox="0 0 16 16" fill="none">
       <Path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M4 8C4 7.72386 4.22386 7.5 4.5 7.5H11.5C11.7761 7.5 12 7.72386 12 8C12 8.27614 11.7761 8.5 11.5 8.5H4.5C4.22386 8.5 4 8.27614 4 8Z"
         fill={color}
       />
