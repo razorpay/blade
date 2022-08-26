2001f5651eaa573255efd08ef0578cd2ee756119
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-08-26 19:30:56 +0530

    chore: animate without Keyframe for native

--- packages/blade/src/components/Input/BaseInput/BaseInputAnimatedBorder.native.tsx
+++ packages/blade/src/components/Input/BaseInput/BaseInputAnimatedBorder.native.tsx
@@ -1,7 +1,16 @@
 import styled from 'styled-components/native';
-import type { EasingFn } from 'react-native-reanimated';
-import Animated, { Keyframe } from 'react-native-reanimated';
+import Animated, {
+  EasingFn,
+  useAnimatedStyle,
+  useSharedValue,
+  Keyframe,
+  interpolate,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated';
+
 import type { ReactNode } from 'react';
+import { useEffect } from 'react';
 // import type { CSSObject } from 'styled-components';
 import { makeBorderSize } from '~utils';
 import { useTheme } from '~components/BladeProvider';
@@ -11,9 +20,8 @@ const BaseInputStyledAnimatedBorder = styled(Animated.View)(({ theme }) => ({
   position: 'absolute',
   bottom: 0,
   left: 0,
-  // right: 0,
-  opacity: 0,
-  width: 0,
+  right: 0,
+  opacity: 1,
   backgroundColor: theme.colors.brand.primary[500],
   height: makeBorderSize(theme.border.width.thin),
 }));
@@ -24,33 +32,74 @@ export const BaseInputAnimatedBorder = ({
   currentInteraction: keyof ActionStates;
 }): ReactNode => {
   const { theme } = useTheme();
-  const borderAnimationEasing = (theme.motion.easing.standard.effective as unknown) as EasingFn;
+  // const borderAnimationEasing = (theme.motion.easing.standard.effective as unknown) as EasingFn;
 
-  const scaleBorder = new Keyframe({
-    from: {
-      width: 0,
-      // right: -100,
-      opacity: 0,
-      easing: borderAnimationEasing,
-    },
-    to: {
-      width: 100,
-      // right: 0,
-      opacity: 1,
-      easing: borderAnimationEasing,
-    },
-  }).duration(theme.motion.duration.moderate);
+  // const scaleBorder = new Keyframe({
+  //   from: {
+  //     width: 0,
+  //     // right: -100,
+  //     opacity: 0,
+  //     easing: borderAnimationEasing,
+  //   },
+  //   to: {
+  //     width: 100,
+  //     // right: 0,
+  //     opacity: 1,
+  //     easing: borderAnimationEasing,
+  //   },
+  // }).duration(theme.motion.duration.moderate);
 
-  const fadeOutBorder = new Keyframe({
-    from: {
-      opacity: 1,
-      easing: borderAnimationEasing,
-    },
-    to: {
-      opacity: 0,
-      easing: borderAnimationEasing,
-    },
-  }).duration(theme.motion.duration.xquick);
+  // const fadeOutBorder = new Keyframe({
+  //   from: {
+  //     opacity: 1,
+  //     easing: borderAnimationEasing,
+  //   },
+  //   to: {
+  //     opacity: 0,
+  //     easing: borderAnimationEasing,
+  //   },
+  // }).duration(theme.motion.duration.xquick);
+
+  const widthTrigger = useSharedValue(0);
+  const opacityTrigger = useSharedValue(1);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      width: `${interpolate(widthTrigger.value, [0, 1], [0, 100], {})}%`,
+      opacity: interpolate(opacityTrigger.value, [0, 1], [0, 1], {}),
+    };
+  });
+
+  // setTimeout(() => {
+  //   widthTrigger.value = withTiming(1, {
+  //     duration: 2000,
+  //     easing: Easing.bezier(0.3, 0.0, 0.2, 1.0),
+  //   });
+  // }, 1000);
+
+  // setTimeout(() => {
+  //   opacityTrigger.value = withTiming(0, {
+  //     duration: 2000,
+  //     easing: Easing.bezier(0.3, 0.0, 0.2, 1.0),
+  //   });
+  // }, 5000);
+  useEffect(() => {
+    if (currentInteraction == 'focus') {
+      widthTrigger.value = withTiming(1, {
+        duration: 2000,
+        easing: Easing.bezier(0.3, 0.0, 0.2, 1.0),
+      });
+    } else {
+      opacityTrigger.value = withTiming(0, {
+        duration: 2000,
+        easing: Easing.bezier(0.3, 0.0, 0.2, 1.0),
+      });
+    }
+  }, [currentInteraction, opacityTrigger, widthTrigger]);
+  console.log(
+    '🚀 ~ file: BaseInputAnimatedBorder.native.tsx ~ line 99 ~ currentInteraction',
+    currentInteraction,
+  );
 
   // let borderAnimation = null;
   // if (currentInteraction === 'focus') {
@@ -60,6 +109,6 @@ export const BaseInputAnimatedBorder = ({
   // }
 
   return currentInteraction === 'focus' ? (
-    <BaseInputStyledAnimatedBorder entering={scaleBorder} exiting={fadeOutBorder} />
+    <BaseInputStyledAnimatedBorder style={animatedStyle} />
   ) : null;
 };
