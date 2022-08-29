783c0b65b4c34a8247185df8b3868af247182055
anuraghazra <anurag.hazra@razorpay.com>
2022-08-29 15:48:38 +0530

    chore: added Exact type example

--- packages/blade/src/components/Icons/_Svg/Path/Path.d.ts
+++ packages/blade/src/components/Icons/_Svg/Path/Path.d.ts
@@ -1,4 +1,20 @@
-// https://github.com/microsoft/TypeScript/issues/12936#issuecomment-368244671
+/**
+ * Exact<> type ensures that a generic function accepts exact props, no exccess props
+ * @see https://github.com/microsoft/TypeScript/issues/12936#issuecomment-368244671
+ * @example
+ *
+ * ```ts
+ * type UserOptions = { name: string; age: number };
+ *
+ * function makeUser<T extends UserOptions>(props: T) {}
+ * makeUser({ name: '', age: 1, nice: 2 });
+ * //                           ^ doesn't throw error
+ *
+ * function makeUserExact<T extends Exact<UserOptions, T>>(props: T) {}
+ * makeUserExact({ name: '', age: 1, nice: 2 });
+ * //                                ^ correctly throw error
+ * ```
+ */
 export type Exact<T, X extends T> = T &
   {
     [K in keyof X]: K extends keyof T ? X[K] : never;
