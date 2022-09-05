a402ef1de67ef29def305cfe3954f443b32f372b
Sujoy Saha <78787105+sujoys12@users.noreply.github.com>
2022-09-05 10:40:03 +0530

    feat(icons): refresh-left icon (#691)
    * feat(icons): refresh-left icon
    
    * Create spicy-shrimps-begin.md
    
    Co-authored-by: Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>

--- packages/blade/src/components/Icons/index.tsx
+++ packages/blade/src/components/Icons/index.tsx
@@ -71,3 +71,4 @@ export { default as TrashIcon } from './TrashIcon';
 export { default as AlertTriangleIcon } from './AlertTriangleIcon';
 export { default as AlertOctagonIcon } from './AlertOctagonIcon';
 export { default as CheckCircleIcon } from './CheckCircleIcon';
+export { default as RefreshLeftIcon } from './RefreshLeftIcon';
