0ea03576e26b95410e30ecaa298ef000a32aafd3
Saurabh Daware <saurabh.daware@razorpay.com>
2022-09-16 16:02:26 +0530

    feat: add dynamic variant-based props

--- packages/blade/src/components/Typography/Code/Code.tsx
+++ packages/blade/src/components/Typography/Code/Code.tsx
@@ -1,16 +1,50 @@
-import styled from 'styled-components';
+import type { CSSProperties } from 'styled-components';
 import { BaseText } from '../BaseText';
+import type { BaseTextProps as BaseTextPropsWithChildren } from '../BaseText';
+import { useTheme } from '~components/BladeProvider';
+import type { ThemeContext } from '~components/BladeProvider/useTheme';
+import { getPlatformType } from '~utils';
 
-const StyledCode = styled(BaseText)((props) => {
-  return {
-    backgroundColor: props.theme.colors.brand.gray[300],
-    color: props.theme.colors.surface.text.subtle.lowContrast,
-    padding: `0 ${props.theme.spacing[2]}`,
+// @TODO
+// - check comments
+// - Test on native
+
+type BaseTextProps = Omit<BaseTextPropsWithChildren, 'children'>;
+const getCodeStyles = (
+  codeProps: Omit<CodeProps, 'children'> & { theme: ThemeContext['theme'] },
+): BaseTextProps => {
+  const isPlatformWeb = getPlatformType() === 'browser' || getPlatformType() === 'node';
+
+  const baseTextProps: BaseTextProps = {
+    color: 'surface.text.subtle.lowContrast', // update as per type
+    fontFamily: 'code', // check fontFamily with saurav
+    fontSize: 75,
+    as: isPlatformWeb ? 'code' : undefined,
+    style: {
+      padding: `${codeProps.theme.spacing[1]}px ${codeProps.theme.spacing[3]}px`,
+      backgroundColor: codeProps.theme.colors.brand.gray[300],
+      borderRadius: `${codeProps.theme.border.radius.medium}px`,
+    },
   };
-});
 
-function Code({ children }: { children: string }): JSX.Element {
-  return <StyledCode as="code">{children}</StyledCode>;
+  if (codeProps.variant === 'large') {
+    baseTextProps.fontSize = 100;
+    (baseTextProps.style as CSSProperties).padding = `0px ${codeProps.theme.spacing[2]}px`;
+  }
+
+  return baseTextProps;
+};
+
+type CodeProps = {
+  children: string;
+  variant?: 'large' | 'medium';
+  // Add `type`
+};
+
+function Code({ children, variant = 'medium' }: CodeProps): JSX.Element {
+  const { theme } = useTheme();
+  const baseTextProps = getCodeStyles({ variant, theme });
+  return <BaseText {...baseTextProps}>{children}</BaseText>; // Use `<Text />` view on React Native
 }
 
 export { Code };
