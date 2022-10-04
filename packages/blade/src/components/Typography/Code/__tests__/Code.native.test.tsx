3033dac5b2f9ed89bf4fe13c8d3cc6e6b0c286e5
Saurabh Daware <saurabh.daware@razorpay.com>
2022-10-04 18:06:18 +0530

    tests: improve tests with specific style check

--- packages/blade/src/components/Typography/Code/__tests__/Code.native.test.tsx
+++ packages/blade/src/components/Typography/Code/__tests__/Code.native.test.tsx
@@ -5,17 +5,21 @@ import renderWithTheme from '~src/_helpers/testing/renderWithTheme.native';
 describe('<Code />', () => {
   it('should render Code with default properties', () => {
     const { getByText, toJSON } = renderWithTheme(<Code>TEST_TOKEN</Code>);
-    expect(getByText('TEST_TOKEN')).toBeTruthy();
+    getByText('TEST_TOKEN');
     expect(toJSON()).toMatchSnapshot();
   });
 
   it('should render large Code with different types', () => {
-    const { toJSON } = renderWithTheme(<Code size="small">SMALL</Code>);
+    const { getByText, toJSON } = renderWithTheme(<Code size="small">SMALL</Code>);
+    const renderedCode = getByText('SMALL');
+    expect(renderedCode).toHaveStyle({ fontSize: 12 });
     expect(toJSON()).toMatchSnapshot();
   });
 
   it('should render medium Code with different types', () => {
-    const { toJSON } = renderWithTheme(<Code size="medium">MEDIUM</Code>);
+    const { getByText, toJSON } = renderWithTheme(<Code size="medium">MEDIUM</Code>);
+    const renderedCode = getByText('MEDIUM');
+    expect(renderedCode).toHaveStyle({ fontSize: 15 });
     expect(toJSON()).toMatchSnapshot();
   });
 });
--- packages/blade/src/components/Typography/Code/__tests__/Code.web.test.tsx
+++ packages/blade/src/components/Typography/Code/__tests__/Code.web.test.tsx
@@ -4,17 +4,23 @@ import renderWithTheme from '~src/_helpers/testing/renderWithTheme.web';
 
 describe('<Code />', () => {
   it('should render Code with default properties', () => {
-    const { container } = renderWithTheme(<Code>TEST_TOKEN</Code>);
+    const { container, getByText } = renderWithTheme(<Code>TEST_TOKEN</Code>);
+    expect(getByText('TEST_TOKEN')).toHaveStyle('font-size: 0.75rem');
+    expect(container.querySelector('span')).toHaveStyle('padding: 0px 4px');
     expect(container).toMatchSnapshot();
   });
 
   it('should render small Code with different types', () => {
-    const { container } = renderWithTheme(<Code size="small">SMALL</Code>);
+    const { container, getByText } = renderWithTheme(<Code size="small">SMALL</Code>);
+    expect(getByText('SMALL')).toHaveStyle('font-size: 0.75rem');
+    expect(container.querySelector('span')).toHaveStyle('padding: 0px 4px');
     expect(container).toMatchSnapshot();
   });
 
   it('should render medium Code with different types', () => {
-    const { container } = renderWithTheme(<Code size="medium">MEDIUM</Code>);
+    const { container, getByText } = renderWithTheme(<Code size="medium">MEDIUM</Code>);
+    expect(getByText('MEDIUM')).toHaveStyle('font-size: 0.875rem');
+    expect(container.querySelector('span')).toHaveStyle('padding: 0px 4px');
     expect(container).toMatchSnapshot();
   });
 });
