513d7d251d575cb6f6497fb5ac983a9144186943
Saurabh Daware <saurabh.daware@razorpay.com>
2022-10-06 10:34:12 +0530

    refactor(Code/tests): use tokens instead of hard coded values

--- packages/blade/src/components/Typography/Code/__tests__/Code.native.test.tsx
+++ packages/blade/src/components/Typography/Code/__tests__/Code.native.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Code } from '../Code';
 import renderWithTheme from '~src/_helpers/testing/renderWithTheme.native';
+import typography from '~tokens/global/typography';
 
 describe('<Code />', () => {
   it('should render Code with default properties', () => {
@@ -9,17 +10,17 @@ describe('<Code />', () => {
     expect(toJSON()).toMatchSnapshot();
   });
 
-  it('should render large Code with different types', () => {
+  it('should render small Code', () => {
     const { getByText, toJSON } = renderWithTheme(<Code size="small">SMALL</Code>);
     const renderedCode = getByText('SMALL');
-    expect(renderedCode).toHaveStyle({ fontSize: 12 });
+    expect(renderedCode).toHaveStyle({ fontSize: typography.onMobile.fonts.size[75] });
     expect(toJSON()).toMatchSnapshot();
   });
 
-  it('should render medium Code with different types', () => {
+  it('should render medium Code', () => {
     const { getByText, toJSON } = renderWithTheme(<Code size="medium">MEDIUM</Code>);
     const renderedCode = getByText('MEDIUM');
-    expect(renderedCode).toHaveStyle({ fontSize: 15 });
+    expect(renderedCode).toHaveStyle({ fontSize: typography.onMobile.fonts.size[100] });
     expect(toJSON()).toMatchSnapshot();
   });
 });
--- packages/blade/src/components/Typography/Code/__tests__/Code.web.test.tsx
+++ packages/blade/src/components/Typography/Code/__tests__/Code.web.test.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import { Code } from '../Code';
 import renderWithTheme from '~src/_helpers/testing/renderWithTheme.web';
+import { makeSpace, makeTypographySize } from '~utils';
+import typography from '~tokens/global/typography';
+import spacing from '~tokens/global/spacing';
 
 describe('<Code />', () => {
   it('should render Code with default properties', () => {
     const { container, getByText } = renderWithTheme(<Code>TEST_TOKEN</Code>);
-    expect(getByText('TEST_TOKEN')).toHaveStyle('font-size: 0.75rem');
-    expect(container.querySelector('span')).toHaveStyle('padding: 0px 4px');
+    expect(getByText('TEST_TOKEN')).toHaveStyle(
+      `font-size: ${makeTypographySize(typography.onDesktop.fonts.size[75])}`,
+    );
+    expect(container.querySelector('span')).toHaveStyle(
+      `padding: ${makeSpace(spacing[0])} ${makeSpace(spacing[2])}`,
+    );
     expect(container).toMatchSnapshot();
   });
 
-  it('should render small Code with different types', () => {
+  it('should render small Code', () => {
     const { container, getByText } = renderWithTheme(<Code size="small">SMALL</Code>);
-    expect(getByText('SMALL')).toHaveStyle('font-size: 0.75rem');
-    expect(container.querySelector('span')).toHaveStyle('padding: 0px 4px');
+    expect(getByText('SMALL')).toHaveStyle(
+      `font-size: ${makeTypographySize(typography.onDesktop.fonts.size[75])}`,
+    );
     expect(container).toMatchSnapshot();
   });
 
-  it('should render medium Code with different types', () => {
+  it('should render medium Code', () => {
     const { container, getByText } = renderWithTheme(<Code size="medium">MEDIUM</Code>);
-    expect(getByText('MEDIUM')).toHaveStyle('font-size: 0.875rem');
-    expect(container.querySelector('span')).toHaveStyle('padding: 0px 4px');
+    expect(getByText('MEDIUM')).toHaveStyle(
+      `font-size: ${makeTypographySize(typography.onDesktop.fonts.size[100])}`,
+    );
     expect(container).toMatchSnapshot();
   });
 });
