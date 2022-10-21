72fe81d22735e7e73244de1f11e175986d770d92
Saurabh Daware <saurabh.daware@razorpay.com>
2022-10-21 10:53:15 +0530

    docs: add comment with context about the approach

--- packages/blade/scripts/publish-to-npm.js
+++ packages/blade/scripts/publish-to-npm.js
@@ -1,3 +1,20 @@
+/**
+ * Context about this approach:
+ *
+ * Q. Why not use `npm publish --registry https://registry.npmjs.org` instead of creating .npmrc?
+ *
+ * -> It doesn't support any clean way of providing access token and only works for already logged in users
+ *
+ * Q. Why not change actual `.npmrc`, drop github package registry and move to NPM only?
+ *
+ * -> `.npmrc` doesn't support multiple registries on one scope.
+ *  So for our internal users who have `@razorpay` scope pointing to github package registry, we have to keep
+ *  publishing on github package registry. We can do this when we move all `@razorpay` scope packages (like universe) to NPM.
+ *
+ * Checkout description of this PR for more context -> https://github.com/razorpay/blade/pull/757
+ *
+ */
+
 const fs = require('fs');
 const path = require('path');
 const execa = require('execa');
