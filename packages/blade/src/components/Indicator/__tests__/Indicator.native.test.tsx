e0a995fdbf57ec768c407afed8d4367c0d4c1cac
Divyanshu Maithani <div.blackcat@gmail.com>
2022-11-03 22:51:02 +0530

    tests(Indicator): add web and native tests

---
+++ packages/blade/src/components/Indicator/__tests__/Indicator.native.test.tsx
@@ -0,0 +1,39 @@
+import { Indicator } from '..';
+import renderWithTheme from '~src/_helpers/testing/renderWithTheme.native';
+
+describe('<Indicator />', () => {
+  it('should render', () => {
+    const { toJSON } = renderWithTheme(<Indicator accessibilityLabel="Success" />);
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should render different variants for size and intent', () => {
+    const { toJSON } = renderWithTheme(
+      <Indicator accessibilityLabel="Warning" size="small" intent="notice" />,
+    );
+
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  it('should render text label', () => {
+    const label = 'Active';
+    const { getByText } = renderWithTheme(<Indicator>{label}</Indicator>);
+
+    getByText(label);
+  });
+
+  it('should have a11y label when text label is passed', () => {
+    const label = 'Active';
+    const { getByLabelText } = renderWithTheme(<Indicator>{label}</Indicator>);
+
+    getByLabelText(label);
+  });
+
+  it('should have role and a11y label when no text label is passed', () => {
+    const a11yLabel = 'Active';
+    const { getByLabelText } = renderWithTheme(<Indicator accessibilityLabel={a11yLabel} />);
+
+    getByLabelText(a11yLabel);
+  });
+});
---
+++ packages/blade/src/components/Indicator/__tests__/Indicator.web.test.tsx
@@ -0,0 +1,48 @@
+import { Indicator } from '..';
+import renderWithTheme from '~src/_helpers/testing/renderWithTheme.web';
+import assertAccessible from '~src/_helpers/testing/assertAccessible.web';
+
+describe('<Indicator />', () => {
+  it('should render', () => {
+    const { container } = renderWithTheme(<Indicator accessibilityLabel="Success" />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render different variants for size and intent', () => {
+    const { container } = renderWithTheme(
+      <Indicator accessibilityLabel="Warning" size="small" intent="notice" />,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render text label', () => {
+    const label = 'Active';
+    const { getByText } = renderWithTheme(<Indicator>{label}</Indicator>);
+
+    getByText(label);
+  });
+
+  it('should have role and a11y label when text label is passed', async () => {
+    const label = 'Active';
+    const { getByRole, getByLabelText } = renderWithTheme(<Indicator>{label}</Indicator>);
+
+    getByRole('status');
+    const indicator = getByLabelText(label);
+
+    await assertAccessible(indicator);
+  });
+
+  it('should have role and a11y label when no text label is passed', async () => {
+    const a11yLabel = 'Active';
+    const { getByRole, getByLabelText } = renderWithTheme(
+      <Indicator accessibilityLabel={a11yLabel} />,
+    );
+
+    getByRole('status');
+    const indicator = getByLabelText(a11yLabel);
+
+    await assertAccessible(indicator);
+  });
+});
