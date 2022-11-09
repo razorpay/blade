bcf9e1c641bcf6b850072b07f97d937536f4075d
Saurabh Daware <saurabh.daware@razorpay.com>
2022-11-09 14:42:38 +0530

    fix(Icon): add Icon usage example

--- packages/blade/src/components/Icons/Icons.stories.tsx
+++ packages/blade/src/components/Icons/Icons.stories.tsx
@@ -1,11 +1,10 @@
 import type { ComponentType, ReactElement } from 'react';
 import type { ComponentStory, Meta } from '@storybook/react';
-import { Title } from '@storybook/addon-docs';
-import { Highlight } from '@storybook/design-system';
 import iconMap from './iconMap';
 import type { IconProps } from '.';
 import { CreditCardIcon } from '.';
 import StoryPageWrapper from '~src/_helpers/storybook/StoryPageWrapper';
+import Sandbox from '~src/_helpers/storybook/Sandbox';
 
 const Page = (): ReactElement => {
   return (
@@ -13,6 +12,7 @@ const Page = (): ReactElement => {
       componentDescription="We provide a bunch of icons out-of-the-box for Blade in 6 different sizes. You can choose which size & color fits the best for your use case using the color & size props."
       componentName="Icon"
       note="For now, we have added only a few icons but you can contribute to Blade by adding more icons that are available on the Figma board as and when a use-case arises"
+      imports={`// Replace IconName with actual Icon's name that you would like to use \nimport { IconName } from '@razorpay/blade/components' \n// IconProps are generic Icon props for all icons, don't replace it with your IconName \nimport type { IconProps } from '@razorpay/blade/components'`}
       figmaURL={{
         paymentTheme:
           'https://www.figma.com/file/jubmQL9Z8V7881ayUD95ps/Blade---Payment-Light?node-id=59%3A177',
@@ -20,8 +20,26 @@ const Page = (): ReactElement => {
           'https://www.figma.com/file/sAdplk2uYnI2ILnDKUxycW/Blade---Banking-Dark?node-id=9308%3A64839',
       }}
     >
-      <Title>Usage</Title>
-      <Highlight language="tsx">{`// Replace IconName with actual Icon's name that you would like to use \nimport { IconName } from '@razorpay/blade/components' \n// IconProps are generic Icon props for all icons, don't replace it with your IconName \nimport type { IconProps } from '@razorpay/blade/components'`}</Highlight>
+      <Sandbox>
+        {`
+        import { Button, ArrowRightIcon } from '@razorpay/blade/components';
+
+        function App(): JSX.Element {
+          // Icon component is meant to be used inside \`icon\` prop 
+          // along with other components like \`Button\`, \`Badge\`, etc
+          return (
+            <Button 
+              icon={ArrowRightIcon}
+              iconPosition="right"
+            >
+              Button with Icon
+            </Button>
+          )
+        }
+
+        export default App;
+        `}
+      </Sandbox>
     </StoryPageWrapper>
   );
 };
