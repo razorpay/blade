6ce3927c86049a57741eb81fd72c5f0cb82563e4
Saurabh Daware <saurabh.daware@razorpay.com>
2022-11-16 12:44:26 +0530

    debug: log env variable for testing

--- packages/blade/src/_helpers/storybook/Sandbox/Sandbox.web.tsx
+++ packages/blade/src/_helpers/storybook/Sandbox/Sandbox.web.tsx
@@ -14,6 +14,8 @@ export type SandboxProps = {
   editorWidthPercentage?: number;
 };
 
+console.log('ENVVVV', { SHA: process.env.GITHUB_SHA });
+
 function Sandbox({
   children,
   language = 'tsx',
