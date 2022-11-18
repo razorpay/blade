f648caca9f3619286421c0617c52082306385a94
Saurabh Daware <saurabh.daware@razorpay.com>
2022-11-18 14:40:58 +0530

    feat: add italic fonts

--- examples/basic/src/App.tsx
+++ examples/basic/src/App.tsx
@@ -1,7 +1,9 @@
 import { BladeProvider, Button } from '@razorpay/blade/components';
 import { paymentTheme } from '@razorpay/blade/tokens';
 import '@fontsource/lato/400.css';
+import '@fontsource/lato/400-italic.css';
 import '@fontsource/lato/700.css';
+import '@fontsource/lato/700-italic.css';
 
 function App(): JSX.Element {
   return (
