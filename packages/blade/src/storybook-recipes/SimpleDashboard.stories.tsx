c3f0342f88a59d772d14f6e177835b03f5ef4ebe
Saurabh Daware <saurabh.daware@razorpay.com>
2022-11-21 17:59:45 +0530

    feat(storybook): add dashboard recipe (#853)
    * feat: 2nd attempt to add recipes
    
    * feat: use local sandbox in ci
    
    * fix: ci path
    
    * feat: add basic recipe story
    
    * refactor: create makeRecipe wrapper
    
    * feat: fallback on react native
    
    * fix: rn typescript error
    
    * feat: remove recipe builds and add codesandbox embeds
    
    * feat: hide all panels in recipe stories
    
    * fix: story for ios
    
    * feat: rename file, fix editor height and width
    
    * feat: change viewMode
    
    * feat: add italic fonts
    
    * feat: disable snapshots on recipes
    
    * feat: add dashboard recipe

--- packages/blade/src/storybook-recipes/SimpleDashboard.stories.tsx
+++ packages/blade/src/storybook-recipes/SimpleDashboard.stories.tsx
@@ -2,18 +2,19 @@ import type { Meta } from '@storybook/react';
 import { RecipeSandbox } from '~src/_helpers/storybook/Sandbox';
 import parameters from '~src/_helpers/storybook/recipeParameters';
 
-export function SimpleForm(): JSX.Element {
+export function SimpleDashboard(): JSX.Element {
   return (
     <RecipeSandbox
-      title="Blade Form"
-      codesandboxId="blade-form-7holu5"
-      activeFile="/src/Form.tsx"
+      title="Blade Dashboard"
+      codesandboxId="blade-dashboard-uyijy3"
+      activeFile="/src/Dashboard.tsx"
+      view="preview"
     />
   );
 }
 
 export default {
-  title: 'Recipes/Simple Form',
-  component: SimpleForm,
+  title: 'Recipes/Simple Dashboard',
+  component: SimpleDashboard,
   parameters,
 } as Meta;
