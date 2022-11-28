c93152a2963a58b932106bf89a54f6c84d1937be
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-11-28 17:12:20 +0530

    add basic progress structure

--- packages/blade/src/components/ProgressBar/ProgressBar.tsx
+++ packages/blade/src/components/ProgressBar/ProgressBar.tsx
@@ -1,6 +1,9 @@
 import type { ReactElement } from 'react';
+import styled from 'styled-components';
+import { useTheme } from '..';
 import Box from '~components/Box';
 import type { ColorContrastTypes, Feedback } from '~tokens/theme/theme';
+import { makeSize } from '~utils';
 
 type ProgressBarProps = {
   accessibilityLabel?: string;
@@ -9,19 +12,44 @@ type ProgressBarProps = {
   isIndeterminate?: boolean;
   label?: string;
   showPercentage?: boolean;
-  size?: 'small' | 'large';
+  size?: 'small' | 'medium';
   value?: number;
+};
+
+type ProgressBarUnfilledProps = {
+  backgroundColor: string;
+};
+
+const ProgressBarUnfilled = styled(Box)<ProgressBarUnfilledProps>(({ backgroundColor }) => ({
+  backgroundColor,
+}));
+
+type ProgressBarFilledProps = {
+  backgroundColor: string;
+};
+
+const ProgressBarFilled = styled(Box)<ProgressBarFilledProps>(({ backgroundColor }) => ({
+  backgroundColor,
+}));
+
+const getProgress = (value: number): number => {
+  return Math.floor(Math.min(100, Math.max(0, value)));
+};
+
+const progressBarHeight: Record<NonNullable<ProgressBarProps['size']>, 2 | 4> = {
+  small: 2,
+  medium: 4,
 };
 
 const ProgressBar = ({
   accessibilityLabel,
-  contrast,
+  contrast = 'low',
   intent,
   isIndeterminate,
   label,
   showPercentage,
-  size,
-  value,
+  size = 'small',
+  value = 0,
 }: ProgressBarProps): ReactElement => {
   console.log(
     accessibilityLabel,
@@ -33,9 +61,24 @@ const ProgressBar = ({
     size,
     value,
   );
+
+  const { theme } = useTheme();
+  const unfilledBackgroundColor = theme.colors.brand.gray.a100[`${contrast}Contrast`];
+  const filledBackgroundColor = theme.colors.brand.primary[500];
+
   return (
     <Box>
-      <Box />
+      <ProgressBarUnfilled
+        backgroundColor={unfilledBackgroundColor}
+        height={makeSize(progressBarHeight[size])}
+        flex={1}
+      >
+        <ProgressBarFilled
+          backgroundColor={filledBackgroundColor}
+          height="100%"
+          width={`${getProgress(value)}%`}
+        />
+      </ProgressBarUnfilled>
     </Box>
   );
 };
