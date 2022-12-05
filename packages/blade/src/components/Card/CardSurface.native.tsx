605c0311cc09554e90bba67472c4fabef305b16c
anuraghazra <anurag.hazra@razorpay.com>
2022-12-05 17:32:29 +0530

    chore: added shadows in card surface

--- packages/blade/src/components/Card/CardSurface.native.tsx
+++ packages/blade/src/components/Card/CardSurface.native.tsx
@@ -1,13 +1,24 @@
+import React from 'react';
 import styled from 'styled-components/native';
+import { useTheme } from '~components/BladeProvider';
 import Box from '~components/Box';
 
-const CardSurface = styled(Box)(() => {
+const CardSurfaceStyled = styled(Box)<{ elevation: number }>(({ theme }) => {
   return {
-    shadowOpacity: 1,
-    shadowRadius: '5px',
-    shadowColor: 'blue',
-    shadowOffset: '0px 0px',
+    shadowOpacity: '1',
+    shadowRadius: theme.shadows.blurRadius.level[1],
+    shadowColor: theme.shadows.color.level[1],
+    shadowOffset: `0px ${theme.shadows.offsetY.level[1]}px`,
   };
 });
+
+const CardSurface = ({ children }: { children: React.ReactNode }): React.ReactElement => {
+  const { theme } = useTheme();
+  return (
+    <CardSurfaceStyled elevation={theme.shadows.androidElevation.level[1]}>
+      {children}
+    </CardSurfaceStyled>
+  );
+};
 
 export { CardSurface };
--- packages/blade/src/components/Card/CardSurface.web.tsx
+++ packages/blade/src/components/Card/CardSurface.web.tsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 import Box from '~components/Box';
 
-const CardSurface = styled(Box)(() => {
+const CardSurface = styled(Box)(({ theme }) => {
+  const offsetX = theme.shadows.offsetX.level[1];
+  const offsetY = theme.shadows.offsetY.level[1];
+  const blur = theme.shadows.blurRadius.level[1];
+  const shadowColor = theme.shadows.color.level[1];
+
+  const shadow1 = `${offsetX}px ${offsetY}px ${blur}px 0px ${shadowColor}`;
+  const shadow2 = `0px 0px 1px 0px ${shadowColor}`;
+
   return {
-    boxShadow: '0px 1px 2px 0 blue',
+    boxShadow: `${shadow1}, ${shadow2}`,
   };
 });
 
