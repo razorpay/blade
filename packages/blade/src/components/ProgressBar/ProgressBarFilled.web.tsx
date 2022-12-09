c341a040cc24d283be7dd3d9ca8d0f794d2afd64
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-12-09 15:43:06 +0530

    update secondary animation

--- packages/blade/src/components/ProgressBar/ProgressBarFilled.web.tsx
+++ packages/blade/src/components/ProgressBar/ProgressBarFilled.web.tsx
@@ -1,43 +1,81 @@
 import styled, { css, keyframes } from 'styled-components';
+import React from 'react';
 import type { ProgressBarFilledProps } from './types';
 import Box from '~components/Box';
 import { castWebType, getIn, makeMotionTime } from '~utils';
 
 const pulse = keyframes`
   0% {
-    opacity: 1;
+    opacity: 0;
   }
   50% {
-    opacity: 0.65;
+    opacity: 0.25;
   }
   100% {
-    opacity: 1;
+    opacity: 0;
   }
 `;
 
-const ProgressBarFilledWithFillAnimation = styled(Box)<ProgressBarFilledProps>(
-  ({ backgroundColor, theme, progress, fillMotionDuration, motionEasing }) => ({
-    backgroundColor,
-    height: '100%',
-    width: `${progress}%`,
-    transitionProperty: 'width',
-    transitionDuration: castWebType(makeMotionTime(getIn(theme.motion, fillMotionDuration))),
-    transitionTimingFunction: castWebType(getIn(theme.motion, motionEasing)),
-  }),
+const BoxWithProgressBarFilledTransition = styled(Box)<
+  Pick<ProgressBarFilledProps, 'fillMotionDuration' | 'motionEasing'>
+>(({ theme, fillMotionDuration, motionEasing }) => ({
+  transitionProperty: 'width',
+  transitionDuration: castWebType(makeMotionTime(getIn(theme.motion, fillMotionDuration))),
+  transitionTimingFunction: castWebType(getIn(theme.motion, motionEasing)),
+}));
+
+const ProgressBarFilledContainer = styled(BoxWithProgressBarFilledTransition)<
+  Pick<ProgressBarFilledProps, 'backgroundColor' | 'progress'>
+>(({ backgroundColor, progress }) => ({
+  backgroundColor,
+  height: '100%',
+  width: `${progress}%`,
+}));
+
+const ProgressBarPulseAnimation = styled(BoxWithProgressBarFilledTransition)<
+  Pick<
+    ProgressBarFilledProps,
+    'pulseMotionDuration' | 'pulseMotionDelay' | 'motionEasing' | 'variant'
+  >
+>(({ theme, pulseMotionDuration, pulseMotionDelay, motionEasing, variant }) =>
+  variant === 'progress'
+    ? css`
+        height: 100%;
+        width: 100%;
+        opacity: 0;
+        background-color: white;
+        animation: ${pulse} ${castWebType(makeMotionTime(getIn(theme.motion, pulseMotionDuration)))}
+          ${castWebType(getIn(theme.motion, motionEasing))} infinite;
+        animation-delay: ${castWebType(makeMotionTime(getIn(theme.motion, pulseMotionDelay)))};
+      `
+    : '',
 );
 
-const ProgressBarFilledWithPulseAnimation = styled(
-  ProgressBarFilledWithFillAnimation,
-)<ProgressBarFilledProps>(
-  ({ theme, pulseMotionDuration, pulseMotionDelay, motionEasing, variant }) =>
-    variant === 'progress'
-      ? css`
-          animation: ${pulse}
-            ${castWebType(makeMotionTime(getIn(theme.motion, pulseMotionDuration)))}
-            ${castWebType(getIn(theme.motion, motionEasing))} infinite;
-          animation-delay: ${castWebType(makeMotionTime(getIn(theme.motion, pulseMotionDelay)))};
-        `
-      : '',
-);
+const ProgressBarFilled = ({
+  backgroundColor,
+  fillMotionDuration,
+  motionEasing,
+  progress,
+  pulseMotionDelay,
+  pulseMotionDuration,
+  variant,
+}: ProgressBarFilledProps): React.ReactElement => {
+  return (
+    <ProgressBarFilledContainer
+      backgroundColor={backgroundColor}
+      fillMotionDuration={fillMotionDuration}
+      motionEasing={motionEasing}
+      progress={progress}
+    >
+      <ProgressBarPulseAnimation
+        fillMotionDuration={fillMotionDuration}
+        motionEasing={motionEasing}
+        variant={variant}
+        pulseMotionDelay={pulseMotionDelay}
+        pulseMotionDuration={pulseMotionDuration}
+      />
+    </ProgressBarFilledContainer>
+  );
+};
 
-export { ProgressBarFilledWithPulseAnimation as ProgressBarFilled };
+export { ProgressBarFilled };
