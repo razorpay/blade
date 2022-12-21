e4a60d8ae19851af4c31ee3b35b5ba340c1faf4c
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2022-12-21 11:21:18 +0530

    fix story

--- packages/blade/src/components/ProgressBar/ProgressBar.stories.tsx
+++ packages/blade/src/components/ProgressBar/ProgressBar.stories.tsx
@@ -57,6 +57,15 @@ const ProgressBarTemplate: ComponentStory<typeof ProgressBarComponent> = ({ ...a
   return <ProgressBarComponent {...args} />;
 };
 
+export const Default = ProgressBarTemplate.bind({});
+// Need to do this because of storybook's weird naming convention, More details here: https://storybook.js.org/docs/react/writing-stories/naming-components-and-hierarchy#single-story-hoisting
+Default.storyName = 'Default';
+Default.args = {
+  label: 'Label',
+  value: 20,
+  contrast: 'low',
+};
+
 const ProgressBarWithUpdatingValuesTemplate: ComponentStory<typeof ProgressBarComponent> = ({
   ...args
 }) => {
@@ -76,16 +85,6 @@ const ProgressBarWithUpdatingValuesTemplate: ComponentStory<typeof ProgressBarCo
   }, [value]);
 
   return <ProgressBarComponent {...args} value={value} />;
-};
-
-export const Default = ProgressBarWithUpdatingValuesTemplate.bind({});
-// Need to do this because of storybook's weird naming convention, More details here: https://storybook.js.org/docs/react/writing-stories/naming-components-and-hierarchy#single-story-hoisting
-Default.storyName = 'Default';
-Default.args = {
-  label: 'Label',
-  value: 20,
-  contrast: 'low',
-  isIndeterminate: true,
 };
 
 const ProgressBarWithIntentsTemplate: ComponentStory<typeof ProgressBarComponent> = ({
