44f90f1812d5f0d443dac8465407df3c9841ed2e
kamleshchandnani <kamlesh.chandnani@gmail.com>
2022-12-27 14:16:51 +0530

    test: add mock for not showing expected errors on console

--- packages/blade/src/components/Alert/__tests__/Alert.ssr.test.tsx
+++ packages/blade/src/components/Alert/__tests__/Alert.ssr.test.tsx
@@ -2,6 +2,9 @@
 import { Alert } from '../';
 import renderWithSSR from '~src/_helpers/testing/renderWithSSR.web';
 
+beforeAll(() => jest.spyOn(console, 'error').mockImplementation());
+afterAll(() => jest.restoreAllMocks());
+
 describe('<Alert />', () => {
   it('should render Alert on server', () => {
     const { container } = renderWithSSR(
