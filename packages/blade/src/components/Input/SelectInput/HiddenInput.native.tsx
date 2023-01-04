7ce5df2d3f7405eeeffb8e229c540ccc10d84122
Saurabh Daware <saurabh.daware@razorpay.com>
2023-01-04 14:46:10 +0530

    fix: typecheck for native

--- packages/blade/src/components/Input/SelectInput/HiddenInput.native.tsx
+++ packages/blade/src/components/Input/SelectInput/HiddenInput.native.tsx
@@ -1,4 +1,6 @@
 // We don't need HiddenInput in React Native so we render nothing
-const HiddenInput = (): null => null;
+// Giving any to avoid typecheck errors on react native
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const HiddenInput = (_props: any): null => null;
 
 export { HiddenInput };
