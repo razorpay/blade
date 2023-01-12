322f41c3e4fd10f0b175e9c559d5804396ed865c
anuraghazra <anurag.hazra@razorpay.com>
2023-01-12 13:55:20 +0530

    chore: remove uneeded props

--- packages/blade/src/hooks/useExposeInputRef.ts
+++ packages/blade/src/hooks/useExposeInputRef.ts
@@ -1,9 +1,7 @@
 import React from 'react';
 import type { TextInput as TextInputReactNative } from 'react-native';
 
-type NativeHTMLInputRefProps = Omit<HTMLInputElement, keyof HTMLElement> & {
-  focus: HTMLElement['focus'];
-};
+type NativeHTMLInputRefProps = Pick<HTMLElement, 'focus' | 'scrollIntoView'>;
 
 /**
  * A hook which only exposes the properties of html input element via imparative hook
@@ -23,59 +21,7 @@ const useExposeInputRef = (
       };
       return {
         focus: (opts) => input.focus(opts),
-        accept: input.accept,
-        dirName: input.dirName,
-        capture: input.capture,
-        checked: input.checked,
-        defaultChecked: input.defaultChecked,
-        disabled: input.disabled,
-        readOnly: input.readOnly,
-        required: input.required,
-        formTarget: input.formTarget,
-        formAction: input.formAction,
-        formEnctype: input.formEnctype,
-        formMethod: input.formMethod,
-        formNoValidate: input.formNoValidate,
-        indeterminate: input.indeterminate,
-        list: input.list,
-        min: input.min,
-        max: input.max,
-        minLength: input.minLength,
-        maxLength: input.maxLength,
-        multiple: input.multiple,
-        pattern: input.pattern,
-        name: input.name,
-        placeholder: input.placeholder,
-        selectionDirection: input.selectionDirection,
-        selectionEnd: input.selectionEnd,
-        selectionStart: input.selectionStart,
-        selectedOptions: input.selectedOptions,
-        step: input.step,
-        type: input.type,
-        validationMessage: input.validationMessage,
-        validity: input.validity,
-        willValidate: input.willValidate,
-        value: input.value,
-        defaultValue: input.defaultValue,
-        valueAsDate: input.valueAsDate,
-        valueAsNumber: input.valueAsNumber,
-        checkValidity: () => input.checkValidity(),
-        reportValidity: () => input.reportValidity(),
-        select: () => input.select(),
-        setCustomValidity: (err) => input.setCustomValidity(err),
-        // @ts-expect-error args are incompatible because of an overload
-        setRangeText: (replacement, start, end, selectionMode) =>
-          input.setRangeText(replacement, start, end, selectionMode),
-        setSelectionRange: (start, end, direction) =>
-          input.setSelectionRange(start, end, direction),
-        showPicker: () => input.showPicker(),
-        stepDown: (n) => input.stepDown(n),
-        stepUp: (n) => input.stepUp(n),
-        // @ts-expect-error args are any because of an overload
-        addEventListener: (type, listener, opts) => input.addEventListener(type, listener, opts),
-        // @ts-expect-error args are any because of an overload
-        removeEventListener: (type, listener, opts) =>
-          input.removeEventListener(type, listener, opts),
+        scrollIntoView: (opts) => input.scrollIntoView(opts),
       };
     },
     [inputRef],
