99500b5f5966b5623b463a5daa937a95034020b8
Saurabh Daware <saurabh.daware@razorpay.com>
2023-01-25 18:07:16 +0530

    feat: add multiselect tests

--- packages/blade/src/components/Dropdown/__tests__/Dropdown.web.test.tsx
+++ packages/blade/src/components/Dropdown/__tests__/Dropdown.web.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Dropdown, DropdownOverlay } from '../index';
 import renderWithTheme from '~src/_helpers/testing/renderWithTheme.web';
 import { SelectInput } from '~components/Input/SelectInput/SelectInput';
-import { ActionList, ActionListItem } from '~components/ActionList';
+import { ActionList, ActionListFooter, ActionListItem } from '~components/ActionList';
+import { Button } from '~components/Button';
 
 describe('<Dropdown />', () => {
   it('should render dropdown and make it visible on click', async () => {
@@ -60,6 +60,38 @@ describe('<Dropdown />', () => {
     await user.click(getByRole('option', { name: 'Orange' }));
     expect(container.querySelector('[role=listbox]')).not.toBeVisible();
     expect(selectInput.textContent).toBe('Orange');
+  });
+
+  it('should handle accessibility of multiselect', async () => {
+    const user = userEvent.setup();
+    const { container, getByRole } = renderWithTheme(
+      <Dropdown selectionType="multiple">
+        <SelectInput label="Fruits" />
+        <DropdownOverlay>
+          <ActionList>
+            <ActionListItem title="Apple" value="mingo" />
+            <ActionListItem title="Mango" value="mango" />
+          </ActionList>
+        </DropdownOverlay>
+      </Dropdown>,
+    );
+
+    const selectInput = getByRole('combobox', { name: 'Fruits' });
+
+    expect(selectInput).toBeInTheDocument();
+    expect(container.querySelector('[role=listbox]')).not.toBeVisible();
+
+    await user.click(selectInput);
+    expect(getByRole('listbox', { name: 'Fruits' })).toBeVisible();
+    expect(getByRole('option', { name: 'Apple' }).getAttribute('aria-selected')).toBe('false');
+
+    await user.click(getByRole('option', { name: 'Apple' }));
+    expect(selectInput.textContent).toBe('Apple');
+    expect(getByRole('option', { name: 'Apple' }).getAttribute('aria-selected')).toBe('true');
+
+    await user.click(getByRole('option', { name: 'Mango' }));
+    expect(selectInput.textContent).toBe('Apple, Mango');
+    expect(getByRole('option', { name: 'Mango' }).getAttribute('aria-selected')).toBe('true');
   });
 
   // KEYBOARD ACCESSIBILITY TESTS
@@ -130,8 +162,65 @@ describe('<Dropdown />', () => {
     expect(getActiveDescendant()).toBe('Mango');
 
     // Select option
-    await user.keyboard('{ }');
+    await user.keyboard('[Space]');
     expect(selectInput.textContent).toBe('Mango');
+  });
+
+  it('should move focus between items with arrow key in multiselect', async () => {
+    const user = userEvent.setup();
+    const { container, getByRole } = renderWithTheme(
+      <Dropdown selectionType="multiple">
+        <SelectInput label="Fruits" />
+        <DropdownOverlay>
+          <ActionList>
+            <ActionListItem title="Apple" value="apple" />
+            <ActionListItem title="Mango" value="mango" />
+            <ActionListItem title="Orange" value="orange" />
+          </ActionList>
+        </DropdownOverlay>
+      </Dropdown>,
+    );
+
+    const selectInput = getByRole('combobox', { name: 'Fruits' });
+
+    expect(selectInput).toBeInTheDocument();
+    expect(selectInput.textContent).toBe('Select Option');
+    expect(container.querySelector('[role=listbox]')).not.toBeVisible();
+
+    const getActiveDescendant = (): string | null | undefined => {
+      const activeDescendantId = selectInput.getAttribute('aria-activedescendant');
+      const activeDescendantElement = container.querySelector(`#${activeDescendantId}`);
+      return activeDescendantElement?.textContent;
+    };
+
+    // Dropdown open
+    selectInput.focus();
+    await user.keyboard('{ArrowDown}');
+    expect(getByRole('listbox', { name: 'Fruits' })).toBeVisible();
+
+    // Move to first item
+    await user.keyboard('{ArrowDown}');
+    expect(getActiveDescendant()).toBe('Apple');
+
+    // Move to second item and select
+    await user.keyboard('{ArrowDown}');
+    expect(getActiveDescendant()).toBe('Mango');
+    await user.keyboard('[Space]');
+
+    expect(selectInput.textContent).toBe('Mango');
+
+    // Ensure menu did not close
+    expect(getByRole('listbox', { name: 'Fruits' })).toBeVisible();
+
+    // Move to third item and select
+    await user.keyboard('{ArrowDown}');
+    expect(getActiveDescendant()).toBe('Orange');
+    await user.keyboard('[Space]');
+
+    expect(selectInput.textContent).toBe('Mango, Orange');
+    expect(getByRole('option', { name: 'Apple' }).getAttribute('aria-selected')).toBe('false');
+    expect(getByRole('option', { name: 'Mango' }).getAttribute('aria-selected')).toBe('true');
+    expect(getByRole('option', { name: 'Orange' }).getAttribute('aria-selected')).toBe('true');
   });
 
   it('should work with type ahead', async () => {
@@ -157,7 +246,7 @@ describe('<Dropdown />', () => {
     // Dropdown Open and Jump to option starting with "M"
     selectInput.focus();
     await user.keyboard('m');
-    await waitFor(() => expect(getByRole('listbox', { name: 'Fruits' })).toBeVisible());
+    expect(getByRole('listbox', { name: 'Fruits' })).toBeVisible();
 
     // Jump to option starting with "Ma"
     await user.keyboard('a');
@@ -169,6 +258,7 @@ describe('<Dropdown />', () => {
     // Select active option
     await user.keyboard('{Enter}');
     expect(selectInput.textContent).toBe('Mango');
+    expect(getByRole('option', { name: 'Mango' }).getAttribute('aria-selected')).toBe('true');
   });
 
   it('should move focus away on TAB', async () => {
@@ -196,8 +286,45 @@ describe('<Dropdown />', () => {
     await user.keyboard('{ArrowDown}');
     expect(selectInput).toHaveFocus();
 
-    // Dropdown Open and Jump to option starting with "M"
     await user.keyboard('{Tab}');
     expect(selectInput).not.toHaveFocus();
+  });
+
+  it('should move focus to footer button', async () => {
+    const user = userEvent.setup();
+    const applyClickHandler = jest.fn();
+    const { container, getByRole } = renderWithTheme(
+      <Dropdown>
+        <SelectInput label="Fruits" />
+        <DropdownOverlay>
+          <ActionList>
+            <ActionListItem title="Mingo" value="mingo" />
+            <ActionListItem title="Mango" value="mango" />
+            <ActionListFooter trailing={<Button onClick={applyClickHandler}>Apply</Button>} />
+          </ActionList>
+        </DropdownOverlay>
+      </Dropdown>,
+    );
+
+    const selectInput = getByRole('combobox', { name: 'Fruits' });
+
+    expect(selectInput).toBeInTheDocument();
+    expect(container.querySelector('[role=listbox]')).not.toBeVisible();
+
+    await user.click(selectInput);
+    expect(getByRole('listbox', { name: 'Fruits' })).toBeVisible();
+
+    // Check focus
+    await user.keyboard('{ArrowDown}');
+    expect(selectInput).toHaveFocus();
+
+    // Move focus to footer button
+    await user.keyboard('{Tab}');
+    expect(getByRole('listbox', { name: 'Fruits' })).toBeVisible();
+    expect(getByRole('button', { name: 'Apply' })).toHaveFocus();
+
+    // Press footer button
+    await user.keyboard('{Enter}');
+    expect(applyClickHandler).toBeCalledTimes(1);
   });
 });
