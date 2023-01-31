aa0c3df02e8e85b6a776e8a2dbb930e60306434d
anuraghazra <anurag.hazra@razorpay.com>
2023-01-31 18:22:35 +0530

    chore: added stories for ref

--- packages/blade/src/components/Radio/Radio.stories.tsx
+++ packages/blade/src/components/Radio/Radio.stories.tsx
@@ -8,6 +8,9 @@ import { RadioGroup as RadioGroupComponent } from './RadioGroup/RadioGroup';
 import { Radio as RadioComponent } from './Radio';
 import { Sandbox } from '~src/_helpers/storybook/Sandbox';
 import StoryPageWrapper from '~src/_helpers/storybook/StoryPageWrapper';
+import Box from '~components/Box';
+import type { BladeElementRef } from '~src/hooks/useBladeInnerRef';
+import { Button } from '~components/Button';
 
 const Page = (): React.ReactElement => {
   return (
@@ -240,4 +243,30 @@ export const KitchenSink = (): React.ReactElement => {
       </RadioGroupComponent>
     </>
   );
+};
+
+export const radioRef: ComponentStory<typeof RadioComponent> = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const radioRef = React.useRef<BladeElementRef>(null);
+
+  return (
+    <Box gap="spacing.3" display="flex" alignItems="center">
+      <RadioGroupComponent label="Radio ref example">
+        <RadioComponent ref={radioRef} value="1">
+          Radio
+        </RadioComponent>
+      </RadioGroupComponent>
+      <Button onClick={() => radioRef?.current?.focus()}>Click to focus the Radio</Button>
+    </Box>
+  );
+};
+
+radioRef.storyName = 'Radio Ref';
+radioRef.parameters = {
+  docs: {
+    description: {
+      story:
+        'Radio component exposes the `ref` prop. The `ref` exposes two methods `focus` & `scrollIntoView` which can be used to programatically control the DOM element',
+    },
+  },
 };
