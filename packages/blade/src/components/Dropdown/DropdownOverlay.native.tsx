13b082d3d788bb8026c49327255a1863cc5d3f3d
Saurabh Daware <saurabh.daware@razorpay.com>
2023-01-31 18:52:52 +0530

    fix: react native pressable area

--- packages/blade/src/components/Dropdown/DropdownOverlay.native.tsx
+++ packages/blade/src/components/Dropdown/DropdownOverlay.native.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Pressable } from 'react-native';
 import styled from 'styled-components/native';
 import { componentIds } from './dropdownUtils';
 import type { DropdownOverlayProps } from './DropdownOverlay.web';
@@ -11,24 +12,38 @@ const StyledDropdownOverlay = styled(Box)((props) => ({
   transform: `translateY(${makeSize(props.theme.spacing[3])})`,
 }));
 
+const StyledClosableArea = styled(Pressable)<{ display: 'flex' | 'none' }>((props) => ({
+  position: 'static',
+  height: '100%',
+  width: '100%',
+  display: props.display,
+}));
+
 /**
  * Overlay for dropdown.
  *
  * Wrap your ActionList with this this component
  */
 const DropdownOverlay: WithComponentId<DropdownOverlayProps> = ({ children }): JSX.Element => {
-  const { isOpen } = useDropdown();
+  const { isOpen, setIsOpen } = useDropdown();
 
   return (
     <Box position="relative">
-      <StyledDropdownOverlay
+      <StyledClosableArea
         display={isOpen ? 'flex' : 'none'}
-        position="absolute"
-        width="100%"
-        {...metaAttribute(MetaConstants.Component, MetaConstants.DropdownOverlay)}
+        onPress={() => {
+          setIsOpen(false);
+        }}
       >
-        {children}
-      </StyledDropdownOverlay>
+        <StyledDropdownOverlay
+          display={isOpen ? 'flex' : 'none'}
+          position="absolute"
+          width="100%"
+          {...metaAttribute(MetaConstants.Component, MetaConstants.DropdownOverlay)}
+        >
+          {children}
+        </StyledDropdownOverlay>
+      </StyledClosableArea>
     </Box>
   );
 };
