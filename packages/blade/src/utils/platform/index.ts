8a054815443cacaeb5233423c3533707efb450af
Saurabh Daware <saurabh.daware@razorpay.com>
2023-02-02 18:30:17 +0530

    refactor: rename getNativePlatform to getOS

--- packages/blade/src/utils/platform/index.ts
+++ packages/blade/src/utils/platform/index.ts
@@ -1,4 +1,4 @@
 export { isReactNative } from './isReactNative';
-export * from './getNativePlatform';
+export * from './getOS';
 export { Platform } from './platform';
 export * from './castUtils';
