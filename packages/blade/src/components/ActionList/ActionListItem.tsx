06e7e8996f048a00f5c6479f239db63e838807a7
Saurabh Daware <saurabh.daware@razorpay.com>
2023-02-08 17:24:14 +0530

    docs: add comment to explain ignoreanimation logic

--- packages/blade/src/components/ActionList/ActionListItem.tsx
+++ packages/blade/src/components/ActionList/ActionListItem.tsx
@@ -259,9 +259,12 @@ const ActionListItem: WithComponentId<ActionListItemProps> = (props): JSX.Elemen
         {...metaAttribute(MetaConstants.Component, MetaConstants.ActionListItem)}
         onMouseDown={() => {
           setShouldIgnoreBlur(true);
+          // We want to keep focus on Dropdown's trigger while option is being clicked
+          // So We set this flag that ignores the blur animation to avoid the flicker between focus out + focus in
           setShouldIgnoreBlurAnimation(true);
         }}
         onMouseUp={() => {
+          // (Contd from above comment...) We set this flag back to false since blur of SelectInput is done calling by this time
           setShouldIgnoreBlurAnimation(false);
         }}
         data-value={props.value}
