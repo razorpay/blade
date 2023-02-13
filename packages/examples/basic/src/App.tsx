1f3996bcf1565ce9abfc37ced014425ec0b1932a
Saurabh Daware <saurabh.daware@razorpay.com>
2023-02-13 10:51:28 +0530

    benchmark example

--- packages/examples/basic/src/App.tsx
+++ packages/examples/basic/src/App.tsx
@@ -1,4 +1,25 @@
-import { BladeProvider, Button } from '@razorpay/blade/components';
+import {
+  BladeProvider,
+  Dropdown,
+  DropdownOverlay,
+  SelectInput,
+  ActionList,
+  // ActionListFooter,
+  // ActionListFooterIcon,
+  // ActionListHeader,
+  ActionListItem,
+  // ActionListHeaderIcon,
+  // ActionListSection,
+  // ActionListItemAsset,
+  ActionListItemIcon,
+  // Button,
+  // HistoryIcon,
+  ArrowRightIcon,
+  SettingsIcon,
+  // DownloadIcon,
+  // SearchIcon,
+  HomeIcon,
+} from '@razorpay/blade/components';
 import { paymentTheme } from '@razorpay/blade/tokens';
 import '@fontsource/lato/400.css';
 import '@fontsource/lato/400-italic.css';
@@ -8,7 +29,53 @@ import '@fontsource/lato/700-italic.css';
 function App(): JSX.Element {
   return (
     <BladeProvider themeTokens={paymentTheme} colorScheme="light">
-      <Button onClick={() => console.log('hi')}>Hello</Button>
+      <Dropdown>
+        <SelectInput
+          label="Select Action"
+          onChange={({ name, values }) => {
+            console.log(name, values);
+          }}
+        />
+        <DropdownOverlay>
+          <ActionList>
+            {/* <ActionListHeader
+              title="Recent Searches"
+              leading={<ActionListHeaderIcon icon={HistoryIcon} />}
+            /> */}
+            <ActionListItem
+              leading={<ActionListItemIcon icon={HomeIcon} />}
+              trailing={<ActionListItemIcon icon={ArrowRightIcon} />}
+              title="Home"
+              value="home"
+              description="Home sweet home"
+            />
+            <ActionListItem
+              leading={<ActionListItemIcon icon={SettingsIcon} />}
+              title="Settings"
+              value="settings"
+              isDisabled={true}
+            />
+            {/* <ActionListSection title="Options">
+              
+              <ActionListItem
+                leading={<ActionListItemIcon icon={DownloadIcon} />}
+                title="Download"
+                value="download"
+              />
+            </ActionListSection> */}
+            {/* <ActionListItem
+              leading={<ActionListItemAsset src="https://flagcdn.com/w20/in.png" alt="india" />}
+              title="Pricing"
+              value="pricing"
+            /> */}
+            {/* <ActionListFooter
+              title="Search"
+              leading={<ActionListFooterIcon icon={SearchIcon} />}
+              trailing={<Button onClick={console.log}>Apply</Button>}
+            /> */}
+          </ActionList>
+        </DropdownOverlay>
+      </Dropdown>
     </BladeProvider>
   );
 }
