396174d0a7d222711243025482b825229978315f
Saurabh Daware <saurabh.daware@razorpay.com>
2023-02-22 09:26:41 +0530

    rip div and br

--- packages/blade/src/components/Alert/Alert.stories.tsx
+++ packages/blade/src/components/Alert/Alert.stories.tsx
@@ -33,30 +33,28 @@ const Page = (): ReactElement => {
               <Button onClick={() => setShowAlert(!showAlert)}>
                 Click to be alerted!
               </Button>
-              <br /><br />
-              <div>
-                { 
-                  showAlert 
-                  ? <Alert 
-                      title="The Button is Clicked 👀" 
-                      description="Click the Button again to hide alert"
-                      actions={{
-                        primary: {
-                          onClick: () => {
-                            alert('Alert from the alert hehe')
-                          },
-                          text: 'Primary Action'
+              { 
+                showAlert 
+                ? <Alert 
+                    title="The Button is Clicked 👀" 
+                    description="Click the Button again to hide alert"
+                    marginTop="spacing.4"
+                    actions={{
+                      primary: {
+                        onClick: () => {
+                          alert('Alert from the alert hehe')
                         },
-                        secondary: {
-                          href: 'https://razorpay.com',
-                          target: '_blank',
-                          text: 'Go to Razorpay.com'
-                        }
-                      }}
-                    /> 
-                  : null 
-                }
-              </div>
+                        text: 'Primary Action'
+                      },
+                      secondary: {
+                        href: 'https://razorpay.com',
+                        target: '_blank',
+                        text: 'Go to Razorpay.com'
+                      }
+                    }}
+                  /> 
+                : null 
+              }
             </div>
           )
         }
@@ -192,7 +190,7 @@ export const FullWidth: ComponentStory<typeof AlertComponent> = ({ ...args }) =>
   return (
     <BaseBox height="200px" position="relative">
       <BaseBox position="absolute" width="100%">
-        <AlertComponent {...args} />
+        <AlertComponent position="absolute" {...args} />
       </BaseBox>
     </BaseBox>
   );
