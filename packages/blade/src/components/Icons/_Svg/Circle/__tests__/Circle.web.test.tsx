a60885f191f8d1a642f4edcd3e460aea8b3925a5
Saurabh Daware <saurabh.daware@razorpay.com>
2023-02-23 15:15:31 +0530

    fix: ts

--- packages/blade/src/components/Icons/_Svg/Circle/__tests__/Circle.web.test.tsx
+++ packages/blade/src/components/Icons/_Svg/Circle/__tests__/Circle.web.test.tsx
@@ -5,7 +5,7 @@ import renderWithTheme from '~src/_helpers/testing/renderWithTheme.web';
 describe('<Circle />', () => {
   it('should render html circle component', () => {
     const { container } = renderWithTheme(
-      <Svg width="400" height="110" viewBox="0 0 100 100">
+      <Svg width="400px" height="110px" viewBox="0 0 100 100">
         <Circle cx="200" cy="100" x="10" y="10" r="4" strokeWidth="1" />
       </Svg>,
     );
