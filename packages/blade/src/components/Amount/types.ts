7dbd8db438bdc347f8d74969a9f60368359d1c0e
Priyanshu Chauhan <63398163+Priyanshu-C@users.noreply.github.com>
2023-03-06 18:15:01 +0530

    type check

--- packages/blade/src/components/Amount/types.ts
+++ packages/blade/src/components/Amount/types.ts
@@ -1,7 +1,5 @@
-import type { AmountProps } from './Amount';
 import type { Theme } from '~components/BladeProvider';
 
 export type StyledAmountProps = {
-  size: NonNullable<AmountProps['size']>;
   theme: Theme;
 };
