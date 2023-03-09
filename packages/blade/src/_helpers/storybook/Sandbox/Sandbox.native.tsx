d71da5c8b26a3c867ffa1391df61ec60fb5baaeb
Saurabh Daware <saurabh.daware@razorpay.com>
2023-03-09 10:36:49 +0530

    fix: ts

--- packages/blade/src/_helpers/storybook/Sandbox/Sandbox.native.tsx
+++ packages/blade/src/_helpers/storybook/Sandbox/Sandbox.native.tsx
@@ -3,6 +3,7 @@ import type { CodeViewerProps } from '@codesandbox/sandpack-react';
 import type { RecipeSandboxProps, SandboxProps } from './Sandbox.web';
 import { BaseText } from '~components/Typography/BaseText';
 import { Link } from '~components/Link';
+import { Text } from '~components/Typography';
 
 // In React Native, the codesandbox doesn't work. So replacing it with normal text display for native
 function Sandbox({ children }: SandboxProps): JSX.Element {
@@ -13,9 +14,13 @@ function SandboxHighlighter({ children }: { children: string } & CodeViewerProps
   return <BaseText fontFamily="code">{dedent(children)}</BaseText>;
 }
 
+function SandboxProvider(_props: { children: React.ReactNode; code: string }): JSX.Element {
+  return <Text>Not supported in React Native</Text>;
+}
+
 const RecipeSandbox = (props: RecipeSandboxProps): JSX.Element => {
   const codesandboxURL = `https://codesandbox.io/s/${props.codesandboxId}?file=${props.activeFile}`;
   return <Link href={codesandboxURL}>Open in CodeSandbox</Link>;
 };
 
-export { Sandbox, SandboxHighlighter, RecipeSandbox };
+export { Sandbox, SandboxProvider, SandboxHighlighter, RecipeSandbox };
