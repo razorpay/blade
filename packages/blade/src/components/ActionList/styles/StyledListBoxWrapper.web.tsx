9e3aecfa57bb5e711dff6835c6164ff362126c03
anuraghazra <anurag.hazra@razorpay.com>
2023-03-29 16:51:42 +0530

    chore: fix web types

--- packages/blade/src/components/ActionList/styles/StyledListBoxWrapper.web.tsx
+++ packages/blade/src/components/ActionList/styles/StyledListBoxWrapper.web.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import styled from 'styled-components';
+import type { SectionListProps } from 'react-native';
 import { getBaseListBoxWrapperStyles } from './getBaseListBoxWrapperStyles';
 import BaseBox from '~components/Box/BaseBox';
 
-const StyledListBoxWrapper = styled(BaseBox)((props) => {
+const StyledListBoxWrapper = styled(BaseBox)<SectionListProps<any, any>>((props) => {
   return {
     // Hides the last Divider (we don't want divider on last section)
     [`& [role=group]:last-child > [role=separator]:last-child`]: {
