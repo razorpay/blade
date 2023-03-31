0c8e5f1bc79d8bc0d647efedc336083bafff9c4f
Priyanshu Chauhan <63398163+Priyanshu-C@users.noreply.github.com>
2023-03-31 12:50:35 +0530

    feat(Amount): add `Amount` component (#1010)

---
+++ .changeset/dull-otters-play.md
@@ -0,0 +1,11 @@
+---
+'@razorpay/blade': minor
+---
+
+feat: add `Amount` component
+
+### Usage
+
+```tsx
+<Amount value={10000} />
+```
--- packages/blade/src/components/index.ts
+++ packages/blade/src/components/index.ts
@@ -25,6 +25,7 @@ export * from './Spinner';
 export * from './SkipNav';
 export * from './Typography';
 export * from './VisuallyHidden';
+export * from './Amount';
 // @TODO: Remove this and replace it with Box component when it is ready.
 // Added it to simplify testing
 export { default as InternalDontUsePleaseWillBeRemovedSoonBaseBox } from './Box/BaseBox';
