8a6ff105614afd22905467175e96076344b5139f
Saurabh Daware <saurabh.daware@razorpay.com>
2023-04-12 10:32:16 +0530

    fix(Box): react-native style types

--- packages/blade/src/_helpers/types.ts
+++ packages/blade/src/_helpers/types.ts
@@ -1,6 +1,9 @@
 import type React from 'react';
+import type { ViewStyle } from 'react-native';
+import type { CSSObject } from 'styled-components';
 import type { Spacing } from '~tokens/global';
 import type { EasingFunctionFactory } from '~tokens/global/motion';
+import type { Platform } from '~utils';
 
 /**
  * @template TokenType token type generic
@@ -83,6 +86,46 @@ type TestID = {
   testID?: string;
 };
 
+/**
+ * Returns the object as it is in web and sets all properties of object to type `never` in react native
+ */
+type MakeObjectWebOnly<T> = Platform.Select<{
+  web: T;
+  native: Record<keyof T, never>;
+}>;
+
+/**
+ * Similar to `Pick` except this returns `never` when value doesn't exist (native `Pick` returns `unknown`).
+ *
+ * You might have to ts-ignore the non-existing type error while using this. This is done so that you can get jsdoc from actual type.
+ *
+ * E.g. This will pick from ViewStyle prop if value exists else returns undefined.
+ *
+ * ```ts
+ * // @ts-expect-error: T passed here may not neccessarily exist. We return `never` type when it doesn't
+ * native: PickIfExist<ViewStyle, T>;
+ * ```
+ */
+type PickIfExist<T, K extends keyof T> = {
+  [P in K]: P extends keyof T ? T[P] : never;
+};
+
+/**
+ * Picks the types based on the platform (web / native).
+ *
+ * E.g.
+ * ```ts
+ * type CSSObject = PickCSSByPlatform<'display'>
+ * // On Web --> This will be all possible web display properties like `block`, `flex`, `inline`, and more.
+ * // On Native --> This will be just `flex` and `none`
+ * ```
+ */
+type PickCSSByPlatform<T extends keyof CSSObject | keyof ViewStyle> = Platform.Select<{
+  web: Pick<CSSObject, T>;
+  // @ts-expect-error: T passed here may not neccessarily exist. We return `never` type when it doesn't
+  native: PickIfExist<ViewStyle, T>;
+}>;
+
 export {
   DotNotationColorStringToken,
   DotNotationMotionStringToken,
@@ -91,4 +134,7 @@ export {
   StringChildrenType,
   StringWithAutocomplete,
   TestID,
+  MakeObjectWebOnly,
+  PickIfExist,
+  PickCSSByPlatform,
 };
