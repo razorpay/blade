0582742a3b9057680303f28b769b1cf3448338d3
Saurabh Daware <saurabh.daware@razorpay.com>
2023-05-17 06:30:16 +0530

    refactor: use BaseButton instead of Button

--- packages/blade/src/components/Dropdown/DropdownButton.tsx
+++ packages/blade/src/components/Dropdown/DropdownButton.tsx
@@ -2,12 +2,34 @@ import React from 'react';
 import { useDropdown } from './useDropdown';
 import { componentIds } from './dropdownUtils';
 import type { ButtonProps } from '~components/Button';
-import { Button } from '~components/Button';
 import { assignWithoutSideEffects } from '~src/utils/assignWithoutSideEffects';
 import { makeAccessible } from '~utils';
 import { getActionListContainerRole } from '~components/ActionList/getA11yRoles';
+import BaseButton from '~components/Button/BaseButton';
+import type { BaseButtonProps } from '~components/Button/BaseButton/BaseButton';
 
-const _DropdownButton = (props: ButtonProps): JSX.Element => {
+type DropdownButtonProps = ButtonProps & {
+  onBlur?: BaseButtonProps['onBlur'];
+  onKeyDown?: BaseButtonProps['onKeyDown'];
+};
+
+const _DropdownButton = ({
+  children,
+  icon,
+  iconPosition = 'left',
+  isDisabled = false,
+  isFullWidth = false,
+  isLoading = false,
+  onClick,
+  onBlur,
+  onKeyDown,
+  size = 'medium',
+  type = 'button',
+  variant = 'primary',
+  accessibilityLabel,
+  testID,
+  ...styledProps
+}: DropdownButtonProps): JSX.Element => {
   const {
     onTriggerClick,
     onTriggerBlur,
@@ -18,11 +40,24 @@ const _DropdownButton = (props: ButtonProps): JSX.Element => {
     hasFooterAction,
     triggererRef,
   } = useDropdown();
+
   return (
-    <Button
+    // Using BaseButton here to avoid exporting onBlur and onKeyDown from Button
+    // If in future we decide to export onBlur and onKeyDown on Button, this can be replaced with Button
+    <BaseButton
+      {...styledProps}
+      {...(icon ? { icon, children } : { children })}
+      iconPosition={iconPosition}
+      isDisabled={isDisabled}
+      isFullWidth={isFullWidth}
+      isLoading={isLoading}
+      size={size}
+      type={type}
+      variant={variant}
+      accessibilityLabel={accessibilityLabel}
+      testID={testID}
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       ref={triggererRef as any}
-      {...props}
       {...makeAccessible({
         hasPopup: getActionListContainerRole(hasFooterAction, 'DropdownButton'),
         expanded: isOpen,
@@ -31,20 +66,24 @@ const _DropdownButton = (props: ButtonProps): JSX.Element => {
         activeDescendant: activeIndex >= 0 ? `${dropdownBaseId}-${activeIndex}` : undefined,
       })}
       onClick={(e) => {
-        if (props.onClick) {
-          // Setting it for web fails it on native typecheck and vice versa
-          // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion, @typescript-eslint/no-explicit-any
-          props.onClick(e as any);
-        }
         onTriggerClick();
+        // Setting it for web fails it on native typecheck and vice versa
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion, @typescript-eslint/no-explicit-any
+        onClick?.(e as any);
       }}
-      onBlur={() => {
+      onBlur={(e) => {
         // With button trigger, there is no "value" as such. It's just clickable items
         onTriggerBlur?.({ name: undefined, value: undefined });
+        // Setting it for web fails it on native typecheck and vice versa
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion, @typescript-eslint/no-explicit-any
+        onBlur?.(e as any);
       }}
       onKeyDown={(e) => {
         // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion, @typescript-eslint/no-explicit-any
         onTriggerKeydown?.({ event: e as any });
+        // Setting it for web fails it on native typecheck and vice versa
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion, @typescript-eslint/no-explicit-any
+        onKeyDown?.(e as any);
       }}
     />
   );
