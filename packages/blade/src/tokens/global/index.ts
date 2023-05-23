bf0b6cd966421686200d76fdf5addd0068104dc4
kamleshchandnani <kamlesh.chandnani@gmail.com>
2023-05-23 10:42:23 +0530

    chore: upadte the way we re-export from theme

--- packages/blade/src/tokens/global/index.ts
+++ packages/blade/src/tokens/global/index.ts
@@ -1,25 +1,21 @@
-import { border } from './border';
-import { breakpoints } from './breakpoints';
-import { colors } from './colors';
-import { opacity } from './opacity';
-import { typography } from './typography';
-import { spacing } from './spacing';
-import { motion } from './motion';
-import { size } from './size';
-import { elevation } from './elevation';
-
 export type { Border } from './border';
+export { border } from './border';
 export type { Breakpoints } from './breakpoints';
+export { breakpoints } from './breakpoints';
 export type { Color } from './colors';
-export type { FontFamily } from './fontFamily';
+export { colors } from './colors';
 export type { Opacity } from './opacity';
+export { opacity } from './opacity';
+export type { FontFamily } from './fontFamily';
 export type {
   Typography,
   TypographyWithPlatforms,
   FontSize,
   TypographyPlatforms,
 } from './typography';
+export { typography } from './typography';
 export type { Spacing } from './spacing';
+export { spacing } from './spacing';
 export type {
   Motion,
   EasingFunctionFactory,
@@ -27,7 +23,8 @@ export type {
   EasingString,
   DelayString,
 } from './motion';
+export { motion } from './motion';
 export type { Size } from './size';
+export { size } from './size';
 export type { Elevation, ElevationWithColorModes } from './elevation';
-
-export { border, breakpoints, colors, elevation, opacity, size, spacing, typography, motion };
+export { elevation } from './elevation';
