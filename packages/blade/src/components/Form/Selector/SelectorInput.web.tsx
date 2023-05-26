43d1565fb734328c0a6cbe3670f70772f4b6b9d7
anuraghazra <anurag.hazra@razorpay.com>
2023-05-26 13:40:56 +0530

    chore: fix outline color

--- packages/blade/src/components/Form/Selector/SelectorInput.web.tsx
+++ packages/blade/src/components/Form/Selector/SelectorInput.web.tsx
@@ -37,6 +37,7 @@ const StyledInput = styled.input<HoverProps>(
     ...screenReaderStyles,
     '&:focus + div': {
       // TODO: Replace with focus outline token
+      outline: `1px solid ${theme.colors.surface.background.level1.lowContrast}`,
       boxShadow: `0px 0px 0px 4px ${theme.colors.brand.primary[400]}`,
     },
     '&:hover + div, &:focus + div': {
