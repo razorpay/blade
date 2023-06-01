a2d3c7379c9127d8f503b3bdba0ae1dfea1ba224
Abhishek Kumar <k.abhishek@razorpay.com>
2023-06-01 13:18:02 +0530

    Making builds work

--- packages/blade/src/utils/index.ts
+++ packages/blade/src/utils/index.ts
@@ -4,3 +4,10 @@ export * from './getPlatformType';
 export * from './useBreakpoint';
 export * from './useColorScheme';
 export * from './platform';
+
+// https://github.com/razorpay/blade/pull/1193#discussion_r1212616031
+export * from './makeBorderSize';
+export * from './makeMotionTime';
+export * from './makeSpace';
+export * from './makeTypographySize';
+export * from './makeSize';
