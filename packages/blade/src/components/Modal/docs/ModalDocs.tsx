1c6c402b32ac9b5565ba87fef193876d8c6dc272
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2023-06-02 16:12:22 +0530

    feat: add Modal stories

---
+++ packages/blade/src/components/Modal/docs/ModalDocs.tsx
@@ -0,0 +1,173 @@
+import React from 'react';
+import { Playground } from './stories';
+import StoryPageWrapper from '~src/_helpers/storybook/StoryPageWrapper';
+import { Code, Heading, Text, Title } from '~components/Typography';
+import { Sandbox } from '~src/_helpers/storybook/Sandbox';
+import { Box } from '~components/Box';
+import { ArgsTable } from '~src/_helpers/storybook/ArgsTable';
+import { List, ListItem } from '~components/List';
+import BaseBox from '~components/Box/BaseBox';
+
+const ModalDocs = (): JSX.Element => {
+  return (
+    <StoryPageWrapper
+      componentName="Modal"
+      componentDescription="Modals are often used to present information, notifications, or requests that require immediate attention or confirmation from the user"
+      imports=""
+      showStorybookControls={false}
+      figmaURL={{
+        paymentTheme:
+          'https://www.figma.com/file/jubmQL9Z8V7881ayUD95ps/Blade---Payment-Light?type=design&node-id=39809-559333&t=j3ZK6zZSwP5CJ8Al-0',
+        bankingTheme:
+          'https://www.figma.com/file/sAdplk2uYnI2ILnDKUxycW/Blade---Banking-Dark?type=design&node-id=16459-684636&t=7KYEW7ENNS5GmLIO-0',
+      }}
+    >
+      <Box as="section">
+        <Title size="medium">Playground</Title>
+        <Sandbox editorHeight={400}>{Playground}</Sandbox>
+      </Box>
+      <BaseBox id="modal" as="section">
+        <Title size="medium" marginBottom="spacing.4">
+          Modal
+        </Title>
+        <Text>A Modal in Blade is usually a composite of 4 elements -</Text>
+        <List variant="ordered-filled" marginY="spacing.5">
+          <ListItem>Modal - Handles opening / closing of Modal</ListItem>
+          <ListItem>ModalHeader - Renders the header for the Modal</ListItem>
+          <ListItem>ModalBody - Renders the body of the Modal</ListItem>
+          <ListItem>ModalFooter - Renders the footer for the Modal</ListItem>
+        </List>
+      </BaseBox>
+      <Heading size="medium">Modal Props</Heading>
+      <ArgsTable
+        marginBottom="spacing.4"
+        data={{
+          isOpen: (
+            <Text size="small" type="subdued">
+              <Text size="small" type="subdued">
+                <Code size="medium">boolean</Code> (required)
+              </Text>
+              Toggles modal state
+            </Text>
+          ),
+          onDismiss: (
+            <Text size="small" type="subdued">
+              <Text size="small" type="subdued">
+                <Code size="medium">function</Code> (required)
+              </Text>
+              Called when the modal is closed, either by user state, hitting esc or tapping backdrop
+            </Text>
+          ),
+          size: (
+            <Text size="small" type="subdued">
+              <Text size="small">{`'small' | 'medium' | 'large'`}</Text>
+              Size of the modal
+            </Text>
+          ),
+          initialFocusRef: (
+            <Text size="small" type="subdued">
+              <Text size="small" type="subdued">
+                <Code size="medium">function</Code>
+              </Text>
+              ref element you want to get keyboard focus when opening the modal
+            </Text>
+          ),
+          accessibilityLabel: (
+            <Text size="small" type="subdued">
+              <Text size="small" type="subdued">
+                <Code size="medium">string</Code>
+              </Text>
+              The accessibility label (aria-label) for the Modal.
+            </Text>
+          ),
+          children: (
+            <Text>
+              [<Code>{'<ModalBody />'}</Code>, <Code>{'<ModalHeader />'}</Code>,
+              <Code>{'<ModalHeader />'}</Code>]
+            </Text>
+          ),
+        }}
+      />
+      <Heading size="medium">ModalHeader Props</Heading>
+      <ArgsTable
+        marginBottom="spacing.4"
+        data={{
+          title: (
+            <Text size="small" type="subdued">
+              <Text size="small" type="subdued">
+                <Code size="medium">string</Code>
+              </Text>
+              Title of the Header
+            </Text>
+          ),
+          subTitle: (
+            <Text size="small" type="subdued">
+              <Text size="small" type="subdued">
+                <Code size="medium">string</Code>
+              </Text>
+              Subtitle of the Header
+            </Text>
+          ),
+          leading: (
+            <Text size="small" type="subdued">
+              <Text size="small" type="subdued">
+                <Code size="medium">React.ReactNode</Code>
+              </Text>
+              leading asset or icon to be placed at the left most side of the ModalHeader
+            </Text>
+          ),
+          trailing: (
+            <Text size="small" type="subdued">
+              <Text size="small" type="subdued">
+                <Code size="medium">Badge</Code>,<Code>Link</Code>,<Code>Text</Code>,
+                <Code>IconButton</Code>
+              </Text>
+              trailing component to be placed at the right most side of the ModalHeader
+            </Text>
+          ),
+          titleSuffix: (
+            <Text size="small" type="subdued">
+              <Text size="small" type="subdued">
+                <Code size="medium">Counter</Code>
+              </Text>
+              A component to be placed adjacent to the title text
+            </Text>
+          ),
+        }}
+      />
+
+      <Heading size="medium">ModalBody Props</Heading>
+      <ArgsTable
+        marginBottom="spacing.4"
+        data={{
+          children: (
+            <Text>
+              [<Code>{'<ModalBody />'}</Code>, <Code>{'<ModalHeader />'}</Code>,
+              <Code>{'<ModalHeader />'}</Code>]
+              <Text size="small" type="subdued">
+                (required)
+              </Text>
+            </Text>
+          ),
+        }}
+      />
+
+      <Heading size="medium">ModalFooter Props</Heading>
+      <ArgsTable
+        marginBottom="spacing.4"
+        data={{
+          children: (
+            <Text>
+              <Code>{'JSX.Element'}</Code>
+              <Text size="small" type="subdued">
+                (required)
+              </Text>
+            </Text>
+          ),
+        }}
+      />
+    </StoryPageWrapper>
+  );
+};
+
+export { ModalDocs };
---
+++ packages/blade/src/components/Modal/docs/stories.ts
@@ -0,0 +1,503 @@
+const Playground = `
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
+  RadioGroup,
+  Radio,
+  Box,
+  Badge,
+  Counter
+} from "@razorpay/blade/components";
+import React from "react";
+
+function App(): JSX.Element {
+  const buttonRef = React.useRef(null);
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <>
+      <Button ref={buttonRef} onClick={() => setIsOpen(!isOpen)}>
+        Open Modal
+      </Button>
+      <Modal isOpen={isOpen} onDismiss={() => setIsOpen(false)} size="large">
+        <ModalHeader
+          title="Address Details"
+        />
+        <ModalBody>
+          <RadioGroup label="Addresses">
+            <Radio value="home">
+              Home - 11850 Florida 24, Cedar Key, Florida
+            </Radio>
+            <Radio value="office-1">
+              Office - 2033 Florida 21, Cedar Key, Florida
+            </Radio>
+            <Radio value="office-2">Work - 5938 New York, Main Street</Radio>
+          </RadioGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Box
+            display="flex"
+            gap="spacing.3"
+            justifyContent="flex-end"
+            width="100%"
+          >
+            <Button variant="secondary">Remove address</Button>
+            <Button>Add address</Button>
+          </Box>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
+}
+
+export default App;
+`;
+const BasicModalStory = `
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Text
+} from "@razorpay/blade/components";
+import React from "react";
+
+function App(): JSX.Element {
+  const buttonRef = React.useRef(null);
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <>
+      <Button ref={buttonRef} onClick={() => setIsOpen(!isOpen)}>
+        Open Modal
+      </Button>
+      <Modal isOpen={isOpen} onDismiss={() => setIsOpen(false)} size="medium">
+        <ModalHeader title="Basic Modal" />
+        <ModalBody>
+          <Text>
+            This is a basic Modal. Close the Modal by clicking on the backdrop,
+            clicking on the close icon or pressing escape key.
+          </Text>
+        </ModalBody>
+      </Modal>
+    </>
+  );
+}
+
+export default App;
+`;
+
+const ModalWithHeaderFooterStory = `
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  ModalFooter,
+  RadioGroup,
+  Radio,
+  Box,
+  Badge,
+  Counter
+} from "@razorpay/blade/components";
+import React from "react";
+
+function App(): JSX.Element {
+  const buttonRef = React.useRef(null);
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <>
+      <Button ref={buttonRef} onClick={() => setIsOpen(!isOpen)}>
+        Open Modal with Header & Footer
+      </Button>
+      <Modal isOpen={isOpen} onDismiss={() => setIsOpen(false)} size="large">
+        <ModalHeader
+          title="Address Details"
+          subtitle="Saving addresses will improve your checkout experience"
+          trailing={<Badge variant="notice">Action Needed</Badge>}
+          titleSuffix={<Counter intent="positive" value={2} />}
+        />
+        <ModalBody>
+          <RadioGroup label="Addresses">
+            <Radio value="home">
+              Home - 11850 Florida 24, Cedar Key, Florida
+            </Radio>
+            <Radio value="office-1">
+              Office - 2033 Florida 21, Cedar Key, Florida
+            </Radio>
+            <Radio value="office-2">Work - 5938 New York, Main Street</Radio>
+          </RadioGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Box
+            display="flex"
+            gap="spacing.3"
+            justifyContent="flex-end"
+            width="100%"
+          >
+            <Button variant="secondary">Remove address</Button>
+            <Button>Add address</Button>
+          </Box>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
+}
+
+export default App;
+`;
+
+const ModalWithScrollableBackgroundStory = `
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Text
+} from "@razorpay/blade/components";
+import React from "react";
+
+function App(): JSX.Element {
+  const buttonRef = React.useRef(null);
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <>
+      <Button
+        ref={buttonRef}
+        onClick={() => setIsOpen(!isOpen)}
+        marginBottom="spacing.4"
+      >
+        Open Modal
+      </Button>
+      <Text marginBottom="500px">
+        This is an example for a modal with a scrollable background. The
+        background contains content that is longer than the screen height. When
+        the modal is opened, the background locked from scrolling but when the
+        modal is closed, the background content can be scrolled again.
+      </Text>
+      <Text marginBottom="500px">
+        This is an example for a modal with a scrollable background. The
+        background contains content that is longer than the screen height. When
+        the modal is opened, the background locked from scrolling but when the
+        modal is closed, the background content can be scrolled again.
+      </Text>
+      <Text marginBottom="500px">
+        This is an example for a modal with a scrollable background. The
+        background contains content that is longer than the screen height. When
+        the modal is opened, the background locked from scrolling but when the
+        modal is closed, the background content can be scrolled again.
+      </Text>
+      <Modal isOpen={isOpen} onDismiss={() => setIsOpen(false)} size="small">
+        <ModalHeader title="Scroll Lock Demo" />
+        <ModalBody>
+          <Text>
+            The background content is no longer scrollable. You can scroll the
+            background content again once you close this Modal.
+          </Text>
+        </ModalBody>
+      </Modal>
+    </>
+  );
+}
+
+export default App;
+`;
+
+const ModalWithScrollableContentStory = `
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Radio,
+  RadioGroup,
+  Text,
+  TextArea,
+  TextInput
+} from "@razorpay/blade/components";
+import React from "react";
+
+function App(): JSX.Element {
+  const buttonRef = React.useRef(null);
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <>
+      <Button
+        ref={buttonRef}
+        onClick={() => setIsOpen(!isOpen)}
+        marginBottom="spacing.4"
+      >
+        Open Scrollable Modal
+      </Button>
+      <Text>
+        This is an example for a scrollable modal. The modal contains content
+        that is longer than the Modal maximum height. When the modal is opened,
+        the modal content is scrollable.
+      </Text>
+      <Modal isOpen={isOpen} onDismiss={() => setIsOpen(false)} size="large">
+        <ModalHeader title="Contact Details" subtitle="How can we reach you?" />
+        <ModalBody>
+          <Box>
+            <TextInput
+              label="First Name"
+              placeholder="John"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Last Name"
+              placeholder="Doe"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Email"
+              placeholder="john.doe@gmail.com"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Phone Number"
+              placeholder="+1 234 567 890"
+              marginBottom="spacing.3"
+            />
+            <TextArea
+              label="Address"
+              placeholder="11850 Florida 24, Cedar Key, Florida"
+              marginBottom="spacing.3"
+            />
+
+            <RadioGroup label="Address Type">
+              <Radio value="home">Home</Radio>
+              <Radio value="office">Office</Radio>
+              <Radio value="spacestation">Space Station</Radio>
+            </RadioGroup>
+          </Box>
+          <Box>
+            <TextInput
+              label="First Name"
+              placeholder="John"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Last Name"
+              placeholder="Doe"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Email"
+              placeholder="john.doe@gmail.com"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Phone Number"
+              placeholder="+1 234 567 890"
+              marginBottom="spacing.3"
+            />
+            <TextArea
+              label="Address"
+              placeholder="11850 Florida 24, Cedar Key, Florida"
+              marginBottom="spacing.3"
+            />
+
+            <RadioGroup label="Address Type">
+              <Radio value="home">Home</Radio>
+              <Radio value="office">Office</Radio>
+              <Radio value="spacestation">Space Station</Radio>
+            </RadioGroup>
+          </Box>
+        </ModalBody>
+        <ModalFooter>
+          <Box
+            display="flex"
+            gap="spacing.3"
+            justifyContent="flex-end"
+            width="100%"
+          >
+            <Button variant="secondary">Remove address</Button>
+            <Button>Add address</Button>
+          </Box>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
+}
+
+export default App;
+`;
+
+const ModalStackingStory = `
+import {
+  Box,
+  Button,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Radio,
+  RadioGroup,
+  Text,
+  TextArea,
+  TextInput
+} from "@razorpay/blade/components";
+import React from "react";
+
+function App(): JSX.Element {
+  const buttonRef = React.useRef(null);
+  const [is1stOpen, set1stIsOpen] = React.useState(false);
+  const [is2ndOpen, setIs2ndOpen] = React.useState(false);
+  const [is3rdOpen, setIs3rdOpen] = React.useState(false);
+  return (
+    <>
+      <Button ref={buttonRef} onClick={() => set1stIsOpen(true)}>
+        Open First Modal
+      </Button>
+      {/* Modal 1 */}
+      <Modal
+        isOpen={is1stOpen}
+        onDismiss={() => set1stIsOpen(false)}
+        size="large"
+      >
+        <ModalHeader title="First Modal" />
+        <ModalBody>
+          <Box>
+            <TextInput
+              label="First Name"
+              placeholder="John"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Last Name"
+              placeholder="Doe"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Email"
+              placeholder="john.doe@gmail.com"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Phone Number"
+              placeholder="+1 234 567 890"
+              marginBottom="spacing.3"
+            />
+            <TextArea
+              label="Address"
+              placeholder="11850 Florida 24, Cedar Key, Florida"
+              marginBottom="spacing.3"
+            />
+
+            <RadioGroup label="Address Type">
+              <Radio value="home">Home</Radio>
+              <Radio value="office">Office</Radio>
+              <Radio value="spacestation">Space Station</Radio>
+            </RadioGroup>
+          </Box>
+          <Box>
+            <TextInput
+              label="First Name"
+              placeholder="John"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Last Name"
+              placeholder="Doe"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Email"
+              placeholder="john.doe@gmail.com"
+              marginBottom="spacing.3"
+            />
+            <TextInput
+              label="Phone Number"
+              placeholder="+1 234 567 890"
+              marginBottom="spacing.3"
+            />
+            <TextArea
+              label="Address"
+              placeholder="11850 Florida 24, Cedar Key, Florida"
+              marginBottom="spacing.3"
+            />
+
+            <RadioGroup label="Address Type">
+              <Radio value="home">Home</Radio>
+              <Radio value="office">Office</Radio>
+              <Radio value="spacestation">Space Station</Radio>
+            </RadioGroup>
+          </Box>
+        </ModalBody>
+        <ModalFooter>
+          <Box
+            display="flex"
+            gap="spacing.3"
+            justifyContent="flex-end"
+            width="100%"
+          >
+            <Button variant="secondary" onClick={() => set1stIsOpen(false)}>
+              Cancel
+            </Button>
+            <Button onClick={() => setIs2ndOpen(true)}>
+              Open Second Modal
+            </Button>
+          </Box>
+        </ModalFooter>
+      </Modal>
+      {/* Modal 2 */}
+      <Modal
+        isOpen={is2ndOpen}
+        onDismiss={() => setIs2ndOpen(false)}
+        size="medium"
+      >
+        <ModalHeader title="Second Modal" />
+        <ModalBody>
+          <RadioGroup label="Addresses">
+            <Radio value="home">
+              Home - 11850 Florida 24, Cedar Key, Florida
+            </Radio>
+            <Radio value="office-1">
+              Office - 2033 Florida 21, Cedar Key, Florida
+            </Radio>
+            <Radio value="office-2">Work - 5938 New York, Main Street</Radio>
+          </RadioGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Box
+            display="flex"
+            gap="spacing.3"
+            justifyContent="flex-end"
+            width="100%"
+          >
+            <Button variant="secondary" onClick={() => setIs2ndOpen(false)}>
+              Cancel
+            </Button>
+            <Button onClick={() => setIs3rdOpen(true)}>Open Third Modal</Button>
+          </Box>
+        </ModalFooter>
+      </Modal>
+      {/* Modal 3 */}
+      <Modal
+        isOpen={is3rdOpen}
+        onDismiss={() => setIs3rdOpen(false)}
+        size="small"
+      >
+        <ModalHeader title="Third Modal" />
+        <ModalBody>
+          <Text>This is the 3rd modal</Text>
+        </ModalBody>
+      </Modal>
+    </>
+  );
+}
+
+export default App;
+`;
+
+export {
+  Playground,
+  BasicModalStory,
+  ModalWithHeaderFooterStory,
+  ModalWithScrollableBackgroundStory,
+  ModalWithScrollableContentStory,
+  ModalStackingStory,
+};
