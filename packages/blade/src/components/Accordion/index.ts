947d08eec82a898509ce15c95271c6b2ab432261
Divyanshu Maithani <div.blackcat@gmail.com>
2023-06-13 13:27:33 +0530

    feat(Accordion/Collapsible): pre-release
    Adds Accordion and Collapsible for pre-release

--- packages/blade/src/components/Accordion/index.ts
+++ packages/blade/src/components/Accordion/index.ts
@@ -1,2 +1,15 @@
-export * from './Accordion';
-export * from './AccordionItem';
+import {
+  Accordion as Accordion_PRE_RELEASE,
+  AccordionProps as AccordionProps_PRE_RELEASE,
+} from './Accordion';
+import {
+  AccordionItem as AccordionItem_PRE_RELEASE,
+  AccordionItemProps as AccordionItemProps_PRE_RELEASE,
+} from './AccordionItem';
+
+export {
+  Accordion_PRE_RELEASE,
+  AccordionItem_PRE_RELEASE,
+  AccordionProps_PRE_RELEASE,
+  AccordionItemProps_PRE_RELEASE,
+};
--- packages/blade/src/components/Collapsible/index.ts
+++ packages/blade/src/components/Collapsible/index.ts
@@ -1,3 +1,27 @@
-export * from './Collapsible';
-export * from './CollapsibleButton';
-export * from './CollapsibleBody';
+import {
+  Collapsible as Collapsible_PRE_RELEASE,
+  CollapsibleProps as CollapsibleProps_PRE_RELEASE,
+} from './Collapsible';
+import {
+  CollapsibleButton as CollapsibleButton_PRE_RELEASE,
+  CollapsibleButtonProps as CollapsibleButtonProps_PRE_RELEASE,
+} from './CollapsibleButton';
+import {
+  CollapsibleLink as CollapsibleLink_PRE_RELEASE,
+  CollapsibleLinkProps as CollapsibleLinkProps_PRE_RELEASE,
+} from './CollapsibleLink';
+import {
+  CollapsibleBody as CollapsibleBody_PRE_RELEASE,
+  CollapsibleBodyProps as CollapsibleBodyProps_PRE_RELEASE,
+} from './CollapsibleBody';
+
+export {
+  Collapsible_PRE_RELEASE,
+  CollapsibleProps_PRE_RELEASE,
+  CollapsibleButton_PRE_RELEASE,
+  CollapsibleButtonProps_PRE_RELEASE,
+  CollapsibleLink_PRE_RELEASE,
+  CollapsibleLinkProps_PRE_RELEASE,
+  CollapsibleBody_PRE_RELEASE,
+  CollapsibleBodyProps_PRE_RELEASE,
+};
