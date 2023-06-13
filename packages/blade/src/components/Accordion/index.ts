63224fff37dc6cab1ccee45ec34956d2b2d5df19
Divyanshu Maithani <div.blackcat@gmail.com>
2023-06-13 15:13:07 +0530

    Fix

--- packages/blade/src/components/Accordion/index.ts
+++ packages/blade/src/components/Accordion/index.ts
@@ -1,5 +1,4 @@
 export { Accordion as Accordion_PRE_RELEASE } from './Accordion';
 export type { AccordionProps } from './Accordion';
 
-export { AccordionItem as AccordionItem_PRE_RELEASE } from './AccordionItem';
-export type { AccordionItemProps } from './AccordionItem';
+export * from './AccordionItem';
--- packages/blade/src/components/Collapsible/index.ts
+++ packages/blade/src/components/Collapsible/index.ts
@@ -1,11 +1,6 @@
 export { Collapsible as Collapsible_PRE_RELEASE } from './Collapsible';
 export type { CollapsibleProps } from './Collapsible';
 
-export { CollapsibleButton as CollapsibleButton_PRE_RELEASE } from './CollapsibleButton';
-export type { CollapsibleButtonProps } from './CollapsibleButton';
-
-export { CollapsibleLink as CollapsibleLink_PRE_RELEASE } from './CollapsibleLink';
-export type { CollapsibleLinkProps } from './CollapsibleLink';
-
-export { CollapsibleBody as CollapsibleBody_PRE_RELEASE } from './CollapsibleBody';
-export type { CollapsibleBodyProps } from './CollapsibleBody';
+export * from './CollapsibleButton';
+export * from './CollapsibleLink';
+export * from './CollapsibleBody';
