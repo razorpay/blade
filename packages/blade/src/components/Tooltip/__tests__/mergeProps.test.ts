44fa1e628eced9a1d0dc735f7216bd07c30cf90b
anuraghazra <anurag.hazra@razorpay.com>
2023-06-14 14:32:29 +0530

    chore: added mergeProps tests

---
+++ packages/blade/src/components/Tooltip/__tests__/mergeProps.test.ts
@@ -0,0 +1,23 @@
+import { mergeProps } from '../utils';
+
+describe('mergeProps', () => {
+  it('should merge two basic props', () => {
+    expect(mergeProps({ a: 1, b: 2 }, { c: 3 })).toStrictEqual({ a: 1, b: 2, c: 3 });
+  });
+
+  it('should merge two function props', () => {
+    const functionOne = jest.fn();
+    const functionTwo = jest.fn();
+    const mergedFunction = mergeProps({ callback: functionOne }, { callback: functionTwo });
+    expect(mergedFunction).toBeTruthy();
+
+    expect(functionOne).toHaveBeenCalledTimes(0);
+    expect(functionTwo).toHaveBeenCalledTimes(0);
+
+    const passedArgs = 'passed args';
+    mergedFunction.callback(passedArgs);
+
+    expect(functionOne).toHaveBeenCalledWith(passedArgs);
+    expect(functionTwo).toHaveBeenCalledWith(passedArgs);
+  });
+});
