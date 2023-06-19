168e0b2945d0a35dc9a85daf04c17c4a4ff9fed5
anuraghazra <anurag.hazra@razorpay.com>
2023-06-19 12:18:59 +0530

    feat: added as prop for text component

--- packages/blade/src/components/Typography/Text/Text.tsx
+++ packages/blade/src/components/Typography/Text/Text.tsx
@@ -1,17 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
 import type { ReactElement } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 import { BaseText } from '../BaseText';
 import type { BaseTextProps } from '../BaseText/types';
 import type { Theme } from '~components/BladeProvider';
 import { getStyledProps } from '~components/Box/styledProps';
 import type { StyledPropsBlade } from '~components/Box/styledProps';
-import { assignWithoutSideEffects, getPlatformType } from '~utils';
+import { assignWithoutSideEffects } from '~utils';
 
 import type { ColorContrast, ColorContrastTypes, TextTypes } from '~tokens/theme/theme';
 import type { TestID } from '~src/_helpers/types';
 
+const validAsValues = ['p', 'span', 'div', 'abbr', 'figcaption', 'cite', 'q'] as const;
 type TextCommonProps = {
+  as?: typeof validAsValues[number];
   type?: TextTypes;
   contrast?: ColorContrastTypes;
   truncateAfterLines?: number;
@@ -53,11 +55,7 @@ export type TextProps<T> = T extends {
     : T
   : T;
 
-type TextForwardedAs = {
-  forwardedAs?: BaseTextProps['as'];
-};
-
-type GetTextPropsReturn = Omit<BaseTextProps, 'children'> & TextForwardedAs;
+type GetTextPropsReturn = Omit<BaseTextProps, 'children'>;
 type GetTextProps<T extends { variant: TextVariant }> = Pick<
   TextProps<T>,
   'type' | 'variant' | 'weight' | 'size' | 'contrast' | 'color' | 'testID' | 'textAlign'
@@ -72,7 +70,6 @@ const getTextProps = <T extends { variant: TextVariant }>({
   testID,
   textAlign,
 }: GetTextProps<T>): GetTextPropsReturn => {
-  const isPlatformWeb = getPlatformType() === 'browser' || getPlatformType() === 'node';
   const colorContrast: keyof ColorContrast = contrast ? `${contrast!}Contrast` : 'lowContrast';
   const props: GetTextPropsReturn = {
     color: color ?? `surface.text.${type ?? 'normal'}.${colorContrast}`,
@@ -81,7 +78,6 @@ const getTextProps = <T extends { variant: TextVariant }>({
     fontStyle: 'normal',
     lineHeight: 100,
     fontFamily: 'text',
-    forwardedAs: isPlatformWeb ? 'p' : undefined,
     componentName: 'text',
     testID,
     textAlign,
@@ -118,23 +114,8 @@ const getTextProps = <T extends { variant: TextVariant }>({
   return props;
 };
 
-const StyledText = styled(BaseText)(({ truncateAfterLines }) => {
-  if (truncateAfterLines) {
-    if (getPlatformType() === 'react-native') {
-      return null;
-    }
-    return {
-      overflow: 'hidden',
-      display: '-webkit-box',
-      'line-clamp': `${truncateAfterLines}`,
-      '-webkit-line-clamp': `${truncateAfterLines}`,
-      '-webkit-box-orient': 'vertical',
-    };
-  }
-  return {};
-});
-
 const _Text = <T extends { variant: TextVariant }>({
+  as = 'p',
   variant = 'body',
   weight = 'regular',
   size = 'medium',
@@ -147,7 +128,8 @@ const _Text = <T extends { variant: TextVariant }>({
   textAlign,
   ...styledProps
 }: TextProps<T>): ReactElement => {
-  const props: Omit<BaseTextProps, 'children'> & TextForwardedAs = {
+  const props: Omit<BaseTextProps, 'children'> = {
+    as,
     truncateAfterLines,
     ...getTextProps({
       variant,
@@ -160,10 +142,21 @@ const _Text = <T extends { variant: TextVariant }>({
       textAlign,
     }),
   };
+
+  React.useEffect(() => {
+    if (as && !validAsValues.includes(as)) {
+      throw new Error(
+        `[Blade Text]: Invalid \`as\` prop value - ${as}. Only ${validAsValues.join(
+          ', ',
+        )} are accepted`,
+      );
+    }
+  }, [as]);
+
   return (
-    <StyledText {...props} {...getStyledProps(styledProps)}>
+    <BaseText {...props} {...getStyledProps(styledProps)}>
       {children}
-    </StyledText>
+    </BaseText>
   );
 };
 
