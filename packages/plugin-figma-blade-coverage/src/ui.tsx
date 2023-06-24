0860dc378511326c0ced4e9059b20014429c0c0b
kamleshchandnani <kamlesh.chandnani@gmail.com>
2023-06-24 18:30:54 +0530

    feat: add logic for measuring coverage

--- packages/plugin-figma-blade-coverage/src/ui.tsx
+++ packages/plugin-figma-blade-coverage/src/ui.tsx
@@ -1,25 +1,17 @@
 import { Button, Container, render, VerticalSpace } from '@create-figma-plugin/ui';
-import { emit } from '@create-figma-plugin/utilities';
-import React from 'react';
+// import { emit } from '@create-figma-plugin/utilities';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { h } from 'preact';
-
-import styles from './styles.css';
-import type { InsertCodeHandler } from './types';
+// import { Button } from '@razorpay/blade';
 
 const Plugin = (): JSX.Element => {
-  const handleInsertCodeButtonClick = React.useCallback(() => {
-    emit<InsertCodeHandler>('INSERT_CODE', 'abc');
-  }, []);
   return (
     <div>
       <Container space="medium">
         <VerticalSpace space="small" />
-        Hello, Word!
+        Hello, Worldddd!
         <VerticalSpace space="large" />
-        <Button fullWidth onClick={handleInsertCodeButtonClick}>
-          Insert Code
-        </Button>
+        <Button fullWidth>Insert Code</Button>
         <VerticalSpace space="small" />
       </Container>
     </div>
