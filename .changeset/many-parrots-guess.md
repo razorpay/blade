dd0695d7f52992d4160e58d0c4b4ffe41eef94fb
Anurag Hazra <anurag.hazra@razorpay.com>
2023-06-27 15:09:23 +0530

    feat(blade): added bottomsheet body zero padding option (#1346)
    * feat: web implementation
    
    * chore: add native padding
    
    * chore: update snapshot
    
    * fix: react native bottomsheet fix
    
    * chore: update snapshot and test
    
    * chore: update
    
    * chore: review updates
    
    * Create many-parrots-guess.md
    
    * Update .changeset/many-parrots-guess.md
    
    Co-authored-by: Saurabh Daware <saurabh.daware@razorpay.com>
    
    ---------
    
    Co-authored-by: Saurabh Daware <saurabh.daware@razorpay.com>

---
+++ .changeset/many-parrots-guess.md
@@ -0,0 +1,5 @@
+---
+"@razorpay/blade": minor
+---
+
+feat(blade): added bottomsheet body zero padding option
--- packages/blade/src/components/BottomSheet/BottomSheetGrabHandle.web.tsx
+++ packages/blade/src/components/BottomSheet/BottomSheetGrabHandle.web.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import { getBottomSheetGrabHandleStyles } from './getBottomSheetGrabHandleStyles';
 
-const BottomSheetGrabHandle = styled.div(({ theme }) => {
-  return getBottomSheetGrabHandleStyles({ theme });
-});
+const BottomSheetGrabHandle = styled.div<{ isHeaderFloating?: boolean }>(
+  ({ theme, isHeaderFloating }) => {
+    return getBottomSheetGrabHandleStyles({ theme, isHeaderFloating });
+  },
+);
 
 export { BottomSheetGrabHandle };
