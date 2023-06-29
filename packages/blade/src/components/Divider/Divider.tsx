0516c78458fb7b44635aff51aa4ee3d02cf70f87
Abhishek Kumar <k.abhishek@razorpay.com>
2023-06-29 12:47:23 +0530

    Used basebox for divider

--- packages/blade/src/components/Divider/Divider.tsx
+++ packages/blade/src/components/Divider/Divider.tsx
@@ -14,23 +14,24 @@ const Divider = ({
 }: DividerProps): React.ReactElement => {
   const { theme } = useTheme();
 
-  const borderPosition = orientation === 'horizontal' ? 'Bottom' : 'Left';
+  const isDividerHorizontal = orientation === 'horizontal';
+  const borderPosition = isDividerHorizontal ? 'borderBottom' : 'borderLeft';
+  const size = isDividerHorizontal ? { width: '100%' } : { height: '100%' };
+
   const borderWidth =
     thickness === 'thinner' ? makeSize(0.5) : makeSize(theme.border.width[thickness]);
-  const colorContrast: keyof ColorContrast = `${contrast}Contrast`;
-  const dimension = orientation === 'vertical' ? { height: '100%' } : { width: '100%' };
 
+  const colorContrast: keyof ColorContrast = `${contrast}Contrast`;
   const borderColor = theme.colors.surface.border[variant][colorContrast];
+
+  console.log(size, style);
 
   return (
     <BaseBox
-      style={{
-        border: 'none',
-        [`border${borderPosition}Width`]: borderWidth,
-        [`border${borderPosition}Style`]: style,
-        [`border${borderPosition}Color`]: borderColor,
-        ...dimension,
-      }}
+      border="0"
+      {...size}
+      {...{ [`${borderPosition}Color`]: borderColor }}
+      style={{ [`${borderPosition}Style`]: style, [`${borderPosition}Width`]: borderWidth }}
     />
   );
 };
