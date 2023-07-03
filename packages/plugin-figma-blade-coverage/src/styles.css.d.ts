6fdd0c3dbb19ed4a41250e81898a339ba4605855
kamleshchandnani <kamlesh.chandnani@gmail.com>
2023-07-03 16:05:40 +0530

    fix: minor bugs

--- packages/plugin-figma-blade-coverage/src/styles.css.d.ts
+++
@@ -1,5 +0,0 @@
-declare const styles: {
-  readonly container: string;
-  readonly editor: string;
-};
-export = styles;
--- packages/plugin-figma-blade-coverage/src/types.ts
+++
@@ -1,6 +0,0 @@
-import type { EventHandler } from '@create-figma-plugin/utilities';
-
-export interface InsertCodeHandler extends EventHandler {
-  name: 'INSERT_CODE';
-  handler: (code: string) => void;
-}
