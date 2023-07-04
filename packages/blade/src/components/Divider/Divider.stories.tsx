0ea52fd8c0a36393109ad9c2511420ba6bfcdd35
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2023-07-04 11:40:42 +0530

    chore: update divider stories

--- packages/blade/src/components/Divider/Divider.stories.tsx
+++ packages/blade/src/components/Divider/Divider.stories.tsx
@@ -7,6 +7,8 @@ import BaseBox from '~components/Box/BaseBox';
 import { Sandbox } from '~utils/storybook/Sandbox';
 import StoryPageWrapper from '~utils/storybook/StoryPageWrapper';
 import { Text } from '~components/Typography/Text';
+import { Card, CardBody } from '~components/Card';
+import { Button } from '~components/Button';
 
 const paragraph = (
   <BaseBox width="32%">
@@ -32,16 +34,41 @@ const Page = (): React.ReactElement => {
       <Title>Usage</Title>
       <Sandbox>
         {`
-        import { Divider, Box } from '@razorpay/blade/components';
-        
-        function App(): JSX.Element {
-          return (
-            <Box display="flex">
-              <Divider />
-            </Box>
-          );
-        }
-        export default App;
+          import {
+            Divider,
+            Box,
+            Heading,
+            Text,
+            Card,
+            CardBody
+          } from "@razorpay/blade/components";
+          
+          function App(): JSX.Element {
+            const paragraph = (
+              <Text marginTop="spacing.2">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+              </Text>
+            );
+          
+            return (
+              <Card>
+                <CardBody>
+                  <Box display="flex" flexDirection="column">
+                    <Heading marginBottom="spacing.2">Lorem Epsum</Heading>
+                    <Divider />
+                    <Box display="flex" gap="spacing.4" flexDirection="row">
+                      {paragraph}
+                      <Divider orientation="vertical" />
+                      {paragraph}
+                      <Divider orientation="vertical" />
+                      {paragraph}
+                    </Box>
+                  </Box>
+                </CardBody>
+              </Card>
+            );
+          }
+          export default App;          
         `}
       </Sandbox>
     </StoryPageWrapper>
@@ -60,9 +87,21 @@ export default {
 
 const DividerDefaultTemplate: ComponentStory<typeof DividerComponent> = (args) => {
   return (
-    <BaseBox height="100px" display="flex">
-      <DividerComponent {...args} />
-    </BaseBox>
+    <Card>
+      <CardBody>
+        <BaseBox display="flex" flexDirection={args.orientation == 'vertical' ? 'row' : 'column'}>
+          <Text margin="spacing.4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla lacus a
+            neque placerat ullamcorper
+          </Text>
+          <DividerComponent {...args} />
+          <Text margin="spacing.4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla lacus a
+            neque placerat ullamcorper
+          </Text>
+        </BaseBox>
+      </CardBody>
+    </Card>
   );
 };
 
@@ -71,9 +110,16 @@ Divider.storyName = 'Default';
 
 const DividerHorizontalTemplate: ComponentStory<typeof DividerComponent> = () => {
   return (
-    <BaseBox display="flex">
-      <DividerComponent />
-    </BaseBox>
+    <Card>
+      <CardBody>
+        <Heading marginBottom="spacing.2">Payment Links</Heading>
+        <DividerComponent />
+        <Text marginTop="spacing.3">
+          Share payment link via an email, SMS, messenger, chatbot etc. and get paid immediately.
+          Accepting payments from customers is now just a link away.
+        </Text>
+      </CardBody>
+    </Card>
   );
 };
 
@@ -84,15 +130,25 @@ DividerHorizontal.storyName = 'Horizontal';
 
 const DividerVerticalTemplate: ComponentStory<typeof DividerComponent> = () => {
   return (
-    <BaseBox
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="row"
-      height="100px"
-    >
-      <DividerComponent orientation="vertical" />
-    </BaseBox>
+    <Card>
+      <CardBody>
+        <BaseBox
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="row"
+          height="50px"
+        >
+          <Button variant="secondary" marginRight="spacing.4" isFullWidth>
+            Sign up
+          </Button>
+          <DividerComponent orientation="vertical" />
+          <Button variant="primary" marginLeft="spacing.4" isFullWidth>
+            Login
+          </Button>
+        </BaseBox>
+      </CardBody>
+    </Card>
   );
 };
 
@@ -103,17 +159,21 @@ DividerVertical.storyName = 'Vertical';
 
 const DividerWithTextTemplate: ComponentStory<typeof DividerComponent> = () => {
   return (
-    <BaseBox display="flex" flexDirection="column">
-      <Heading marginBottom="spacing.4">Lorem Epsum</Heading>
-      <DividerComponent />
-      <BaseBox display="flex" gap="spacing.6" flexDirection="row">
-        {paragraph}
-        <DividerComponent orientation="vertical" />
-        {paragraph}
-        <DividerComponent orientation="vertical" />
-        {paragraph}
-      </BaseBox>
-    </BaseBox>
+    <Card>
+      <CardBody>
+        <BaseBox display="flex" flexDirection="column">
+          <Heading marginBottom="spacing.4">Lorem Epsum</Heading>
+          <DividerComponent />
+          <BaseBox display="flex" gap="spacing.6" flexDirection="row">
+            {paragraph}
+            <DividerComponent orientation="vertical" />
+            {paragraph}
+            <DividerComponent orientation="vertical" />
+            {paragraph}
+          </BaseBox>
+        </BaseBox>
+      </CardBody>
+    </Card>
   );
 };
 
