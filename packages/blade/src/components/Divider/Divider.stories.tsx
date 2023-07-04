fcc2ebfe1a2be8e48e17f31d69d82e3f47ad29a0
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2023-07-04 15:07:32 +0530

    chore: update divider stories (#1390)

--- packages/blade/src/components/Divider/Divider.stories.tsx
+++ packages/blade/src/components/Divider/Divider.stories.tsx
@@ -7,15 +7,10 @@ import BaseBox from '~components/Box/BaseBox';
 import { Sandbox } from '~utils/storybook/Sandbox';
 import StoryPageWrapper from '~utils/storybook/StoryPageWrapper';
 import { Text } from '~components/Typography/Text';
-
-const paragraph = (
-  <BaseBox width="32%">
-    <Text margin="spacing.4">
-      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
-      Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur cursus
-    </Text>
-  </BaseBox>
-);
+import { Card, CardBody } from '~components/Card';
+import { Button } from '~components/Button';
+import { Box } from '~components/Box';
+import { List, ListItem } from '~components/List';
 
 const Page = (): React.ReactElement => {
   return (
@@ -32,16 +27,41 @@ const Page = (): React.ReactElement => {
       <Title>Usage</Title>
       <Sandbox>
         {`
-        import { Divider, Box } from '@razorpay/blade/components';
-        
-        function App(): JSX.Element {
-          return (
-            <Box display="flex">
-              <Divider />
-            </Box>
-          );
-        }
-        export default App;
+          import {
+            Divider,
+            Box,
+            Heading,
+            Text,
+            Card,
+            CardBody
+          } from "@razorpay/blade/components";
+          
+          function App(): JSX.Element {
+            return (
+              <Card>
+                <CardBody>
+                  <Box display="flex" flexDirection="column">
+                    <Heading marginBottom="spacing.2">Explore multiple payment options</Heading>
+                    <Divider />
+                    <Box display="flex" gap="spacing.4" flexDirection="row">
+                      <Text marginTop="spacing.2">
+                      Accept payments with a custom-branded online store using Payment Pages.
+                      </Text>
+                    <Divider orientation="vertical" />
+                      <Text marginTop="spacing.2">
+                      Create payment links which can be shared via an email, SMS, messenger, chatbot etc.
+                      </Text>
+                      <Divider orientation="vertical" />
+                      <Text marginTop="spacing.2">
+                      Accept one time and subscription payments using payment button on your website in less than 5 minutes.
+                      </Text>
+                    </Box>
+                  </Box>
+                </CardBody>
+              </Card>
+            );
+          }
+          export default App;          
         `}
       </Sandbox>
     </StoryPageWrapper>
@@ -60,9 +80,22 @@ export default {
 
 const DividerDefaultTemplate: ComponentStory<typeof DividerComponent> = (args) => {
   return (
-    <BaseBox height="100px" display="flex">
-      <DividerComponent {...args} />
-    </BaseBox>
+    <Card>
+      <CardBody>
+        <BaseBox display="flex" flexDirection={args.orientation == 'vertical' ? 'row' : 'column'}>
+          <Heading margin="spacing.4">Payment Links</Heading>
+          <DividerComponent {...args} />
+          <Box margin="spacing.4">
+            <Text>Share payment link via:</Text>
+            <List>
+              <ListItem>Email</ListItem>
+              <ListItem>SMS</ListItem>
+              <ListItem>Messenger</ListItem>
+            </List>
+          </Box>
+        </BaseBox>
+      </CardBody>
+    </Card>
   );
 };
 
@@ -71,9 +104,16 @@ Divider.storyName = 'Default';
 
 const DividerHorizontalTemplate: ComponentStory<typeof DividerComponent> = () => {
   return (
-    <BaseBox display="flex">
-      <DividerComponent />
-    </BaseBox>
+    <Card>
+      <CardBody>
+        <Heading marginBottom="spacing.2">Payment Links</Heading>
+        <DividerComponent />
+        <Text marginTop="spacing.3">
+          Share payment link via an email, SMS, messenger, chatbot etc. and get paid immediately.
+          Accepting payments from customers is now just a link away.
+        </Text>
+      </CardBody>
+    </Card>
   );
 };
 
@@ -84,15 +124,25 @@ DividerHorizontal.storyName = 'Horizontal';
 
 const DividerVerticalTemplate: ComponentStory<typeof DividerComponent> = () => {
   return (
-    <BaseBox
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      flexDirection="row"
-      height="100px"
-    >
-      <DividerComponent orientation="vertical" />
-    </BaseBox>
+    <Card>
+      <CardBody>
+        <BaseBox
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="row"
+          height="50px"
+        >
+          <Button variant="secondary" marginRight="spacing.4" isFullWidth>
+            Sign up
+          </Button>
+          <DividerComponent orientation="vertical" />
+          <Button variant="primary" marginLeft="spacing.4" isFullWidth>
+            Login
+          </Button>
+        </BaseBox>
+      </CardBody>
+    </Card>
   );
 };
 
@@ -103,17 +153,38 @@ DividerVertical.storyName = 'Vertical';
 
 const DividerWithTextTemplate: ComponentStory<typeof DividerComponent> = () => {
   return (
-    <BaseBox display="flex" flexDirection="column">
-      <Heading marginBottom="spacing.4">Lorem Epsum</Heading>
-      <DividerComponent />
-      <BaseBox display="flex" gap="spacing.6" flexDirection="row">
-        {paragraph}
-        <DividerComponent orientation="vertical" />
-        {paragraph}
-        <DividerComponent orientation="vertical" />
-        {paragraph}
-      </BaseBox>
-    </BaseBox>
+    <Card>
+      <CardBody>
+        <BaseBox display="flex" flexDirection="column">
+          <Heading marginBottom="spacing.4">Explore multiple payment options</Heading>
+          <DividerComponent />
+          <BaseBox display="flex" gap="spacing.6" flexDirection="row">
+            <BaseBox width="30%">
+              <Text margin="spacing.4">
+                Accept payments with a custom-branded online store using Payment Pages. Accept
+                international and domestic payments with automated payment receipts. Take your store
+                online instantly with zero coding.
+              </Text>
+            </BaseBox>
+            <DividerComponent orientation="vertical" />
+            <BaseBox width="30%">
+              <Text margin="spacing.4">
+                Create payment links which can be shared via an email, SMS, messenger, chatbot etc.
+                and get paid immediately. Accepting payments from customers is now just a link away.
+              </Text>
+            </BaseBox>
+            <DividerComponent orientation="vertical" />
+            <BaseBox width="30%">
+              <Text margin="spacing.4">
+                Accept one time and subscription payments on your website in less than 5 minutes.
+                Thousands of NGOs, SMEs, and freelancers are collecting payments by adding a payment
+                button to their website on their own.
+              </Text>
+            </BaseBox>
+          </BaseBox>
+        </BaseBox>
+      </CardBody>
+    </Card>
   );
 };
 
