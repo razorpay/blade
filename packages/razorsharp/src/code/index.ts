eca0239b43b4aa768e35d7522adc84d0e815fcff
Burhanuddin Udaipurwala <burhanuddin.u@razorpay.com>
2023-07-06 19:40:22 +0530

    working

--- packages/razorsharp/src/code/index.ts
+++ packages/razorsharp/src/code/index.ts
@@ -2,6 +2,8 @@ import { generateServerCode } from './blade/server';
 import { convertIntoBladeNodes } from './figmaUtils/convertIntoNodes';
 import type { BladeNode } from './types/Blade';
 
+let latestGeneration: string = '';
+
 if (figma.editorType === 'dev' && figma.mode === 'codegen') {
   // Register a callback to the "generate" event
   figma.codegen.on('generate', async ({ node }) => {
@@ -11,15 +13,30 @@ if (figma.editorType === 'dev' && figma.mode === 'codegen') {
       bladeNodes: convertedSelection,
     });
 
-    const minify = figma.codegen.preferences.customSettings.minifyOutput === 'yes';
+    // const minify = figma.codegen.preferences.customSettings.minifyOutput === 'yes';
+
+    latestGeneration = code;
 
     return [
       {
         title: 'Code',
         language: 'TYPESCRIPT',
-        code: minify ? JSON.stringify(code) : JSON.stringify(code, null, 2),
+        code: false ? JSON.stringify(JSON.stringify(code)) : JSON.stringify(code, null, 2),
       },
     ];
+  });
+
+  figma.codegen.on('preferenceschange', ({ propertyName }) => {
+    if (propertyName === 'publish') {
+      figma.showUI(__html__);
+
+      setTimeout(() => {
+        figma.ui.postMessage({
+          type: 'result',
+          code: latestGeneration,
+        });
+      }, 1000);
+    }
   });
 } else {
   figma.notify('Razorsharp can only be run in Dev mode.');
