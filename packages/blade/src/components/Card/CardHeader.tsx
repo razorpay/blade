1f89a2aebf27f1265a8539e6046c9be5ae84afb9
Nitin Kumar <nitin.kumar@razorpay.com>
2023-07-13 17:02:19 +0530

    fix: add margin between `CardHeaderLeading` &  `CardHeaderTrailing`

--- packages/blade/src/components/Card/CardHeader.tsx
+++ packages/blade/src/components/Card/CardHeader.tsx
@@ -107,7 +107,7 @@ const _CardHeader = ({ children, testID }: CardHeaderProps): React.ReactElement 
         marginBottom="spacing.7"
         display="flex"
         flexDirection="row"
-        justifyContent="space-between"
+        justifyContent="space-evenly"
       >
         {children}
       </BaseBox>
@@ -158,7 +158,7 @@ const _CardHeaderLeading = ({
       <BaseBox marginRight="spacing.3" alignSelf="center" display="flex">
         {prefix}
       </BaseBox>
-      <BaseBox>
+      <BaseBox marginRight="spacing.5">
         <BaseBox display="flex" flexDirection="row" alignItems="center" flexWrap="wrap">
           <Heading size="small" variant="regular" type="normal">
             {title}
