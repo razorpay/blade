68786b4b825244c456a1a47dae0f92635fcfc900
kamleshchandnani <kamlesh.chandnani@gmail.com>
2023-07-13 09:34:25 +0530

    fix: add plugin id via env

--- packages/plugin-figma-blade-coverage/build-figma-plugin.manifest.js
+++ packages/plugin-figma-blade-coverage/build-figma-plugin.manifest.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 module.exports = function buildManifest(manifest) {
   return {
     ...manifest,
+    id: process.env.FIGMA_PLUGIN_ID,
     permissions: ['currentuser'],
   };
 };
