b1a1faefb6f7afffc9f27f3f8dba704a9087f408
Anurag Hazra <anurag.hazra@razorpay.com>
2023-07-18 15:29:08 +0530

    feat(blade): added Skeleton Component  (#1398)
    * feat: initial skeleton implementation
    
    * chore: update card story skeleton
    
    * chore: added skeleton stories
    
    * chore: remove type prop
    
    * chore: update animation
    
    * chore: fix types
    
    * chore: added comment
    
    * chore: update skeleton tokens
    
    * chore: update storybook
    
    * chore: added a11y story
    
    * chore: minor change
    
    * chore: add margin
    
    * chore: fix rn stories
    
    * feat: add fade in animation
    
    * fix: box react-native shadows
    
    * chore: maintain backward styles
    
    * chore: refactor BaseBox
    
    * fix: box types
    
    * chore: update snapshots
    
    * chore: animation review updates
    
    * chore: review updates
    
    * chore: update docs
    
    * chore: rename duration tokens
    
    * chore: fix dts bug
    
    * chore: fix packagejson
    
    * Create wise-adults-push.md
    
    * test: added skeleton tests (#1399)
    
    * test: added skeleton tests
    
    * chore: add more tests
    
    * chore: add chromatic delay

--- packages/blade/src/components/Box/BaseBox/BaseBox.native.tsx
+++ packages/blade/src/components/Box/BaseBox/BaseBox.native.tsx
@@ -1,8 +1,14 @@
 import { View } from 'react-native';
+import type { DefaultTheme } from 'styled-components/native';
 import styled from 'styled-components/native';
-import { getBaseBoxStyles } from './baseBoxStyles';
+import React from 'react';
+import type { StyledComponent } from 'styled-components';
+import { getBaseBoxStyles, getElevationValue } from './baseBoxStyles';
 import type { BaseBoxProps } from './types';
 import { metaAttribute, MetaConstants } from '~utils/metaAttribute';
+import { useTheme } from '~components/BladeProvider';
+import type { ElevationStyles } from '~tokens/global/elevation';
+import { assignWithoutSideEffects } from '~utils/assignWithoutSideEffects';
 
 /**
  * Some prop go to React Native DOM and fail with type errors.
@@ -13,7 +19,7 @@ const isSupportedOnReactNativeElement = (prop: string): boolean => {
   return !prop.startsWith('padding') && !prop.startsWith('margin') && prop !== 'flex';
 };
 
-const BaseBox = styled(View)
+const StyledBaseBox = styled(View)
   .attrs<BaseBoxProps>((props) => {
     return {
       ...metaAttribute({
@@ -28,5 +34,19 @@ const BaseBox = styled(View)
   const cssObject = getBaseBoxStyles(props);
   return cssObject;
 });
+
+// @ts-expect-error we have to use `as StyledComponent<>` to type this otherwise `as` prop's types break
+// Since we loose the generic nature of the component
+const _BaseBox = (props, ref): React.ReactElement => {
+  const { theme } = useTheme();
+  const shadow = (getElevationValue(props.elevation, theme) as unknown) as ElevationStyles;
+  return (
+    <StyledBaseBox ref={ref} {...props} style={shadow ? [shadow, props.style] : props.style} />
+  );
+};
+
+const BaseBox = assignWithoutSideEffects(React.forwardRef(_BaseBox), {
+  displayName: 'BaseBox',
+}) as StyledComponent<typeof View, DefaultTheme, BaseBoxProps>;
 
 export { BaseBox };
