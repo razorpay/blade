25ef73f6d8d49a67a665fd5d937f52659d0239e6
Saurabh Daware <saurabh.daware@razorpay.com>
2023-07-21 12:50:02 +0530

    fix: snapshots

--- packages/blade/src/components/Tag/Tag.tsx
+++ packages/blade/src/components/Tag/Tag.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Box } from '~components/Box';
 import type { StyledPropsBlade } from '~components/Box/styledProps';
 import { getStyledProps } from '~components/Box/styledProps';
+import type { IconButtonProps } from '~components/Button/IconButton';
 import { IconButton } from '~components/Button/IconButton';
-import type { IconComponent } from '~components/Icons';
+import type { IconComponent, IconProps } from '~components/Icons';
 import { CloseIcon } from '~components/Icons';
+import type { TextProps } from '~components/Typography';
 import { Text } from '~components/Typography';
 import { metaAttribute, MetaConstants } from '~utils/metaAttribute';
 import type { StringChildrenType, TestID } from '~utils/types';
 import { isReactNative } from '~utils';
 import BaseBox from '~components/Box/BaseBox';
+import type { PaddingProps } from '~components/Box/BaseBox/types/spacingTypes';
 
 type TagProps = {
   /**
@@ -38,8 +42,35 @@ type TagProps = {
    * Disable tag
    */
   isDisabled?: boolean;
+
+  /**
+   *
+   */
+  _isTagFocussed?: boolean;
 } & StyledPropsBlade &
   TestID;
+
+const FocussableTag = styled(BaseBox)<{ isTagFocussed: TagProps['_isTagFocussed'] }>((props) => {
+  if (props.isTagFocussed) {
+    return {
+      outline: `1px solid ${props.theme.colors.surface.background.level1.lowContrast}`,
+      boxShadow: `0px 0px 0px 4px ${props.theme.colors.brand.primary[400]}`,
+    };
+  }
+
+  return {};
+});
+const Desktop = ({ children }: { children: (React.ReactElement | null)[] }): React.ReactElement => (
+  <Box display={{ base: 'none', m: 'flex' }} alignItems="center">
+    {children}
+  </Box>
+);
+
+const Mobile = ({ children }: { children: (React.ReactElement | null)[] }): React.ReactElement => (
+  <Box display={{ base: 'flex', m: 'none' }} alignItems="center">
+    {children}
+  </Box>
+);
 
 const Tag = ({
   size = 'medium',
@@ -48,44 +79,74 @@ const Tag = ({
   children,
   isDisabled,
   testID,
+  _isTagFocussed,
   ...styledProps
 }: TagProps): React.ReactElement | null => {
   const textColor = isDisabled
     ? 'surface.text.placeholder.lowContrast'
     : 'surface.text.subtle.lowContrast';
 
+  const mediumPadding: PaddingProps['padding'] = {
+    base: ['spacing.2', 'spacing.3', 'spacing.2', 'spacing.4'],
+    m: ['spacing.1', 'spacing.2', 'spacing.1', 'spacing.3'],
+  };
+
+  const largePadding: PaddingProps['padding'] = {
+    base: ['spacing.2', 'spacing.3', 'spacing.2', 'spacing.4'],
+    m: ['spacing.2', 'spacing.3', 'spacing.2', 'spacing.4'],
+  };
+
+  const getLeadingIcon = ({ size }: { size: IconProps['size'] }): React.ReactElement | null =>
+    Icon ? <Icon color={textColor} size={size} marginRight="spacing.2" /> : null;
+
+  const getTagText = ({
+    size,
+  }: {
+    size: TextProps<{ variant: 'body' }>['size'];
+  }): React.ReactElement => (
+    <Text marginRight="spacing.2" color={textColor} size={size}>
+      {children}
+    </Text>
+  );
+
+  const getCloseIcon = ({ size }: { size: IconButtonProps['size'] }): React.ReactElement => (
+    <IconButton
+      size={size}
+      icon={CloseIcon}
+      accessibilityLabel={`Close ${children} tag`}
+      isDisabled={isDisabled}
+      onClick={() => {
+        onDismiss();
+      }}
+    />
+  );
+
   return (
-    <BaseBox
+    <FocussableTag
       display={(isReactNative() ? 'flex' : 'inline-flex') as never}
       alignSelf={isReactNative() ? 'center' : undefined}
       flexDirection="row"
       flexWrap="nowrap"
       backgroundColor="brand.gray.a100.lowContrast"
       borderRadius="max"
-      padding={
-        size === 'medium'
-          ? ['spacing.1', 'spacing.2', 'spacing.1', 'spacing.3']
-          : ['spacing.2', 'spacing.3', 'spacing.2', 'spacing.4']
-      }
+      padding={size === 'medium' ? mediumPadding : largePadding}
       {...getStyledProps(styledProps)}
       {...metaAttribute({ name: MetaConstants.Tag, testID })}
+      isTagFocussed={_isTagFocussed}
     >
       <Box display="flex" flexDirection="row" flexWrap="nowrap" alignItems="center">
-        {Icon ? <Icon color={textColor} size="small" marginRight="spacing.2" /> : null}
-        <Text marginRight="spacing.2" color={textColor} size="small">
-          {children}
-        </Text>
-        <IconButton
-          size="small"
-          icon={CloseIcon}
-          accessibilityLabel={`Close ${children} tag`}
-          isDisabled={isDisabled}
-          onClick={() => {
-            onDismiss();
-          }}
-        />
+        <Desktop>
+          {getLeadingIcon({ size: 'small' })}
+          {getTagText({ size: 'small' })}
+          {getCloseIcon({ size: 'small' })}
+        </Desktop>
+        <Mobile>
+          {getLeadingIcon({ size: size === 'large' ? 'medium' : 'small' })}
+          {getTagText({ size: size === 'large' ? 'medium' : 'small' })}
+          {getCloseIcon({ size: size === 'large' ? 'medium' : 'small' })}
+        </Mobile>
       </Box>
-    </BaseBox>
+    </FocussableTag>
   );
 };
 
