64596f69c8e0b060ba0c9728f0ceea8946995876
Kamlesh Chandnani <kamlesh.chandnani@gmail.com>
2023-07-25 15:28:30 +0530

    fix: persist number keys as object keys (#1466)
    Co-authored-by: rzpcibot <tools+cibot@razorpay.com>

--- packages/token-publisher/src/plugin/makeThemeColorTokens.ts
+++ packages/token-publisher/src/plugin/makeThemeColorTokens.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-implicit-any-catch */
-import setValue from 'set-value';
+import setValue from '../utils/setValue';
 import showNotification from './showNotification';
 /**
  * @TODO:
