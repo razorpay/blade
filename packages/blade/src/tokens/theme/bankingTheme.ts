fb4fe8b395c37352fe74a3f02cfe72dd5c2b8b38
rzpcibot <tools+cibot@razorpay.com>
2023-07-25 09:01:26 +0000

    feat(tokens): add new tokens

--- packages/blade/src/tokens/theme/bankingTheme.ts
+++ packages/blade/src/tokens/theme/bankingTheme.ts
@@ -29,11 +29,11 @@ const colors: ColorsWithModes = {
       border: {
         normal: {
           lowContrast: globalColors.neutral.navyGrayLight.a100,
-          highContrast: globalColors.neutral.navyGrayLight.a100,
+          highContrast: globalColors.neutral.navyGrayLight.a400,
         },
         subtle: {
           lowContrast: globalColors.neutral.navyGrayLight.a50,
-          highContrast: globalColors.neutral.navyGrayLight.a50,
+          highContrast: globalColors.neutral.navyGrayLight.a300,
         },
       },
       text: {
@@ -77,14 +77,17 @@ const colors: ColorsWithModes = {
             highContrast: globalColors.neutral.navyGrayLight[0],
           },
           disabled: {
-            lowContrast: globalColors.neutral.navyGrayLight[300],
+            lowContrast: globalColors.neutral.navyGrayLight.a100,
             highContrast: globalColors.neutral.navyGrayLight.a100,
           },
         },
       },
     },
     overlay: {
-      background: globalColors.neutral.ashGrayLight.a1100,
+      background: {
+        dark: globalColors.neutral.ashGrayLight.a1100,
+        light: globalColors.neutral.navyGrayLight.a1100,
+      },
     },
     brand: {
       primary: {
@@ -1097,11 +1100,11 @@ const colors: ColorsWithModes = {
       border: {
         normal: {
           lowContrast: globalColors.neutral.navyGrayDark.a100,
-          highContrast: globalColors.neutral.navyGrayDark.a100,
+          highContrast: globalColors.neutral.navyGrayDark.a400,
         },
         subtle: {
           lowContrast: globalColors.neutral.navyGrayDark.a50,
-          highContrast: globalColors.neutral.navyGrayDark.a50,
+          highContrast: globalColors.neutral.navyGrayDark.a300,
         },
       },
       text: {
@@ -1145,14 +1148,17 @@ const colors: ColorsWithModes = {
             highContrast: globalColors.neutral.navyGrayDark[0],
           },
           disabled: {
-            lowContrast: globalColors.neutral.navyGrayDark[600],
+            lowContrast: globalColors.neutral.navyGrayDark.a100,
             highContrast: globalColors.neutral.navyGrayDark.a100,
           },
         },
       },
     },
     overlay: {
-      background: globalColors.neutral.ashGrayDark.a1100,
+      background: {
+        dark: globalColors.neutral.ashGrayDark.a1100,
+        light: globalColors.neutral.navyGrayDark.a1100,
+      },
     },
     brand: {
       primary: {
