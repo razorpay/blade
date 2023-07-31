82aacb31721dbb024d15974ffc0c91ef85a7999a
Saurabh Daware <saurabh.daware@razorpay.com>
2023-07-31 10:06:07 +0530

    refactor: remove console log

--- packages/blade/src/components/Box/BaseBox/BaseBox.web.tsx
+++ packages/blade/src/components/Box/BaseBox/BaseBox.web.tsx
@@ -6,7 +6,6 @@ import { metaAttribute, MetaConstants } from '~utils/metaAttribute';
 
 const BaseBox = styled.div
   .attrs<BaseBoxProps>((props) => {
-    console.log((props as never)['data-testid']);
     return {
       ...metaAttribute({
         name: (props as never)['data-blade-component'] || MetaConstants.BaseBox,
