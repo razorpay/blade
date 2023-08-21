b018dde48852e6a31bdc6cda729d056c92e0394c
Anurag Hazra <anurag.hazra@razorpay.com>
2023-08-21 19:04:04 +0530

    feat(blade): added carousel web implementation (#1508)
    * feat: added nav buttons
    
    * feat: added indicators
    
    * chore: wip carousel functionality
    
    * chore: wip initial implementation
    
    * feat: responsive carousel
    
    * feat: new carousel behaviour + indicator fixes
    
    * refactor: remove bleed prop & add carouselItemWidth
    
    * feat: indicator sync initial version
    
    * feat: add autoplay
    
    * feat: added scroll overlay start/end behaviour
    
    * chore: fix mobile overlay
    
    * chore: fix overlay in mobile
    
    * feat: prevent autoplay on user hover/focus
    
    * feat: carousel accessibility
    
    * chore: added onchange and other prop functionality
    
    * fix: start end padding bug / indicator sync bug
    
    * chore: lint
    
    * chore: skip tsc
    
    * feat: added type docs
    
    * feat: add width/height to card, fix carousel scroll, add stories
    
    * feat: remove few props and update based on api doc
    
    * fix: add indicator special case
    
    * chore: improve example
    
    * fix: use debounce for scroll sync
    
    * chore: also update indicator on goToSlideIndex
    
    * chore: review update
    
    * test: added carousel web tests (#1520)
    
    * test: added carousel web tests
    
    * chore: snapshots
    
    * chore: update tests
    
    * chore: fix indicator bug
    
    * chore: update tests
    
    * chore: fix storybook control bug
    
    * chore: update storybook
    
    * chore: fix a11y
    
    * chore: review update
    
    * chore: update
    
    * chore: added carousel ssr test
    
    * chore: add test
    
    * chore: cleanup
    
    * feat(blade): added carousel native implementation (#1515)
    
    * feat: wip carousel native
    
    * fix: indicator bug
    
    * feat: add rn indicator animation
    
    * chore: fix stories
    
    * chore: use iphone 13
    
    * chore: review updates
    
    * test: add carousel native tests (#1526)
    
    * test: added carousel react native test
    
    * chore: use math round
    
    * test: snapshots and indicator test
    
    * chore: minor changes
    
    * fix: tests
    
    * chore: minor changes
    
    * chore: build fix
    
    * chore: update snapshot
    
    * chore: fix tests
    
    * chore: fix overlay color
    
    * chore: review update
    
    * chore: use gap
    
    * chore: update snapshot
    
    * chore: fix types
    
    * chore: fix build
    
    * chore: update snapshot
    
    * chore: update pod file
    
    * chore: minor
    
    * chore: revert run command
    
    * chore: update snapshot
    
    * Create wild-jeans-perform.md
    
    * chore: fix storybook

---
+++ packages/blade/src/components/Carousel/CarouselContext.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import type { CarouselProps } from './types';
+import { throwBladeError } from '~utils/logger';
+
+type CarouselContextProps =
+  | (Pick<
+      CarouselProps,
+      'visibleItems' | 'carouselItemWidth' | 'shouldAddStartEndSpacing' | 'carouselItemAlignment'
+    > & {
+      carouselContainerRef: React.RefObject<HTMLDivElement>;
+      setActiveIndicator: (value: React.SetStateAction<number>) => void;
+      carouselId: string | null;
+      totalNumberOfSlides: number;
+      /**
+       * React native only
+       */
+      slideWidth?: number;
+      activeSlide?: number;
+    })
+  | null;
+const CarouselContext = React.createContext<CarouselContextProps>(null);
+
+const useCarouselContext = (): NonNullable<CarouselContextProps> => {
+  const state = React.useContext<CarouselContextProps>(CarouselContext);
+
+  if (!state) {
+    throwBladeError({
+      moduleName: 'Carousel',
+      message: 'useCarouselContext must be used within Carousel',
+    });
+  }
+
+  return state!;
+};
+
+export { CarouselContext, useCarouselContext };
+export type { CarouselContextProps };
---
+++ packages/blade/src/components/Carousel/NavigationButton/NavigationButton.tsx
@@ -0,0 +1,46 @@
+import type { ReactElement } from 'react';
+import React from 'react';
+import type { NavigationButtonProps } from './types';
+import { StyledNavigationButton } from './StyledNavigationButton';
+import { ChevronLeftIcon, ChevronRightIcon } from '~components/Icons';
+import { isReactNative, useTheme } from '~utils';
+import { metaAttribute } from '~utils/metaAttribute';
+import { makeAccessible } from '~utils/makeAccessible';
+
+const NavigationButton = ({ type, variant, onClick }: NavigationButtonProps): ReactElement => {
+  const { platform } = useTheme();
+  const isMobile = platform === 'onMobile';
+  const iconSize = isMobile ? 'small' : 'medium';
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const getIconColor = () => {
+    if (!isReactNative()) {
+      return 'currentColor' as const;
+    }
+
+    // on react native we cannot rely on currentColor thus need to manually pass iconColor
+    const iconColor = {
+      filled: 'action.icon.tertiary.default',
+      stroked: 'surface.action.icon.active.highContrast',
+    } as const;
+
+    return iconColor[variant];
+  };
+
+  return (
+    <StyledNavigationButton
+      onClick={onClick}
+      variant={variant}
+      {...metaAttribute({ name: 'NavigationButton' })}
+      {...makeAccessible({ label: type === 'previous' ? 'Previous Slide' : 'Next Slide' })}
+    >
+      {type === 'next' ? (
+        <ChevronRightIcon size={iconSize} color={getIconColor()} />
+      ) : (
+        <ChevronLeftIcon size={iconSize} color={getIconColor()} />
+      )}
+    </StyledNavigationButton>
+  );
+};
+
+export { NavigationButton, NavigationButtonProps };
