3547895c68a0b0522c4cdea468b6f9bf2458aeaa
anuraghazra <anurag.hazra@razorpay.com>
2023-08-21 15:21:12 +0530

    chore: fix build

--- packages/blade/src/components/Carousel/Indicators/StyledIndicatorButton.native.tsx
+++ packages/blade/src/components/Carousel/Indicators/StyledIndicatorButton.native.tsx
@@ -18,7 +18,7 @@ const StyledIndicatorButton = ({
   onClick,
   accessibilityLabel,
   ...props
-}: IndicatorButtonProps & { accessibilityLabel?: string; as: never }): React.ReactElement => {
+}: IndicatorButtonProps & { accessibilityLabel?: string }): React.ReactElement => {
   const { theme } = useTheme();
   const easing = castNativeType(theme.motion.easing.standard.effective);
   const duration = castNativeType(makeMotionTime(theme.motion.duration.gentle));
