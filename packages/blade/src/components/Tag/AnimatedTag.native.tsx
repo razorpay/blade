f3db27570fae54e3681fff173ae25bdac0d44ec0
Saurabh Daware <saurabh.daware@razorpay.com>
2023-08-23 11:45:37 +0530

    feat: add tag enter animation

--- packages/blade/src/components/Tag/AnimatedTag.native.tsx
+++ packages/blade/src/components/Tag/AnimatedTag.native.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import type { EasingFn } from 'react-native-reanimated';
 import Animated, {
   Easing,
   useSharedValue,
   withTiming,
   useAnimatedStyle,
   runOnJS,
+  Keyframe,
 } from 'react-native-reanimated';
+import type { ReanimatedKeyframe } from 'react-native-reanimated/lib/typescript/reanimated2/layoutReanimation/animationBuilder/Keyframe';
 import { Tag } from './Tag';
 import {
   TAG_MAX_WIDTH_END,
@@ -19,6 +22,7 @@ import { castNativeType, useTheme } from '~utils';
 const useAnimatedTag = (
   onAnimationEndCallback: () => void,
 ): {
+  entering: ReanimatedKeyframe;
   animate: () => void;
   animatedStyle: {
     maxWidth: number;
@@ -28,6 +32,16 @@ const useAnimatedTag = (
   const maxWidth = useSharedValue(TAG_MAX_WIDTH_START); // Initial max-width value
   const opacity = useSharedValue(TAG_OPACITY_START); // Initial opacity value
   const { theme } = useTheme();
+
+  const entering = new Keyframe({
+    0: {
+      opacity: TAG_OPACITY_END,
+    },
+    100: {
+      opacity: TAG_OPACITY_START,
+      easing: (theme.motion.easing.exit.effective as unknown) as EasingFn,
+    },
+  }).duration(theme.motion.duration.xquick);
 
   const animate = (): void => {
     maxWidth.value = withTiming(
@@ -55,7 +69,7 @@ const useAnimatedTag = (
     };
   });
 
-  return { animate, animatedStyle };
+  return { animate, animatedStyle, entering };
 };
 
 const AnimatedTag = ({
@@ -68,10 +82,10 @@ const AnimatedTag = ({
     onDismiss({ tagIndex: currentTagIndex, tagName: children });
   };
 
-  const { animate, animatedStyle } = useAnimatedTag(onAnimationEnd);
+  const { animate, animatedStyle, entering } = useAnimatedTag(onAnimationEnd);
 
   return (
-    <Animated.View style={animatedStyle}>
+    <Animated.View style={animatedStyle} entering={entering}>
       <Tag
         _isVirtuallyFocussed={currentTagIndex === activeTagIndex}
         _isTagInsideInput={true}
