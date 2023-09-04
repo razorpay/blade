23806c9de02d4cdb5f8ef1620b226b0b51cb8331
Nitin Kumar <nitin.kumar@razorpay.com>
2023-09-04 07:02:26 +0530

    test: add ssr tests

--- packages/blade/src/components/Typography/Display/__tests__/Display.ssr.test.tsx
+++ packages/blade/src/components/Typography/Display/__tests__/Display.ssr.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Display } from '../';
 import renderWithSSR from '~utils/testing/renderWithSSR.web';
 
-describe('<Switch />', () => {
-  it('should render switch ssr', () => {
+describe('<Display />', () => {
+  it('should render', () => {
     const displayText = 'Displaying Landing Page Display';
     const { container, getByRole, getByText } = renderWithSSR(<Display>{displayText}</Display>);
     expect(getByRole('heading', { level: 1 })).toBeInTheDocument();
