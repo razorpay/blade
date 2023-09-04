df06c3295926ddc19b85a91269c6cfd1ee8e1bab
anuraghazra <anurag.hazra@razorpay.com>
2023-09-04 18:09:18 +0530

    chore: update test for useUpdateEffect

---
+++ packages/blade/src/utils/useUpdateEffect.test.ts
@@ -0,0 +1,35 @@
+import { renderHook } from '@testing-library/react';
+import { useUpdateEffect } from './useUpdateEffect';
+
+describe('useUpdateEffect', () => {
+  it('should run the effect only when the component is updated', () => {
+    const effect = jest.fn();
+    const { rerender } = renderHook(() => {
+      return useUpdateEffect(effect, [{}]);
+    });
+
+    expect(effect).not.toHaveBeenCalled();
+
+    rerender();
+
+    expect(effect).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not run the effect on the first render', () => {
+    const effect = jest.fn();
+    renderHook(() => useUpdateEffect(effect, [{}]));
+
+    expect(effect).not.toHaveBeenCalled();
+  });
+
+  it('should not run the effect when the dependencies do not change', () => {
+    const effect = jest.fn();
+    const { rerender } = renderHook(() => useUpdateEffect(effect, [1, 2, 3]));
+
+    expect(effect).not.toHaveBeenCalled();
+
+    rerender();
+
+    expect(effect).not.toHaveBeenCalled();
+  });
+});
