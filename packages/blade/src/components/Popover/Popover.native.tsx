c0f8ae65f0304b91d224c8ba0dd6e12d17560917
Anurag Hazra <anurag.hazra@razorpay.com>
2023-09-18 13:21:53 +0530

    feat(blade): added popover component (#1606)
    * chore: wip popover impl
    
    * chore: native impl
    
    * feat: fix rn popover, refactor popover arrow
    
    * chore: minor
    
    * fix: fix rn layout issues
    
    * chore: fix a11y for popover interactive wrapper
    
    * chore: remove test
    
    * chore: added stories
    
    * chore: fix rn stories
    
    * chore: storybook review feedback changes
    
    * chore: update controlled state
    
    * refactor: getPlacementParts
    
    * chore: review comments
    
    * chore: update
    
    * feat: added title prop in tooltip
    
    * chore: update docs
    
    * chore: add popover vs tooltip table
    
    * chore: update nits
    
    * test(blade): added popover tests (#1621)
    
    * chore: add web test
    
    * chore: new tests
    
    * chore: update tests
    
    * chore: fix build
    
    * chore: review comments
    
    * chore: fix tests
    
    * Create curly-cooks-whisper.md
    
    * chore: update
    
    * chore: hardcode background color for now
    
    * chore: minor
    
    * chore: fix build

---
+++ packages/blade/src/components/Popover/Popover.native.tsx
@@ -0,0 +1,157 @@
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+import { arrow, shift, useFloating, flip, offset } from '@floating-ui/react-native';
+import React from 'react';
+import { Modal, TouchableOpacity } from 'react-native';
+import { PopoverContent } from './PopoverContent';
+import type { PopoverProps } from './types';
+import {
+  ARROW_HEIGHT,
+  ARROW_WIDTH,
+  popoverZIndex,
+  POPOVER_BG_DARK,
+  POPOVER_BG_LIGHT,
+} from './constants';
+import { PopoverContext } from './PopoverContext';
+import { useTheme } from '~components/BladeProvider';
+import { metaAttribute, MetaConstants } from '~utils/metaAttribute';
+import { mergeProps } from '~utils/mergeProps';
+import { useControllableState } from '~utils/useControllable';
+import { PopupArrow } from '~components/PopupArrow';
+import { getFloatingPlacementParts } from '~utils/getFloatingPlacementParts';
+
+const Popover = ({
+  content,
+  children,
+  placement = 'top',
+  onOpenChange,
+  zIndex = popoverZIndex,
+  title,
+  titleLeading,
+  footer,
+  isOpen,
+  defaultIsOpen,
+}: PopoverProps): React.ReactElement => {
+  const { theme, colorScheme } = useTheme();
+  const defaultInitialFocusRef = React.useRef(null);
+  const [controllableIsOpen, controllableSetIsOpen] = useControllableState({
+    value: isOpen,
+    defaultValue: defaultIsOpen,
+    onChange: (isOpen) => onOpenChange?.({ isOpen }),
+  });
+
+  const gap = theme.spacing[2];
+  const [side] = getFloatingPlacementParts(placement);
+  const isHorizontal = side === 'left' || side === 'right';
+  const arrowRef = React.useRef();
+  const context = useFloating({
+    sameScrollView: false,
+    placement,
+    middleware: [
+      shift({ crossAxis: false, padding: gap }),
+      flip({ padding: gap, fallbackAxisSideDirection: 'start' }),
+      offset(gap + ARROW_HEIGHT),
+      arrow({
+        element: arrowRef,
+        padding: isHorizontal ? 0 : ARROW_WIDTH,
+      }),
+    ],
+  });
+
+  const { refs, floatingStyles } = context;
+  const [computedSide] = getFloatingPlacementParts(context.placement);
+
+  const handleOpen = React.useCallback(() => {
+    controllableSetIsOpen(() => true);
+    onOpenChange?.({ isOpen: true });
+  }, [controllableSetIsOpen, onOpenChange]);
+
+  const handleClose = React.useCallback(() => {
+    controllableSetIsOpen(() => false);
+    onOpenChange?.({ isOpen: false });
+  }, [controllableSetIsOpen, onOpenChange]);
+
+  // wait for animation to finish before unmounting modal
+  const [isVisible, setIsVisible] = React.useState(() => controllableIsOpen);
+  React.useEffect(() => {
+    const id = setTimeout(() => {
+      if (!controllableIsOpen) {
+        setIsVisible(false);
+      }
+    }, theme.motion.duration.gentle);
+
+    if (controllableIsOpen) {
+      setIsVisible(true);
+    }
+    return () => clearTimeout(id);
+  }, [controllableIsOpen]);
+
+  const contextValue = React.useMemo(() => {
+    return {
+      close: handleClose,
+      defaultInitialFocusRef,
+      titleId: undefined,
+    };
+  }, [handleClose]);
+
+  return (
+    <PopoverContext.Provider value={contextValue}>
+      {/* Cloning the trigger children to enhance it with ref and event handler */}
+      {React.cloneElement(children, {
+        ...mergeProps(
+          {
+            onTouchEnd: children.props.onTouchEnd,
+          },
+          { onTouchEnd: handleOpen },
+        ),
+        ref: refs.setReference,
+      })}
+      <Modal
+        collapsable={false}
+        transparent
+        visible={Boolean(isVisible)}
+        {...metaAttribute({ testID: 'popover-modal' })}
+      >
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+          }}
+          onPress={handleClose}
+          activeOpacity={1}
+          testID="popover-modal-backdrop"
+          {...metaAttribute({ name: MetaConstants.Popover })}
+        />
+        <PopoverContent
+          titleLeading={titleLeading}
+          title={title}
+          footer={footer}
+          isVisible={controllableIsOpen}
+          ref={refs.setFloating}
+          side={computedSide}
+          style={{
+            ...floatingStyles,
+            // TODO: Tokenize zIndex values
+            zIndex,
+          }}
+          arrow={
+            <PopupArrow
+              ref={arrowRef as never}
+              context={context}
+              width={ARROW_WIDTH}
+              height={ARROW_HEIGHT}
+              fillColor={colorScheme === 'dark' ? POPOVER_BG_DARK : POPOVER_BG_LIGHT}
+              strokeColor={theme.colors.brand.gray[400].lowContrast}
+            />
+          }
+        >
+          {content}
+        </PopoverContent>
+      </Modal>
+    </PopoverContext.Provider>
+  );
+};
+
+export { Popover };
