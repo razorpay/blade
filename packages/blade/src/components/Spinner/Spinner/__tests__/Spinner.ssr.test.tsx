b57f92b66236536749f4f7396d476c5e925c1a0f
Nitin Kumar <nitin.kumar@razorpay.com>
2023-09-19 11:47:55 +0530

    feat(Spinner): add new `color` prop

--- packages/blade/src/components/Spinner/Spinner/__tests__/Spinner.ssr.test.tsx
+++ packages/blade/src/components/Spinner/Spinner/__tests__/Spinner.ssr.test.tsx
@@ -2,9 +2,16 @@ import { Spinner } from '../Spinner';
 import renderWithSSR from '~utils/testing/renderWithSSR.web';
 
 describe('<Spinner />', () => {
-  it('should render high contrast Spinner', () => {
+  it('should render default color Spinner', () => {
     const { container, getByText } = renderWithSSR(
-      <Spinner accessibilityLabel="Loading" contrast="high" label="Loading" />,
+      <Spinner accessibilityLabel="Loading" color="default" label="Loading" />,
+    );
+    expect(getByText('Loading')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+  it('should render white color Spinner', () => {
+    const { container, getByText } = renderWithSSR(
+      <Spinner accessibilityLabel="Loading" color="white" label="Loading" />,
     );
     expect(getByText('Loading')).toBeInTheDocument();
     expect(container).toMatchSnapshot();
