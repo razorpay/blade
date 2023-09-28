a08074129397577870edced91cdc89237ea5473f
Nitin <nitin.kumar@razorpay.com>
2023-09-28 14:51:16 +0530

    fix(paymentTheme): fix overlay color tokens

--- packages/blade/src/tokens/theme/paymentTheme.ts
+++ packages/blade/src/tokens/theme/paymentTheme.ts
@@ -91,7 +91,7 @@ const colors: ColorsWithModes = {
       popup: { background: globalColors.neutral.blueGrayLight[100] },
     },
     overlay: {
-      background: globalColors.neutral.ashGrayDark.a1100,
+      background: globalColors.neutral.ashGrayLight.a1100,
     },
     brand: {
       primary: {
