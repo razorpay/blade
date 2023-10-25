2f76106755ea067393e8accda1834cbbac093b56
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2023-10-25 17:35:45 +0530

    feat: add checkout home to ThemePlayground (#1719)

---
+++ packages/blade/src/storybook-recipes/ThemePlayground/Checkout/CheckoutCard.tsx
@@ -0,0 +1,33 @@
+import { Box, TextInput, Checkbox, Heading, PasswordInput } from '../../../components';
+import { CheckoutShell } from './CheckoutShell';
+
+const CheckoutCard = (): React.ReactElement => {
+  return (
+    <CheckoutShell>
+      <Box padding="spacing.4">
+        <Heading marginTop="spacing.3">Add New Card</Heading>
+        <Box display="flex" gap="spacing.6" marginTop="spacing.8">
+          <Box flex="2">
+            <TextInput label="Card Number" />
+          </Box>
+          <Box flex="1">
+            <TextInput label="Expiry" />
+          </Box>
+        </Box>
+        <Box display="flex" gap="spacing.6" marginTop="spacing.8">
+          <Box flex="2">
+            <TextInput label="Card Holder's Name" />
+          </Box>
+          <Box flex="1">
+            <PasswordInput label="CVV" />
+          </Box>
+        </Box>
+        <Box marginTop="spacing.10">
+          <Checkbox defaultChecked>Save card securely for future payments</Checkbox>
+        </Box>
+      </Box>
+    </CheckoutShell>
+  );
+};
+
+export { CheckoutCard };
