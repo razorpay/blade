2bb29396f5702e003018dd1aab0b57f828959586
Harshal Kaigaonkar <harshalkaigaonkar31@gmail.com>
2023-10-30 18:23:30 +0530

    feat(ActionList): added stories and kitchensink for combinations (#1764)
    * feat(ActionList): added stories and kitchensink for combinations (draft)
    
    * fix: lint, move ActionList stories
    
    ---------
    
    Co-authored-by: rzpcibot <tools+cibot@razorpay.com>

---
+++ packages/blade/src/components/ActionList/_KitchenSink.ActionList.stories.tsx
@@ -0,0 +1,31 @@
+import { composeStories } from '@storybook/react';
+import * as actionListStories from './ActionList.stories';
+import { Box } from '~components/Box';
+import { Heading } from '~components/Typography';
+
+const allStories = Object.values(composeStories(actionListStories));
+
+export const ActionList = (): JSX.Element => {
+  return (
+    <Box display="flex" flexDirection="column" gap="spacing.4">
+      {allStories.map((Story) => {
+        return (
+          <>
+            <Heading>{Story.storyName}</Heading>
+            <Story />
+          </>
+        );
+      })}
+    </Box>
+  );
+};
+
+export default {
+  title: 'Components/KitchenSink/ActionList',
+  component: ActionList,
+  parameters: {
+    // enable Chromatic's snapshotting only for kitchensink
+    chromatic: { disableSnapshot: false },
+    options: { showPanel: false },
+  },
+};
