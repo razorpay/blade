46950d8af4ab28991d896b9310279a0e8f8440c7
anuraghazra <anurag.hazra@razorpay.com>
2023-10-31 13:34:14 +0530

    chore: add file zip icon

--- packages/blade/src/components/Icons/index.tsx
+++ packages/blade/src/components/Icons/index.tsx
@@ -71,6 +71,7 @@ export { default as EditIcon } from './EditIcon';
 export { default as EyeIcon } from './EyeIcon';
 export { default as EyeOffIcon } from './EyeOffIcon';
 export { default as FileTextIcon } from './FileTextIcon';
+export { default as FileZipIcon } from './FileZipIcon';
 export { default as HistoryIcon } from './HistoryIcon';
 export { default as HomeIcon } from './HomeIcon';
 export { default as InfoIcon } from './InfoIcon';
