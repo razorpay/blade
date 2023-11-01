a5c3acfbd48059496d161f80c339ae71f01dca96
Nitin <nitin.kumar@razorpay.com>
2023-11-01 17:04:06 +0530

    fix: CSS issues

--- packages/blade/src/components/BladeProvider/stylisCSSHigherSpecificity.ts
+++ packages/blade/src/components/BladeProvider/stylisCSSHigherSpecificity.ts
@@ -1,18 +1,23 @@
 import type { StylisPlugin } from 'styled-components';
 
 export default function createStylisCSSHigherSpecificityPlugin(): StylisPlugin {
-  const stylisCSSHigherSpecificity = (
-    context: number,
-    content: string[],
-    selectors: string[],
-  ): void => {
+  const stylisCSSHigherSpecificity = (context: number, _: string[], selectors: string[]): void => {
     if (context !== -1) return;
 
     const [selector] = selectors;
-    console.log(
-      '🚀 ~ file: stylisCSSHigherSpecificity.ts:12 ~ createStylisCSSHigherSpecificityPlugin ~ selector:',
-      selector,
-    );
+
+    setTimeout(() => {
+      const elements = document.querySelectorAll(selector);
+
+      elements.forEach((element) => {
+        const className = element.getAttribute('class');
+        if (className?.includes('sc-')) {
+          const allClasses = className.split(' ');
+          allClasses.pop();
+          element.setAttribute('class', allClasses.join(' '));
+        }
+      });
+    }, 0);
 
     selectors[0] = `${selector.repeat(5)}`;
   };
