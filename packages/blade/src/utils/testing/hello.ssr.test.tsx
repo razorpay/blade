e9aa54b6f05cee54a1c55a0a83ea12dae3bc3d42
anuraghazra <anurag.hazra@razorpay.com>
2023-11-02 14:45:14 +0530

    chore: remove hello test

--- packages/blade/src/utils/testing/hello.ssr.test.tsx
+++
@@ -1,8 +0,0 @@
-import { Hello } from './Hello';
-import renderWithSSR from './renderWithSSR.web';
-
-describe('Hello SSR', () => {
-  it('should render hello world', () => {
-    renderWithSSR(<Hello />);
-  });
-});
