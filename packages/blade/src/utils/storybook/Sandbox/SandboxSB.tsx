6ee492660691c6ed8928af25661771c0423d50ec
saurabhdaware <saurabh.daware@razorpay.com>
2023-11-07 13:32:09 +0530

    customized sandbox

--- packages/blade/src/utils/storybook/Sandbox/SandboxSB.tsx
+++ packages/blade/src/utils/storybook/Sandbox/SandboxSB.tsx
@@ -5,7 +5,19 @@ import type { Project } from '@stackblitz/sdk';
 import styled from 'styled-components';
 
 const INDEX_HTML = `
-<div id="root"></div>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vite + React + TS</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/index.tsx"></script>
+  </body>
+</html>
 `;
 
 const INDEX_JS = `
@@ -29,33 +41,94 @@ root.render(<App />);
 const javascriptProject: Project = {
   title: 'Dynamically Generated Project',
   description: 'Simple example using the EngineBlock "javascript" template.',
-  template: 'create-react-app',
+  template: 'node',
   files: {
-    'public/index.html': INDEX_HTML,
-    'src/index.js': INDEX_JS,
+    '.vscode/settings.json': JSON.stringify(
+      {
+        'editor.fontFamily': 'Menlo, monospace, Cascadia Code, Consolas, Liberation Mono',
+        'editor.fontSize': 13,
+        'editor.lineHeight': 2,
+        'editor.lineNumbers': 'off',
+        'editor.renderLineHighlight': 'none',
+        'editor.renderIndentGuides': false,
+      },
+      null,
+      4,
+    ),
+    'index.html': INDEX_HTML,
+    'index.tsx': INDEX_JS,
+    'vite.config.ts': `import { defineConfig } from 'vite'
+    import react from '@vitejs/plugin-react'
+    
+    // https://vitejs.dev/config/
+    export default defineConfig({
+      plugins: [react()],
+    })
+    `,
+    'tsconfig.json': `{
+  "compilerOptions": {
+    "target": "ES2020",
+    "useDefineForClassFields": true,
+    "lib": ["ES2020", "DOM", "DOM.Iterable"],
+    "module": "ESNext",
+    "skipLibCheck": true,
+
+    /* Bundler mode */
+    "moduleResolution": "bundler",
+    "allowImportingTsExtensions": true,
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "noEmit": true,
+    "jsx": "react-jsx",
+
+    /* Linting */
+    "strict": true,
+    "noUnusedLocals": true,
+    "noUnusedParameters": true,
+    "noFallthroughCasesInSwitch": true
   },
-  dependencies: {
-    react: '*',
-    'react-dom': '*',
-    '@types/react': '*',
-    '@types/react-dom': '*',
-    '@razorpay/blade': '*',
-    'styled-components': '*',
+  "include": ["src"],
+}
+    `,
+    'package.json': JSON.stringify(
+      {
+        scripts: {
+          dev: 'vite',
+          build: 'vite build',
+        },
+        dependencies: {
+          react: '*',
+          'react-dom': '*',
+          '@types/react': '*',
+          '@types/react-dom': '*',
+          '@razorpay/blade': '*',
+          'styled-components': '*',
+        },
+        devDependencies: {
+          vite: '*',
+          '@vitejs/plugin-react': '*',
+        },
+      },
+      null,
+      4,
+    ),
   },
 };
 
 const StyledEmbed = styled.div`
   & iframe {
-    border: none !important;
+    border: 1px solid #efefef !important;
+    border-radius: 4px;
   }
 `;
 
 export const SandboxSB = (): JSX.Element => {
   React.useEffect(() => {
     void sdk.embedProject('sb-embed', javascriptProject, {
-      height: 400,
-      openFile: 'index.js',
-      terminalHeight: 50,
+      height: 500,
+      openFile: 'index.tsx',
+      terminalHeight: 0,
+      hideDevTools: true,
       hideNavigation: true,
       hideExplorer: true,
       theme: 'light',
