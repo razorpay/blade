237e583ec768ff091cd65ff7e4a8fa8f9ca6ccf0
anuraghazra <anurag.hazra@razorpay.com>
2023-11-09 13:20:38 +0530

    chore: add scroll example

--- packages/blade/src/components/Tour/Tour.stories.tsx
+++ packages/blade/src/components/Tour/Tour.stories.tsx
@@ -11,7 +11,6 @@ import { Code, Text } from '~components/Typography';
 import { InfoIcon } from '~components/Icons';
 import StoryPageWrapper from '~utils/storybook/StoryPageWrapper';
 import { Sandbox } from '~utils/storybook/Sandbox';
-import iconMap from '~components/Icons/iconMap';
 import { Card, CardBody } from '~components/Card';
 import { Amount } from '~components/Amount';
 import { Link } from '~components/Link';
@@ -491,70 +490,211 @@ export const CustomPlacement = () => {
 };
 CustomPlacement.storyName = 'Custom Placement';
 
-const InterruptibleTourFooter = ({
-  activeStep,
-  goToNext,
-  goToStep,
-  stopTour,
-  goToPrevious,
-  totalSteps,
-  setIsTourSkipped,
-}: StepRenderProps & {
-  setIsTourSkipped: React.Dispatch<React.SetStateAction<boolean>>;
-}): React.ReactElement => {
-  const isLast = activeStep === totalSteps - 1;
-  const isFirst = activeStep === 0;
+export const WithScrollablePage = () => {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [isOpen, setIsOpen] = React.useState(false);
+  const steps = React.useMemo<TourSteps>(
+    () => [
+      {
+        name: 'razorpay-dashboard',
+        title: 'Powerful Dashboard',
+        content: () => {
+          return (
+            <Box>
+              <Text>
+                Razorpay provides a Powerful Dashboard for you to get reports and detailed
+                statistics on payments, settlements, refunds and much more for you to take better
+                business decisions.
+              </Text>
+            </Box>
+          );
+        },
+        placement: 'bottom',
+        footer: TourFooter,
+      },
+      {
+        name: 'amazon-aws',
+        title: 'Infrastructure At Scale',
+        content: () => {
+          return (
+            <Box>
+              <Text>
+                With Amazon AWS, we are built for scale. To ensure that products built with Razorpay
+                are always available, we have a highly scalable and reliable infrastructure.
+              </Text>
+            </Box>
+          );
+        },
+        placement: 'bottom',
+        footer: TourFooter,
+      },
+      {
+        name: 'razorpay-docs',
+        title: 'Developer Friendly APIs',
+        content: () => {
+          return (
+            <Box>
+              <Text>
+                With SDKs and documentation for all major languages and platforms, Razorpay is built
+                for developers.
+              </Text>
+            </Box>
+          );
+        },
+        placement: 'left',
+        footer: TourFooter,
+      },
+    ],
+    [],
+  );
+
   return (
-    <Box display="flex" justifyContent="space-between" alignItems="center" gap="spacing.7">
-      <Text size="small" weight="bold">
-        {activeStep + 1} / {totalSteps}
-      </Text>
-      <Box display="flex" gap="spacing.4">
-        <Button
-          size="small"
-          variant="tertiary"
-          onClick={() => {
-            setIsTourSkipped(true);
-            goToStep(totalSteps - 1);
-          }}
-        >
-          Skip Tour
-        </Button>
-        {!isFirst ? (
-          <Button
-            size="small"
-            variant="secondary"
-            onClick={() => {
-              goToPrevious();
-            }}
-          >
-            Prev
-          </Button>
-        ) : null}
-        {isLast ? (
-          <Button
-            size="small"
-            onClick={() => {
-              // done
-              stopTour();
-            }}
-          >
-            Done
-          </Button>
-        ) : (
-          <Button
-            size="small"
-            onClick={() => {
-              goToNext();
-            }}
-          >
-            Next
-          </Button>
-        )}
-      </Box>
+    <Box>
+      <Button
+        marginBottom="spacing.5"
+        onClick={() => {
+          setIsOpen((prev) => !prev);
+        }}
+      >
+        {isOpen ? 'Tour In Progress' : 'Start Tour'}
+      </Button>
+      <Tour
+        steps={steps}
+        isOpen={isOpen}
+        activeStep={activeStep}
+        onFinish={() => {
+          console.log('finished');
+          setActiveStep(0);
+          setIsOpen(false);
+        }}
+        onOpenChange={({ isOpen }) => {
+          console.log('open change', isOpen);
+          setIsOpen(isOpen);
+        }}
+        onStepChange={(step) => {
+          console.log('step change', step);
+          setActiveStep(step);
+        }}
+      >
+        <Text>
+          You can pass individual placement values to each step in the popover. It supports same
+          placement values as Popover (top, bottom, left, right, top-start, top-end, bottom-start,
+          bottom-end, left-start, left-end, right-start, right-end)
+        </Text>
+        <Box>
+          <Text marginY="spacing.9">
+            A{' '}
+            <TourStep name="razorpay-dashboard">
+              <Link href="https://dashboard.razorpay.com">Powerful Dashboard</Link>
+            </TourStep>{' '}
+            for you to get reports and detailed statistics on payments, settlements, refunds and
+            much more for you to take better business decisions.
+          </Text>
+          <Box>
+            <Text>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+              has been the industry's standard dummy text ever since the 1500s, when an unknown
+              printer took a galley of type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </Text>
+            <Text>
+              The standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+              ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum." Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45
+              BC "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+              doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis
+              et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+              voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+              qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+              quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi
+              tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
+              minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
+              aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
+              voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum
+              fugiat quo voluptas nulla pariatur?" 1914 translation by H. Rackham "But I must
+              explain to you how all this mistaken idea of denouncing pleasure and praising pain was
+              born and I will give you a complete account of the system, and expound the actual
+              teachings of the great explorer of the truth, the master-builder of human happiness.
+              No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but
+              because those who do not know how to pursue pleasure rationally encounter consequences
+              that are extremely painful. Nor again is there anyone who loves or pursues or desires
+              to obtain pain of itself, because it is pain, but because occasionally circumstances
+              occur in which toil and pain can procure him some great pleasure. To take a trivial
+              example, which of us ever undertakes laborious physical exercise, except to obtain
+              some advantage from it? But who has any right to find fault with a man who chooses to
+              enjoy a pleasure that has no annoying consequences, or one who avoids a pain that
+              produces no resultant pleasure?" Section 1.10.33 of "de Finibus Bonorum et Malorum",
+              written by Cicero in 45 BC "At vero eos et accusamus et iusto odio dignissimos ducimus
+              qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
+              molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa
+              qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem
+              rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est
+              eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus,
+              omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et
+              aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates
+              repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
+              sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut
+              perferendis doloribus asperiores repellat."
+            </Text>
+            <Text>
+              1914 translation by H. Rackham "On the other hand, we denounce with righteous
+              indignation and dislike men who are so beguiled and demoralized by the charms of
+              pleasure of the moment, so blinded by desire, that they cannot foresee the pain and
+              trouble that are bound to ensue; and equal blame belongs to those who fail in their
+              duty through weakness of will, which is the same as saying through shrinking from toil
+              and pain. These cases are perfectly simple and easy to distinguish. In a free hour,
+              when our power of choice is untrammelled and when nothing prevents our being able to
+              do what we like best, every pleasure is to be welcomed and every pain avoided. But in
+              certain circumstances and owing to the claims of duty or the obligations of business
+              it will frequently occur that pleasures have to be repudiated and annoyances accepted.
+              The wise man therefore always holds in these matters to this principle of selection:
+              he rejects pleasures to secure other greater pleasures, or else he endures pains to
+              avoid worse pains."
+            </Text>
+          </Box>
+          <Text marginY="spacing.9">
+            Over the last couple of years, we have worked hard with our banking partners so you
+            don’t have to. Razorpay's servers are completely hosted on
+            <TourStep name="amazon-aws">
+              <Link href="https://aws.amazon.com/">&nbsp;Amazon AWS</Link>
+            </TourStep>{' '}
+            with auto-scaling systems that scale up to handle any traffic that you throw at it today
+            or in the future.
+          </Text>
+          <Text marginY="spacing.9">
+            Built for Developers: Robust, clean,{' '}
+            <TourStep name="razorpay-docs">
+              <Link href="https://razorpay.com/docs/api/">developer friendly APIs</Link>
+            </TourStep>{' '}
+            , plugins and libraries for all major languages and platforms that let you focus on
+            building great products.
+          </Text>
+          <Text>
+            Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC "At vero
+            eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+            deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
+            cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi,
+            id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
+            distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+            quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis
+            dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum
+            necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non
+            recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
+            voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
+          </Text>
+        </Box>
+      </Tour>
     </Box>
   );
 };
+WithScrollablePage.storyName = 'With Scrollable Page';
 
 export const MultipleTourFlows = () => {
   const [activeStep, setActiveStep] = React.useState(0);
@@ -691,6 +831,71 @@ export const MultipleTourFlows = () => {
           </Box>
         </Center>
       </Tour>
+    </Box>
+  );
+};
+
+const InterruptibleTourFooter = ({
+  activeStep,
+  goToNext,
+  goToStep,
+  stopTour,
+  goToPrevious,
+  totalSteps,
+  setIsTourSkipped,
+}: StepRenderProps & {
+  setIsTourSkipped: React.Dispatch<React.SetStateAction<boolean>>;
+}): React.ReactElement => {
+  const isLast = activeStep === totalSteps - 1;
+  const isFirst = activeStep === 0;
+  return (
+    <Box display="flex" justifyContent="space-between" alignItems="center" gap="spacing.7">
+      <Text size="small" weight="bold">
+        {activeStep + 1} / {totalSteps}
+      </Text>
+      <Box display="flex" gap="spacing.4">
+        <Button
+          size="small"
+          variant="tertiary"
+          onClick={() => {
+            setIsTourSkipped(true);
+            goToStep(totalSteps - 1);
+          }}
+        >
+          Skip Tour
+        </Button>
+        {!isFirst ? (
+          <Button
+            size="small"
+            variant="secondary"
+            onClick={() => {
+              goToPrevious();
+            }}
+          >
+            Prev
+          </Button>
+        ) : null}
+        {isLast ? (
+          <Button
+            size="small"
+            onClick={() => {
+              // done
+              stopTour();
+            }}
+          >
+            Done
+          </Button>
+        ) : (
+          <Button
+            size="small"
+            onClick={() => {
+              goToNext();
+            }}
+          >
+            Next
+          </Button>
+        )}
+      </Box>
     </Box>
   );
 };
