71ffd3fa70346cbe57872c40ac1b08383bf975b2
anuraghazra <anurag.hazra@razorpay.com>
2023-11-10 17:11:42 +0530

    chore: rename native & web files

---
+++ packages/blade/src/components/Tour/TourPopover.native.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { throwBladeError } from '~utils/logger';
+
+const TourPopoveer = (_: any): React.ReactElement => {
+  throwBladeError({
+    message: 'Tour is not yet implemented for native',
+    moduleName: 'Tour',
+  });
+
+  return <></>;
+};
+
+export { TourPopoveer };
