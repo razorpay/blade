b35108ac573f30694d7759e967a519fef710ea73
saurabhdaware <saurabh.daware@razorpay.com>
2023-11-10 15:17:18 +0530

    feat: change font for storybook

--- packages/blade/.storybook/react/preview.js
+++ packages/blade/.storybook/react/preview.js
@@ -9,6 +9,7 @@ import { INTERNAL_STORY_ADDON_PARAM } from './constants';
 const { GlobalStyle } = global;
 import { DocsContainer } from '@storybook/addon-docs/blocks';
 import './global.css';
+import '../../fonts.css';
 
 export const parameters = {
   // disable snapshot by default and then enable it only for kitchen sink
