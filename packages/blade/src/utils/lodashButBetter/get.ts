4b68e60378ddaca1278c762f7f2252649699e74e
saurabhdaware <saurabh.daware@razorpay.com>
2023-11-19 13:50:24 +0530

    fix: get function

--- packages/blade/src/utils/lodashButBetter/get.ts
+++ packages/blade/src/utils/lodashButBetter/get.ts
@@ -1,16 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-function getIn(obj: Record<string, any>, path: string, defaultValue?: any): any {
+function getIn(obj: Record<string, any>, path: string): any {
+  if (!path) {
+    return undefined;
+  }
+
   const keys = path.split('.');
   let result = obj;
 
   for (const key of keys) {
     result = result?.[key];
     if (result === undefined) {
-      return defaultValue;
+      return undefined;
     }
   }
 
-  return result !== undefined ? result : defaultValue;
+  return result;
 }
 
 export default getIn;
