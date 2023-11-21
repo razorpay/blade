86782a1c5343a4171ada47449c0fb01225eb6d7e
rzpcibot <tools+cibot@razorpay.com>
2023-11-21 18:37:03 +0530

    fix: update the workflow script

--- packages/blade/scripts/uploadTokens.js
+++ packages/blade/scripts/uploadTokens.js
@@ -9,43 +9,41 @@ const GITHUB_BOT_USERNAME = 'rzpcibot';
 const uploadColorTokens = async () => {
   try {
     // 1. read the tokens object
-    const tokens = JSON.parse(process.argv[2]);
+    const colorTokens = JSON.parse(process.argv[2]);
 
     const colorRegex = /const colors: ColorsWithModes = {(.|\n)+?};/gm;
 
-    if (tokens.paymentThemeColors) {
-      // 2. read the paymentTheme File
-      const paymentThemePath = path.resolve(__dirname, '../src/tokens/theme/paymentTheme.ts');
-      const paymentTheme = fs.readFileSync(paymentThemePath, 'utf8');
+    // if (tokens.bladeThemeColors) {
+    // 2. read the bladeTheme File
+    const bladeThemePath = path.resolve(__dirname, '../src/tokens/theme/bladeTheme.ts');
+    const bladeTheme = fs.readFileSync(bladeThemePath, 'utf8');
 
-      // 3. write the new tokens to paymentTheme file
-      const updatedPaymentThemeColors = JSON.stringify(tokens.paymentThemeColors.colors).replace(
-        /"/g,
-        '',
-      );
-      const updatedPaymentTheme = paymentTheme.replace(
-        colorRegex,
-        `const colors: ColorsWithModes = ${updatedPaymentThemeColors};`,
-      );
-      fs.writeFileSync(paymentThemePath, updatedPaymentTheme);
-    }
+    // 3. write the new tokens to bladeTheme file
+    console.log(colorTokens);
+    const updatedbladeThemeColors = JSON.stringify(colorTokens).replace(/"/g, '');
+    const updatedbladeTheme = bladeTheme.replace(
+      colorRegex,
+      `const colors: ColorsWithModes = ${updatedbladeThemeColors};`,
+    );
+    fs.writeFileSync(bladeThemePath, updatedbladeTheme);
+    // }
 
-    if (tokens.bankingThemeColors) {
-      // 4. read the bankingTheme File
-      const bankingThemePath = path.resolve(__dirname, '../src/tokens/theme/bankingTheme.ts');
-      const bankingTheme = fs.readFileSync(bankingThemePath, 'utf8');
+    // if (tokens.bankingThemeColors) {
+    //   // 4. read the bankingTheme File
+    //   const bankingThemePath = path.resolve(__dirname, '../src/tokens/theme/bankingTheme.ts');
+    //   const bankingTheme = fs.readFileSync(bankingThemePath, 'utf8');
 
-      // 5. write the new tokens to bankingTheme file
-      const updatedBankingThemeColors = JSON.stringify(tokens.bankingThemeColors.colors).replace(
-        /"/g,
-        '',
-      );
-      const updatedBankingTheme = bankingTheme.replace(
-        colorRegex,
-        `const colors: ColorsWithModes = ${updatedBankingThemeColors};`,
-      );
-      fs.writeFileSync(bankingThemePath, updatedBankingTheme);
-    }
+    //   // 5. write the new tokens to bankingTheme file
+    //   const updatedBankingThemeColors = JSON.stringify(tokens.bankingThemeColors.colors).replace(
+    //     /"/g,
+    //     '',
+    //   );
+    //   const updatedBankingTheme = bankingTheme.replace(
+    //     colorRegex,
+    //     `const colors: ColorsWithModes = ${updatedBankingThemeColors};`,
+    //   );
+    //   fs.writeFileSync(bankingThemePath, updatedBankingTheme);
+    // }
 
     // 6. create branch
     const branchName = randomNameGenerator
@@ -56,14 +54,14 @@ const uploadColorTokens = async () => {
     execa.commandSync(`git config user.name ${GITHUB_BOT_USERNAME}`);
 
     // 7. Commit all changes
-    execa.commandSync('yarn prettier --write src/tokens/theme/*.ts');
+    execa.commandSync('yarn prettier --write src/tokens/theme/bladeTheme.ts');
     execa.commandSync('git status');
     execa.commandSync('git add -A');
     execa.commandSync(`git commit -m feat(tokens):\\ add\\ new\\ tokens`, {
       env: { HUSKY_SKIP_HOOKS: 1 },
     });
 
-    // 8. Raise a PR
+    // // 8. Raise a PR
     execa.commandSync(`git push origin ${branchName}`);
     execa.commandSync(
       `gh pr create --title feat(tokens):\\ add\\ new\\ tokens --head ${branchName} --repo razorpay/blade --body This\\ PR\\ was\\ opened\\ by\\ the\\ Token\\ Upload\\ GitHub\\ action.\\ It\\ updates\\ source\\ token\\ files\\ based\\ on\\ the\\ payload\\ from\\ Figma\\ Plugin.`,
