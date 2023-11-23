4261a5a0840e3ad31871c0a15f65354367ff6376
anuraghazra <anurag.hazra@razorpay.com>
2023-11-23 13:44:25 +0530

    chore: update examples

--- packages/examples/basic/src/App.tsx
+++ packages/examples/basic/src/App.tsx
@@ -1,17 +1,12 @@
-// @ts-nocheck
-import { BladeProvider } from '@razorpay/blade/components';
-// import '@fontsource/lato/400.css';
-// import '@fontsource/lato/400-italic.css';
-// import '@fontsource/lato/700.css';
-// import '@fontsource/lato/700-italic.css';
+import { BladeProvider, Button } from '@razorpay/blade/components';
+import { paymentTheme } from '@razorpay/blade/tokens';
 
-function App(): JSX.Element {
-  return <BladeProvider>hello</BladeProvider>;
-  // return (
-  //   // <BladeProvider themeTokens={paymentTheme} colorScheme="light">
-  //   //   {/* <Button onClick={() => console.log('hi')}>Hello</Button> */}
-  //   // </BladeProvider>
-  // );
+function App(): React.ReactElement {
+  return (
+    <BladeProvider colorScheme="light" themeTokens={paymentTheme}>
+      <Button onClick={() => console.log('hi')}>Hello</Button>
+    </BladeProvider>
+  );
 }
 
 export default App;
