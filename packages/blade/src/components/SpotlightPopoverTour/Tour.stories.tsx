ff2b4c0e44f8a8d8b5a87da24f1be65e2b99d9f6
anuraghazra <anurag.hazra@razorpay.com>
2023-11-27 16:36:43 +0530

    chore: fix stackblitz

--- packages/blade/src/components/SpotlightPopoverTour/Tour.stories.tsx
+++ packages/blade/src/components/SpotlightPopoverTour/Tour.stories.tsx
@@ -43,11 +43,80 @@ const Page = (): React.ReactElement => {
           SpotlightPopoverTour,
           SpotlightPopoverTourStep,
           SpotlightPopoverTourFooter,
+          SpotlightPopoverStepRenderProps,
+          SpotlightPopoverTourSteps,
           Box,
           Text,
           Button
         } from '@razorpay/blade/components';
-        
+        import type {
+          SpotlightPopoverStepRenderProps,
+          SpotlightPopoverTourSteps,
+        } from '@razorpay/blade/components';
+
+        const CustomTourFooter = ({
+          activeStep,
+          totalSteps,
+          goToNext,
+          goToPrevious,
+          stopTour,
+        }: SpotlightPopoverStepRenderProps) => {
+          const isLast = activeStep === totalSteps - 1;
+          const isFirst = activeStep === 0;
+          return (
+            <SpotlightPopoverTourFooter
+              activeStep={activeStep}
+              totalSteps={totalSteps}
+              actions={{
+                primary: isLast
+                  ? {
+                      text: 'Done',
+                      onClick: stopTour,
+                    }
+                  : {
+                      text: 'Next',
+                      onClick: goToNext,
+                    },
+                secondary: isFirst
+                  ? undefined
+                  : {
+                      text: 'Prev',
+                      onClick: goToPrevious,
+                    },
+              }}
+            />
+          );
+        };
+
+        const steps: SpotlightPopoverTourSteps = [
+          {
+            name: 'step-1',
+            title: 'Step 1',
+            content: () => {
+              return (
+                <Box>
+                  <Text>This is step 1</Text>
+                </Box>
+              );
+            },
+            placement: 'top',
+            footer: CustomTourFooter,
+          },
+          {
+            name: 'step-2',
+            title: 'Step 2',
+            content: () => {
+              return (
+                <Box>
+                  <Text>This is step 2</Text>
+                </Box>
+              );
+            },
+            placement: 'bottom',
+            footer: CustomTourFooter,
+          },
+        ];
+
         function App(): React.ReactElement {
           const [activeStep, setActiveStep] = React.useState(0);
           const [isOpen, setIsOpen] = React.useState(false);
