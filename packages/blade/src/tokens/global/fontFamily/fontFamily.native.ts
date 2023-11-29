a9ca50d85f89e62500eb3b10fa7a791bdf88e196
Saurabh Daware <saurabh.daware@razorpay.com>
2023-11-29 14:06:37 +0530

    feat(Rebranding / Typography): font-family changes (#1804)
    * feat: add fonts file and css
    
    * feat: add fonts to files
    
    * feat: add fonts export
    
    * feat: add lighter font
    
    * add display swap for tasa
    
    * feat: change font family tokens
    
    * feat: add heading font family for native
    
    * feat: change font for storybook
    
    * feat: add fonts from csb
    
    * feat: use inter variable font
    
    * feat: generate razorpay's latin fonts
    
    * fix: unicode range
    
    * refactor: remove unused fonts
    
    * fix: standardise font file names
    
    * fix: blade-extra name

--- packages/blade/src/tokens/global/fontFamily/fontFamily.native.ts
+++ packages/blade/src/tokens/global/fontFamily/fontFamily.native.ts
@@ -2,6 +2,7 @@ import { Platform } from 'react-native';
 import type { FontFamily } from './types';
 
 export const fontFamily: FontFamily = {
-  text: 'Lato',
+  text: 'Inter',
+  heading: 'Tasa Orbiter',
   code: Platform.OS === 'ios' ? 'Courier' : 'monospace',
 };
