8987dfb68bbd58551b65cf2b48a03f024747d3b2
anuraghazra <anurag.hazra@razorpay.com>
2023-12-14 15:18:04 +0530

    chore: typesafe getIn

--- packages/blade/src/utils/lodashButBetter/get.ts
+++ packages/blade/src/utils/lodashButBetter/get.ts
@@ -1,5 +1,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-function getIn(obj: Record<string, any>, path: string, defaultValue?: any): any {
+/* eslint-disable @typescript-eslint/no-unnecessary-type-constraint */
+import type { EasingFactoryFn } from '~tokens/global';
+
+/**
+ * @template TokenType token type generic
+ * @description Tokenises objects to dot notation strings, eg: `surface.text.normal.lowContrast`
+ */
+export type DotNotationToken<TokenType> = {
+  [K in keyof TokenType]: `${Extract<K, number | string>}.${TokenType[K] extends Record<
+    string,
+    string | number | boolean | EasingFactoryFn
+  >
+    ? Extract<keyof TokenType[K], number | string>
+    : DotNotationToken<TokenType[K]>}`;
+}[keyof TokenType];
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+function getIn<
+  T extends Record<string, any>,
+  K extends DotNotationToken<T>,
+  Default extends any = any
+>(obj: T, path: K, defaultValue?: Default): any {
   if (!path) {
     return defaultValue;
   }
