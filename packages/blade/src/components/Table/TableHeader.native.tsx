ff3dec6230a6230cd35003af12b2dabc6561dac1
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2023-12-20 15:37:16 +0530

    fix types

--- packages/blade/src/components/Table/TableHeader.native.tsx
+++ packages/blade/src/components/Table/TableHeader.native.tsx
@@ -1,17 +1,18 @@
 /* eslint-disable react/no-unused-prop-types */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
+import type { TableHeaderCellProps, TableHeaderProps, TableHeaderRowProps } from './types';
 import { Text } from '~components/Typography';
 
-const TableHeader = (props: unknown): React.ReactElement => {
+const TableHeader = (props: TableHeaderProps): React.ReactElement => {
   return <Text>Table Component is not available for Native mobile apps.</Text>;
 };
 
-const TableHeaderRow = (props: unknown): React.ReactElement => {
+const TableHeaderRow = (props: TableHeaderRowProps): React.ReactElement => {
   return <Text>Table Component is not available for Native mobile apps.</Text>;
 };
 
-const TableHeaderCell = (props: unknown): React.ReactElement => {
+const TableHeaderCell = (props: TableHeaderCellProps): React.ReactElement => {
   return <Text>Table Component is not available for Native mobile apps.</Text>;
 };
 
