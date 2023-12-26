4486cd96359c900833432ed6d6281ddc698db6e7
anuraghazra <anurag.hazra@razorpay.com>
2023-12-26 16:29:45 +0530

    refactor: rebrand carousel

--- packages/blade/.storybook/react/main.js
+++ packages/blade/.storybook/react/main.js
@@ -24,6 +24,7 @@ module.exports = {
     '../../src/components/Indicator/**/*.stories.@(ts|tsx|js|jsx)',
     '../../src/components/Button/**/*.stories.@(ts|tsx|js|jsx)',
     '../../src/components/Divider/**/*.stories.@(ts|tsx|js|jsx)',
+    '../../src/components/Carousel/**/*.stories.@(ts|tsx|js|jsx)',
     '../../src/components/List/**/*.stories.@(ts|tsx|js|jsx)',
     '../../src/components/Link/**/*.stories.@(ts|tsx|js|jsx)',
     '../../src/components/Tooltip/**/*.stories.@(ts|tsx|js|jsx)',
