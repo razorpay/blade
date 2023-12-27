77ae0a48147ee076108c2cab970f04f4e7cc8395
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2023-12-27 17:12:38 +0530

    fix tokens for web

--- packages/blade/src/components/Accordion/StyledAccordionButton.web.tsx
+++ packages/blade/src/components/Accordion/StyledAccordionButton.web.tsx
@@ -18,25 +18,20 @@ const StyledAccordionButton = styled.button<StyledAccordionButtonProps>((props) 
     transitionDuration: castWebType(getTransitionDuration(theme)),
     transitionTimingFunction: castWebType(getTransitionEasing(theme)),
     cursor: 'pointer',
-    color: theme.colors.surface.text.gray.muted,
+    color: theme.colors.interactive.icon.gray[isExpanded ? 'subtle' : 'muted'],
     width: '100%',
     border: 'none',
     textAlign: 'left',
 
     '&:hover, &:focus-visible': {
       backgroundColor: getBackgroundColor({ theme, isExpanded, isActive: true }),
-    },
-
-    '&:hover': {
       color: theme.colors.interactive.icon.gray.subtle,
     },
-
     '&:focus-visible': {
       outline: 'none',
       boxShadow: `0px 0px 0px 4px ${theme.colors.surface.border.primary.muted}`,
       // only need border radius on the focus ring
       borderRadius: theme.border.radius.small,
-      color: theme.colors.interactive.icon.gray.subtle,
     },
   };
 });
