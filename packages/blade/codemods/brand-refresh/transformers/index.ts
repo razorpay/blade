86b1f70eafc6499d7ab1064b6e600a900118357f
Nitin <nitin.kumar@razorpay.com>
2023-12-28 13:01:21 +0530

    fix: migrate Divider component

--- packages/blade/codemods/brand-refresh/transformers/index.ts
+++ packages/blade/codemods/brand-refresh/transformers/index.ts
@@ -635,6 +635,27 @@ const transformer: Transform = (file, api, options) => {
     );
   }
 
+  // Divider component <Divider variant=”normal”> -> <Divider variant=”muted”>
+  try {
+    root
+      .find(j.JSXElement)
+      .filter((path) => ['Divider'].includes(path.value.openingElement.name.name))
+      .find(j.JSXAttribute)
+      .filter((path) => path.node.name.name === 'variant' && path.node.value.value === 'normal')
+      .replaceWith((path) => {
+        const { node } = path;
+
+        node.value.value = 'muted';
+
+        return node;
+      });
+  } catch (error) {
+    console.error(
+      red(`⛔️ ${file.path}: Oops! Ran into an issue while updating the Divider component.`),
+      `\n${red(error.stack)}\n`,
+    );
+  }
+
   // Return the updated source code
   return (
     root
