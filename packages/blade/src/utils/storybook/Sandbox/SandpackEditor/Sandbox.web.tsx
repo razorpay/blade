a591251054587cd2d887963f1d8a04438b042df1
saurabhdaware <saurabh.daware@razorpay.com>
2024-01-02 14:50:18 +0530

    fix: css issues in storybook docs

--- packages/blade/src/utils/storybook/Sandbox/SandpackEditor/Sandbox.web.tsx
+++ packages/blade/src/utils/storybook/Sandbox/SandpackEditor/Sandbox.web.tsx
@@ -51,7 +51,9 @@ const CodeLineHighlighterContainer = styled(BaseBox)((_props) => ({
     padding: '2px',
   },
   '& pre': {
+    fontFamily: 'Menlo, monospace',
     padding: '0px',
+    lineHeight: '1000 !important',
   },
   borderRadius: '4px',
   overflow: 'auto',
@@ -69,6 +71,9 @@ const SandboxHighlighter = ({
         theme={theme}
         files={{
           '/src/index.ts': dedent(children),
+        }}
+        style={{
+          fontFamily: 'Arial',
         }}
       >
         <SandpackCodeViewer showLineNumbers {...sandpackCodeViewerProps} />
