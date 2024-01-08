3066e653bbe38ee985c03b9c74a069af05ac49bc
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2024-01-08 16:17:19 +0530

    update focus ring styles

--- packages/blade/src/utils/getFocusRingStyles/getFocusRingStyles.web.test.ts
+++ packages/blade/src/utils/getFocusRingStyles/getFocusRingStyles.web.test.ts
@@ -7,6 +7,9 @@ describe('getFocusRingStyles', () => {
     expect(result).toMatchObject({
       outline: '4px solid hsla(227, 100%, 59%, 0.18)',
       outlineOffset: '1px',
+      transitionDuration: '70ms',
+      transitionProperty: 'outline-width',
+      transitionTimingFunction: 'cubic-bezier(0.3, 0, 0.2, 1)',
     });
   });
 });
--- packages/blade/src/utils/getFocusRingStyles/getFocusRingStyles.web.ts
+++ packages/blade/src/utils/getFocusRingStyles/getFocusRingStyles.web.ts
@@ -1,10 +1,14 @@
 import type { CSSProperties } from 'styled-components';
 import type { Theme } from '../../components/BladeProvider';
+import { castWebType, makeMotionTime } from '~utils';
 
 function getFocusRingStyles(theme: Theme): CSSProperties {
   return {
     outline: `4px solid ${theme.colors.surface.border.primary.muted}`,
     outlineOffset: '1px',
+    transitionProperty: 'outline-width',
+    transitionDuration: castWebType(makeMotionTime(theme.motion.duration['2xquick'])),
+    transitionTimingFunction: castWebType(theme.motion.easing.standard.effective),
   };
 }
 
