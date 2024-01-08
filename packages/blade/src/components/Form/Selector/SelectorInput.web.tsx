bb02c792e89629d35842809b605ba9a4a32b7efe
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2024-01-08 15:39:48 +0530

    update SelectorInput

--- packages/blade/src/components/Form/Selector/SelectorInput.web.tsx
+++ packages/blade/src/components/Form/Selector/SelectorInput.web.tsx
@@ -28,8 +28,13 @@ const getHoverStyles = ({
   const borderColor = hoverTokens.default.border?.[checked];
 
   return {
-    borderColor: borderColor ? getIn(theme, borderColor) : undefined,
-    backgroundColor: getIn(theme, backgroundColor),
+    borderColor: borderColor
+      ? borderColor === 'transparent'
+        ? 'transparent'
+        : getIn(theme, borderColor)
+      : undefined,
+    backgroundColor:
+      backgroundColor === 'transparent' ? 'transparent' : getIn(theme, backgroundColor),
     transitionTimingFunction: theme.motion.easing.standard.effective as string,
     transitionDuration: castWebType(makeMotionTime(theme.motion.duration['2xquick'])),
   };
