c5cb2b918cefc6b84a3fd31f7115062da0955f6f
saurabhdaware <saurabh.daware@razorpay.com>
2024-01-11 18:48:54 +0530

    feat: move major interaction tests to e2e

--- packages/blade/src/components/Dropdown/__tests__/Dropdown.test.stories.tsx
+++ packages/blade/src/components/Dropdown/__tests__/Dropdown.test.stories.tsx
@@ -1,191 +1,188 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import type { StoryFn } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
-import { expect, jest } from '@storybook/jest';
-import type { Mock } from 'jest-mock';
 import React from 'react';
+import type { StoryFn } from '@storybook/react';
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { Dropdown, DropdownOverlay, DropdownFooter } from '../';
 import type { DropdownProps } from '../';
-import { Dropdown } from '../';
-import { Box } from '~components/Box';
+import { SelectInput } from '~components/Input/DropdownInputTriggers';
+import { ActionList, ActionListItem } from '~components/ActionList';
+import { Button } from '~components/Button';
 
-const sleep = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
+const getActiveDescendant = (selectInput: HTMLElement): string | null | undefined => {
+  const activeDescendantId = selectInput.getAttribute('aria-activedescendant');
+  const activeDescendantElement = document.querySelector(`#${activeDescendantId}`);
+  return activeDescendantElement?.textContent;
+};
 
-let onChange: Mock<unknown, unknown[]> | null = null;
-
-const BasicDropdown = (props: DropdownProps): React.ReactElement => (
-  <Box margin="auto" width={{ base: '100%', m: '100%' }} padding="spacing.4">
-    <Dropdown {...props} />
-  </Box>
+const BasicDropdown = ({
+  items = ['Mumbai', 'Bengaluru', 'Pune'],
+  ...props
+}: DropdownProps & {
+  items?: string[];
+}): React.ReactElement => (
+  <Dropdown {...props}>
+    <SelectInput label="City" />
+    <DropdownOverlay testID="dropdown-overlay">
+      <ActionList>
+        {items.map((item) => (
+          <ActionListItem key={item} title={item} value={item.toLowerCase()} />
+        ))}
+      </ActionList>
+    </DropdownOverlay>
+  </Dropdown>
 );
 
-export const TestCarouselOnChange: StoryFn<typeof Dropdown> = (props): React.ReactElement => {
-  onChange = jest.fn();
-  return <BasicCarousel {...props} onChange={onChange} />;
+export const BasicSelectItem: StoryFn<typeof Dropdown> = (props): React.ReactElement => {
+  return <BasicDropdown {...props} />;
 };
 
-TestCarouselOnChange.play = async ({ canvasElement }) => {
-  const { getByRole, getByText } = within(canvasElement);
-  const nextButton = getByRole('button', { name: 'Next Slide' });
-  const previousButton = getByRole('button', { name: 'Previous Slide' });
-  await userEvent.click(nextButton);
-  await sleep(1000);
-  await expect(onChange).toBeCalledWith(1);
-  await userEvent.click(previousButton);
-  await sleep(1000);
-  await expect(onChange).toBeCalledWith(0);
-  getByText(/Single Flow To Collect And Disburse Payments/)?.scrollIntoView({ behavior: 'smooth' });
-  await sleep(1000);
-  await expect(onChange).toBeCalledWith(3);
-  await expect(onChange).toBeCalledTimes(3);
-};
-
-export const TestIndicatorButton: StoryFn<typeof CarouselComponent> = (
-  props,
-): React.ReactElement => {
-  onChange = jest.fn();
-  return <BasicCarousel {...props} visibleItems={1} onChange={onChange} />;
-};
-
-TestIndicatorButton.play = async ({ canvasElement }) => {
-  const { getByLabelText } = within(canvasElement);
-  const indicatorButton = getByLabelText('Slide 7');
-  await userEvent.click(indicatorButton);
-  await sleep(1000);
-  await expect(onChange).toBeCalledWith(6);
-  await expect(onChange).toBeCalledTimes(1);
-};
-
-export const TestStartOverAfterStartEnd: StoryFn<typeof CarouselComponent> = (
-  props,
-): React.ReactElement => {
-  onChange = jest.fn();
-  return <BasicCarousel {...props} visibleItems={1} onChange={onChange} />;
-};
-
-TestStartOverAfterStartEnd.play = async ({ canvasElement }) => {
+BasicSelectItem.play = async ({ canvasElement }) => {
   const { getByRole } = within(canvasElement);
-  const nextButton = getByRole('button', { name: 'Next Slide' });
-  const previousButton = getByRole('button', { name: 'Previous Slide' });
-  await userEvent.click(previousButton);
-  await sleep(1000);
-  await expect(onChange).toBeCalledWith(6);
-  await userEvent.click(nextButton);
-  await sleep(1000);
-  await expect(onChange).toBeCalledWith(0);
-  await expect(onChange).toBeCalledTimes(2);
+  const selectInput = getByRole('combobox', { name: 'City' });
+  await userEvent.click(selectInput);
+  const option = getByRole('option', { name: 'Bengaluru' });
+  await userEvent.click(option);
+  await expect(getByRole('combobox', { name: 'City' })).toHaveTextContent('Bengaluru');
 };
 
-export const TestAutoPlay: StoryFn<typeof CarouselComponent> = (props): React.ReactElement => {
-  onChange = jest.fn();
-  return <BasicCarousel {...props} autoPlay visibleItems={2} onChange={onChange} />;
+export const MultiSelectItem: StoryFn<typeof Dropdown> = (props): React.ReactElement => {
+  return <BasicDropdown {...props} selectionType="multiple" />;
 };
 
-TestAutoPlay.play = async ({ canvasElement }) => {
-  const { getByRole } = within(canvasElement);
-  await sleep(8000);
-  await expect(onChange).toBeCalledWith(1);
-  await expect(getByRole('tab', { selected: true })).toHaveAccessibleName('Slide 3');
-  await expect(onChange).toBeCalledTimes(1);
+MultiSelectItem.play = async ({ canvasElement }) => {
+  const { getByRole, getByLabelText, queryByLabelText } = within(canvasElement);
+  const selectInput = getByRole('combobox', { name: 'City' });
+  await userEvent.click(selectInput);
+  await expect(queryByLabelText('Close Bengaluru tag')).toBeFalsy();
+  await userEvent.click(getByRole('option', { name: 'Bengaluru' }));
+  await userEvent.click(getByRole('option', { name: 'Pune' }));
+  await expect(getByLabelText('Close Bengaluru tag')).toBeInTheDocument();
+  await expect(getByLabelText('Close Pune tag')).toBeInTheDocument();
+  await expect(queryByLabelText('Close Mumbai tag')).toBeFalsy();
 };
 
-export const TestAutofit: StoryFn<typeof CarouselComponent> = (props): React.ReactElement => {
-  onChange = jest.fn();
+export const Accessibility: StoryFn<typeof Dropdown> = (props): React.ReactElement => {
   return (
-    <BasicCarousel
+    <BasicDropdown
       {...props}
-      visibleItems="autofit"
-      navigationButtonPosition="side"
-      showIndicators={true}
-      onChange={onChange}
-      shouldAddStartEndSpacing
-      carouselItemWidth="300px"
+      items={[
+        'Mumbai',
+        'Bengaluru',
+        'Pune',
+        'Delhi',
+        'Hyderabad',
+        'Chennai',
+        'Kolkata',
+        'Ahmedabad',
+        'Jaipur',
+        'Lucknow',
+        'Kanpur',
+        'Nagpur',
+        'Patna',
+      ]}
     />
   );
 };
 
-TestAutofit.play = async ({ canvasElement }) => {
-  await sleep(1000);
-  const { getByLabelText, queryByRole } = within(canvasElement);
-  const lastIndicatorButton = getByLabelText('Slide 7');
-  await expect(onChange).not.toBeCalled();
-  await userEvent.click(lastIndicatorButton);
-  await sleep(1000);
-  const nextButton = queryByRole('button', { name: 'Next Slide' });
-  await expect(nextButton).toBeNull();
-  const firstIndicatorButton = getByLabelText('Slide 1');
-  await userEvent.click(firstIndicatorButton);
-  await sleep(1000);
-  const previousButton = queryByRole('button', { name: 'Previous Slide' });
-  await expect(previousButton).toBeNull();
-  await expect(onChange).toBeCalledTimes(2);
+Accessibility.play = async ({ canvasElement }) => {
+  const { getByRole, getByTestId } = within(canvasElement);
+  const selectInput = getByRole('combobox', { name: 'City' });
+  selectInput.focus();
+  await userEvent.keyboard('{ArrowDown}');
+
+  // move to 1st item
+  await userEvent.keyboard('{ArrowDown}');
+  await expect(getActiveDescendant(selectInput)).toBe('Mumbai');
+
+  // move to 2nd item
+  await userEvent.keyboard('{ArrowDown}');
+  await expect(getActiveDescendant(selectInput)).toBe('Bengaluru');
+
+  // move to 1st item
+  await userEvent.keyboard('{Home}');
+  await expect(getActiveDescendant(selectInput)).toBe('Mumbai');
+
+  // move 10 items down or to last item
+  await userEvent.keyboard('{PageDown}');
+  await expect(getActiveDescendant(selectInput)).toBe('Kanpur');
+  await userEvent.keyboard('{PageDown}');
+  await expect(getActiveDescendant(selectInput)).toBe('Patna');
+
+  // move 10 items up
+  await userEvent.keyboard('{PageUp}');
+  await expect(getActiveDescendant(selectInput)).toBe('Pune');
+
+  // Move to last item
+  await userEvent.keyboard('{End}');
+  await expect(getActiveDescendant(selectInput)).toBe('Patna');
+
+  // [TypeAhead tests] Move to item starting with "P"
+  await userEvent.keyboard('p');
+  await expect(getActiveDescendant(selectInput)).toBe('Pune');
+  await userEvent.keyboard('a');
+  await expect(getActiveDescendant(selectInput)).toBe('Patna');
+
+  // select item
+  await userEvent.keyboard('{Enter}');
+  await expect(getByRole('combobox', { name: 'City' })).toHaveTextContent('Patna');
+  await waitFor(() => expect(getByTestId('dropdown-overlay')).not.toBeVisible());
+
+  // close dropdown
+  await userEvent.keyboard('{ArrowDown}');
+  await waitFor(() => expect(getByTestId('dropdown-overlay')).toBeVisible());
+  await userEvent.keyboard('{Escape}');
+  await waitFor(() => expect(getByTestId('dropdown-overlay')).not.toBeVisible());
+
+  // Input focus test
+  await expect(selectInput).toHaveFocus();
+  await userEvent.keyboard('{TAB}');
+  await expect(selectInput).not.toHaveFocus();
 };
 
-export const TestAutoPlayPause: StoryFn<typeof CarouselComponent> = (props): React.ReactElement => {
-  onChange = jest.fn();
-  return <BasicCarousel {...props} autoPlay visibleItems={2} onChange={onChange} />;
+export const FooterActions: StoryFn<typeof Dropdown> = (): React.ReactElement => {
+  const [hasApplied, setHasApplied] = React.useState(false);
+  return (
+    <Dropdown>
+      <SelectInput label="Fruits" />
+      <DropdownOverlay testID="dropdown-overlay">
+        <ActionList>
+          <ActionListItem title="Apple" value="apple" />
+          <ActionListItem title="Mango" value="mango" />
+        </ActionList>
+        <DropdownFooter>
+          {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
+          <Button onClick={() => setHasApplied(true)}>{hasApplied ? 'Applied' : 'Apply'}</Button>
+          <Button marginLeft="spacing.4" variant="secondary" onClick={() => setHasApplied(false)}>
+            Cancel
+          </Button>
+        </DropdownFooter>
+      </DropdownOverlay>
+    </Dropdown>
+  );
 };
 
-TestAutoPlayPause.play = async ({ canvasElement }) => {
-  const { getByText } = within(canvasElement);
-  const slide = getByText(/Acquire Customers From New Customer Segments/);
-  await userEvent.hover(slide);
-  await sleep(7000);
-  await expect(onChange).not.toHaveBeenCalled();
-};
+FooterActions.play = async ({ canvasElement }) => {
+  const { getByRole, queryByRole, getByTestId } = within(canvasElement);
+  const selectInput = getByRole('combobox', { name: 'Fruits' });
+  await userEvent.click(selectInput);
+  await waitFor(() => expect(getByRole('dialog', { name: 'Fruits' })).toBeVisible());
+  await userEvent.keyboard('{ArrowDown}');
+  // virtual focus on first item, actual focus on select
+  await expect(getActiveDescendant(selectInput)).toBe('Apple');
+  await expect(selectInput).toHaveFocus();
 
-export const TestVisibleItemsOnMobile: StoryFn<typeof CarouselComponent> = (
-  props,
-): React.ReactElement => {
-  onChange = jest.fn();
-  return <BasicCarousel {...props} visibleItems={3} onChange={onChange} />;
-};
+  // move focus to next interactive item in footer without closing the dropdown
+  await userEvent.keyboard('{TAB}');
+  await expect(getByRole('button', { name: 'Apply' })).toHaveFocus();
+  await expect(queryByRole('button', { name: 'Applied' })).toBeFalsy();
+  await userEvent.keyboard('{Enter}');
+  await waitFor(() => expect(getByRole('button', { name: 'Applied' })).toBeInTheDocument());
 
-TestVisibleItemsOnMobile.parameters = {
-  viewport: {
-    defaultViewport: 'iPhone6',
-  },
-};
-
-TestVisibleItemsOnMobile.play = async ({ canvasElement }) => {
-  // on mobile regardless of the visible items prop we always show 1 item
-  const { getByRole } = within(canvasElement);
-  const nextButton = getByRole('button', { name: 'Next Slide' });
-  await userEvent.click(nextButton);
-  await sleep(1000);
-  await expect(onChange).toBeCalledWith(1);
-};
-
-// Test for onChange fires multiple times on parent component update
-// https://github.com/razorpay/blade/issues/1863
-const multipleOnChange = jest.fn();
-export const TestOnChangeParentUpdate: StoryFn<typeof CarouselComponent> = (
-  props,
-): React.ReactElement => {
-  const [, setCount] = React.useState(0);
-
-  React.useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCount((prev) => prev++);
-    }, 100);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  return <BasicCarousel {...props} onChange={multipleOnChange} />;
-};
-
-TestOnChangeParentUpdate.play = async ({ canvasElement }) => {
-  const { getByRole } = within(canvasElement);
-  await expect(multipleOnChange).not.toBeCalled();
-  const nextButton = getByRole('button', { name: 'Next Slide' });
-  const previousButton = getByRole('button', { name: 'Previous Slide' });
-  await userEvent.click(nextButton);
-  await sleep(1000);
-  await expect(multipleOnChange).toBeCalledWith(1);
-  await userEvent.click(previousButton);
-  await sleep(1000);
-  await expect(multipleOnChange).toBeCalledWith(0);
-  await expect(multipleOnChange).toBeCalledTimes(2);
+  await userEvent.keyboard('{TAB}');
+  await expect(getByRole('button', { name: 'Cancel' })).toHaveFocus();
+  await userEvent.keyboard('{Escape}');
+  await waitFor(() => expect(getByTestId('dropdown-overlay')).not.toBeVisible());
 };
 
 export default {
