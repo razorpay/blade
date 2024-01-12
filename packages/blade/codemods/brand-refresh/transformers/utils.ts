647bbec29db708ebd27fb548149ebe2e7347a1ce
Nitin <nitin.kumar@razorpay.com>
2024-01-12 12:05:12 +0530

    Revert "refactor: move component wise codemod logic into separate files"
    This reverts commit 534c0162872fcae422e30842018e228969eb243a.

--- packages/blade/codemods/brand-refresh/transformers/utils.ts
+++
@@ -1,7 +0,0 @@
-import type { JSXAttribute, JSXExpressionContainer } from 'jscodeshift';
-
-export const isExpression = (prop: unknown): prop is JSXExpressionContainer => {
-  return (prop as JSXAttribute)?.value?.type === 'JSXExpressionContainer';
-};
-
-export const red = (message: string): string => `\u001b[1m\u001b[31m${message}\u001b[39m\u001b[22m`;
