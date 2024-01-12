b8785adbe1273387bbcacb3fc21483081786d636
saurabhdaware <saurabh.daware@razorpay.com>
2024-01-12 17:29:17 +0530

    fix: chevron icon color

--- packages/blade/src/components/Input/DropdownInputTriggers/Chevron.tsx
+++ packages/blade/src/components/Input/DropdownInputTriggers/Chevron.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ChevronDownIcon, ChevronUpIcon } from '~components/Icons';
 
 const Chevron = (props: { isOpen?: boolean; isDisabled?: boolean }): React.ReactElement => {
-  const iconColor = `surface.icon.gray.${props.isDisabled ? 'disabled' : 'normal'}` as const;
+  const iconColor = `surface.icon.gray.${props.isDisabled ? 'disabled' : 'muted'}` as const;
 
   return (
     <>
