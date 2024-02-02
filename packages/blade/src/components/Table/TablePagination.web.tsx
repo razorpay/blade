15ff4b9c24863d66afbbe5950004131837426dbb
Chaitanya Deorukhkar <deorukhkarchaitanya@gmail.com>
2024-02-02 12:23:13 +0530

    Update packages/blade/src/components/Table/TablePagination.web.tsx
    Co-authored-by: Anurag Hazra <anurag.hazra@razorpay.com>

--- packages/blade/src/components/Table/TablePagination.web.tsx
+++ packages/blade/src/components/Table/TablePagination.web.tsx
@@ -187,9 +187,9 @@ const _TablePagination = <T extends { paginationType: TablePaginationType }>({
     }
   }, [currentPage, currentPaginationState?.page, setPaginationPage]);
 
-  const totalPages = !isUndefined(totalItemCount)
-    ? Math.ceil(totalItemCount / currentPageSize)
-    : Math.ceil(totalItems / currentPageSize);
+const totalPages = isUndefined(totalItemCount)
+    ? Math.ceil(totalItems / currentPageSize);
+    : Math.ceil(totalItemCount / currentPageSize)
 
   const handlePageChange = useCallback(
     (page: number): void => {
