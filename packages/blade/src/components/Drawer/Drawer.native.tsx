f6c248966f89552c234858ded6626b481616fd86
saurabhdaware <saurabh.daware@razorpay.com>
2024-02-21 12:13:45 +0530

    fix: rn error

--- packages/blade/src/components/Drawer/Drawer.native.tsx
+++ packages/blade/src/components/Drawer/Drawer.native.tsx
@@ -1,15 +1,13 @@
 import type { DrawerProps } from './types';
 import { Text } from '~components/Typography';
-import { logger } from '~utils/logger';
+import { throwBladeError } from '~utils/logger';
 
 const Drawer = (_props: DrawerProps): React.ReactElement => {
-  if (__DEV__) {
-    logger({
-      type: 'warn',
-      moduleName: 'Drawer',
-      message: 'Drawer Component is not available for Native mobile apps.',
-    });
-  }
+  throwBladeError({
+    message: 'Drawer is not yet implemented for native',
+    moduleName: 'Drawer',
+  });
+
   return <Text>Drawer Component is not available for Native mobile apps.</Text>;
 };
 
