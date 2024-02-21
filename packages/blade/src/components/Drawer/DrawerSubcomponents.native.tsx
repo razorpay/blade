944b0afbcf0052dad1ffbd23164be54aa2c8f7e9
saurabhdaware <saurabh.daware@razorpay.com>
2024-02-21 13:05:08 +0530

    fix: ts errors

---
+++ packages/blade/src/components/Drawer/DrawerSubcomponents.native.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import type { DrawerHeaderProps } from './types';
+import { Text } from '~components/Typography';
+import { throwBladeError } from '~utils/logger';
+
+const DrawerHeader = (_props: DrawerHeaderProps): React.ReactElement => {
+  throwBladeError({
+    message: 'Drawer is not yet implemented for native',
+    moduleName: 'Drawer',
+  });
+
+  return <Text>Drawer Component is not available for Native mobile apps.</Text>;
+};
+
+const DrawerBody = (_props: { children: React.ReactNode }): React.ReactElement => {
+  throwBladeError({
+    message: 'Drawer is not yet implemented for native',
+    moduleName: 'Drawer',
+  });
+
+  return <Text>Drawer Component is not available for Native mobile apps.</Text>;
+};
+export { DrawerHeader, DrawerBody };
