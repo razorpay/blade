166b997c8d5650b8376adee86ec4d4e72711b9bf
Nitin <nitin.kumar@razorpay.com>
2024-02-23 00:36:06 +0530

    test: add E2E tests for FileUpload

---
+++ packages/blade/src/components/FileUpload/__tests__/FileUpload.ssr.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FileUpload } from '../FileUpload';
+
+import renderWithSSR from '~utils/testing/renderWithSSR.web';
+
+describe('<FileUpload />', () => {
+  it('should render in SSR', () => {
+    const { container, getByText } = renderWithSSR(
+      <FileUpload
+        selectionType="single"
+        label="Upload GST certificate"
+        helpText=" .jpg, .jpeg, or .png file only"
+        accept="image/*"
+        name="single-file-upload-input"
+      />,
+    );
+    const input = getByText('Drag files here or').closest('div')?.querySelector('input');
+
+    expect(input).toHaveAttribute('type', 'file');
+    expect(input).toHaveAttribute('accept', 'image/*');
+    expect(container).toMatchSnapshot();
+  });
+});
--- packages/blade/src/components/FileUpload/__tests__/FileUpload.test.stories.tsx
+++ packages/blade/src/components/FileUpload/__tests__/FileUpload.test.stories.tsx
@@ -1,71 +1,439 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable import/no-extraneous-dependencies */
 import type { StoryFn } from '@storybook/react';
 import { within, userEvent } from '@storybook/testing-library';
-import { expect, jest } from '@storybook/jest';
-import React from 'react';
-import type { ToastProps } from '~components/Toast/types';
-import { useToast } from '~components/Toast/useToast';
-import { ToastContainer } from '~components/Toast/ToastContainer';
+import { expect } from '@storybook/jest';
+import React, { useState } from 'react';
 import type { Toast } from '~components/Toast/Toast';
+import type { BladeFileList, BladeFile, FileUploadProps } from '~components/FileUpload';
 import { FileUpload } from '~components/FileUpload';
 import { Button } from '~components/Button';
 import { Box } from '~components/Box';
-
-const ToastExample = (props: ToastProps): React.ReactElement => {
-  const toast = useToast();
-
-  React.useEffect(() => {
-    toast.dismiss();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <Box>
-      <Button
-        onClick={() => {
-          toast.show({ ...props });
-        }}
-      >
-        Show Toast
-      </Button>
-      <ToastContainer />
-    </Box>
-  );
-};
 
 const sleep = (ms: number): Promise<void> => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const TestSingleFileUpload: StoryFn<typeof Toast> = (): React.ReactElement => {
   return (
-    <FileUpload
-      selectionType="single"
-      label="Upload GST"
-      helpText="Upload .jpg, .jpeg, or .png file only"
-      ///accept=".jpg, .jpeg, .png"
-      isRequired
-      necessityIndicator="required"
-      name="single-file-upload-input"
-      testID="file-upload"
-    />
+    <Box maxWidth="400px">
+      <FileUpload
+        selectionType="single"
+        label="Upload GST certificate"
+        helpText=" .jpg, .jpeg, or .png file only"
+        accept="image/*"
+        isRequired
+        necessityIndicator="required"
+        name="single-file-upload-input"
+      />
+    </Box>
   );
 };
 
 TestSingleFileUpload.play = async () => {
-  const { getByRole, queryByText, getByTestId, getByLabelText, getByText, querySelector } = within(
-    document.body,
-  );
-  await sleep(1000);
+  const { getByText } = within(document.body);
 
-  const str = JSON.stringify({ name: 'test.json' });
-  const blob = new Blob([str]);
-  const file = new File([blob], 'values.json', {
-    type: 'application/JSON',
+  // Create a file
+  const blob = new Blob([
+    'data:image/png;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw==',
+  ]);
+  const filename = 'my-image.png';
+  const file = new File([blob], filename, {
+    type: 'image/png',
   });
 
-  const input = getByTestId('file-upload-input');
-  await userEvent.upload(input, file);
-  await sleep(4000);
+  const input = getByText('Drag files here or').closest('div')?.querySelector('input');
+
+  await expect(input).not.toHaveAttribute('multiple');
+  await expect(input).toHaveAttribute('name', 'single-file-upload-input');
+
+  await userEvent.upload(input as HTMLElement, file);
+  await expect(getByText(filename)).toBeInTheDocument();
+};
+
+export const TestMultipleFileUpload: StoryFn<typeof Toast> = (): React.ReactElement => {
+  return (
+    <Box maxWidth="400px">
+      <FileUpload
+        selectionType="multiple"
+        label="Upload GST certificate"
+        helpText=" .jpg, .jpeg, or .png file only"
+        accept="image/*"
+        isRequired
+        necessityIndicator="required"
+        name="multiple-file-upload-input"
+      />
+    </Box>
+  );
+};
+
+TestMultipleFileUpload.play = async () => {
+  const { getByText } = within(document.body);
+
+  // Create files
+  const blob = new Blob([
+    'data:image/png;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw==',
+  ]);
+  const file1 = new File([blob], 'my-image.png', {
+    type: 'image/png',
+  });
+  const file2 = new File([blob], 'my-image2.png', {
+    type: 'image/png',
+  });
+
+  const input = getByText('Drag files here or').closest('div')?.querySelector('input');
+
+  await expect(input).toHaveAttribute('multiple');
+  await userEvent.upload(input as HTMLElement, [file1, file2]);
+  await expect(getByText('my-image.png')).toBeInTheDocument();
+  await expect(getByText('my-image2.png')).toBeInTheDocument();
+};
+
+export const TestOnRemove: StoryFn<typeof Toast> = (): React.ReactElement => {
+  return (
+    <Box maxWidth="400px">
+      <FileUpload
+        selectionType="single"
+        label="Upload GST certificate"
+        helpText=" .jpg, .jpeg, or .png file only"
+        accept="image/*"
+        isRequired
+        necessityIndicator="required"
+        name="single-file-upload-input"
+      />
+    </Box>
+  );
+};
+
+TestOnRemove.play = async () => {
+  const { getByText, getByRole, queryByText } = within(document.body);
+
+  // Create a file
+  const blob = new Blob([
+    'data:image/png;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw==',
+  ]);
+  const filename = 'my-image.png';
+  const file = new File([blob], filename, {
+    type: 'image/png',
+  });
+
+  const input = getByText('Drag files here or').closest('div')?.querySelector('input');
+
+  await expect(input).not.toHaveAttribute('multiple');
+  await expect(input).toHaveAttribute('name', 'single-file-upload-input');
+
+  await userEvent.upload(input as HTMLElement, file);
+  await expect(getByText(filename)).toBeInTheDocument();
+
+  const removeButton = getByRole('button', { name: 'Remove File' });
+  await userEvent.click(removeButton);
+  await expect(queryByText(filename)).not.toBeInTheDocument();
+};
+
+export const TestOnRemoveWithMultipleFiles: StoryFn<typeof Toast> = (): React.ReactElement => {
+  return (
+    <Box maxWidth="400px">
+      <FileUpload
+        selectionType="multiple"
+        label="Upload GST certificate"
+        helpText=" .jpg, .jpeg, or .png file only"
+        accept="image/*"
+        isRequired
+        necessityIndicator="required"
+        name="multiple-file-upload-input"
+      />
+    </Box>
+  );
+};
+
+TestOnRemoveWithMultipleFiles.play = async () => {
+  const { getByText, getAllByRole, queryByText } = within(document.body);
+
+  // Create files
+  const blob = new Blob([
+    'data:image/png;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw==',
+  ]);
+  const file1 = new File([blob], 'my-image.png', {
+    type: 'image/png',
+  });
+  const file2 = new File([blob], 'my-image2.png', {
+    type: 'image/png',
+  });
+
+  const input = getByText('Drag files here or').closest('div')?.querySelector('input');
+
+  await expect(input).toHaveAttribute('multiple');
+  await expect(input).toHaveAttribute('type', 'file');
+
+  await userEvent.upload(input as HTMLElement, [file1, file2]);
+  await expect(getByText('my-image.png')).toBeInTheDocument();
+  await expect(getByText('my-image2.png')).toBeInTheDocument();
+
+  const [removeButton1, removeButton2] = getAllByRole('button', { name: 'Remove File' });
+
+  await userEvent.click(removeButton2);
+  await expect(queryByText('my-image.png')).toBeInTheDocument();
+  await expect(queryByText('my-image2.png')).not.toBeInTheDocument();
+
+  await userEvent.click(removeButton1);
+  await expect(queryByText('my-image.png')).not.toBeInTheDocument();
+};
+
+export const TestMaxCountFileUpload: StoryFn<typeof Toast> = (): React.ReactElement => {
+  return (
+    <Box maxWidth="400px">
+      <FileUpload
+        selectionType="multiple"
+        label="Upload GST certificate"
+        helpText=" .jpg, .jpeg, or .png file only"
+        accept="image/*"
+        isRequired
+        maxCount={2}
+        necessityIndicator="required"
+      />
+    </Box>
+  );
+};
+
+TestMaxCountFileUpload.play = async () => {
+  const { getByText, queryByText } = within(document.body);
+
+  // Create files
+  const blob = new Blob([
+    'data:image/png;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw==',
+  ]);
+  const file1 = new File([blob], 'my-image.png', {
+    type: 'image/png',
+  });
+  const file2 = new File([blob], 'my-image2.png', {
+    type: 'image/png',
+  });
+  const file3 = new File([blob], 'my-image3.png', {
+    type: 'image/png',
+  });
+
+  const input = getByText('Drag files here or').closest('div')?.querySelector('input');
+
+  await expect(input).toHaveAttribute('multiple');
+
+  await userEvent.upload(input as HTMLElement, [file1, file2, file3]);
+  await expect(getByText("You can't upload more than 2 files.")).toBeInTheDocument();
+  await expect(queryByText('my-image.png')).not.toBeInTheDocument();
+  await expect(queryByText('my-image2.png')).not.toBeInTheDocument();
+};
+
+export const TestMaxSizeFileUpload: StoryFn<typeof Toast> = (): React.ReactElement => {
+  return (
+    <Box maxWidth="400px">
+      <FileUpload
+        selectionType="single"
+        label="Upload GST certificate"
+        helpText=" .jpg, .jpeg, or .png file only"
+        accept="image/*"
+        isRequired
+        // 10 bytes for testing purpose
+        maxSize={10}
+        necessityIndicator="required"
+      />
+    </Box>
+  );
+};
+
+TestMaxSizeFileUpload.play = async () => {
+  const { getByText } = within(document.body);
+
+  // Create a file
+  const blob = new Blob([
+    'data:image/png;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw==',
+  ]);
+  const filename = 'my-image.png';
+  const file = new File([blob], filename, {
+    type: 'image/png',
+  });
+
+  const input = getByText('Drag files here or').closest('div')?.querySelector('input');
+
+  await expect(input).not.toHaveAttribute('multiple');
+
+  await userEvent.upload(input as HTMLElement, file);
+  await expect(getByText('File size exceeded.')).toBeInTheDocument();
+};
+
+export const TestAcceptProp: StoryFn<typeof Toast> = (): React.ReactElement => {
+  return (
+    <Box maxWidth="400px">
+      <FileUpload
+        selectionType="single"
+        label="Upload GST certificate"
+        helpText=" .jpg, .jpeg, or .png file only"
+        accept="image/*"
+        isRequired
+        necessityIndicator="required"
+      />
+    </Box>
+  );
+};
+
+TestAcceptProp.play = async () => {
+  const { getByText, queryByText } = within(document.body);
+
+  // Create a file
+  const blob = new Blob([
+    'data:application/pdf;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw==',
+  ]);
+  const filename = 'my-pdf.pdf';
+  const file = new File([blob], filename, {
+    type: 'application/pdf',
+  });
+
+  const input = getByText('Drag files here or').closest('div')?.querySelector('input');
+
+  await expect(input).not.toHaveAttribute('multiple');
+
+  await userEvent.upload(input as HTMLElement, file);
+  await expect(queryByText(filename)).not.toBeInTheDocument();
+};
+
+export const TestFileUploadError: StoryFn<typeof Toast> = (): React.ReactElement => {
+  const [uploadedFiles, setUploadedFiles] = useState<BladeFileList>([]);
+
+  const uploadFile = async (file: BladeFile, fileList: BladeFileList): Promise<Response> => {
+    setUploadedFiles(
+      fileList.map((f) => {
+        if (f.id === file.id) {
+          f.status = 'uploading';
+        }
+        return file;
+      }),
+    );
+    const data = new FormData();
+    data.append('file', file);
+    data.append('upload_preset', 'blade-file-upload-demo');
+    data.append('cloud_name', 'snitin315');
+
+    // Wait for 2 seconds to simulate the file upload progress
+    await sleep(2000);
+
+    return fetch('https://api.cloudinary.com/v1_1/snitin315invalid/image/upload', {
+      method: 'POST',
+      body: data,
+    })
+      .then((res) => {
+        setUploadedFiles(
+          fileList.map((f) => {
+            if (f.id === file.id) {
+              f.status = 'success';
+            }
+            return file;
+          }),
+        );
+
+        return res.json();
+      })
+      .then((data) => {
+        if (data.error) {
+          setUploadedFiles(
+            fileList.map((f) => {
+              if (f.id === file.id) {
+                f.status = 'error';
+                f.errorText = `Oops! Something went wrong. ${data.error.message}`;
+              }
+              return file;
+            }),
+          );
+        }
+        return data;
+      })
+      .catch((error) => {
+        setUploadedFiles(
+          fileList.map((f) => {
+            if (f.id === file.id) {
+              f.status = 'error';
+              f.errorText = `Oops! Something went wrong. ${error.message}`;
+            }
+            return file;
+          }),
+        );
+      });
+  };
+
+  const handleFileChange: FileUploadProps['onChange'] = async ({ fileList }) => {
+    await uploadFile(fileList[0], fileList);
+  };
+
+  return (
+    <Box maxWidth="400px">
+      <FileUpload
+        selectionType="single"
+        label="Upload GST certificate"
+        helpText=" .jpg, .jpeg, or .png file only"
+        accept="image/*"
+        isRequired
+        necessityIndicator="required"
+        onChange={handleFileChange}
+        fileList={uploadedFiles}
+      />
+    </Box>
+  );
+};
+
+TestFileUploadError.play = async () => {
+  const { getByText, getByRole } = within(document.body);
+
+  // Create a file
+  const blob = new Blob([
+    'data:image/png;base64,R0lGODlhDwAPAKECAAAAzMzM/////wAAACwAAAAADwAPAAACIISPeQHsrZ5ModrLlN48CXF8m2iQ3YmmKqVlRtW4MLwWACH+H09wdGltaXplZCBieSBVbGVhZCBTbWFydFNhdmVyIQAAOw==',
+  ]);
+  const filename = 'my-image.png';
+  const file = new File([blob], filename, {
+    type: 'image/png',
+  });
+
+  const input = getByText('Drag files here or').closest('div')?.querySelector('input');
+
+  await expect(input).not.toHaveAttribute('multiple');
+
+  await userEvent.upload(input as HTMLElement, file);
+
+  await expect(getByRole('progressbar')).toBeInTheDocument();
+
+  await sleep(3000);
+  await expect(getByText(filename)).toBeInTheDocument();
+  await expect(getByText('Oops! Something went wrong. Unknown API key')).toBeInTheDocument();
+};
+
+export const TestRefProp: StoryFn<typeof Toast> = (): React.ReactElement => {
+  const ref = React.useRef<HTMLInputElement>(null);
+
+  return (
+    <Box maxWidth="400px">
+      <FileUpload
+        selectionType="single"
+        label="Upload GST certificate"
+        helpText=" .jpg, .jpeg, or .png file only"
+        accept="image/*"
+        isRequired
+        necessityIndicator="required"
+        ref={ref}
+      />
+      <Button
+        onClick={() => {
+          ref.current?.focus();
+        }}
+      >
+        Focus
+      </Button>
+    </Box>
+  );
+};
+
+TestRefProp.play = async () => {
+  const { getByText, getByRole } = within(document.body);
+
+  const input = getByText('Drag files here or').closest('div')?.querySelector('input');
+  const button = getByRole('button', { name: 'Focus' });
+
+  await expect(input).not.toHaveFocus();
+  await expect(input).toHaveAttribute('type', 'file');
+
+  await userEvent.click(button);
+  await expect(input).toHaveFocus();
 };
 
 export default {
