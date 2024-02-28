e126797c17b9d919a80e93bd230c213e9444db47
Nitin Kumar <snitin315@gmail.com>
2024-02-28 12:22:16 +0530

    test(FileUpload): add E2E & unit test cases  (#2040)

--- packages/blade/src/components/FileUpload/FileUpload.native.tsx
+++ packages/blade/src/components/FileUpload/FileUpload.native.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-no-useless-fragment */
+import type { FileUploadProps } from './types';
 import { throwBladeError } from '~utils/logger';
 
-const FileUpload = (): React.ReactElement => {
+const FileUpload = (_props: FileUploadProps): React.ReactElement => {
   throwBladeError({
     message: 'FileUpload is not yet implemented for React Native',
     moduleName: 'FileUpload',
