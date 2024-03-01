eeea0f966ec511e975b660f4316debe21af0e882
saurabhdaware <saurabh.daware@razorpay.com>
2024-03-01 12:27:56 +0530

    feat: remove font css variable

--- packages/blade/src/tokens/global/fontFamily/fontFamily.web.ts
+++ packages/blade/src/tokens/global/fontFamily/fontFamily.web.ts
@@ -2,7 +2,7 @@ import type { FontFamily } from './types';
 
 // Fallbacks here are defined in `packages/blade/fonts.css`
 export const fontFamily: FontFamily = {
-  text: '"Inter", var(--blade-inter-fallback), Arial',
-  heading: '"TASA Orbiter", var(--blade-tasa-orbiter-fallback), Arial',
+  text: '"Inter", "Inter Fallback Arial", Arial',
+  heading: '"TASA Orbiter", "TASA Orbiter Fallback Arial", Arial',
   code: '"Menlo", San Francisco Mono, Courier New, Roboto Mono, monospace',
 };
