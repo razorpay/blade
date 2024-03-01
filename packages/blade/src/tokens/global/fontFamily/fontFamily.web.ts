914631fb82a0e793619f245840599cd24c4253de
saurabhdaware <saurabh.daware@razorpay.com>
2024-03-01 11:04:12 +0530

    fix: add blade prefix, update fonts.css url

--- packages/blade/src/tokens/global/fontFamily/fontFamily.web.ts
+++ packages/blade/src/tokens/global/fontFamily/fontFamily.web.ts
@@ -2,7 +2,7 @@ import type { FontFamily } from './types';
 
 // Fallbacks here are defined in `packages/blade/fonts.css`
 export const fontFamily: FontFamily = {
-  text: '"Inter", var(--inter-fallback), Arial',
-  heading: '"TASA Orbiter", var(--tasa-orbiter-fallback), Arial',
+  text: '"Inter", var(--blade-inter-fallback), Arial',
+  heading: '"TASA Orbiter", var(--blade-tasa-orbiter-fallback), Arial',
   code: '"Menlo", San Francisco Mono, Courier New, Roboto Mono, monospace',
 };
