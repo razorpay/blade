12624b195ce927720ac92091bf437d737e836cd8
anuraghazra <anurag.hazra@razorpay.com>
2024-03-26 11:47:44 +0530

    chore: update native types

--- packages/blade/src/components/Input/PhoneNumberInput/PhoneNumberInput.native.tsx
+++ packages/blade/src/components/Input/PhoneNumberInput/PhoneNumberInput.native.tsx
@@ -1,6 +1,7 @@
+import type { PhoneNumberInputProps } from './types';
 import { throwBladeError } from '~utils/logger';
 
-const PhoneNumberInput = (_props: never): React.ReactElement => {
+const PhoneNumberInput = (_props: PhoneNumberInputProps): React.ReactElement => {
   throwBladeError({
     message: 'PhoneNumberInput is not yet implemented for native',
     moduleName: 'PhoneNumberInput',
