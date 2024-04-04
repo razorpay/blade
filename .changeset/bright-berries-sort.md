8c5231d4004a9fde71f1ad6de690f5aac9fa16ce
Nitin Kumar <snitin315@gmail.com>
2024-04-04 12:42:12 +0530

    feat: add large size in FileUpload component (#2108)

---
+++ .changeset/bright-berries-sort.md
@@ -0,0 +1,17 @@
+---
+"@razorpay/blade": minor
+---
+
+feat: add large size in FileUpload component
+
+#### Usage
+
+```js
+<FileUpload
+  size="large"
+  uploadType="single"
+  label="Upload GST certificate"
+  helpText="Upload .jpg, .jpeg, or .png file only"
+  accept="image/*"
+/>
+```
--- packages/blade/src/components/FileUpload/StyledFileUploadItemWrapper.tsx
+++ packages/blade/src/components/FileUpload/StyledFileUploadItemWrapper.tsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
 import type { StyledFileUploadItemWrapperProps } from './types';
-import { fileUploadItemBackgroundColors, fileUploadMotionTokens } from './fileUploadTokens';
+import {
+  fileUploadItemBackgroundColors,
+  fileUploadMotionTokens,
+  fileUploadHeightTokens,
+} from './fileUploadTokens';
 import getIn from '~utils/lodashButBetter/get';
 import BaseBox from '~components/Box/BaseBox';
 import { makeMotionTime } from '~utils/makeMotionTime';
 import { castWebType, makeSize } from '~utils';
-import { size } from '~tokens/global';
 
 const StyledFileUploadItemWrapper = styled(BaseBox)<StyledFileUploadItemWrapperProps>(
-  ({ theme, status }) => {
+  ({ theme, status, size }) => {
     const easing = getIn(theme.motion, fileUploadMotionTokens.easing);
     const duration = castWebType(
       makeMotionTime(getIn(theme.motion, fileUploadMotionTokens.duration)),
@@ -20,7 +23,7 @@ const StyledFileUploadItemWrapper = styled(BaseBox)<StyledFileUploadItemWrapperP
       display: 'flex',
       justifyContent: 'space-between',
       borderStyle: 'solid',
-      height: makeSize(size[56]),
+      height: makeSize(fileUploadHeightTokens[size]),
       width: '100%',
       backgroundColor: getIn(theme.colors, fileUploadItemBackgroundColors[status].default),
       transitionProperty: 'background-color',
