c0db25b74d8f4f4b16ec7906b7069f7a985da397
tarun khanna <tarun.khanna@razorpay.com>
2024-05-06 14:00:25 +0530

    [chore]: added en-IN locale in amount test case set

--- packages/blade/src/components/Amount/__tests__/mock.ts
+++ packages/blade/src/components/Amount/__tests__/mock.ts
@@ -10,6 +10,7 @@ export const AMOUNT_SUFFIX_TEST_SET: {
   { value: 1000000.0, suffix: 'decimals', output: '₹1,000,000.00', locale: 'en-US' },
   { value: 10000000, suffix: 'none', output: '₹10,000,000', locale: 'en-US' },
   { value: 2.07, suffix: 'decimals', output: '2,07₹', locale: 'fr-FR' },
-  { value: 2.077, suffix: 'decimals', output: '₹2.08' },
-  { value: 2.3, suffix: 'decimals', output: '₹2.30' },
+  { value: 2.077, suffix: 'decimals', output: '₹2.08', locale: 'en-IN' },
+  { value: 2.3, suffix: 'decimals', output: '₹2.30', locale: 'en-IN' },
+  { value: 1000000.12, suffix: 'decimals', output: '₹10,00,000.12', locale: 'en-IN' },
 ];
