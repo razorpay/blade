e6a8d2b9672b66a960fc31bb244b489b7e7abe6a
saurabhdaware <saurabh.daware@razorpay.com>
2024-05-10 15:48:44 +0530

    fix: remove unused code

--- packages/blade/src/components/SideNav/SideNavLevel.tsx
+++ packages/blade/src/components/SideNav/SideNavLevel.tsx
@@ -1,49 +1,21 @@
 import React from 'react';
 import BaseBox from '~components/Box/BaseBox';
 
-type SideNavLevelContextType = {
-  level: number;
-  isSideNavLevelActive?: boolean;
-  setIsSideNavLevelActive?: (isSideNavLevelActive: boolean) => void;
-};
-
-const SideNavLevelContext = React.createContext<SideNavLevelContextType>({
-  level: 1,
-});
-
-const useSideNavLevel = (): SideNavLevelContextType => {
-  return React.useContext(SideNavLevelContext);
-};
-
 const SideNavLevel = ({ children }: { children: React.ReactNode }): React.ReactElement => {
-  const { level: prevLevel } = useSideNavLevel();
-  const [isSideNavLevelActive, setIsSideNavLevelActive] = React.useState(false);
-
-  const contextValue = React.useMemo<SideNavLevelContextType>(
-    () => ({
-      level: prevLevel + 1,
-      isSideNavLevelActive,
-      setIsSideNavLevelActive,
-    }),
-    [prevLevel, isSideNavLevelActive],
-  );
-
   return (
-    <SideNavLevelContext.Provider value={contextValue}>
-      <BaseBox
-        marginLeft="52px"
-        padding={['spacing.3', 'spacing.4']}
-        onMouseOver={(e) => {
-          e.stopPropagation();
-        }}
-        onMouseOut={(e) => {
-          e.stopPropagation();
-        }}
-      >
-        {children}
-      </BaseBox>
-    </SideNavLevelContext.Provider>
+    <BaseBox
+      marginLeft="52px"
+      padding={['spacing.3', 'spacing.4']}
+      onMouseOver={(e) => {
+        e.stopPropagation();
+      }}
+      onMouseOut={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      {children}
+    </BaseBox>
   );
 };
 
-export { SideNavLevel, useSideNavLevel };
+export { SideNavLevel };
