aca570fc889a4c7fb774f71da27d60ea0902015b
saurabhdaware <saurabh.daware@razorpay.com>
2024-05-28 10:32:04 +0530

    fix: small mobile scroll

--- packages/blade/src/components/SideNav/SideNavBody.web.tsx
+++ packages/blade/src/components/SideNav/SideNavBody.web.tsx
@@ -4,7 +4,7 @@ import BaseBox from '~components/Box/BaseBox';
 
 const SideNavBody = ({ children }: SideNavBodyProps): React.ReactElement => {
   return (
-    <BaseBox className={classes.L1_ITEM_WRAPPER} overflowY="auto">
+    <BaseBox className={classes.L1_ITEM_WRAPPER} overflowY="auto" overflowX="hidden">
       {children}
     </BaseBox>
   );
