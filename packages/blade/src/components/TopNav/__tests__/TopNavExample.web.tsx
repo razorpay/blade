b3a9f41fc6b24447c2a2fffb8b1805b021170c82
Anurag Hazra <anurag.hazra@razorpay.com>
2024-10-29 15:38:36 +0530

    test: add new topnav tests (#2394)
    * chore: add new topnav tests
    
    * chore: update more to button
    
    * chore: update snaps
    
    * chore: snaps
    
    * chore: revert sidenab width
    
    * chore: update snaps
    
    * chore: test for null

--- packages/blade/src/components/TopNav/__tests__/TopNavExample.web.tsx
+++ packages/blade/src/components/TopNav/__tests__/TopNavExample.web.tsx
@@ -1,10 +1,13 @@
+import { TabNav, TabNavItem, TabNavItems } from '../TabNav';
 import { TopNav, TopNavActions, TopNavBrand, TopNavContent } from '../TopNav';
 import { Avatar } from '~components/Avatar';
 import { Box } from '~components/Box';
 import { Button } from '~components/Button';
-import { ActivityIcon, AnnouncementIcon } from '~components/Icons';
+import { ActivityIcon, AnnouncementIcon, ChevronDownIcon } from '~components/Icons';
 import { RazorpayLogo } from '~components/SideNav/docs/RazorpayLogo';
 import { Tooltip } from '~components/Tooltip';
+import { Menu, MenuItem, MenuOverlay } from '~components/Menu';
+import { Text } from '~components/Typography';
 
 const TopNavExample = (): React.ReactElement => {
   return (
@@ -14,7 +17,68 @@ const TopNavExample = (): React.ReactElement => {
           <RazorpayLogo />
         </TopNavBrand>
         <TopNavContent>
-          <p>hi</p>
+          <TabNav
+            items={[
+              { title: 'Home', href: '/home' },
+              {
+                href: '/payroll',
+                title: 'Payroll',
+              },
+              {
+                href: '/payments',
+                title: 'Payments',
+              },
+              {
+                href: '/magic-checkout',
+                title: 'Magic Checkout',
+              },
+              {
+                href: '/rize',
+                title: 'Rize',
+                isAlwaysOverflowing: true,
+              },
+            ]}
+          >
+            {({ items, overflowingItems }) => {
+              return (
+                <>
+                  <TabNavItems>
+                    {items.map((item) => {
+                      return (
+                        <TabNavItem
+                          key={item.title}
+                          title={item.title}
+                          href={item.href}
+                          icon={item.icon}
+                          isActive={item.isActive}
+                          trailing={item.trailing}
+                        />
+                      );
+                    })}
+                  </TabNavItems>
+                  {overflowingItems.length ? (
+                    <Menu openInteraction="hover">
+                      <TabNavItem href="#" title="More" trailing={<ChevronDownIcon />} />
+                      <MenuOverlay>
+                        {overflowingItems.map((item) => {
+                          return (
+                            <MenuItem
+                              key={item.href}
+                              onClick={() => {
+                                console.log('clicked', item.title);
+                              }}
+                            >
+                              <Text>{item.title}</Text>
+                            </MenuItem>
+                          );
+                        })}
+                      </MenuOverlay>
+                    </Menu>
+                  ) : null}
+                </>
+              );
+            }}
+          </TabNav>
         </TopNavContent>
         <TopNavActions>
           <Tooltip content="View Ecosystem Health">
