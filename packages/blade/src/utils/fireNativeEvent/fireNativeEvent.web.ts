4a5abdcd2c5985cf6d1234078f8d0316bad4299a
tewarig <gaurav.tewari@razorpay.com>
2024-11-08 15:55:39 +0530

    chore: change fire native event

--- packages/blade/src/utils/fireNativeEvent/fireNativeEvent.web.ts
+++ packages/blade/src/utils/fireNativeEvent/fireNativeEvent.web.ts
@@ -19,12 +19,10 @@ export const fireNativeEvent = (
   ref: React.RefObject<HTMLElement> | null,
   eventTypes: Array<'change' | 'input'>,
 ): void => {
-  if (ref) {
-    eventTypes.forEach((eventType) => {
-      const event = new Event(eventType, { bubbles: true });
-      ref.current?.dispatchEvent(event);
-    });
-  } else {
-    console.warn('ref is not defined');
-  }
+  if (!ref) return;
+
+  eventTypes.forEach((eventType) => {
+    const event = new Event(eventType, { bubbles: true });
+    ref.current?.dispatchEvent(event);
+  });
 };
