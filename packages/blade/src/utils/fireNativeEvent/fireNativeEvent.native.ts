500e30f8ca189bab6de26b516057be5264716ff5
tewarig <gaurav.tewari@razorpay.com>
2024-11-14 11:23:23 +0530

    fix: failing build

--- packages/blade/src/utils/fireNativeEvent/fireNativeEvent.native.ts
+++ packages/blade/src/utils/fireNativeEvent/fireNativeEvent.native.ts
@@ -3,7 +3,10 @@ import { throwBladeError } from '../logger';
  * FireNativeEvent is not supported on react-native
  */
 
-export const fireNativeEvent = (): void => {
+export const fireNativeEvent = (
+  _ref: React.RefObject<HTMLElement> | null,
+  _eventTypes: Array<'change' | 'input'>,
+): void => {
   throwBladeError({
     message: 'FireNativeEvent is not supported on react-native',
     moduleName: 'FireNativeEvent',
