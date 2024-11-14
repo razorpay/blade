277c1b964e742fa2e203563e722450d290ad5e32
tewarig <gaurav.tewari@razorpay.com>
2024-11-14 12:30:48 +0530

    chore: add fixes to fireNativeEvent

--- packages/blade/src/utils/fireNativeEvent/fireNativeEvent.native.test.ts
+++ packages/blade/src/utils/fireNativeEvent/fireNativeEvent.native.test.ts
@@ -1,8 +1,9 @@
 import { fireNativeEvent } from './fireNativeEvent.native';
 
 describe('fireNativeEvent', () => {
+  const ref: React.RefObject<HTMLElement> = { current: null };
   it('should throw specific error', () => {
-    expect(() => fireNativeEvent()).toThrowError(
+    expect(() => fireNativeEvent(ref, ['change'])).toThrowError(
       '[Blade: FireNativeEvent]: FireNativeEvent is not supported on react-native',
     );
   });
