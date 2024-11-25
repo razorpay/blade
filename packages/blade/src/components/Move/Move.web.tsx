a2662f1eae842e88f10e0da7e5a73faa9dbb7eca
saurabhdaware <saurabh.daware@razorpay.com>
2024-11-25 09:59:24 +0530

    feat: add token valyes for move

--- packages/blade/src/components/Move/Move.web.tsx
+++ packages/blade/src/components/Move/Move.web.tsx
@@ -1,22 +1,37 @@
 import { BaseMotionEntryExit } from '~components/BaseMotion';
 import type { BaseMotionEntryExitProps, MotionVariantsType } from '~components/BaseMotion';
 import React from 'react';
+import { makeSecondsDuration } from '~utils/makeSecondsDuration';
+import { cssBezierToMotionFn } from '~utils/cssBezierToMotionFn';
+import { castWebType, makeSpace, useTheme } from '~utils';
 
 export type MoveProps = BaseMotionEntryExitProps;
 
 export const Move = ({ children, type = 'inout', isVisible, motionTriggers }: MoveProps) => {
+  const { theme } = useTheme();
+
+  const movePx = makeSpace(theme.spacing[5]);
+
   const moveVariants: MotionVariantsType = {
     initial: {
       opacity: 0,
-      transform: 'translateY(20px)',
+      transform: `translateY(${movePx})`,
     },
     animate: {
       opacity: 1,
-      transform: 'translateY(0px)',
+      transform: `translateY(${makeSpace(theme.spacing[0])})`,
+      transition: {
+        duration: makeSecondsDuration(theme.motion.duration.xmoderate),
+        easings: cssBezierToMotionFn(castWebType(theme.motion.easing.entrance)),
+      },
     },
     exit: {
       opacity: 0,
-      transform: 'translateY(20px)',
+      transform: `translateY(${movePx})`,
+      transition: {
+        duration: makeSecondsDuration(theme.motion.duration.quick),
+        easings: cssBezierToMotionFn(castWebType(theme.motion.easing.exit)),
+      },
     },
   };
 
