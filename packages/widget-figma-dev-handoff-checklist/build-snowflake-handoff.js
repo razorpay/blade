5f6bdb53500605bf5813e03bf94fdd5ef5f8c068
Kamlesh Chandnani <kamlesh.chandnani@gmail.com>
2024-11-27 21:18:51 +0530

    feat: add analytics for handoff checklist (#2413)
    * feat: add analytics for handoff checklist
    
    * fix: coverage plugin bug
    
    * fix: add widget analytics
    
    * fix: add more data for analytics
    
    * fix: snowflake widget bug
    
    * fix: snowflake typo

---
+++ packages/widget-figma-dev-handoff-checklist/build-snowflake-handoff.js
@@ -0,0 +1,35 @@
+const esbuild = require('esbuild');
+require('dotenv').config();
+
+async function build() {
+  // Check if the `--watch` argument is passed in the command line
+  const isWatchMode = process.argv.includes('--watch');
+
+  // Create the build context
+  const ctx = await esbuild.context({
+    entryPoints: ['snowflake-handoff-checklist/code.tsx'],
+    bundle: true,
+    outfile: 'snowflake-handoff-checklist/dist/code.js',
+    target: 'es6',
+    define: {
+      'process.env.SEGMENT_WRITE_KEY': JSON.stringify(btoa(process.env.SEGMENT_WRITE_KEY ?? '')),
+    },
+  });
+
+  if (isWatchMode) {
+    // Enable watch mode
+    await ctx.watch();
+    console.log('Watching for changes...');
+  } else {
+    // Perform a single build
+    await ctx.rebuild();
+    console.log('Build completed.');
+    await ctx.dispose();
+  }
+}
+
+// Execute the build function
+build().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
