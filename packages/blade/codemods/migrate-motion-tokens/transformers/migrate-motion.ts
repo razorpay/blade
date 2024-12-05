5a984ca59cebf09f5e80e26e7ce79b3dc611155b
saurabhdaware <saurabh.daware@razorpay.com>
2024-12-05 09:40:37 +0530

    fix: regex lint

--- packages/blade/codemods/migrate-motion-tokens/transformers/migrate-motion.ts
+++ packages/blade/codemods/migrate-motion-tokens/transformers/migrate-motion.ts
@@ -5,6 +5,7 @@ import * as tokenMapping from './motionTokenMapping.json';
 const transformer = (file: FileInfo): string => {
   // Fairly simple usecases of motion tokens in razorpay files so this works.
   // .replace has also worked well during rebranding
+  // eslint-disable-next-line no-useless-escape
   const newSource = file.source.replace(/motion\.easing\.[\w\.]+/g, (matchingToken) => {
     const match = Object.entries(tokenMapping).find(
       ([oldToken, _newToken]) => oldToken === matchingToken,
