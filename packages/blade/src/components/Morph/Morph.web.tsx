ee905a990fed8fb93d18ba728e940f9f9e36e860
saurabhdaware <saurabh.daware@razorpay.com>
2024-12-05 13:47:21 +0530

    feat: add borderRadius and backgroundColor morph support

--- packages/blade/src/components/Morph/Morph.web.tsx
+++ packages/blade/src/components/Morph/Morph.web.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import { MotionDiv } from '~components/BaseMotion';
+import { useMemoizedStyles } from '~components/Box/BaseBox/useMemoizedStyles.web';
+import { useTheme } from '~utils';
 import { MorphProps } from './types';
 
 const Morph = ({ children, layoutId }: MorphProps) => {
-  return <MotionDiv as={children.type} {...children.props} layout layoutId={layoutId} />;
+  // Apart from framer-motion's layout morphing, we also support morph of backgroundColor and borderRadius
+  const { borderRadius, backgroundColor, ...rest } = children.props;
+  const { theme } = useTheme();
+  const cssProps = useMemoizedStyles({ borderRadius, backgroundColor, theme });
+
+  return (
+    <MotionDiv
+      as={children.type}
+      {...rest}
+      layout
+      layoutId={layoutId}
+      // The animations that are not supported by layout animation are animated using `animate` prop here
+      animate={cssProps}
+    />
+  );
 };
 
 export { Morph };
