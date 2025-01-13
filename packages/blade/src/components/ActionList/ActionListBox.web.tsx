de29640ea1005b9e004ca19ceb9b2d8f60a8d9ce
saurabhdaware <saurabh.daware@razorpay.com>
2025-01-13 19:25:20 +0530

    feat: initial virtulization code

--- packages/blade/src/components/ActionList/ActionListBox.web.tsx
+++ packages/blade/src/components/ActionList/ActionListBox.web.tsx
@@ -3,11 +3,16 @@ import React from 'react';
 import { FixedSizeList as VirtualizedList } from 'react-window';
 import { StyledListBoxWrapper } from './styles/StyledListBoxWrapper';
 import type { SectionData } from './actionListUtils';
+import { actionListMaxHeight, getActionListPadding } from './styles/getBaseListBoxWrapperStyles';
 import { useBottomSheetContext } from '~components/BottomSheet/BottomSheetContext';
 import { assignWithoutSideEffects } from '~utils/assignWithoutSideEffects';
 import { makeAccessible } from '~utils/makeAccessible';
 import type { DataAnalyticsAttribute } from '~utils/types';
 import { makeAnalyticsAttribute } from '~utils/makeAnalyticsAttribute';
+import { useIsMobile } from '~utils/useIsMobile';
+import { getItemHeight } from '~components/BaseMenu/BaseMenuItem/tokens';
+import { useTheme } from '~utils';
+import type { Theme } from '~components/BladeProvider';
 
 type ActionListBoxProps = {
   childrenWithId?: React.ReactNode[] | null;
@@ -19,7 +24,6 @@ type ActionListBoxProps = {
 
 const _ActionListBox = React.forwardRef<HTMLDivElement, ActionListBoxProps>(
   ({ childrenWithId, actionListItemWrapperRole, isMultiSelectable, ...rest }, ref) => {
-    const items = React.Children.toArray(childrenWithId); // Convert children to an array
     const { isInBottomSheet } = useBottomSheetContext();
 
     return (
@@ -32,8 +36,68 @@ const _ActionListBox = React.forwardRef<HTMLDivElement, ActionListBoxProps>(
         })}
         {...makeAnalyticsAttribute(rest)}
       >
-        {/* {childrenWithId} */}
-        <VirtualizedList height={284} width="100%" itemSize={40} itemCount={items.length}>
+        {childrenWithId}
+      </StyledListBoxWrapper>
+    );
+  },
+);
+
+const ActionListBox = assignWithoutSideEffects(React.memo(_ActionListBox), {
+  displayName: 'ActionListBox',
+});
+
+const getVirtualItemParams = ({
+  theme,
+  isMobile,
+}: {
+  theme: Theme;
+  isMobile: boolean;
+}): {
+  itemHeight: number;
+  actionListBoxHeight: number;
+} => {
+  const itemHeightResponsive = getItemHeight(theme);
+  const actionListPadding = getActionListPadding(theme);
+  const actionListBoxHeight = actionListMaxHeight - actionListPadding * 2;
+
+  return {
+    itemHeight: isMobile
+      ? itemHeightResponsive.itemHeightMobile
+      : itemHeightResponsive.itemHeightDesktop,
+    actionListBoxHeight,
+  };
+};
+
+const _ActionListVirtualizedBox = React.forwardRef<HTMLDivElement, ActionListBoxProps>(
+  ({ childrenWithId, actionListItemWrapperRole, isMultiSelectable, ...rest }, ref) => {
+    const items = React.Children.toArray(childrenWithId); // Convert children to an array
+    const { isInBottomSheet } = useBottomSheetContext();
+    const isMobile = useIsMobile();
+    const { theme } = useTheme();
+    const { itemHeight, actionListBoxHeight } = React.useMemo(
+      () => getVirtualItemParams({ theme, isMobile }),
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [theme.name, isMobile],
+    );
+
+    console.log({ itemHeight, actionListBoxHeight });
+
+    return (
+      <StyledListBoxWrapper
+        isInBottomSheet={isInBottomSheet}
+        ref={ref}
+        {...makeAccessible({
+          role: actionListItemWrapperRole,
+          multiSelectable: actionListItemWrapperRole === 'listbox' ? isMultiSelectable : undefined,
+        })}
+        {...makeAnalyticsAttribute(rest)}
+      >
+        <VirtualizedList
+          height={actionListBoxHeight}
+          width="100%"
+          itemSize={itemHeight}
+          itemCount={items.length}
+        >
           {({ index, style }) => (
             <div style={style} key={`virtual-item-${index}`}>
               {items[index]}
@@ -45,8 +109,8 @@ const _ActionListBox = React.forwardRef<HTMLDivElement, ActionListBoxProps>(
   },
 );
 
-const ActionListBox = assignWithoutSideEffects(React.memo(_ActionListBox), {
-  displayName: 'ActionListBox',
+const ActionListVirtualizedBox = assignWithoutSideEffects(React.memo(_ActionListVirtualizedBox), {
+  displayName: 'ActionListVirtualizedBox',
 });
 
-export { ActionListBox };
+export { ActionListBox, ActionListVirtualizedBox };
