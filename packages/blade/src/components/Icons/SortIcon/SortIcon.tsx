a36e51ffc752ad77ed21ab9086bd0b60397b2f48
tewarig <gaurav.tewari@razorpay.com>
2025-01-30 12:02:30 +0530

    chore: sort icon

--- packages/blade/src/components/Icons/SortIcon/SortIcon.tsx
+++ packages/blade/src/components/Icons/SortIcon/SortIcon.tsx
@@ -6,8 +6,8 @@ const SortIcon: IconComponent = ({ size, color, ...styledProps }) => {
   const { height, width, iconColor } = useIconProps({ size, color });
 
   return (
-    <Svg {...styledProps}  width={width} height={height} viewBox="0 0 24 24" fill="none">
-      <Path
+    <Svg {...styledProps} width={width} height={height} viewBox="0 0 24 24" fill="none">
+        <Path
         d="M8.54893 1.29663C8.359 1.1067 8.1014 1 7.8328 1C7.5642 1 7.3066 1.1067 7.11667 1.29663L2.29662 6.11675C1.90112 6.51225 1.90113 7.1535 2.29663 7.549C2.69214 7.9445 3.33338 7.9445 3.72889 7.54899L6.88779 4.39005V12.9499C6.88779 13.5092 7.34121 13.9627 7.90054 13.9627C8.45987 13.9627 8.9133 13.5092 8.9133 12.9499V4.5255L11.9368 7.54899C12.3323 7.9445 12.9735 7.9445 13.369 7.54899C13.7645 7.15349 13.7645 6.51225 13.369 6.11674L8.54893 1.29663Z"
         fill={iconColor}
       />
