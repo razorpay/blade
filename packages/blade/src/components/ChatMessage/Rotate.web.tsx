d077a31b1132f23167eb6cd4160019392a98b84e
tewarig <gaurav.tewari@razorpay.com>
2025-02-04 09:37:20 +0530

    chore: remove lazy motion

--- packages/blade/src/components/ChatMessage/Rotate.web.tsx
+++ packages/blade/src/components/ChatMessage/Rotate.web.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LazyMotion, domAnimation, m } from 'framer-motion';
+import { m } from 'framer-motion';
 import { castWebType } from '~utils';
 import { useTheme } from '~components/BladeProvider';
 import { msToSeconds } from '~utils/msToSeconds';
@@ -15,22 +15,20 @@ const Rotate = ({
   const { theme } = useTheme();
 
   return (
-    <LazyMotion features={domAnimation}>
-      <m.div
-        style={{
-          display: 'flex',
-        }}
-        animate={{ rotate: animate ? 90 : 0 }}
-        transition={{
-          duration: msToSeconds(theme.motion.duration.gentle),
-          repeat: Infinity,
-          ease: cssBezierToArray(castWebType(theme.motion.easing.emphasized)),
-          delay: msToSeconds(theme.motion.delay.gentle),
-        }}
-      >
-        {children}
-      </m.div>
-    </LazyMotion>
+    <m.div
+      style={{
+        display: 'flex',
+      }}
+      animate={{ rotate: animate ? 90 : 0 }}
+      transition={{
+        duration: msToSeconds(theme.motion.duration.gentle),
+        repeat: Infinity,
+        ease: cssBezierToArray(castWebType(theme.motion.easing.emphasized)),
+        delay: msToSeconds(theme.motion.delay.gentle),
+      }}
+    >
+      {children}
+    </m.div>
   );
 };
 
