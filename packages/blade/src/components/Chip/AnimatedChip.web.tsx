8adc36671974a044394efa12785ef4c06e39f8bd
tewarig <gaurav.tewari@razorpay.com>
2025-02-12 16:42:58 +0530

    chore: refactor width

--- packages/blade/src/components/Chip/AnimatedChip.web.tsx
+++ packages/blade/src/components/Chip/AnimatedChip.web.tsx
@@ -6,17 +6,28 @@ import getIn from '~utils/lodashButBetter/get';
 import BaseBox from '~components/Box/BaseBox';
 import { makeMotionTime } from '~utils/makeMotionTime';
 import { castWebType } from '~utils';
+import { getBaseBoxStyles } from '~components/Box/BaseBox/baseBoxStyles';
+import { useTheme } from '~components/BladeProvider';
 
 const AnimatedChip = styled(BaseBox)<AnimatedChipProps>((props) => {
   const easing = getIn(props.theme.motion, chipMotionTokens.easing);
   const duration = castWebType(
     makeMotionTime(getIn(props.theme.motion, chipMotionTokens.duration)),
   );
+  const { theme } = useTheme();
+  const animatedChipStyles = getBaseBoxStyles({
+    width: props?.width,
+    maxWidth: props?.maxWidth,
+    minWidth: props?.minWidth,
+    theme,
+  });
+
   return {
     ...getAnimatedChipStyles(props),
-    width: props?.width ? (props.width as string | number) : 'fit-content',
-    maxWidth: props?.maxWidth as string | number,
-    minWidth: props?.minWidth as string | number,
+    ...animatedChipStyles,
+    ...(!props?.width && {
+      width: 'fit-content',
+    }),
     transform: `scale(${props.isPressed ? '0.92' : '1'})`,
     transitionDuration: duration,
     transitionTimingFunction: easing,
