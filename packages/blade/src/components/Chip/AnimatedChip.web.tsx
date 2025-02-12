3443de87f12cd58291409db19146cf8f5cad8b55
tewarig <gaurav.tewari@razorpay.com>
2025-02-12 16:50:49 +0530

    chore: refactor

--- packages/blade/src/components/Chip/AnimatedChip.web.tsx
+++ packages/blade/src/components/Chip/AnimatedChip.web.tsx
@@ -6,25 +6,14 @@ import getIn from '~utils/lodashButBetter/get';
 import BaseBox from '~components/Box/BaseBox';
 import { makeMotionTime } from '~utils/makeMotionTime';
 import { castWebType } from '~utils';
-import { getBaseBoxStyles } from '~components/Box/BaseBox/baseBoxStyles';
-import { useTheme } from '~components/BladeProvider';
 
 const AnimatedChip = styled(BaseBox)<AnimatedChipProps>((props) => {
   const easing = getIn(props.theme.motion, chipMotionTokens.easing);
   const duration = castWebType(
     makeMotionTime(getIn(props.theme.motion, chipMotionTokens.duration)),
   );
-  const { theme } = useTheme();
-  const animatedChipStyles = getBaseBoxStyles({
-    width: props?.width,
-    maxWidth: props?.maxWidth,
-    minWidth: props?.minWidth,
-    theme,
-  });
-
   return {
     ...getAnimatedChipStyles(props),
-    ...animatedChipStyles,
     ...(!props?.width && {
       width: 'fit-content',
     }),
