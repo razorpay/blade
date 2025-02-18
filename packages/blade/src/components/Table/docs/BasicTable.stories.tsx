115762173a00f27296c69d34ae67829500c3a6a9
tewarig <gaurav.tewari@razorpay.com>
2025-02-18 14:51:18 +0530

    chore: remove basic table example

--- packages/blade/src/components/Table/docs/BasicTable.stories.tsx
+++ packages/blade/src/components/Table/docs/BasicTable.stories.tsx
@@ -11,14 +11,20 @@ import {
   TableCell,
   TableToolbar,
   TableToolbarActions,
+  TableFooter,
+  TableFooterRow,
+  TableFooterCell,
+  TablePagination,
 } from '../../Table';
-import { TableVirtualizedWrapper } from '../TableBody';
 import StoryPageWrapper from '~utils/storybook/StoryPageWrapper';
 import { Box } from '~components/Box';
-import { Code, Heading } from '~components/Typography';
+import { Amount } from '~components/Amount';
+import { Code } from '~components/Typography';
 import { Badge } from '~components/Badge';
 import { getStyledPropsArgTypes } from '~components/Box/BaseBox/storybookArgTypes';
 import { Button } from '~components/Button';
+import { IconButton } from '~components/Button/IconButton';
+import { CheckIcon, CloseIcon } from '~components/Icons';
 
 export default {
   title: 'Components/Table',
@@ -64,8 +70,8 @@ export default {
   },
 } as Meta<TableProps<unknown>>;
 
-const getNodes = (number: number): Item[] => [
-  ...Array.from({ length: number }, (_, i) => ({
+const nodes: Item[] = [
+  ...Array.from({ length: 100 }, (_, i) => ({
     id: (i + 1).toString(),
     paymentId: `rzp${Math.floor(Math.random() * 1000000)}`,
     amount: Number((Math.random() * 10000).toFixed(2)),
@@ -88,9 +94,6 @@ const getNodes = (number: number): Item[] => [
   })),
 ];
 
-const nodes: Item[] = getNodes(20);
-
-const largeNodes: Item[] = getNodes(500);
 type Item = {
   id: string;
   paymentId: string;
@@ -107,19 +110,16 @@ const data: TableData<Item> = {
   nodes,
 };
 
-const largeData: TableData<Item> = {
-  nodes: largeNodes,
-};
-
 const TableTemplate: StoryFn<typeof TableComponent> = ({ ...args }) => {
   return (
-    <Box padding="spacing.5" height="500px">
-      <Heading> Total rows : {largeNodes.length}</Heading>
+    <Box padding="spacing.5" overflow="auto" minHeight="400px">
       <TableComponent
         {...args}
-        data={largeData}
+        data={data}
+        defaultSelectedIds={['1', '3']}
         onSelectionChange={console.log}
-        selectionType="multiple"
+        isFirstColumnSticky
+        selectionType="single"
         toolbar={
           <TableToolbar title="Showing 1-10 [Items]" selectedTitle="Showing 1-10 [Items]">
             <TableToolbarActions>
@@ -137,13 +137,18 @@ const TableTemplate: StoryFn<typeof TableComponent> = ({ ...args }) => {
           DATE: (array) => array.sort((a, b) => a.date.getTime() - b.date.getTime()),
           STATUS: (array) => array.sort((a, b) => a.status.localeCompare(b.status)),
         }}
-        defaultSelectedIds={['1', '3']}
-        rowDensity="normal"
-        height="600px"
-        isFirstColumnSticky
+        pagination={
+          <TablePagination
+            onPageChange={console.log}
+            defaultPageSize={10}
+            onPageSizeChange={console.log}
+            showPageSizePicker
+            showPageNumberSelector
+          />
+        }
       >
-        {() => (
-          <TableVirtualizedWrapper rowHeight={() => 57}>
+        {(tableData) => (
+          <>
             <TableHeader>
               <TableHeaderRow>
                 <TableHeaderCell headerKey="PAYMENT_ID">ID</TableHeaderCell>
@@ -151,14 +156,37 @@ const TableTemplate: StoryFn<typeof TableComponent> = ({ ...args }) => {
                 <TableHeaderCell headerKey="ACCOUNT">Account</TableHeaderCell>
                 <TableHeaderCell headerKey="DATE">Date</TableHeaderCell>
                 <TableHeaderCell headerKey="METHOD">Method</TableHeaderCell>
-                <TableHeaderCell headerKey="STATUS">Status </TableHeaderCell>
+                <TableHeaderCell headerKey="STATUS">Status</TableHeaderCell>
               </TableHeaderRow>
             </TableHeader>
             <TableBody>
-              {(tableItem: Item, index) => (
+              {tableData.map((tableItem, index) => (
                 <TableRow
                   key={index}
                   item={tableItem}
+                  hoverActions={
+                    <>
+                      <Button variant="tertiary" size="xsmall">
+                        View Details
+                      </Button>
+                      <IconButton
+                        icon={CheckIcon}
+                        isHighlighted
+                        accessibilityLabel="Approve"
+                        onClick={() => {
+                          console.log('Approved', tableItem.id);
+                        }}
+                      />
+                      <IconButton
+                        icon={CloseIcon}
+                        isHighlighted
+                        accessibilityLabel="Reject"
+                        onClick={() => {
+                          console.log('Rejected', tableItem.id);
+                        }}
+                      />
+                    </>
+                  }
                   onClick={() => {
                     console.log('where');
                   }}
@@ -197,208 +225,28 @@ const TableTemplate: StoryFn<typeof TableComponent> = ({ ...args }) => {
                     </Badge>
                   </TableCell>
                 </TableRow>
-              )}
+              ))}
             </TableBody>
-          </TableVirtualizedWrapper>
+            <TableFooter>
+              <TableFooterRow>
+                <TableFooterCell>Total</TableFooterCell>
+                <TableFooterCell>-</TableFooterCell>
+                <TableFooterCell>-</TableFooterCell>
+                <TableFooterCell>-</TableFooterCell>
+                <TableFooterCell>-</TableFooterCell>
+                {args.selectionType === 'multiple' ? <TableFooterCell>-</TableFooterCell> : null}
+                <TableFooterCell>
+                  <Amount value={10} />
+                </TableFooterCell>
+              </TableFooterRow>
+            </TableFooter>
+          </>
         )}
       </TableComponent>
     </Box>
   );
 };
 
-export const NormalTable: StoryFn<typeof TableComponent> = ({ ...args }) => {
-  return (
-    <Box>
-      <Heading>Normal Table-</Heading>
-      <Box padding="spacing.5" minHeight="400px">
-        <TableComponent
-          {...args}
-          data={data}
-          defaultSelectedIds={['1', '3']}
-          onSelectionChange={console.log}
-          isFirstColumnSticky
-          selectionType="multiple"
-          height="400px"
-          // eslint-disable-next-line react/jsx-no-duplicate-props
-          onSelectionChange={({ selectedIds }) => {
-            console.log(selectedIds);
-            // setSelectedItems(data.nodes.filter((node) => selectedIds.includes(node.id)));
-          }}
-          toolbar={
-            <TableToolbar title="Showing 1-10 [Items]" selectedTitle="Showing 1-10 [Items]">
-              <TableToolbarActions>
-                <Button variant="secondary" marginRight="spacing.2">
-                  Export
-                </Button>
-                <Button>Refund</Button>
-              </TableToolbarActions>
-            </TableToolbar>
-          }
-          sortFunctions={{
-            ID: (array) => array.sort((a, b) => Number(a.id) - Number(b.id)),
-            AMOUNT: (array) => array.sort((a, b) => a.amount - b.amount),
-            PAYMENT_ID: (array) => array.sort((a, b) => a.paymentId.localeCompare(b.paymentId)),
-            DATE: (array) => array.sort((a, b) => a.date.getTime() - b.date.getTime()),
-            STATUS: (array) => array.sort((a, b) => a.status.localeCompare(b.status)),
-          }}
-        >
-          {(tableData) => (
-            <>
-              <TableHeader>
-                <TableHeaderRow>
-                  <TableHeaderCell headerKey="PAYMENT_ID">ID</TableHeaderCell>
-                  <TableHeaderCell headerKey="AMOUNT">Amount</TableHeaderCell>
-                  <TableHeaderCell headerKey="ACCOUNT">Account</TableHeaderCell>
-                  <TableHeaderCell headerKey="DATE">Date</TableHeaderCell>
-                  <TableHeaderCell headerKey="METHOD">Method</TableHeaderCell>
-                  <TableHeaderCell headerKey="STATUS">Status</TableHeaderCell>
-                </TableHeaderRow>
-              </TableHeader>
-              <TableBody>
-                {tableData.map((tableItem, index) => (
-                  <TableRow
-                    key={index}
-                    item={tableItem}
-                    onClick={() => {
-                      console.log('where');
-                    }}
-                  >
-                    <TableCell>
-                      <Code size="medium">{tableItem.paymentId}</Code>
-                    </TableCell>
-                    <TableEditableCell
-                      accessibilityLabel="Amount"
-                      placeholder="Enter text"
-                      successText="Amount is valid"
-                    />
-                    <TableCell>{tableItem.account}</TableCell>
-                    <TableCell>
-                      {tableItem.date?.toLocaleDateString('en-IN', {
-                        year: 'numeric',
-                        month: '2-digit',
-                        day: '2-digit',
-                      })}
-                    </TableCell>
-                    <TableCell>{tableItem.method}</TableCell>
-                    <TableCell>
-                      <Badge
-                        size="medium"
-                        color={
-                          tableItem.status === 'Completed'
-                            ? 'positive'
-                            : tableItem.status === 'Pending'
-                            ? 'notice'
-                            : tableItem.status === 'Failed'
-                            ? 'negative'
-                            : 'primary'
-                        }
-                      >
-                        {tableItem.status}
-                      </Badge>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </>
-          )}
-        </TableComponent>
-      </Box>
-
-      <Heading>Virtualized Table-</Heading>
-      <Box padding="spacing.5" minHeight="600px">
-        <Heading> Total rows : {largeNodes.length}</Heading>
-        <TableComponent
-          {...args}
-          data={largeData}
-          onSelectionChange={console.log}
-          selectionType="multiple"
-          toolbar={
-            <TableToolbar title="Showing 1-10 [Items]" selectedTitle="Showing 1-10 [Items]">
-              <TableToolbarActions>
-                <Button variant="secondary" marginRight="spacing.2">
-                  Export
-                </Button>
-                <Button>Refund</Button>
-              </TableToolbarActions>
-            </TableToolbar>
-          }
-          sortFunctions={{
-            ID: (array) => array.sort((a, b) => Number(a.id) - Number(b.id)),
-            AMOUNT: (array) => array.sort((a, b) => a.amount - b.amount),
-            PAYMENT_ID: (array) => array.sort((a, b) => a.paymentId.localeCompare(b.paymentId)),
-            DATE: (array) => array.sort((a, b) => a.date.getTime() - b.date.getTime()),
-            STATUS: (array) => array.sort((a, b) => a.status.localeCompare(b.status)),
-          }}
-          defaultSelectedIds={['1', '3']}
-          rowDensity="normal"
-          height="600px"
-          isFirstColumnSticky
-        >
-          {() => (
-            <TableVirtualizedWrapper rowHeight={() => 58}>
-              <TableHeader>
-                <TableHeaderRow>
-                  <TableHeaderCell headerKey="PAYMENT_ID">ID</TableHeaderCell>
-                  <TableHeaderCell headerKey="AMOUNT">Amount</TableHeaderCell>
-                  <TableHeaderCell headerKey="ACCOUNT">Account</TableHeaderCell>
-                  <TableHeaderCell headerKey="DATE">Date</TableHeaderCell>
-                  <TableHeaderCell headerKey="METHOD">Method</TableHeaderCell>
-                  <TableHeaderCell headerKey="STATUS">Status </TableHeaderCell>
-                </TableHeaderRow>
-              </TableHeader>
-              <TableBody>
-                {(tableItem: Item, index) => (
-                  <TableRow
-                    key={index}
-                    item={tableItem}
-                    onClick={() => {
-                      console.log('where');
-                    }}
-                  >
-                    <TableCell>
-                      <Code size="medium">{tableItem.paymentId}</Code>
-                    </TableCell>
-                    <TableEditableCell
-                      accessibilityLabel="Amount"
-                      placeholder="Enter text"
-                      successText="Amount is valid"
-                    />
-                    <TableCell>{tableItem.account}</TableCell>
-                    <TableCell>
-                      {tableItem.date?.toLocaleDateString('en-IN', {
-                        year: 'numeric',
-                        month: '2-digit',
-                        day: '2-digit',
-                      })}
-                    </TableCell>
-                    <TableCell>{tableItem.method}</TableCell>
-                    <TableCell>
-                      <Badge
-                        size="medium"
-                        color={
-                          tableItem.status === 'Completed'
-                            ? 'positive'
-                            : tableItem.status === 'Pending'
-                            ? 'notice'
-                            : tableItem.status === 'Failed'
-                            ? 'negative'
-                            : 'primary'
-                        }
-                      >
-                        {tableItem.status}
-                      </Badge>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </TableVirtualizedWrapper>
-          )}
-        </TableComponent>
-      </Box>
-    </Box>
-  );
-};
-
 export const Table = TableTemplate.bind({});
 // Need to do this because of storybook's weird naming convention, More details here: https://storybook.js.org/docs/react/writing-stories/naming-components-and-hierarchy#single-story-hoisting
-Table.storyName = 'Virtualized Table';
+Table.storyName = 'Basic Table';
