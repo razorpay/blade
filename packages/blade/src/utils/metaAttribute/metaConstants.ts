106705b9464a85d2502ee505330164964204166c
tewarig <gaurav.tewari@razorpay.com>
2025-02-20 18:17:44 +0530

    chore: add list view wrapper

--- packages/blade/src/utils/metaAttribute/metaConstants.ts
+++ packages/blade/src/utils/metaAttribute/metaConstants.ts
@@ -55,6 +55,7 @@ export const MetaConstants = {
   ListItemCode: 'list-item-code',
   ListItemLink: 'list-item-link',
   ListItemText: 'list-item-text',
+  ListView: 'list-view',
   OTPInput: 'otp-input',
   PasswordInput: 'password-input',
   SearchInput: 'search-input',
