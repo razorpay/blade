b66b68b4383887b66c072a2860813f75ddb83c05
tewarig <gaurav.tewari@razorpay.com>
2025-03-03 18:01:57 +0530

    fix: more errors

--- packages/blade/src/components/DatePicker/DatePickerContainer.web.tsx
+++ packages/blade/src/components/DatePicker/DatePickerContainer.web.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { DatesProvider, DatePicker } from '@mantine/dates';
+import { DatesProvider } from '@mantine/dates';
 import React from 'react';
 import { FloatingFocusManager, FloatingPortal } from '@floating-ui/react';
 import { useI18nContext } from '@razorpay/i18nify-react';
