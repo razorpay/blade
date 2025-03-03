e244606b3c99d492a9191300d44cdbdf081eca7f
tewarig <gaurav.tewari@razorpay.com>
2025-03-03 17:14:10 +0530

    chore: remove unwanted warnings

--- packages/blade/src/components/DatePicker/DatePickerWrapper.web.tsx
+++ packages/blade/src/components/DatePicker/DatePickerWrapper.web.tsx
@@ -1,14 +1,10 @@
-/* eslint-disable jsx-a11y/no-autofocus */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import type { DatePickerProps, DateSelectionType } from './types';
 import { DatePicker } from './DatePicker.web';
 
 const DatePickerWrapper = <Type extends DateSelectionType = 'single'>(
   props: DatePickerProps<Type>,
-) => {
+): React.ReactElement => {
   return <DatePicker {...props} inputElementType="datePickerInput" />;
 };
 
