e8c2d69156343ee2b07284913b8ee7a923b92636
tewarig <gaurav.tewari@razorpay.com>
2025-03-24 22:46:50 +0530

    fix: ts issues

--- packages/blade/src/components/Dropdown/FilterChipGroup.native.tsx
+++ packages/blade/src/components/Dropdown/FilterChipGroup.native.tsx
@@ -1,6 +1,6 @@
 import { throwBladeError } from '~utils/logger';
 
-const FilterChipGroup = (_props: any): React.ReactElement => {
+const FilterChipGroup = (_props: unknown): React.ReactElement => {
   throwBladeError({
     message: 'FilterChipGroup is not yet implemented for native',
     moduleName: 'FilterChipGroup',
--- packages/blade/src/components/Dropdown/FilterChipGroupContext.web.tsx
+++ packages/blade/src/components/Dropdown/FilterChipGroupContext.web.tsx
@@ -3,8 +3,10 @@ import type { FilterChipGroupContextType } from './types';
 
 const FilterChipGroupContext = React.createContext<FilterChipGroupContextType>({
   filterChipGroupSelectedFilters: [],
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   setFilterChipGroupSelectedFilters: () => {},
   clearFilterCallbackTriggerer: 0,
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   setClearFilterCallbackTriggerer: () => {},
 });
 const FilterChipGroupProvider = FilterChipGroupContext.Provider;
