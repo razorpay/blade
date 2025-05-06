5b5081d0d83941322a1be597962879749d079a27
tewarig <gaurav.tewari@razorpay.com>
2025-05-06 15:24:53 +0530

    fix: type error

--- packages/blade/src/components/Radio/radioTokens.ts
+++ packages/blade/src/components/Radio/radioTokens.ts
@@ -21,20 +21,20 @@ const radioSizes = {
             desktop: 'spacing.4',
           },
         },
-        horizontal: {
-          gap: {
-            small: {
-              mobile: 'spacing.3',
-              desktop: 'spacing.2',
-            },
-            medium: {
-              mobile: 'spacing.4',
-              desktop: 'spacing.3',
-            },
-            large: {
-              mobile: 'spacing.5',
-              desktop: 'spacing.4',
-            },
+      },
+      horizontal: {
+        gap: {
+          small: {
+            mobile: 'spacing.3',
+            desktop: 'spacing.2',
+          },
+          medium: {
+            mobile: 'spacing.4',
+            desktop: 'spacing.3',
+          },
+          large: {
+            mobile: 'spacing.5',
+            desktop: 'spacing.4',
           },
         },
       },
