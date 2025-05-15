26af41b75306a9f1a4512c44e185db77d2f811ba
tewarig <gaurav.tewari@razorpay.com>
2025-05-15 15:33:04 +0530

    chore: add jsdocs

--- packages/blade/src/components/PreviewWindow/types.ts
+++ packages/blade/src/components/PreviewWindow/types.ts
@@ -1,26 +1,65 @@
 type PreviewWindowProps = {
+  /**
+   * The children of the PreviewWindow component.
+   */
   children: React.ReactElement | React.ReactElement[];
+  /**
+   * The function to call when the full screen button is clicked.
+   */
   onFullScreen?: () => void;
+  /**
+   * Whether the drag and zoom is disabled.
+   */
   isDragAndZoomDisabled?: boolean;
+  /**
+   * The initial zoom of the PreviewWindow component.
+   */
   initialZoom?: number;
+  /**
+   * The function to call when the zoom changes.
+   */
   onZoomChange?: (newZoom: number) => void;
+  /**
+   * The function to call when the drag changes.
+   */
   onDragChange?: (position: { x: number; y: number }) => void;
-  defaultZoom?: number;
+  /**
+   * The step of the zoom.
+   */
   zoomScaleStep?: number;
 };
 
 type PreviewHeaderProps = {
+  /**
+   * The title of the PreviewHeader component.
+   */
   title?: string;
+  /**
+   * The trailing of the PreviewHeader component.
+   */
   trailing?: React.ReactElement | React.ReactElement[];
+  /**
+   * The function to call when the full screen button is clicked.
+   * @private
+   */
   _onFullScreen?: () => void;
 };
 
 type PreviewBodyProps = {
+  /**
+   * The children of the PreviewBody component.
+   */
   children: React.ReactElement;
 };
 
 type PreviewFooterProps = {
+  /**
+   * The trailing of the PreviewFooter component.
+   */
   trailing?: React.ReactElement;
+  /**
+   * Whether to show the zoom percentage.
+   */
   showZoomPercentage?: boolean;
 };
 
