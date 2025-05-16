344673f039515e2c1f6960032797b83633f90365
tewarig <gaurav.tewari@razorpay.com>
2025-05-16 15:28:43 +0530

    chore: update snaps

--- packages/blade/src/components/Preview/__tests__/PreviewWindow.ssr.test.tsx
+++ packages/blade/src/components/Preview/__tests__/PreviewWindow.ssr.test.tsx
@@ -1,30 +1,30 @@
 // test case for ChatMessage component
 // eslint-disable-next-line blade/no-cross-platform-imports
-import { PreviewFooter, PreviewWindow, PreviewBody, PreviewHeader } from '../Preview.web';
+import { PreviewFooter, Preview, PreviewBody, PreviewHeader } from '../Preview.web';
 import renderWithSSR from '~utils/testing/renderWithSSR.web';
 import { Text } from '~components/Typography';
 
 describe('<PreviewWindow/>', () => {
   it('should render preview window correctly', () => {
     const { container } = renderWithSSR(
-      <PreviewWindow>
+      <Preview>
         <PreviewHeader title="Preview Window" />
         <PreviewBody>
           <Text>This is a demo message</Text>
         </PreviewBody>
         <PreviewFooter showZoomPercentage={true} />
-      </PreviewWindow>,
+      </Preview>,
     );
     expect(container).toMatchSnapshot();
   });
   it('should render preview header without title correctly', () => {
     const { container } = renderWithSSR(
-      <PreviewWindow>
+      <Preview>
         <PreviewHeader />
         <PreviewBody>
           <Text>This is a demo message</Text>
         </PreviewBody>
-      </PreviewWindow>,
+      </Preview>,
     );
     expect(container).toMatchSnapshot();
   });
--- packages/blade/src/components/Preview/__tests__/PreviewWindow.web.test.tsx
+++ packages/blade/src/components/Preview/__tests__/PreviewWindow.web.test.tsx
@@ -1,28 +1,28 @@
-import { PreviewWindow, PreviewBody, PreviewFooter, PreviewHeader } from '../Preview.web';
+import { Preview, PreviewBody, PreviewFooter, PreviewHeader } from '../Preview.web';
 import renderWithTheme from '~utils/testing/renderWithTheme.web';
 import { Text } from '~components/Typography';
 
 describe('<PreviewWindow/>', () => {
   it('should render preview window correctly', () => {
     const { container } = renderWithTheme(
-      <PreviewWindow>
+      <Preview>
         <PreviewHeader title="Preview Window" />
         <PreviewBody>
           <Text>This is a demo message</Text>
         </PreviewBody>
         <PreviewFooter showZoomPercentage={true} />
-      </PreviewWindow>,
+      </Preview>,
     );
     expect(container).toMatchSnapshot();
   });
   it('should render preview header without title correctly', () => {
     const { container } = renderWithTheme(
-      <PreviewWindow>
+      <Preview>
         <PreviewHeader />
         <PreviewBody>
           <Text>This is a demo message</Text>
         </PreviewBody>
-      </PreviewWindow>,
+      </Preview>,
     );
     expect(container).toMatchSnapshot();
   });
