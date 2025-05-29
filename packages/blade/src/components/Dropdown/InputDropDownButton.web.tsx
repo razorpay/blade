390c955b1579f841b364014a174eb0148ef477cd
tewarig <gaurav.tewari@razorpay.com>
2025-05-29 09:53:14 +0530

    chore: update js doc

--- packages/blade/src/components/Dropdown/InputDropDownButton.web.tsx
+++ packages/blade/src/components/Dropdown/InputDropDownButton.web.tsx
@@ -21,21 +21,61 @@ import type { IconComponent } from '~components/Icons';
 import { makeAnalyticsAttribute } from '~utils/makeAnalyticsAttribute';
 
 type InputDropDownButtonProps = {
+  /**
+   * isOpen is used to control the open state of the dropdown
+   */
+  isOpen?: boolean;
+  /**
+   * onBlur is the callback function that is called when the dropdown is blurred
+   */
   onBlur?: BaseButtonProps['onBlur'];
+  /**
+   * onKeyDown is the callback function that is called when the dropdown is keyed down
+   */
   onKeyDown?: BaseButtonProps['onKeyDown'];
+  /**
+   * onClick is the callback function that is called when the dropdown is clicked
+   */
   onClick?: BaseButtonProps['onClick'];
+  /**
+   * title is the title of the dropdown
+   */
   title?: string;
+  /**
+   * accessibilityLabel is the label of the dropdown
+   */
   accessibilityLabel?: string;
   /**
    * @private
    */
   _isInsideSearchInput?: boolean;
+  /**
+   * isDisabled is the disabled state of the dropdown
+   */
   isDisabled?: boolean;
+  /**
+   * onChange is the callback function that is called when the dropdown is changed
+   */
   onChange?: (props: { name: string; value: string }) => void;
+  /**
+   * name is the name of the dropdown
+   */
   name?: string;
-  testID?: () => void;
+  /**
+   * testID is the testID of the dropdown
+   */
+  testID?: string;
+  /**
+   * value is the value of the dropdown
+   */
   value?: string;
+  /**
+   * defaultValue is the default value of the dropdown
+   */
   defaultValue?: string;
+  /**
+   * icon is the icon of the dropdown
+   */
   icon?: IconComponent;
 } & DataAnalyticsAttribute;
 
