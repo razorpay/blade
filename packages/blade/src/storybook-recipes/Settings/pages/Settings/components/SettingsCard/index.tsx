df33116c2ea21d3a697d287666021e46650e08df
tewarig <gaurav.tewari@razorpay.com>
2025-06-27 17:13:39 +0530

    fix: github action

--- packages/blade/src/storybook-recipes/Settings/pages/Settings/components/SettingsCard/index.tsx
+++ packages/blade/src/storybook-recipes/Settings/pages/Settings/components/SettingsCard/index.tsx
@@ -1,1 +1,1 @@
-export { default as SettingCard } from './SettingCard.web';
+export { default as SettingCard } from './SettingCard';
--- packages/blade/src/storybook-recipes/Settings/pages/index.ts
+++ packages/blade/src/storybook-recipes/Settings/pages/index.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line blade/no-cross-platform-imports
-export { default as Settings } from './Settings/Settings';
+export { default as Settings } from './Settings/Settings.web';
 // eslint-disable-next-line blade/no-cross-platform-imports
-export { default as UserSettings } from './Settings/User';
+export { default as UserSettings } from './Settings/User/User.web';
