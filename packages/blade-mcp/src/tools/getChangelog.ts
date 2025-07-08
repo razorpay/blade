2dfa5355b95fb0cfa9a898fa4836cf1992c944c6
Anurag Hazra <anurag.hazra@razorpay.com>
2025-07-08 13:18:41 +0530

    chore: changelog tool prompt (#2826)
    * chore: changelog tool prompt
    
    * Create plenty-poets-return.md

--- packages/blade-mcp/src/tools/getChangelog.ts
+++ packages/blade-mcp/src/tools/getChangelog.ts
@@ -19,6 +19,28 @@ Intent examples:
 - what was changed in x.x.x version? -> Just \`fromVersion\` (set isRange to false & omit toVersion)
 - what was changed between x.x.x and y.y.y versions? -> \`fromVersion\` and \`toVersion\` (set isRange to true)
 - what is the latest version of blade? -> \`fromVersion\` as \`latest\` (set isRange to false & omit toVersion)
+
+You will provide high level summary of notable changes in this output format:
+
+\`\`\`
+# Key Changes to Watch Out For:
+- **XYZ component changed (v12.0.0):** description of the change and how it impacts the consumer
+# Visual Changes:
+- **XYZ component** was updated to use new design tokens (v12.0.0)
+# New Components:
+- **XYZ** - Component was added (v12.0.0)
+# Enhancements:
+- **XYZ** now supports new prop (v12.0.0)
+# Bug Fixes:
+- **XYZ** now fixes a bug (v12.0.0)
+# Breaking Changes:
+- **XYZ** component was removed (v12.0.0)
+\`\`\`
+
+Hyperlinking:
+Whenever you find blade versions mentioned ie \`(v12.0.0)\`, make sure to link it to release notes with this link:
+https://github.com/razorpay/blade/releases/tag/%40razorpay%2Fblade%4012.0.0 
+Example: \`(v12.0.0)\` becomes [(v12.0.0)](https://github.com/razorpay/blade/releases/tag/%40razorpay%2Fblade%4012.0.0)
 `;
 
 const getChangelogToolSchema = {
