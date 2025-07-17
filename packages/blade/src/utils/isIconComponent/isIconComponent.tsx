7f3746ee46b1c64ada23bda6361d4ea421a96a15
tewarig <gaurav.tewari@razorpay.com>
2025-07-17 22:46:24 +0530

    chore: implement suggestion

--- packages/blade/src/utils/isIconComponent/isIconComponent.tsx
+++ packages/blade/src/utils/isIconComponent/isIconComponent.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import type { IconComponent } from '~components/Icons';
 import { getComponentId } from '~utils/isValidAllowedChildren';
 
 const isIconComponent = (element: React.ReactNode | IconComponent): boolean => {
   if (typeof element === 'function') {
-    const componentId = getComponentId(React.createElement(element));
+    // eslint-disable-next-line no-restricted-properties
+    const componentId = (element as { componentId?: string }).componentId;
     return componentId?.endsWith('Icon') ?? false;
   }
   return getComponentId(element)?.endsWith('Icon') ?? false;
