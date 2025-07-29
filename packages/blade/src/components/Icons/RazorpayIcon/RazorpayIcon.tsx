71df756d8ef585b1a829e8e222be0c1a92d0656b
tewarig <gaurav.tewari@razorpay.com>
2025-07-29 18:47:14 +0530

    chore: another variant

--- packages/blade/src/components/Icons/RazorpayIcon/RazorpayIcon.tsx
+++ packages/blade/src/components/Icons/RazorpayIcon/RazorpayIcon.tsx
@@ -3,8 +3,21 @@ import type { IconComponent } from '..';
 import useIconProps from '../useIconProps';
 import { assignWithoutSideEffects } from '~utils/assignWithoutSideEffects';
 
-const _RazorpayIcon: IconComponent = ({ size, color, ...styledProps }) => {
-  const { height, width, iconColor } = useIconProps({ size, color });
+const _RazorpayIcon: IconComponent = ({ size, color, variant, ...styledProps }) => {
+  const { height, width, iconColor } = useIconProps({ size, color, variant });
+
+  if (variant === 'filled') {
+    return (
+      <Svg {...styledProps} width={width} height={height} viewBox="0 0 24 24" fill="none">
+        <Path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M9.71749 12.3339L10.7149 8.66299L21 2L15.4844 22.5753L11.6926 22.5722L15.4262 8.64209L9.71749 12.3339ZM3 22.5756L4.57044 16.7193L13.9519 10.6624L10.7718 22.5756H3Z"
+          fill={iconColor}
+        />
+      </Svg>
+    );
+  }
 
   return (
     <Svg {...styledProps} width={width} height={height} viewBox="0 0 24 24" fill="none">
