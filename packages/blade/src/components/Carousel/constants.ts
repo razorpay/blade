029964227bea7b2fcb7633625df71bee37f584e3
Gaurav Tewari <gaurav.tewari@razorpay.com>
2025-08-11 15:32:07 +0530

    feat: add new side navigation button option "overlap" in carousel (#2878)
    * feat: carousel overlap
    
    * feat: add new navigation button position
    
    * chore: change navigation option
    
    * fix: lint
    
    * chore: minor changes
    
    * chore: minor changes
    
    * chore: icons color update
    
    * chore: update example
    
    * fix: lint

--- packages/blade/src/components/Carousel/constants.ts
+++ packages/blade/src/components/Carousel/constants.ts
@@ -1,7 +1,12 @@
 const CAROUSEL_AUTOPLAY_INTERVAL = 6000;
+/*
+ Button have width of 34px in desktop , value from left /right should be 34px/2 = 17px.
+ In mobile we have 24px width of button , value from left /right should be 24px/2 = 12px. (but currently we don't support overlap in mobile)
+*/
+const CAROUSEL_SIDE_OVERLAP_VALUE_OFFSET = '-17px';
 
 const componentIds = {
   CarouselItem: 'CarouselItem',
 };
 
-export { CAROUSEL_AUTOPLAY_INTERVAL, componentIds };
+export { CAROUSEL_AUTOPLAY_INTERVAL, componentIds, CAROUSEL_SIDE_OVERLAP_VALUE_OFFSET };
