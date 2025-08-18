f50f0ec7c97541fdcf4f03ba4daf117618d25b8d
tewarig <gaurav.tewari@razorpay.com>
2025-08-18 15:31:53 +0530

    fix: enable it in desktop

--- packages/blade/src/components/Input/PhoneNumberInput/CountrySelector.web.tsx
+++ packages/blade/src/components/Input/PhoneNumberInput/CountrySelector.web.tsx
@@ -55,7 +55,7 @@ const CountrySelector = ({
   const isMobile = useIsMobile();
 
   const actionList = (
-    <ActionList>
+    <ActionList isVirtualized={!isMobile}>
       {countryData.map((country) => {
         return (
           <ActionListItem
