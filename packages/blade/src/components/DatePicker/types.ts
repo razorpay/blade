6f466749a827ffbdff184fcc6038fe64d9c3246f
tewarig <gaurav.tewari@razorpay.com>
2025-08-21 19:36:48 +0530

    fix: ts errors

--- packages/blade/src/components/DatePicker/types.ts
+++ packages/blade/src/components/DatePicker/types.ts
@@ -226,7 +226,7 @@ type DatePickerInputProps = DatePickerCommonInputProps &
     excludeDate?: (date: Date) => boolean;
     minDate?: Date;
     maxDate?: Date;
-    effectiveSelectionType: 'single' | 'range' | null;
+    effectiveSelectionType?: 'single' | 'range' | null;
   };
 
 type DatePickerFilterChipProps = DatePickerInputProps;
@@ -252,7 +252,7 @@ type DateInputProps = BaseInputProps & {
   excludeDate?: (date: Date) => boolean;
   minDate?: Date;
   maxDate?: Date;
-  effectiveSelectionType: 'single' | 'range' | null;
+  effectiveSelectionType?: 'single' | 'range' | null;
 };
 
 export type {
