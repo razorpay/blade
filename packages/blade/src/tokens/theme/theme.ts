1aa02c5bc906fbeee0a55f9a4f8939de7195b482
tewarig <gaurav.tewari@razorpay.com>
2025-08-28 21:52:41 +0530

    feat: add colors

--- packages/blade/src/tokens/theme/theme.ts
+++ packages/blade/src/tokens/theme/theme.ts
@@ -7,6 +7,7 @@ import type {
   TypographyWithPlatforms,
   ElevationWithColorModes,
 } from '~tokens/global';
+import type { ColorChromaticScale } from '~tokens/global/colors';
 
 export type ColorSchemeNames = 'dark' | 'light';
 export type ColorSchemeNamesInput = ColorSchemeNames | 'system';
@@ -23,6 +24,27 @@ export type Emphasis = {
   muted: string;
   disabled: string;
 };
+
+export type ChartCategoricalEmphasis = Pick<Emphasis, 'subtle' | 'moderate' | 'intense'> & {
+  faint: string;
+  strong: string;
+};
+
+export type ChartSequentialEmphasis = Omit<
+  ColorChromaticScale,
+  'a50' | 'a150' | 'a100' | 'a200' | 'a400'
+>;
+
+export type ChartColorCategories =
+  | 'azure'
+  | 'emerald'
+  | 'crimson'
+  | 'cider'
+  | 'sapphire'
+  | 'orchid'
+  | 'magenta'
+  | 'topaz'
+  | 'gray';
 
 type SubtleOrIntenseEmphasis = Pick<Emphasis, 'subtle' | 'intense'>;
 // Exporting this for usage in other components
@@ -91,6 +113,12 @@ export type Colors = {
     border: SubtleOrIntenseEmphasis;
   };
   transparent: string;
+  chart: {
+    background: {
+      categorical: Record<ChartColorCategories, ChartCategoricalEmphasis>;
+      sequential: Record<ChartColorCategories, ChartSequentialEmphasis>;
+    };
+  };
 };
 
 export type ColorsWithModes = Record<ColorSchemeModes, Colors>;
