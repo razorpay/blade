f6adae54b7ee7181fd6dae6adfc0fdd050bb6bda
tewarig <gaurav.tewari@razorpay.com>
2025-09-11 15:42:44 +0530

    chore: add js docs

--- packages/blade/src/components/Charts/BaseChartComponents/types.ts
+++ packages/blade/src/components/Charts/BaseChartComponents/types.ts
@@ -10,17 +10,38 @@ import type { ComponentProps } from 'react';
 import type { ChartColorCategories, ChartCategoricalEmphasis } from '~tokens/theme/theme';
 
 type ReferenceLineProps = {
+  /**
+   * The y-coordinate of the reference line.
+   */
   y?: number;
+  /**
+   * The x-coordinate  of the reference line.
+   */
   x?: number;
+  /**
+   * The label of the reference line.
+   */
   label: string;
 };
 
 type XAxisProps = Omit<RechartsXAxisProps, 'tick' | 'label' | 'dataKey' | 'stroke'> & {
+  /**
+   * The label of the x-axis.
+   */
   label?: string;
+  /**
+   * The data key of the x-axis.
+   */
   dataKey?: string;
 };
 type YAxisProps = Omit<RechartsYAxisProps, 'tick' | 'label' | 'dataKey' | 'stroke'> & {
+  /**
+   * The label of the y-axis.
+   */
   label?: string;
+  /**
+   * The data key of the y-axis.
+   */
   dataKey?: string;
 };
 
--- packages/blade/src/components/Charts/LineCharts/types.ts
+++ packages/blade/src/components/Charts/LineCharts/types.ts
@@ -3,14 +3,46 @@ import type { ComponentProps } from 'react';
 import type { ChartsCategoricalColorToken } from '../BaseChartComponents/types';
 
 interface LineProps {
+  /**
+   * The type of the line.
+   *  @default : 'linear'
+   */
   type?: 'step' | 'stepAfter' | 'stepBefore' | 'linear' | 'monotone';
+  /**
+   * The dot of the line.
+   */
   dot?: RechartsLineProps['dot'];
+  /**
+   * The active dot we shows at line chart
+   */
   activeDot?: RechartsLineProps['activeDot'];
+  /**
+   * If we don't have data for some points should we connect the line or should skip it.
+   */
   connectNulls?: boolean;
+  /**
+   * Include this particular line in legend.
+   *  @default : true
+   */
   showLegend?: boolean;
+  /**
+   *  The data key of the x-axis
+   */
   dataKey: string;
+  /**
+   * Name of the line in line chart.
+   * if no provided, we will use the data key as the name.
+   */
   name?: string;
+  /**
+   * Color of the line in line chart.
+   * if no provided, we will pick colors from the default theme colors.
+   */
   color?: ChartsCategoricalColorToken;
+  /**
+   * Style of the line in line chart.
+   * @default: solid
+   */
   strokeStyle?: 'dotted' | 'dashed' | 'solid';
   /**
    * @private
@@ -24,6 +56,9 @@ interface LineProps {
 
 // TypeScript prop types
 type LineChartProps = ComponentProps<typeof RechartsLineChart> & {
+  /**
+   * The color theme of the line chart.
+   */
   colorTheme?: 'default' | 'informational';
 };
 
