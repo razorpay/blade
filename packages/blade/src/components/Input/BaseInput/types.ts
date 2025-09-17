7e4457d7a82fa074ac0cddc993a44dade54ffa7d
anuraghazra <anurag.hazra@razorpay.com>
2025-09-17 14:16:59 +0530

    fix: build and ts issues

--- packages/blade/src/components/Input/BaseInput/types.ts
+++ packages/blade/src/components/Input/BaseInput/types.ts
@@ -99,6 +99,7 @@ export type StyledBaseInputProps = {
   | 'autoCapitalize'
   | 'isDropdownTrigger'
   | 'tabIndex'
+  | 'valueSuffix'
 >;
 
 export { StyledBaseInput } from './StyledBaseInput.web';
