92774deb9f3d0b28620e7b68a28147eb6107abb2
tewarig <gaurav.tewari@razorpay.com>
2025-09-21 22:39:50 +0530

    fix: lint

--- packages/blade/src/components/FileUpload/StyledFileUploadWrapper.tsx
+++ packages/blade/src/components/FileUpload/StyledFileUploadWrapper.tsx
@@ -7,36 +7,39 @@ import BaseBox from '~components/Box/BaseBox';
 import { makeMotionTime } from '~utils/makeMotionTime';
 import { castWebType } from '~utils';
 
-const StyledFileUploadWrapper: StyledComponent<typeof BaseBox, any, StyledFileUploadWrapperProps, never> = styled(BaseBox)<StyledFileUploadWrapperProps>(
-  ({ theme, isDisabled, isActive }) => {
-    const easing = getIn(theme.motion, fileUploadMotionTokens.easing);
-    const duration = castWebType(
-      makeMotionTime(getIn(theme.motion, fileUploadMotionTokens.duration)),
-    );
+const StyledFileUploadWrapper: StyledComponent<
+  typeof BaseBox,
+  any,
+  StyledFileUploadWrapperProps,
+  never
+> = styled(BaseBox)<StyledFileUploadWrapperProps>(({ theme, isDisabled, isActive }) => {
+  const easing = getIn(theme.motion, fileUploadMotionTokens.easing);
+  const duration = castWebType(
+    makeMotionTime(getIn(theme.motion, fileUploadMotionTokens.duration)),
+  );
 
-    return {
-      display: 'flex',
-      borderStyle: 'dashed',
-      transitionProperty: 'background-color',
-      transitionTimingFunction: easing,
-      transitionDuration: duration,
-      borderColor: getIn(
-        theme.colors,
-        fileUploadColorTokens.border[isDisabled ? 'disabled' : 'default'],
-      ),
+  return {
+    display: 'flex',
+    borderStyle: 'dashed',
+    transitionProperty: 'background-color',
+    transitionTimingFunction: easing,
+    transitionDuration: duration,
+    borderColor: getIn(
+      theme.colors,
+      fileUploadColorTokens.border[isDisabled ? 'disabled' : 'default'],
+    ),
 
-      ...(isActive && {
-        backgroundColor: getIn(theme.colors, fileUploadColorTokens.background.active),
+    ...(isActive && {
+      backgroundColor: getIn(theme.colors, fileUploadColorTokens.background.active),
+    }),
+
+    ...(!isDisabled &&
+      !isActive && {
+        '&:hover': {
+          backgroundColor: getIn(theme.colors, fileUploadColorTokens.background.hover),
+        },
       }),
-
-      ...(!isDisabled &&
-        !isActive && {
-          '&:hover': {
-            backgroundColor: getIn(theme.colors, fileUploadColorTokens.background.hover),
-          },
-        }),
-    };
-  },
-);
+  };
+});
 
 export { StyledFileUploadWrapper };
