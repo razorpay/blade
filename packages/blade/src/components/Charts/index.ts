555910e949c8f4b1db65c9ebffdc7ca145c1779b
tewarig <gaurav.tewari@razorpay.com>
2025-09-24 16:39:51 +0530

    chore: update index file

--- packages/blade/src/components/Charts/index.ts
+++ packages/blade/src/components/Charts/index.ts
@@ -1,8 +1,5 @@
 export * from './LineChart';
 export * from './AreaChart';
-<<<<<<< HEAD
 export * from './DonutChart';
-=======
 export * from './BarChart';
->>>>>>> origin
 export * from './CommonChartComponents';
