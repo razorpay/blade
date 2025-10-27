8aac0f6bd61907410de0984656f09936d1bd1f5a
tewarig <gaurav.tewari@razorpay.com>
2025-10-27 16:21:20 +0530

    Revert "fix: wrong layout position in bottom sheet  (#2910)"
    This reverts commit 37fbe24ee6d3e525a6b2c6f44f400a6a00782d81.

--- packages/blade/src/components/VisuallyHidden/ScreenReaderStyles.ts
+++ packages/blade/src/components/VisuallyHidden/ScreenReaderStyles.ts
@@ -17,22 +17,4 @@ const screenReaderStyles: CSSObject = {
   wordWrap: 'normal',
 };
 
-// So when we are using screenReaderStyles inside SelectorInput , it is causing wrong layout positioning in bottomSheet. that's why we are using legacyScreenReaderStyles only in SelectorInput.
-//TODO: figure out and remove `legacyScreenReaderStyles` in future.
-
-const legacyScreenReaderStyles: CSSObject = {
-  border: 0,
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
-  height: `1px`,
-  margin: '0 -1px -1px 0',
-  overflow: 'hidden',
-  padding: 0,
-  position: 'absolute',
-  width: `1px`,
-  left: '-10000px',
-  whiteSpace: 'nowrap',
-  wordWrap: 'normal',
-};
-
-export { screenReaderStyles, legacyScreenReaderStyles };
+export { screenReaderStyles };
