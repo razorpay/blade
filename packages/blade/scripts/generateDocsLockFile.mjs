87bc5df31be61e1443e7c90a57e8869a1c074a58
tewarig <gaurav.tewari@razorpay.com>
2025-11-20 23:59:31 +0530

    chore: update log file

--- packages/blade/scripts/generateDocsLockFile.mjs
+++ packages/blade/scripts/generateDocsLockFile.mjs
@@ -7,10 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Read dependencies JSON file
-const dependenciesPath = path.resolve(
-  __dirname,
-  '../src/utils/storybook/Sandbox/dependencies.json',
-);
+const dependenciesPath = path.resolve(__dirname, '../src/utils/storybook/Sandbox/dependencies.ac');
 const dependenciesData = JSON.parse(fs.readFileSync(dependenciesPath, 'utf8'));
 
 const resolve = (resolvePath) => path.resolve(__dirname, resolvePath);
@@ -21,14 +18,27 @@ const generateLockFileContent = ({ dependencies, devDependencies }) => {
     devDependencies,
   };
 
+  // Read tokens from environment variables
+  const npmToken = process.env.NPM_TOKEN;
   fs.mkdirSync(resolve('lock-generation'), { recursive: true });
   try {
     console.log('[lockfile-generation]: Creating package.json');
     fs.writeFileSync(resolve('lock-generation/package.json'), JSON.stringify(packageJSON, null, 2));
-    fs.writeFileSync(resolve('lock-generation/.npmrc'), 'auto-install-peers = false');
+
+    // Build .npmrc content with tokens
+    let npmrcContent = 'auto-install-peers = false\n';
+    if (npmToken) {
+      npmrcContent += `//registry.npmjs.org/:_authToken=${npmToken}\n`;
+    }
+    fs.writeFileSync(resolve('lock-generation/.npmrc'), npmrcContent);
+
+    // Prepare environment variables for yarn install
+    const env = {
+      ...process.env,
+    };
 
     console.log('[lockfile-generation]: Installing Dependencies');
-    execSync('yarn', { cwd: resolve('lock-generation'), stdio: 'inherit' });
+    execSync('yarn', { cwd: resolve('lock-generation'), stdio: 'inherit', env });
 
     console.log('[lockfile-generation]: Moving lock file to /public/docs-yarn-lock.yaml');
     fs.renameSync(resolve('lock-generation/yarn.lock'), resolve('../public/docs-yarn-lock.yaml'));
