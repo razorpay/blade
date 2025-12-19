603ad26eca98747de0e6dba9f3bf67a8f3c2fa77
kashishbehl <104421875+kashishbehl@users.noreply.github.com>
2025-12-19 13:22:40 +0530

    Added storybook implementation

--- packages/blade-svelte/.storybook/main.js
+++ packages/blade-svelte/.storybook/main.js
@@ -1,33 +1,28 @@
-
-
-import { dirname } from "path"
-
-import { fileURLToPath } from "url"
-
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
 function getAbsolutePath(value) {
-  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
 }
 
 /** @type { import('@storybook/svelte-vite').StorybookConfig } */
 const config = {
-  "stories": [
-    "../src/**/*.stories.@(js|ts|svelte)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.@(js|ts|svelte)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "@storybook/addon-svelte-csf",
   ],
-  "framework": {
-    "name": "@storybook/svelte-vite",
-    "options": {},
+  framework: {
+    name: "@storybook/svelte-vite",
+    options: {},
   },
 };
+
 export default config;
