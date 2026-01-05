3840721997a8c09b6067d55d957bd0b93a6e8fe0
Gaurav Tewari <gauravtewari111@gmail.com>
2026-01-05 13:56:55 +0530

    chore: remove unpack version

--- packages/blade/rollup.config.mjs
+++ packages/blade/rollup.config.mjs
@@ -53,7 +53,7 @@ const nativeExtensions = [
 ];
 
 const packageJsonDeps = Object.keys(packagejson.dependencies).filter(
-  (name) => name !== 'patch-package' && name !== 'recharts',
+  (name) => name !== 'patch-package',
 );
 
 const externalDependencies = packageJsonDeps;
