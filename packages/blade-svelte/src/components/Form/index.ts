8ba9b130066a28b621e0955e284ba0f8e9db086b
kashishbehl <104421875+kashishbehl@users.noreply.github.com>
2026-01-15 11:31:58 +0530

    Feat: Added foundational components and setup for Input component

---
+++ packages/blade-svelte/src/components/Form/index.ts
@@ -0,0 +1,11 @@
+export { FormLabel } from './FormLabel';
+export type { FormLabelProps } from './FormLabel';
+
+export { FormHint } from './FormHint';
+export type { FormHintProps } from './FormHint';
+
+export { CharacterCounter } from './CharacterCounter';
+export type { CharacterCounterProps } from './CharacterCounter';
+
+export * from './formTokens';
+
