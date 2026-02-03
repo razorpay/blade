ef34169102e5b911eaab496dfb39dd1cd81e162b
anuraghazra <anurag.hazra@razorpay.com>
2026-02-03 14:51:09 +0530

    chore: fix cycling dep

--- packages/blade/src/tokens/global/elevation/elevation.web.ts
+++ packages/blade/src/tokens/global/elevation/elevation.web.ts
@@ -1,4 +1,3 @@
-import { opacity } from '~tokens/index';
 import type { ElevationWithColorModes } from './types';
 
 /** offset-x | offset-y | blur-radius | spread-radius | color, offset-x | offset-y | blur-radius | spread-radius | color */
@@ -6,15 +5,15 @@ export const elevation: ElevationWithColorModes = {
   // color: neutral.blueGrayLight.a1106
   onLight: {
     none: 'none',
-    lowRaised: `0px 2px 4px 0px hsla(200, 10%, 18%, ${opacity[50]})`,
-    midRaised: `0px 2px 8px 0px hsla(200, 10%, 18%, ${opacity[50]})`,
-    highRaised: `0px 8px 24px -4px hsla(200, 10%, 18%, ${opacity[50]})`,
+    lowRaised: `0px 2px 4px 0px hsla(200, 10%, 18%, 0.06)`,
+    midRaised: `0px 2px 8px 0px hsla(200, 10%, 18%, 0.06)`,
+    highRaised: `0px 8px 24px -4px hsla(200, 10%, 18%, 0.06)`,
   },
   // color: neutral.black.100
   onDark: {
     none: 'none',
-    lowRaised: `0px 2px 4px 0px hsla(0, 0%, 0%, ${opacity[500]})`,
-    midRaised: `0px 2px 8px 0px hsla(0, 0%, 0%, ${opacity[500]})`,
-    highRaised: `0px 8px 24px -4px hsla(0, 0%, 0%, ${opacity[500]})`,
+    lowRaised: `0px 2px 4px 0px hsla(0, 0%, 0%, 0.32)`,
+    midRaised: `0px 2px 8px 0px hsla(0, 0%, 0%, 0.32)`,
+    highRaised: `0px 8px 24px -4px hsla(0, 0%, 0%, 0.32)`,
   },
 };
