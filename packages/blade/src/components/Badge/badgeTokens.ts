9569be548510bfec3afcb63f41f35f354728a53c
Swapnil <swapnil.kr@razorpay.com>
2026-02-09 14:44:13 +0530

    chore: fix lint error

--- packages/blade/src/components/Badge/badgeTokens.ts
+++ packages/blade/src/components/Badge/badgeTokens.ts
@@ -12,12 +12,15 @@ const badgeHeight: Record<NonNullable<BadgeProps['size']>, number> = {
 
 const horizontalPadding: Record<NonNullable<BadgeProps['size']>, DotNotationSpacingStringToken> = {
   xsmall: 'spacing.2',
-  small: 'spacing.3',
+  small: 'spacing.2',
   medium: 'spacing.2',
   large: 'spacing.3',
 };
 
-const textHorizontalMargin: Record<NonNullable<BadgeProps['size']>, DotNotationSpacingStringToken> = {
+const textHorizontalMargin: Record<
+  NonNullable<BadgeProps['size']>,
+  DotNotationSpacingStringToken
+> = {
   xsmall: 'spacing.1',
   small: 'spacing.1',
   medium: 'spacing.2',
