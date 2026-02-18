ff98f10ba553c97c202e4971f604574efbef65bc
saurabhdaware <saurabh.daware@razorpay.com>
2026-02-18 15:10:23 +0530

    feat: motion for file upload

--- packages/blade/src/components/ChatInput/ChatInput.web.tsx
+++ packages/blade/src/components/ChatInput/ChatInput.web.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import { makeSpace } from '~utils';
+import { AnimatePresence } from 'framer-motion';
+import { castWebType, makeSpace } from '~utils';
 import { metaAttribute, MetaConstants } from '~utils/metaAttribute';
 import type { BladeElementRef } from '~utils/types';
 import { makeAnalyticsAttribute } from '~utils/makeAnalyticsAttribute';
+import { useTheme } from '~components/BladeProvider';
 import type { ChatInputProps } from './types';
 import { ChatInputActionBar } from './ChatInputActionBar';
 import { ChatInputGhostSuggestion } from './ChatInputGhostSuggestion';
 import { chatInputMaxTextAreaHeight } from './chatInputTokens';
+import { msToSeconds } from '~utils/msToSeconds';
+import { cssBezierToArray } from '~utils/cssBezierToArray';
+import { BaseMotionBox } from '~components/BaseMotion';
+import type { MotionVariantsType } from '~components/BaseMotion';
 import type { BladeFile, BladeFileList } from '~components/FileUpload/types';
 import { FileUploadItem } from '~components/FileUpload/FileUploadItem';
 import { isFileAccepted } from '~components/FileUpload/isFileAccepted';
 import { BaseInput } from '~components/Input/BaseInput/BaseInput';
 import BaseBox from '~components/Box/BaseBox';
-import { useTheme } from '~components/BladeProvider';
 import { useControllableState } from '~utils/useControllable';
 import { assignWithoutSideEffects } from '~utils/assignWithoutSideEffects';
 import { getStyledProps } from '~components/Box/styledProps';
@@ -212,29 +217,57 @@ const _ChatInput: React.ForwardRefRenderFunction<BladeElementRef, ChatInputProps
   );
 
   // File preview area (topContent)
-  const filePreviewContent = hasFiles ? (
-    <HiddenScrollbarBox
-      display="flex"
-      flexDirection="row"
-      gap="spacing.3"
-      paddingTop="spacing.5"
-      paddingX="spacing.5"
-      overflow="auto"
-      flexWrap="nowrap"
-    >
-      {files.map((file) => (
-        <FileUploadItem
-          flexShrink={0}
-          flexGrow={1}
-          flexBasis={1}
-          minWidth="200px"
-          key={file.id ?? file.name}
-          file={file}
-          onRemove={() => handleFileRemove(file)}
-        />
-      ))}
-    </HiddenScrollbarBox>
-  ) : null;
+  const filePreviewMotionVariants: MotionVariantsType = {
+    initial: { height: '0px', overflow: 'hidden' },
+    animate: {
+      height: 'auto',
+      overflow: 'hidden',
+      transition: {
+        duration: msToSeconds(theme.motion.duration.quick),
+        ease: cssBezierToArray(castWebType(theme.motion.easing.entrance)),
+      },
+    },
+    exit: {
+      height: '0px',
+      overflow: 'hidden',
+      transition: {
+        duration: msToSeconds(theme.motion.duration.quick),
+        ease: cssBezierToArray(castWebType(theme.motion.easing.exit)),
+      },
+    },
+  };
+
+  const filePreviewContent = (
+    <AnimatePresence>
+      {hasFiles ? (
+        <BaseMotionBox motionVariants={filePreviewMotionVariants}>
+          <HiddenScrollbarBox
+            display="flex"
+            flexDirection="row"
+            gap="spacing.3"
+            paddingTop="spacing.5"
+            paddingX="spacing.5"
+            overflowX="auto"
+            height="auto"
+            overflowY="hidden"
+            flexWrap="nowrap"
+          >
+            {files.map((file) => (
+              <FileUploadItem
+                flexShrink={0}
+                flexGrow={1}
+                flexBasis={1}
+                minWidth="200px"
+                key={file.id ?? file.name}
+                file={file}
+                onRemove={() => handleFileRemove(file)}
+              />
+            ))}
+          </HiddenScrollbarBox>
+        </BaseMotionBox>
+      ) : null}
+    </AnimatePresence>
+  );
 
   // Action bar (bottomContent)
   const actionBarContent = (
