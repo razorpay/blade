fad4ba2fbfae0f881fab412c5fa12544088baf0c
Gaurav Tewari <gauravtewari111@gmail.com>
2026-02-18 14:36:38 +0530

    feat: update styles

--- packages/blade/src/components/DatePicker/QuickSelection/QuickSelectionItem.web.tsx
+++ packages/blade/src/components/DatePicker/QuickSelection/QuickSelectionItem.web.tsx
@@ -1,5 +1,32 @@
-import { BaseMenuItem } from '~components/BaseMenu';
+import styled from 'styled-components';
 import { isReactNative } from '~utils';
+import { getFocusRingStyles } from '~utils/getFocusRingStyles';
+import { BaseMenuItem } from '~components/BaseMenu';
+
+const StyledQuickSelectionWrapper = styled.div(({ theme }) => ({
+  '& > *': {
+    borderRadius: theme.border.radius.small,
+    '&:hover:not([aria-disabled=true])': {
+      backgroundColor: `${theme.colors.interactive.background.gray.default} !important`,
+    },
+    '&:focus-visible': {
+      ...getFocusRingStyles({ theme }),
+      backgroundColor: `${theme.colors.interactive.background.gray.default} !important`,
+    },
+    '&[aria-selected=true], &[aria-selected=true]:hover': {
+      backgroundColor: `${theme.colors.interactive.background.gray.fadedHighlighted} !important`,
+    },
+    '&[aria-selected=true]': {
+      backgroundColor: `${theme.colors.interactive.background.gray.fadedHighlighted} !important`,
+    },
+    '& p': {
+      fontSize: `${theme.typography.fonts.size[75]}px !important`,
+      lineHeight: `${theme.typography.lineHeights[75]}px !important`,
+      fontWeight: `${theme.typography.fonts.weight.medium} !important`,
+      fontFamily: `${theme.typography.fonts.family.text} !important`,
+    },
+  },
+}));
 
 type QuickSelectionItemProps = {
   children: string;
@@ -12,15 +39,17 @@ const QuickSelectionItem = ({
   isSelected,
 }: QuickSelectionItemProps): React.ReactElement => {
   return (
-    <BaseMenuItem
-      as={!isReactNative() ? 'button' : undefined}
-      selectionType="single"
-      isSelected={isSelected}
-      onClick={onClick}
-      title={children}
-      role="option"
-      data-analytics-name={children}
-    />
+    <StyledQuickSelectionWrapper>
+      <BaseMenuItem
+        as={!isReactNative() ? 'button' : undefined}
+        selectionType="single"
+        isSelected={isSelected}
+        onClick={onClick}
+        title={children}
+        role="option"
+        data-analytics-name={children}
+      />
+    </StyledQuickSelectionWrapper>
   );
 };
 
