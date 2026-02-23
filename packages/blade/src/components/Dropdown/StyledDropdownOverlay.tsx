37350294d9cab869f5491e664774c658db47e485
Gaurav Tewari <gauravtewari111@gmail.com>
2026-02-23 18:38:32 +0530

    chore: more changes

--- packages/blade/src/components/Dropdown/StyledDropdownOverlay.tsx
+++ packages/blade/src/components/Dropdown/StyledDropdownOverlay.tsx
@@ -1,24 +1,20 @@
 import styled from 'styled-components';
 import BaseBox from '~components/Box/BaseBox';
-import { castWebType, makeSize } from '~utils';
+import { makeSize, getPopupBoxShadowString } from '~utils';
+import type { ColorSchemeNames } from '~tokens/theme';
 
 const StyledDropdownOverlay = styled(BaseBox)<{
   isInBottomSheet?: boolean;
+  colorScheme: ColorSchemeNames;
 }>((props) => {
-  const { theme, isInBottomSheet } = props;
-
-  const dropshadow = castWebType(theme.elevation.midRaised);
-  const innerShadow = `inset 0px 0px 0px 1px ${theme.colors.popup.border.gray.subtle}`;
-
+  const { theme, isInBottomSheet, colorScheme } = props;
   return {
     backgroundColor: theme.colors.popup.background.gray.moderate,
     borderWidth: 'none',
-    borderTopWidth: isInBottomSheet ? undefined : theme.border.width.thin,
-    borderColor: theme.colors.popup.border.gray.moderate,
-    borderTopStyle: isInBottomSheet ? undefined : 'solid',
+    borderTopStyle: undefined,
     borderRadius: makeSize(theme.border.radius.medium),
     backdropFilter: `blur(${theme.backdropBlur.medium}px)`,
-    boxShadow: isInBottomSheet ? undefined : `${dropshadow}, ${innerShadow}`,
+    boxShadow: isInBottomSheet ? undefined : getPopupBoxShadowString(theme, colorScheme),
   };
 });
 
