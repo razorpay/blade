4fe422c67d7c1bf512cfae06b35ef9282373292a
rohitr-raz <rohit.ravipudi@razorpay.com>
2026-06-10 17:03:08 +0530

    Revert "fix(rn): display tags in a horizontal row in Tag story"
    This reverts commit 30d7297aed8e4d2afa21e6c070a28da52c771bbc.

--- packages/blade/src/components/Tag/Tag.stories.tsx
+++ packages/blade/src/components/Tag/Tag.stories.tsx
@@ -135,7 +135,7 @@ export const ControlledTags = (props: TagProps): React.ReactElement => {
 
   return (
     <Box>
-      <Box paddingY="spacing.4" display="flex" flexDirection="row" flexWrap="wrap">
+      <Box paddingY="spacing.4">
         {tags.map((tagName) => (
           <Tag
             key={tagName}
